fd34094fd1977bb838c87fe88f40f214940eb755
fredck <f.knabben@cksource.com>
2014-08-18 12:47:11 +0200

    Added support for jscs and made it a pure grunt task.

--- packages/ckeditor5-utils/gruntfile.js
+++ packages/ckeditor5-utils/gruntfile.js
@@ -1,5 +1,82 @@
 module.exports = function( grunt ) {
 	grunt.initConfig( {
-		pkg: grunt.file.readJSON( 'package.json' )
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		jscs: {
+			src: '*.js',
+			options: jscsConfig
+		}
 	} );
+
+	grunt.loadNpmTasks( 'grunt-jscs' );
+};
+
+// Configurations for JSCS (JavaScript Code Style checker)
+var jscsConfig = {
+	'excludeFiles': [
+		'node_modules/*'
+	],
+	'requireCurlyBraces': [
+		'if', 'else', 'for', 'while', 'do', 'switch', 'try', 'catch'
+	],
+	'requireSpaceAfterKeywords': [
+		'if', 'else', 'for', 'while', 'do', 'switch', 'return', 'try', 'catch'
+	],
+	'requireSpaceBeforeBlockStatements': true,
+	'requireParenthesesAroundIIFE': true,
+	'requireSpacesInConditionalExpression': {
+		'afterTest': true,
+		'beforeConsequent': true,
+		'afterConsequent': true,
+		'beforeAlternate': true
+	},
+	'requireSpacesInFunctionExpression': {
+		'beforeOpeningCurlyBrace': true
+	},
+	'disallowSpacesInFunctionExpression': {
+		'beforeOpeningRoundBrace': true
+	},
+	'requireBlocksOnNewline': true,
+	'requireSpacesInsideObjectBrackets': 'all',
+	'requireSpacesInsideArrayBrackets': 'all',
+	'disallowSpaceAfterObjectKeys': true,
+	'requireCommaBeforeLineBreak': true,
+	'requireOperatorBeforeLineBreak': [
+		'?', '=', '+', '-', '/', '*', '==', '===', '!=', '!==', '>', '>=', '<', '<=', '|', '||', '&', '&&', '^', '+=', '*=',
+		'-=', '/=', '^='
+	],
+	'requireSpaceBeforeBinaryOperators': [
+		'+', '-', '/', '*', '=', '==', '===', '!=', '!==', '>', '>=', '<', '<=', '|', '||', '&', '&&', '^', '+=', '*=', '-=',
+		'/=', '^='
+	],
+	'requireSpaceAfterBinaryOperators': [
+		'+', '-', '/', '*', '=', '==', '===', '!=', '!==', '>', '>=', '<', '<=', '|', '||', '&', '&&', '^', '+=', '*=', '-=',
+		'/=', '^='
+	],
+	'disallowSpaceAfterPrefixUnaryOperators': [
+		'++', '--', '+', '-', '~', '!'
+	],
+	'disallowSpaceBeforePostfixUnaryOperators': [
+		'++', '--'
+	],
+	'disallowKeywords': [
+		'with'
+	],
+	'validateLineBreaks': 'LF',
+	'validateQuoteMarks': {
+		'mark': '\'',
+		'escape': true
+	},
+	'validateIndentation': '\t',
+	'disallowMixedSpacesAndTabs': true,
+	'disallowTrailingWhitespace': true,
+	'disallowKeywordsOnNewLine': [
+		'else', 'catch'
+	],
+	'maximumLineLength': 120,
+	'safeContextKeyword': [
+		'that'
+	],
+	'requireDotNotation': true,
+	'disallowYodaConditions': true
 };
