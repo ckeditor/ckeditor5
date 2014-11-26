aa6500947e8473e6dbd3fac603a60002375edb31
fredck <f.knabben@cksource.com>
2014-11-26 16:07:42 +0100

    Moved githooks to dev/tasks.

---
+++ packages/ckeditor5-ui/dev/tasks/githooks.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function( grunt ) {
+	grunt.config.merge( {
+		githooks: {
+			all: {
+				'pre-commit': 'default'
+			}
+		}
+	} );
+
+	grunt.loadNpmTasks( 'grunt-githooks' );
+};
