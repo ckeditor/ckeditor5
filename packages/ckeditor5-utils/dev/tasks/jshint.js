72c4b320b07dd6a7eefc9fba216e0f1e999d1127
fredck <f.knabben@cksource.com>
2014-11-26 21:42:20 +0100

    Implemented grunt jshint:git, making it inside default.

--- packages/ckeditor5-utils/dev/tasks/jshint.js
+++ packages/ckeditor5-utils/dev/tasks/jshint.js
@@ -1,13 +1,29 @@
 /* global module */
+/* global require */
 
 'use strict';
 
+var tools = require( './res/tools' );
+
 module.exports = function( grunt ) {
+	// Base task configuration, targetting "all" by default.
+	var config = {
+		all: [ '**/*.js' ],
+		options: defaultConfig
+	};
+
+	var isGitTask = ( grunt.cli.tasks.indexOf( 'jshint:git' ) > -1 ),
+		isDefaultTask = ( grunt.cli.tasks.indexOf( 'default' ) > -1 ) || !grunt.cli.tasks.length;
+
+	// Create the :git configuration on the fly, if necessary.
+	if ( isGitTask || isDefaultTask ) {
+		delete config.all;
+		config.git = tools.getGitDirtyFiles();
+	}
+
+	// Merge with configurations set in gruntfile.js.
 	grunt.config.merge( {
-		jshint: {
-			files: [ '**/*.js' ],
-			options: defaultConfig
-		}
+		jshint: config
 	} );
 
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
