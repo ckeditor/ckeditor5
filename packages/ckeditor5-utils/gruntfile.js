546a298e6809ff84003bb9961cb4c3fb94a6aa44
fredck <f.knabben@cksource.com>
2014-11-26 22:08:50 +0100

    Made it possible to change the default task so it'll run accordingly without the :git prefix.

--- packages/ckeditor5-utils/gruntfile.js
+++ packages/ckeditor5-utils/gruntfile.js
@@ -3,6 +3,10 @@
 'use strict';
 
 module.exports = function( grunt ) {
+	// First register the "default" task, so it can be analized by other tasks.
+	grunt.registerTask( 'default', [ 'jshint:git', 'jscs' ] );
+
+	// Basic configuration, which will be overloaded by the tasks.
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' ),
 
@@ -19,11 +23,11 @@ module.exports = function( grunt ) {
 		}
 	} );
 
+	// Finally load the tasks.
 	grunt.loadTasks( 'dev/tasks' );
-
-	grunt.registerTask( 'default', [ 'jshint:git', 'jscs' ] );
 };
 
+// The list of files we want to exclude from linting tasks, like jshint and jscs.
 var lintIgnores = [
 	'node_modules/**',
 	'build/**'
