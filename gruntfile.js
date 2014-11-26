2af6b8dacd7e4d0af42fda0bab81314ae5929605
fredck <f.knabben@cksource.com>
2014-11-26 10:41:11 +0100

    Moved jscs and jshint config out of gruntfile.js.

--- gruntfile.js
+++ gruntfile.js
@@ -4,12 +4,16 @@ module.exports = function( grunt ) {
 
 		jshint: {
 			files: [ '*.js' ],
-			options: jshintConfig
+			options: {
+				jshintrc: 'dev/tasks/jshint-config.json'
+			}
 		},
 
 		jscs: {
 			src: '*.js',
-			options: jscsConfig
+			options: {
+				config: 'dev/tasks/jscs-config.json'
+			}
 		},
 
 		githooks: {
@@ -24,78 +28,4 @@ module.exports = function( grunt ) {
 
 	// Default tasks.
 	grunt.registerTask( 'default', [ 'jshint', 'jscs' ] );
-};
-
-// Configurations for JSHint
-var jshintConfig = {
-};
-
-// Configurations for JSCS (JavaScript Code Style checker)
-var jscsConfig = {
-	'excludeFiles': [
-		'node_modules/*'
-	],
-	'requireCurlyBraces': [
-		'if', 'else', 'for', 'while', 'do', 'switch', 'try', 'catch'
-	],
-	'requireSpaceAfterKeywords': [
-		'if', 'else', 'for', 'while', 'do', 'switch', 'return', 'try', 'catch'
-	],
-	'requireSpaceBeforeBlockStatements': true,
-	'requireParenthesesAroundIIFE': true,
-	'requireSpacesInConditionalExpression': {
-		'afterTest': true,
-		'beforeConsequent': true,
-		'afterConsequent': true,
-		'beforeAlternate': true
-	},
-	'requireSpacesInFunctionExpression': {
-		'beforeOpeningCurlyBrace': true
-	},
-	'disallowSpacesInFunctionExpression': {
-		'beforeOpeningRoundBrace': true
-	},
-	'requireBlocksOnNewline': true,
-	'requireSpacesInsideObjectBrackets': 'all',
-	'requireSpacesInsideArrayBrackets': 'all',
-	'disallowSpaceAfterObjectKeys': true,
-	'requireCommaBeforeLineBreak': true,
-	'requireOperatorBeforeLineBreak': [
-		'?', '=', '+', '-', '/', '*', '==', '===', '!=', '!==', '>', '>=', '<', '<=', '|', '||', '&', '&&', '^', '+=', '*=',
-		'-=', '/=', '^='
-	],
-	'requireSpaceBeforeBinaryOperators': [
-		'+', '-', '/', '*', '=', '==', '===', '!=', '!==', '>', '>=', '<', '<=', '|', '||', '&', '&&', '^', '+=', '*=', '-=',
-		'/=', '^='
-	],
-	'requireSpaceAfterBinaryOperators': [
-		'+', '-', '/', '*', '=', '==', '===', '!=', '!==', '>', '>=', '<', '<=', '|', '||', '&', '&&', '^', '+=', '*=', '-=',
-		'/=', '^='
-	],
-	'disallowSpaceAfterPrefixUnaryOperators': [
-		'++', '--', '+', '-', '~', '!'
-	],
-	'disallowSpaceBeforePostfixUnaryOperators': [
-		'++', '--'
-	],
-	'disallowKeywords': [
-		'with'
-	],
-	'validateLineBreaks': 'LF',
-	'validateQuoteMarks': {
-		'mark': '\'',
-		'escape': true
-	},
-	'validateIndentation': '\t',
-	'disallowMixedSpacesAndTabs': true,
-	'disallowTrailingWhitespace': true,
-	'disallowKeywordsOnNewLine': [
-		'else', 'catch'
-	],
-	'maximumLineLength': 120,
-	'safeContextKeyword': [
-		'that'
-	],
-	'requireDotNotation': true,
-	'disallowYodaConditions': true
 };
