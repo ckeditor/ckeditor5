2c6e785c0fb7a83c97818d268dd995dcf8734252
fredck <f.knabben@cksource.com>
2015-01-09 17:00:14 +0100

    Added minimum configuration for RequireJS.

--- ckeditor.js
+++ ckeditor.js
@@ -3,4 +3,12 @@
  * For licensing, see LICENSE.md.
  */
 
+/* global requirejs */
+
 'use strict';
+
+// Basic Require.js configuration.
+requirejs.config( {
+	// Modules are generally relative to the core project.
+	baseUrl: '../node_modules/ckeditor-core/src/'
+} );
