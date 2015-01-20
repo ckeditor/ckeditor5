09fa91ec71646abfc2c85389e4144bd5370322ff
fredck <f.knabben@cksource.com>
2015-01-20 16:08:37 +0100

    Added code coverage support for bender.

--- bender.js
+++ bender.js
@@ -4,6 +4,9 @@
 
 var config = {
 	plugins: [
+		// Uncomment to enable code coverage.
+		// 'benderjs-coverage',
+
 		'benderjs-mocha',
 		'benderjs-chai',
 		'dev/bender/plugins/ckeditor5'
@@ -29,6 +32,14 @@ var config = {
 				'node_modules/ckeditor5-*/tests/**'
 			]
 		}
+	},
+
+	coverage: {
+		paths: [
+			'ckeditor.js',
+			'src/**/*.js',
+			'node_modules/ckeditor5-*/src/**/*.js'
+		]
 	}
 };
 
