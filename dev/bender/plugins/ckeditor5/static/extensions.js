87f820099e669bc21dd2ebd564efc2b24338e788
fredck <f.knabben@cksource.com>
2015-01-21 10:05:17 +0100

    Changed the ckeditor plugin for bender to user bender.defer().

--- dev/bender/plugins/ckeditor5/static/extensions.js
+++ dev/bender/plugins/ckeditor5/static/extensions.js
@@ -8,11 +8,9 @@
 'use strict';
 
 ( function() {
-	// Override bender.start to wait until the "ckeditor" module is ready.
-	var originalStart = bender.start;
-	bender.start = function() {
-		CKEDITOR.require( [ 'ckeditor' ], function() {
-			originalStart.apply( this, arguments );
-		} );
-	};
+	// Make Bender wait to start running tests.
+	var done = bender.defer();
+
+	// Wait for the "ckeditor" module to be ready to start testing.
+	CKEDITOR.require( [ 'ckeditor' ], done );
 } )();
