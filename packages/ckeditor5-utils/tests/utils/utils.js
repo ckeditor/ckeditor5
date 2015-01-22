03a3969d3f4580bcfcf65e530446c890199f58c9
fredck <f.knabben@cksource.com>
2015-01-22 15:25:08 +0100

    Re-reviewed tests to use bender.amd.require().

--- packages/ckeditor5-utils/tests/utils/utils.js
+++ packages/ckeditor5-utils/tests/utils/utils.js
@@ -8,147 +8,143 @@
 'use strict';
 
 describe( 'extend()', function() {
-	it( 'should extend and override', function( done ) {
-		CKEDITOR.require( [ 'utils' ], function( utils ) {
-			var target = {
-				a: 1,
-				b: 2
-			};
+	var modules = bender.amd.require( 'utils' );
 
-			var extensions = {
-				a: 'A',
-				c: 3,
+	it( 'should extend and override', function() {
+		var utils = modules.utils;
 
-				// Extend by reference (no deep-copy).
-				obj: {
-					a: 1
-				},
+		var target = {
+			a: 1,
+			b: 2
+		};
 
-				// Extend by reference (no deep-copy).
-				arr: [ 1, 2 ],
+		var extensions = {
+			a: 'A',
+			c: 3,
 
-				// Extend by reference.
-				fn: function() {}
-			};
-
-			var ret = utils.extend( target, extensions );
-
-			expect( target.a ).to.equal( 'A' );
-			expect( target.b ).to.equal( 2 );
-			expect( target ).to.have.property( 'c' ).to.equal( 3 );
-
-			expect( target ).to.have.property( 'obj' ).to.equal( extensions.obj );
-			expect( target ).to.have.property( 'arr' ).to.equal( extensions.arr );
-			expect( target ).to.have.property( 'fn' ).to.equal( extensions.fn );
-
-			// "target" should be the return value.
-			expect( ret ).to.equal( target );
-
-			done();
-		} );
-	} );
-
-	it( 'should not be touched by non-objects', function( done ) {
-		CKEDITOR.require( [ 'utils' ], function( utils ) {
-			var target = {
+			// Extend by reference (no deep-copy).
+			obj: {
 				a: 1
-			};
+			},
 
-			expect( utils.extend( target, function() {} ) ).to.equal( target );
-			expect( utils.extend( target, 1 ) ).to.equal( target );
-			expect( utils.extend( target, 'a' ) ).to.equal( target );
-			expect( utils.extend( target, true ) ).to.equal( target );
-			expect( utils.extend( target, undefined ) ).to.equal( target );
-			expect( utils.extend( target, [] ) ).to.equal( target );
-			expect( utils.extend( target, Date.now() ) ).to.equal( target );
-			expect( utils.extend( target ) ).to.equal( target );
+			// Extend by reference (no deep-copy).
+			arr: [ 1, 2 ],
 
-			// None of the above calls should have touched "target".
-			expect( target ).to.have.property( 'a' ).to.equal( 1 );
-			expect( Object.getOwnPropertyNames( target ).length ).to.equal( 1 );
+			// Extend by reference.
+			fn: function() {}
+		};
 
-			done();
-		} );
+		var ret = utils.extend( target, extensions );
+
+		expect( target.a ).to.equal( 'A' );
+		expect( target.b ).to.equal( 2 );
+		expect( target ).to.have.property( 'c' ).to.equal( 3 );
+
+		expect( target ).to.have.property( 'obj' ).to.equal( extensions.obj );
+		expect( target ).to.have.property( 'arr' ).to.equal( extensions.arr );
+		expect( target ).to.have.property( 'fn' ).to.equal( extensions.fn );
+
+		// "target" should be the return value.
+		expect( ret ).to.equal( target );
 	} );
 
-	it( 'should extend by several params in order', function( done ) {
-		CKEDITOR.require( [ 'utils' ], function( utils ) {
-			var target = {
-				a: 0,
-				b: 0
-			};
+	it( 'should not be touched by non-objects', function() {
+		var utils = modules.utils;
 
-			var ext1 = {
-				b: 1,
-				c: 1
-			};
+		var target = {
+			a: 1
+		};
 
-			var ext2 = {
-				c: 2,
-				d: 2
-			};
+		expect( utils.extend( target, function() {} ) ).to.equal( target );
+		expect( utils.extend( target, 1 ) ).to.equal( target );
+		expect( utils.extend( target, 'a' ) ).to.equal( target );
+		expect( utils.extend( target, true ) ).to.equal( target );
+		expect( utils.extend( target, undefined ) ).to.equal( target );
+		expect( utils.extend( target, [] ) ).to.equal( target );
+		expect( utils.extend( target, Date.now() ) ).to.equal( target );
+		expect( utils.extend( target ) ).to.equal( target );
 
-			utils.extend( target, ext1, ext2 );
+		// None of the above calls should have touched "target".
+		expect( target ).to.have.property( 'a' ).to.equal( 1 );
+		expect( Object.getOwnPropertyNames( target ).length ).to.equal( 1 );
+	} );
 
-			expect( target ).to.have.property( 'a' ).to.equal( 0 );
-			expect( target ).to.have.property( 'b' ).to.equal( 1 );
-			expect( target ).to.have.property( 'c' ).to.equal( 2 );
-			expect( target ).to.have.property( 'd' ).to.equal( 2 );
+	it( 'should extend by several params in order', function() {
+		var utils = modules.utils;
 
-			done();
-		} );
+		var target = {
+			a: 0,
+			b: 0
+		};
+
+		var ext1 = {
+			b: 1,
+			c: 1
+		};
+
+		var ext2 = {
+			c: 2,
+			d: 2
+		};
+
+		utils.extend( target, ext1, ext2 );
+
+		expect( target ).to.have.property( 'a' ).to.equal( 0 );
+		expect( target ).to.have.property( 'b' ).to.equal( 1 );
+		expect( target ).to.have.property( 'c' ).to.equal( 2 );
+		expect( target ).to.have.property( 'd' ).to.equal( 2 );
 	} );
 } );
 
 describe( 'isFunction()', function() {
-	it( 'should be true for functions only', function( done ) {
-		CKEDITOR.require( [ 'utils' ], function( utils ) {
-			var f1 = function() {
-			};
+	var modules = bender.amd.require( 'utils' );
 
-			/* jshint -W054 */	// The Function constructor is a form of eval
-			var f2 = new Function( '' );
-			/* jshint +W054 */
+	it( 'should be true for functions only', function() {
+		var utils = modules.utils;
 
-			expect( utils.isFunction( f1 ) ).to.be.true();
-			expect( utils.isFunction( f2 ) ).to.be.true();
+		var f1 = function() {
+		};
 
-			expect( utils.isFunction( 1 ) ).to.be.false();
-			expect( utils.isFunction( 'a' ) ).to.be.false();
-			expect( utils.isFunction( true ) ).to.be.false();
-			expect( utils.isFunction( undefined ) ).to.be.false();
-			expect( utils.isFunction( [] ) ).to.be.false();
-			expect( utils.isFunction( {} ) ).to.be.false();
-			expect( utils.isFunction( Date.now() ) ).to.be.false();
+		/* jshint -W054 */	// The Function constructor is a form of eval
+		var f2 = new Function( '' );
+		/* jshint +W054 */
 
-			done();
-		} );
+		expect( utils.isFunction( f1 ) ).to.be.true();
+		expect( utils.isFunction( f2 ) ).to.be.true();
+
+		expect( utils.isFunction( 1 ) ).to.be.false();
+		expect( utils.isFunction( 'a' ) ).to.be.false();
+		expect( utils.isFunction( true ) ).to.be.false();
+		expect( utils.isFunction( undefined ) ).to.be.false();
+		expect( utils.isFunction( [] ) ).to.be.false();
+		expect( utils.isFunction( {} ) ).to.be.false();
+		expect( utils.isFunction( Date.now() ) ).to.be.false();
 	} );
 } );
 
 describe( 'isObject()', function() {
-	it( 'should be true for pure objects only', function( done ) {
-		CKEDITOR.require( [ 'utils' ], function( utils ) {
-			var f1 = function() {
-			};
+	var modules = bender.amd.require( 'utils' );
 
-			/* jshint -W054 */	// The Function constructor is a form of eval
-			var f2 = new Function( '' );
-			/* jshint +W054 */
+	it( 'should be true for pure objects only', function() {
+		var utils = modules.utils;
 
-			expect( utils.isObject( {} ) ).to.be.true();
-			expect( utils.isObject( [] ) ).to.be.true();
+		var f1 = function() {
+		};
 
-			expect( utils.isObject( 1 ) ).to.be.false();
-			expect( utils.isObject( 'a' ) ).to.be.false();
-			expect( utils.isObject( true ) ).to.be.false();
-			expect( utils.isObject( undefined ) ).to.be.false();
-			expect( utils.isObject( Date.now() ) ).to.be.false();
-			expect( utils.isObject( f1 ) ).to.be.false();
-			expect( utils.isObject( f2 ) ).to.be.false();
-			expect( utils.isObject( null ) ).to.be.false();
+		/* jshint -W054 */	// The Function constructor is a form of eval
+		var f2 = new Function( '' );
+		/* jshint +W054 */
 
-			done();
-		} );
+		expect( utils.isObject( {} ) ).to.be.true();
+		expect( utils.isObject( [] ) ).to.be.true();
+
+		expect( utils.isObject( 1 ) ).to.be.false();
+		expect( utils.isObject( 'a' ) ).to.be.false();
+		expect( utils.isObject( true ) ).to.be.false();
+		expect( utils.isObject( undefined ) ).to.be.false();
+		expect( utils.isObject( Date.now() ) ).to.be.false();
+		expect( utils.isObject( f1 ) ).to.be.false();
+		expect( utils.isObject( f2 ) ).to.be.false();
+		expect( utils.isObject( null ) ).to.be.false();
 	} );
 } );
