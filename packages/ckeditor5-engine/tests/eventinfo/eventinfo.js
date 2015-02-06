49987e64b416a64d840b8961b12eadb243adcf0c
fredck <f.knabben@cksource.com>
2015-02-06 17:25:50 +0100

    Introduced the Emitter mixin with the events API.

---
+++ packages/ckeditor5-engine/tests/eventinfo/eventinfo.js
@@ -0,0 +1,48 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals describe, it, expect, bender */
+
+'use strict';
+
+var modules = bender.amd.require( 'eventinfo' );
+
+describe( 'EventInfo', function() {
+	it( 'should be created properly', function() {
+		var EventInfo = modules.eventinfo;
+
+		var event = new EventInfo( 'test' );
+
+		expect( event.name ).to.equals( 'test' );
+		expect( event.stop.called ).to.not.be.true();
+		expect( event.off.called ).to.not.be.true();
+	} );
+
+	it( 'should have cancel() and off() marked', function() {
+		var EventInfo = modules.eventinfo;
+
+		var event = new EventInfo( 'test' );
+
+		event.stop();
+		event.off();
+
+		expect( event.stop.called ).to.be.true();
+		expect( event.off.called ).to.be.true();
+	} );
+
+	it( 'should not mark "called" in future instances', function() {
+		var EventInfo = modules.eventinfo;
+
+		var event = new EventInfo( 'test' );
+
+		event.stop();
+		event.off();
+
+		event = new EventInfo( 'test' );
+
+		expect( event.stop.called ).to.not.be.true();
+		expect( event.off.called ).to.not.be.true();
+	} );
+} );
