67a56fde1d92cd4e2a1e221efbf98a0e5a0225f4
fredck <f.knabben@cksource.com>
2015-02-12 09:53:57 +0100

    Minor typo fixing.

--- packages/ckeditor5-engine/src/promise.js
+++ packages/ckeditor5-engine/src/promise.js
@@ -8,13 +8,13 @@
 'use strict';
 
 /**
- * A ES6 compatible Promise class, used for deferred and asynchronous computations.
+ * An ES6 compatible Promise class, used for deferred and asynchronous computations.
  *
  * @class Promise
  */
 
 CKEDITOR.define( function() {
-	// For now we're using the native browser implementation of Promise, a ES6 feature. Just IE is not supporting it so
+	// For now we're using the native browser implementation of Promise, an ES6 feature. Just IE is not supporting it so
 	// a polyfill will have to be developed for it.
 	//
 	// http://caniuse.com/#feat=promises
