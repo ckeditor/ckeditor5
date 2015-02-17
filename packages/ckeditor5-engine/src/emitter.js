3d565470859c612f342188751962ddbd39cc9ddb
fredck <f.knabben@cksource.com>
2015-02-17 11:40:02 +0100

    Re-fixed comments in stopListening().

--- packages/ckeditor5-engine/src/emitter.js
+++ packages/ckeditor5-engine/src/emitter.js
@@ -177,14 +177,14 @@ CKEDITOR.define( [ 'eventinfo', 'utils' ], function( EventInfo, utils ) {
 			if ( callback ) {
 				emitter.off( event, callback );
 			}
-			// Only the emitter and event provided. off() all callbacks for that event.
+			// Only `emitter` and `event` provided. off() all callbacks for that event.
 			else if ( eventCallbacks ) {
 				while ( ( callback = eventCallbacks.pop() ) ) {
 					emitter.off( event, callback );
 				}
 				delete emitterInfo.callbacks[ event ];
 			}
-			// Only the emitter provided. off() all events for that emitter.
+			// Only `emitter` provided. off() all events for that emitter.
 			else if ( emitterInfo ) {
 				for ( event in emitterInfo.callbacks ) {
 					this.stopListening( emitter, event );
