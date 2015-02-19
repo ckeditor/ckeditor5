28dd94823003e61b95d418baed83c105a52d2e20
fredck <f.knabben@cksource.com>
2015-02-19 16:19:29 +0100

    Added utils.isObject.

--- packages/ckeditor5-engine/src/lib/lodash/lodash-ckeditor.js
+++ packages/ckeditor5-engine/src/lib/lodash/lodash-ckeditor.js
@@ -1,7 +1,7 @@
 /**
  * @license
  * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
- * Build: `lodash modern exports="amd" include="extend" --debug --output src/lib/lodash/lodash-ckeditor.js`
+ * Build: `lodash modern exports="amd" include="extend,isObject" --debug --output src/lib/lodash/lodash-ckeditor.js`
  * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
  * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
