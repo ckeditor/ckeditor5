cf12fdfb79ca2f7f506054ace2abf2fb82164557
fredck <f.knabben@cksource.com>
2015-02-23 16:46:51 +0100

    Introduced the Editor class, CKEDITOR.create and CKEDITOR.instances.

---
+++ packages/ckeditor5-engine/src/editor.js
@@ -0,0 +1,55 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
+/**
+ * Represents a single editor instance.
+ *
+ * @class Editor
+ */
+
+CKEDITOR.define( [ 'ckeditor', 'mvc/model', 'utils' ], function( CKEDITOR, Model ) {
+	var Editor = Model.extend( {
+		/**
+		 * Creates a new instance of the Editor class.
+		 *
+		 * This constructor should be rarely used. When creating new editor instance use instead the
+		 * {@link CKEDITOR#create CKEDITOR.create() method}.
+		 *
+		 * @param {Object} element The DOM element that will be the source for the created editor.
+		 * @constructor
+		 */
+		constructor: function Editor( element ) {
+			/**
+			 * The original host page element upon which the editor is created. It is only supposed to be provided on
+			 * editor creation and is not subject to be modified.
+			 *
+			 * @readonly
+			 * @property {Object}
+			 */
+			this.element = element;
+		},
+
+		/**
+		 * Destroys the editor instance, releasing all resources used by it. If the editor replaced an element, the
+		 * element will be recovered.
+		 */
+		destroy: function() {
+			this.fire( 'destroy' );
+
+			delete this.element;
+		}
+	} );
+
+	return Editor;
+} );
+
+/**
+ * Fired when this editor instance is destroyed. The editor at this point is not usable and this event should be used to
+ * perform the clean-up in any plugin.
+ *
+ * @event destroy
+ */
---
+++ packages/ckeditor5-engine/tests/editor/editor.js
@@ -0,0 +1,45 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals describe, it, expect, sinon, document */
+
+'use strict';
+
+var modules = bender.amd.require( 'editor' );
+
+describe( 'constructor', function() {
+	it( 'should create a new editor instance', function() {
+		var Editor = modules.editor;
+
+		var editor = new Editor( document.body );
+
+		expect( editor ).to.have.property( 'element' ).to.equals( document.body );
+	} );
+} );
+
+describe( 'destroy', function() {
+	it( 'should fire "destroy"', function() {
+		var Editor = modules.editor;
+
+		var editor = new Editor( document.body );
+		var spy = sinon.spy();
+
+		editor.on( 'destroy', spy );
+
+		editor.destroy();
+
+		sinon.assert.called( spy );
+	} );
+
+	it( 'should delete the "element" property', function() {
+		var Editor = modules.editor;
+
+		var editor = new Editor( document.body );
+
+		editor.destroy();
+
+		expect( editor ).to.not.have.property( 'element' );
+	} );
+} );
