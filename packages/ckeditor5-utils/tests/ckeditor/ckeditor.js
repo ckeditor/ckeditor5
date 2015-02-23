7040a19d1e75e2b7d39d8af572d030d90a2c8953
fredck <f.knabben@cksource.com>
2015-02-23 16:46:51 +0100

    Introduced the Editor class, CKEDITOR.create and CKEDITOR.instances.

---
+++ packages/ckeditor5-utils/tests/ckeditor/ckeditor.js
@@ -0,0 +1,97 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals describe, it, expect, beforeEach, document */
+
+'use strict';
+
+var modules = bender.amd.require( 'ckeditor', 'editor', 'promise' );
+
+var content = document.getElementById( content );
+
+beforeEach( function() {
+	var CKEDITOR = modules.ckeditor;
+
+	// Destroy all editor instances.
+	while ( CKEDITOR.instances.length ) {
+		CKEDITOR.instances.get( 0 ).destroy();
+	}
+} );
+
+describe( 'create', function() {
+	it( 'should return a promise', function() {
+		var CKEDITOR = modules.ckeditor;
+		var Promise = modules.promise;
+
+		expect( CKEDITOR.create( content ) ).to.be.instanceof( Promise );
+	} );
+
+	it( 'should create a new editor instance', function() {
+		var CKEDITOR = modules.ckeditor;
+		var Editor = modules.editor;
+
+		return CKEDITOR.create( content ).then( function( editor ) {
+			expect( editor ).to.be.instanceof( Editor );
+			expect( editor.element ).to.equals( content );
+		} );
+	} );
+
+	it( 'should create a new editor instance (using selector)', function() {
+		var CKEDITOR = modules.ckeditor;
+		var Editor = modules.editor;
+
+		return CKEDITOR.create( '.editor' ).then( function( editor ) {
+			expect( editor ).to.be.instanceof( Editor );
+			expect( editor.element ).to.equals( document.querySelector( '.editor' ) );
+		} );
+	} );
+
+	it( 'should add the editor to the `instances` collection', function() {
+		var CKEDITOR = modules.ckeditor;
+
+		return CKEDITOR.create( content ).then( function( editor ) {
+			expect( CKEDITOR.instances ).to.have.length( 1 );
+			expect( CKEDITOR.instances.get( 0 ) ).to.equals( editor );
+		} );
+	} );
+
+	it( 'should remove the editor from the `instances` collection on `destroy`', function() {
+		var CKEDITOR = modules.ckeditor;
+		var editor1, editor2;
+
+		// Create the first editor.
+		return CKEDITOR.create( content ).then( function( editor ) {
+			editor1 = editor;
+
+			// Create the second editor.
+			return CKEDITOR.create( content ).then( function( editor ) {
+				editor2 = editor;
+
+				// It should have 2 editors.
+				expect( CKEDITOR.instances ).to.have.length( 2 );
+
+				// Destroy one of them.
+				editor1.destroy();
+
+				// It should have 1 editor now.
+				expect( CKEDITOR.instances ).to.have.length( 1 );
+
+				// Ensure that the remaining is the right one.
+				expect( CKEDITOR.instances.get( 0 ) ).to.equals( editor2 );
+			} );
+		} );
+	} );
+
+	it( 'should be rejected on element not found', function() {
+		var CKEDITOR = modules.ckeditor;
+
+		return CKEDITOR.create( '.undefined' ).then( function() {
+			throw( 'It should not enter this function' );
+		} ).catch( function( error ) {
+			expect( error ).to.be.instanceof( Error );
+			expect( error.message ).to.equals( 'Element not found' );
+		} );
+	} );
+} );
