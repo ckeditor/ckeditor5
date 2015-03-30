9fd4567efcf0f7d4cf69198de2d952e891266c2e
fredck <f.knabben@cksource.com>
2015-03-30 13:16:49 +0200

    Added basic tests for Plugin.

---
+++ packages/ckeditor5-utils/tests/plugin/plugin.js
@@ -0,0 +1,27 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals describe, it, expect, before, document */
+
+'use strict';
+
+var modules = bender.amd.require( 'plugin', 'editor' );
+var editor;
+
+before( function() {
+	var Editor = modules.editor;
+
+	editor = new Editor( document.body.appendChild( document.createElement( 'div' ) ) );
+} );
+
+describe( 'constructor', function() {
+	it( 'should set the `editor` property', function() {
+		var Plugin = modules.plugin;
+
+		var plugin = new Plugin( editor );
+
+		expect( plugin ).to.have.property( 'editor' ).to.equal( editor );
+	} );
+} );
