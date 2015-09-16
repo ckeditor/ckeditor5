93a69d364a6cd631217bec3650cd7a1e33ad88b3
pjasiun <p.jasiun@cksource.com>
2015-09-16 09:01:35 +0200

    Range.

---
+++ packages/ckeditor5-engine/src/document/range.js
@@ -0,0 +1,39 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
+CKEDITOR.define( function() {
+	/**
+	 * Range class.
+	 *
+	 * @class document.Range
+	 */
+	class Range {
+		/**
+		 * Create an range.
+		 *
+		 * @param {document.Position} start Start position.
+		 * @param {document.Position} end End position.
+		 */
+		constructor( start, end ) {
+			/**
+			 * Start position.
+			 *
+			 * @type {document.Position}
+			 */
+			this.start = start;
+
+			/**
+			 * End position.
+			 *
+			 * @type {document.Position}
+			 */
+			this.end = end;
+		}
+	}
+
+	return Range;
+} );
