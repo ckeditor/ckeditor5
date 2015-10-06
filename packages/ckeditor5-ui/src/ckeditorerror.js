aba17888e3a08197a2c6026abcf1986671e3aa4c
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-10-06 14:45:47 +0200

    Add error data to the error message, to make the message more useful.

--- packages/ckeditor5-ui/src/ckeditorerror.js
+++ packages/ckeditor5-ui/src/ckeditorerror.js
@@ -28,9 +28,15 @@ CKEDITOR.define( function() {
 		 * @param {String} message The error message in an `error-name: Error message.` format.
 		 * During the minification process the "Error message" part will be removed to limit the code size
 		 * and a link to this error documentation will be added to the `message`.
-		 * @param {Object} [data] Additional data describing the error.
+		 * @param {Object} [data] Additional data describing the error. A stringified version of this object
+		 * will be appended to the error {@link #message}, so the data are quickly visible in the console. The original
+		 * data object will also be later available under the {@link #data} property.
 		 */
 		constructor( message, data ) {
+			if ( data ) {
+				message += ' ' + JSON.stringify( data );
+			}
+
 			super( message );
 
 			this.name = 'CKEditorError';
