3bb01e38f4d065586efa00814ed28a95e869c1eb
Piotr Jasiun <p.jasiun@cksource.com>
2015-11-13 17:09:55 +0100

    Introduced range static constructors.

--- packages/ckeditor5-engine/src/document/range.js
+++ packages/ckeditor5-engine/src/document/range.js
@@ -35,10 +35,29 @@ CKEDITOR.define( [ 'document/positioniterator', 'document/position' ], ( Positio
 			this.end = end;
 		}
 
+		/**
+		 * Creates a range inside an element which starts before the first child and ends after the last child.
+		 *
+		 * @param {document.Element} element Element which is a parent for the range.
+		 * @returns {document.Range} Created range.
+		 */
 		static createFromElement( element ) {
+			return Range.createFromParentsAndOffsets( element, 0, element, element.getChildCount() );
+		}
+
+		/**
+		 * Creates a range from given parents and offsets.
+		 *
+		 * @param {document.Element} startElement Start position parent element.
+		 * @param {Number} startOffset Start position offset.
+		 * @param {document.Element} endElement End position parent element.
+		 * @param {Number} endOffset End position offset.
+		 * @returns {document.Range} Created range.
+		 */
+		static createFromParentsAndOffsets( startElement, startOffset, endElement, endOffset ) {
 			return new Range(
-					Position.createFromParentAndOffset( element, 0 ),
-					Position.createFromParentAndOffset( element, element.getChildCount() )
+					Position.createFromParentAndOffset( startElement, startOffset ),
+					Position.createFromParentAndOffset( endElement, endOffset )
 				);
 		}
 
