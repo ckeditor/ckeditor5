ac30090a9368b92ae22d09561538753a05eec71c
Szymon Cofalik <s.cofalik@cksource.com>
2015-12-15 12:14:56 +0100

    Changed Batch.remove to Batch.removeFlat. Added Batch.remove, Batch.removeNode.

--- packages/ckeditor5-engine/src/treemodel/delta/removedelta.js
+++ packages/ckeditor5-engine/src/treemodel/delta/removedelta.js
@@ -8,8 +8,9 @@
 CKEDITOR.define( [
 	'treemodel/delta/delta',
 	'treemodel/delta/register',
-	'treemodel/operation/removeoperation'
-], ( Delta, register, RemoveOperation ) => {
+	'treemodel/operation/removeoperation',
+	'treemodel/position'
+], ( Delta, register, RemoveOperation, Position ) => {
 	/**
 	 * To provide specific OT behavior and better collisions solving, {@link treeModel.Batch#remove} method
 	 * uses the `RemoveDelta` class which inherits from the `Delta` class and may overwrite some methods.
@@ -18,25 +19,48 @@ CKEDITOR.define( [
 	 */
 	class RemoveDelta extends Delta {}
 
+	function addRemoveOperation( batch, delta, position, howMany ) {
+		const operation = new RemoveOperation( position, howMany, batch.doc.version );
+		batch.doc.applyOperation( operation );
+		delta.addOperation( operation );
+	}
+
 	/**
-	 * Removes nodes starting from the given position.
+	 * Removes given node. Use this only when you have to remove one node (may contain children). If you want to remove
+	 * multiple nodes, use {@link treeModel.Batch#removeFlat removeFlat} or {@link treeModel.Batch#remove remove} instead.
 	 *
 	 * @chainable
-	 * @method remove
+	 * @method removeNode
 	 * @memberOf treeModel.Batch
-	 * @param {treeModel.Position} position Position before the first node to remove.
-	 * @param {Number} howMany How many nodes to remove.
+	 * @param {treeModel.Node} node Node to remove.
 	 */
-	register( 'remove', function( position, howMany ) {
-		if ( typeof howMany !== 'number' ) {
-			howMany = 1;
-		}
-
+	register( 'removeNode', function( node ) {
 		const delta = new RemoveDelta();
 
-		const operation = new RemoveOperation( position, howMany, this.doc.version );
-		this.doc.applyOperation( operation );
-		delta.addOperation( operation );
+		addRemoveOperation( this, delta, Position.createBefore( node ), 1 );
+
+		this.addDelta( delta );
+
+		return this;
+	} );
+
+	/**
+	 * Removes given range of nodes.
+	 *
+	 * @chainable
+	 * @method move
+	 * @memberOf treeModel.Batch
+	 * @param {treeModel.Range} range Range to remove.
+	 */
+	register( 'remove', function( range ) {
+		const delta = new RemoveDelta();
+
+		// The array is reversed, so the ranges are correct and do not have to be updated.
+		let ranges = range.getMinimalFlatRanges().reverse();
+
+		for ( let flat of ranges ) {
+			addRemoveOperation( this, delta, flat.start, flat.end.offset - flat.start.offset );
+		}
 
 		this.addDelta( delta );
 
--- packages/ckeditor5-engine/tests/treemodel/delta/removedelta.js
+++ packages/ckeditor5-engine/tests/treemodel/delta/removedelta.js
@@ -9,52 +9,89 @@
 
 const modules = bender.amd.require(
 	'treemodel/document',
-	'treemodel/position' );
+	'treemodel/position',
+	'treemodel/range',
+	'treemodel/element'
+);
 
 describe( 'Batch', () => {
-	let Document, Position;
+	let Document, Position, Range, Element;
 
-	let doc, root;
+	let doc, root, div, p, batch, chain;
 
 	before( () => {
 		Document = modules[ 'treemodel/document' ];
 		Position = modules[ 'treemodel/position' ];
+		Range = modules[ 'treemodel/range' ];
+		Element = modules[ 'treemodel/element' ];
 	} );
 
 	beforeEach( () => {
 		doc = new Document();
 		root = doc.createRoot( 'root' );
-		root.insertChildren( 0, 'foobar' );
+
+		div = new Element( 'div', [], 'foobar' );
+		p = new Element( 'p', [], 'abcxyz' );
+
+		root.insertChildren( 0, [ div, p ] );
+
+		batch = doc.batch();
 	} );
 
-	describe( 'remove', () => {
-		it( 'should remove one element', () => {
-			const position = new Position( root, [ 1 ] );
-			doc.batch().remove( position );
+	function getNodesAndText( element ) {
+		let range = Range.createFromElement( element );
+		let txt = '';
 
-			expect( root.getChildCount() ).to.equal( 5 );
-			expect( root.getChild( 0 ).character ).to.equal( 'f' );
-			expect( root.getChild( 1 ).character ).to.equal( 'o' );
-			expect( root.getChild( 2 ).character ).to.equal( 'b' );
-			expect( root.getChild( 3 ).character ).to.equal( 'a' );
-			expect( root.getChild( 4 ).character ).to.equal( 'r' );
+		for ( let step of range ) {
+			let node = step.node;
+
+			if ( node.character ) {
+				txt += node.character.toLowerCase();
+			} else if ( node.name ) {
+				txt += node.name.toUpperCase();
+			}
+		}
+
+		return txt;
+	}
+
+	describe( 'removeNode', () => {
+		beforeEach( () => {
+			chain = batch.removeNode( div );
 		} );
 
-		it( 'should remove 3 elements', () => {
-			const position = new Position( root, [ 1 ] );
-			doc.batch().remove( position, 3 );
-
-			expect( root.getChildCount() ).to.equal( 3 );
-			expect( root.getChild( 0 ).character ).to.equal( 'f' );
-			expect( root.getChild( 1 ).character ).to.equal( 'a' );
-			expect( root.getChild( 2 ).character ).to.equal( 'r' );
+		it( 'should remove specified node', () => {
+			expect( root.getChildCount() ).to.equal( 1 );
+			expect( getNodesAndText( root.getChild( 0 ) ) ).to.equal( 'abcxyz' );
 		} );
 
 		it( 'should be chainable', () => {
-			const position = new Position( root, [ 1 ] );
-			const batch = doc.batch();
+			expect( chain ).to.equal( batch );
+		} );
+	} );
 
-			const chain = batch.remove( position );
+	describe( 'remove', () => {
+		beforeEach( () => {
+			div.insertChildren( 4, [ new Element( 'p', [], 'gggg' ) ] );
+			div.insertChildren( 2, [ new Element( 'p', [], 'hhhh' ) ] );
+
+			// Range starts in ROOT > DIV > P > gg|gg.
+			// Range ends in ROOT > DIV > ...|ar.
+			let range = new Range( new Position( root, [ 0, 2, 2 ] ), new Position( root, [ 0, 6 ] ) );
+			chain = batch.remove( range, new Position( root, [ 1, 3 ] ) );
+		} );
+
+		it( 'should move any range of nodes', () => {
+			expect( getNodesAndText( root.getChild( 0 ) ) ).to.equal( 'foPhhPar' );
+			expect( getNodesAndText( root.getChild( 1 ) ) ).to.equal( 'abcxyz' );
+		} );
+
+		it( 'should create minimal number of operations', () => {
+			expect( batch.deltas.length ).to.equal( 1 );
+			expect( batch.deltas[ 0 ].operations.length ).to.equal( 2 );
+		} );
+
+		it( 'should be chainable', () => {
 			expect( chain ).to.equal( batch );
 		} );
 	} );
