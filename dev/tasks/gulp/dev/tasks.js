593709f0e2a5339bdac138bcf72c2af57aaf9de7
Szymon Kupś <szymonkups@gmail.com>
2016-01-13 11:10:35 +0100

    Moved development tasks to gulp.

---
+++ dev/tasks/gulp/dev/tasks.js
@@ -0,0 +1,66 @@
+/* jshint node: true, esnext: true */
+
+'use strict';
+
+const gulp = require( 'gulp' );
+const minimist = require( 'minimist' );
+const statusTask = require( '../../utils/dev-status' );
+const initTask = require( '../../utils/dev-init' );
+const installTask = require( '../../utils/dev-install' );
+const pluginCreateTask = require( '../../utils/dev-plugin-create' );
+const updateTask = require( '../../utils/dev-update' );
+const boilerplateUpdateTask = require( '../../utils/dev-boilerplate-update' );
+const relinkTask = require( '../../utils/dev-relink' );
+
+module.exports = ( config ) => {
+	const ckeditor5Path = process.cwd();
+	const packageJSON = require( '../../../../package.json' );
+
+	gulp.task( 'dev-init', () => {
+		initTask( installTask, ckeditor5Path, packageJSON, config.WORKSPACE_DIR, console.log );
+	} );
+
+	gulp.task( 'dev-plugin-create', ( done ) => {
+		pluginCreateTask( ckeditor5Path, config.WORKSPACE_DIR, console.log )
+			.then( done )
+			.catch( ( error )  => done( error ) );
+	} );
+
+	gulp.task( 'dev-update', () => {
+		const options = minimist( process.argv.slice( 2 ), {
+			boolean: [ 'npm-update' ],
+			default: {
+				'npm-update': false
+			}
+		} );
+
+		updateTask( ckeditor5Path, packageJSON, config.WORKSPACE_DIR, console.log, options[ 'npm-update' ] );
+	} );
+
+	gulp.task( 'dev-status', () => {
+		statusTask( ckeditor5Path, packageJSON, config.WORKSPACE_DIR, console.log, console.error );
+	} );
+
+	gulp.task( 'dev-boilerplate-update', () => {
+		boilerplateUpdateTask( ckeditor5Path, packageJSON, config.WORKSPACE_DIR, console.log, console.error );
+	} );
+
+	gulp.task( 'dev-relink', () => {
+		relinkTask( ckeditor5Path, packageJSON, config.WORKSPACE_DIR, console.log, console.error );
+	} );
+
+	gulp.task( 'dev-install', () => {
+		const options = minimist( process.argv.slice( 2 ), {
+			string: [ 'plugin' ],
+			default: {
+				plugin: ''
+			}
+		} );
+
+		if ( options.plugin ) {
+			installTask( ckeditor5Path, config.WORKSPACE_DIR, options.plugin, console.log );
+		} else {
+			throw new Error( 'Please provide a plugin to install: gulp dev-install --plugin <path|GitHub URL|name>' );
+		}
+	} );
+};
--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,11 @@ const gulp = require( 'gulp' );
 
 const config = {
 	ROOT_DIR: '.',
-	DIST_DIR: 'dist'
+	DIST_DIR: 'dist',
+	WORKSPACE_DIR: '..'
 };
 
 require( './dev/tasks/gulp/build/tasks' )( config );
+require( './dev/tasks/gulp/dev/tasks' )( config );
 
 gulp.task( 'default', [ 'build' ] );
