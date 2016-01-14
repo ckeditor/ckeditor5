77c08ae0f753285f83150832439fe2048d3f7442
Szymon Kupś <szymonkups@gmail.com>
2016-01-14 14:31:38 +0100

    Added pre-commit git hook with jshint and jscs.

--- dev/tasks/gulp/lint/tasks.js
+++ dev/tasks/gulp/lint/tasks.js
@@ -6,16 +6,37 @@ const gulp = require( 'gulp' );
 const jshint = require( 'gulp-jshint' );
 const jscs = require( 'gulp-jscs' );
 const fs = require( 'fs' );
+const guppy = require( 'git-guppy' )( gulp );
+const gulpFilter = require( 'gulp-filter' );
+const gutil = require( 'gulp-util' );
 
 module.exports = ( config ) => {
 	const src = [ '**/*.js' ].concat( config.IGNORED_FILES.map( i => '!' + i ), getGitIgnore() );
 
 	gulp.task( 'lint', () => {
 		return gulp.src( src )
-			.pipe( jscs() )
-			.pipe( jshint() )
-			.pipe( jscs.reporter() )
-			.pipe( jshint.reporter( 'jshint-reporter-jscs' ) );
+			.pipe( lint() );
+	} );
+
+	gulp.task( 'pre-commit', () => {
+		return guppy.stream( 'pre-commit' )
+			.pipe( gulpFilter( src ) )
+			.pipe( lint() )
+
+			// Error reporting for gulp.
+			.pipe( jscs.reporter( 'fail' ) )
+			.on( 'error', errorHandler )
+			.pipe( jshint.reporter( 'fail' ) )
+			.on( 'error', errorHandler );
+
+		/**
+		 * Handles error from jscs and jshint fail reporters. Stops node process with error code
+		 * and prints error message to the console.
+		 */
+		function errorHandler() {
+			gutil.log( gutil.colors.red( 'Linting failed, commit aborted' ) );
+			process.exit( 1 );
+		}
 	} );
 
 	/**
@@ -35,5 +56,19 @@ module.exports = ( config ) => {
 			.filter( ( path ) => !!path )
 			// Add `!` for ignore glob.
 			.map( i => '!' + i );
+	}
+
+	/**
+	 * Returns stream with all linting plugins combined.
+	 * @returns { Stream }
+	 */
+	function lint() {
+		const stream = jscs();
+		stream
+			.pipe( jshint() )
+			.pipe( jscs.reporter() )
+			.pipe( jshint.reporter( 'jshint-reporter-jscs' ) );
+
+		return stream;
 	}
 };
