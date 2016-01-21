e2eb3f643ba2ffd6e570eb633c0fe689af2fa05d
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-01-21 09:42:46 +0100

    Introduced Creator.get/setDataFrom/InElement.

--- packages/ckeditor5-engine/src/creator.js
+++ packages/ckeditor5-engine/src/creator.js
@@ -15,4 +15,31 @@ import Plugin from './plugin.js';
  */
 
 export default class Creator extends Plugin {
+	/**
+	 * Gets data from a given source element.
+	 *
+	 * @param {HTMLElement} el The element from which the data will be retrieved.
+	 * @returns {String} The data string.
+	 */
+	static getDataFromElement( el ) {
+		if ( el instanceof HTMLTextAreaElement ) {
+			return el.value;
+		}
+
+		return el.innerHTML;
+	}
+
+	/**
+	 * Sets data in a given element.
+	 *
+	 * @param {HTMLElement} el The element in which the data will be set.
+	 * @param {String} data The data string.
+	 */
+	static setDataInElement( el, data ) {
+		if ( el instanceof HTMLTextAreaElement ) {
+			el.value = data;
+		}
+
+		el.innerHTML = data;
+	}
 }
