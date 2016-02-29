afa93c0d28154ac97206b9cb99f8735ddb49c208
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-02-29 13:16:42 +0100

    Better explained one field.

--- dev/tasks/dev/utils/inquiries.js
+++ dev/tasks/dev/utils/inquiries.js
@@ -57,7 +57,7 @@ module.exports = {
 		return new Promise( ( resolve ) => {
 			inquirer.prompt( [ {
 				name: 'description',
-				message: 'Package description:'
+				message: 'Package description (one sentence):'
 			} ], ( answers ) => {
 				resolve( answers.description || '' );
 			} );
