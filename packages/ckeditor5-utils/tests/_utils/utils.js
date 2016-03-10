3131ba48959a435fb8a0969890e6fbcb4156d76c
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-03-10 10:19:53 +0100

    Removed a method which should stay in the core.

--- packages/ckeditor5-utils/tests/_utils/utils.js
+++ packages/ckeditor5-utils/tests/_utils/utils.js
@@ -7,35 +7,9 @@
 
 /* global console:false */
 
-import moduleUtils from '/tests/_utils/module.js';
 import EmitterMixin from '/ckeditor5/utils/emittermixin.js';
 
 const utils = {
-	/**
-	 * Defines CKEditor plugin which is a mock of an editor creator.
-	 *
-	 * The mocked creator is available under:
-	 *
-	 *		editor.plugins.get( 'creator-thename' );
-	 *
-	 * @param {String} creatorName Name of the creator.
-	 * @param {Object} [proto] Prototype of the creator. Properties from the proto param will
-	 * be copied to the prototype of the creator.
-	 */
-	defineEditorCreatorMock( creatorName, proto ) {
-		moduleUtils.define( 'creator-' + creatorName, [ 'creator' ], ( Creator ) => {
-			class TestCreator extends Creator {}
-
-			if ( proto ) {
-				for ( let propName in proto ) {
-					TestCreator.prototype[ propName ] = proto[ propName ];
-				}
-			}
-
-			return TestCreator;
-		} );
-	},
-
 	/**
 	 * Returns the number of elements return by the iterator.
 	 *
