aeb3df04f940617c8f5c4b420dd6923d57b26217
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-04-21 15:35:12 +0200

    Moved diff from the engine.

---
+++ packages/ckeditor5-utils/tests/diff.js
@@ -0,0 +1,31 @@
+/**
+ * @license Copyright (c) 2003-20'INSERT'6, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
+import diff from '/ckeditor5/utils/diff.js';
+
+describe( 'diff', () => {
+	it( 'should diff arrays', () => {
+		expect( diff( 'aba', 'acca' ) ).to.deep.equals( [ 'EQUAL', 'INSERT', 'INSERT', 'DELETE', 'EQUAL' ] );
+	} );
+
+	it( 'should reverse result if the second array is shorter', () => {
+		expect( diff( 'acca', 'aba' ) ).to.deep.equals( [ 'EQUAL', 'DELETE', 'DELETE', 'INSERT', 'EQUAL' ] );
+	} );
+
+	it( 'should diff if arrays are same', () => {
+		expect( diff( 'abc', 'abc' ) ).to.deep.equals( [ 'EQUAL', 'EQUAL', 'EQUAL' ] );
+	} );
+
+	it( 'should diff if one array is empty', () => {
+		expect( diff( '', 'abc' ) ).to.deep.equals( [ 'INSERT', 'INSERT', 'INSERT' ] );
+	} );
+
+	it( 'should use custom comparator', () => {
+		expect( diff( 'aBc', 'abc' ) ).to.deep.equals( [ 'EQUAL', 'INSERT', 'DELETE', 'EQUAL' ] );
+		expect( diff( 'aBc', 'abc', ( a, b ) => a.toLowerCase() == b.toLowerCase() ) ).to.deep.equals( [ 'EQUAL', 'EQUAL', 'EQUAL' ] );
+	} );
+} );
