06ed3aff9333e67670e7392ca0ff5b4e409ad27a
Szymon Kupś <szymonkups@gmail.com>
2016-05-16 14:24:36 +0200

    Change node testing task name to test-node, added code coverage.

--- dev/tasks/test/tasks.js
+++ dev/tasks/test/tasks.js
@@ -3,49 +3,40 @@
 'use strict';
 
 const gulp = require( 'gulp' );
+const istanbul = require( 'gulp-istanbul' );
 const gutil = require( 'gulp-util' );
 const mocha = require( 'gulp-mocha' );
 const chai = require( 'chai' );
 const filterBy = require( 'gulp-filter-by' );
 const filter = require( 'gulp-filter' );
 const sinon = require( 'sinon' );
-const minimist = require( 'minimist' );
 const devTools = require( '../dev/utils/tools' );
 const semver = require( 'semver' );
+const minimist = require( 'minimist' );
+const buildUtils = require( '../build/utils' );
 
 module.exports = () => {
 	const ignoreRegexp = /\/\* ?bender-tags:.*\bbrowser-only\b.*\*\//;
+	const options = minimist( process.argv.slice( 2 ), {
+		boolean: [
+			'coverage'
+		],
+
+		default: {
+			coverage: false
+		}
+	} );
 
 	// Inject globals before running tests.
 	global.should = chai.should;
 	global.expect = chai.expect;
 	global.assert = chai.assert;
 	global.sinon = sinon;
+	global.bender = { model: {}, view: {} };
 
 	const tasks = {
-		testEditor() {
-			const options = minimist( process.argv.slice( 2 ), {
-				boolean: [ 'node' ],
-				default: {
-					'node': false
-				}
-			} );
 
-			if ( options.node ) {
-				return tasks.testInNode();
-			}
-		},
-
-		testInNode() {
-			const minVersion = '6.1.0';
-
-			if ( semver.lt( process.version, minVersion ) ) {
-				throw new gutil.PluginError( {
-					plugin: 'test-editor',
-					message: `Wrong Node.js version. Please use Node.js in version v${ minVersion } or higher.`
-				} );
-			}
-
+		src() {
 			const src = [
 				'build/cjs/tests/**/*.js',
 				'!**/_utils/**/*.js',
@@ -55,8 +46,39 @@ module.exports = () => {
 
 			return gulp.src( src )
 				.pipe( tasks.skipManual() )
-				.pipe( tasks.skipIgnored() )
-				.pipe( mocha() );
+				.pipe( tasks.skipIgnored() );
+		},
+
+		prepareCoverage() {
+			return tasks.src()
+				// Covering files
+				.pipe( istanbul() )
+				// Force `require` to return covered files
+				.pipe( istanbul.hookRequire() );
+		},
+
+		/**
+		 * Runs tests in Node.js environment.
+		 *
+		 * @returns {Stream}
+		 */
+		testInNode() {
+			const minVersion = '6.1.0';
+
+			if ( semver.lt( process.version, minVersion ) ) {
+				throw new gutil.PluginError( {
+					plugin: 'test-node',
+					message: `Wrong Node.js version. Please use Node.js in version v${ minVersion } or higher.`
+				} );
+			}
+
+			// Include global test tools.
+			global.bender.model = require( '../../../build/cjs/tests/engine/_utils/model.js' );
+			global.bender.view = require( '../../../build/cjs/tests/engine/_utils/view.js' );
+
+			return tasks.src()
+				.pipe( mocha( { reporter: 'progress' } ) )
+				.pipe( options.coverage ? istanbul.writeReports() : buildUtils.noop() );
 		},
 
 		/**
@@ -81,7 +103,13 @@ module.exports = () => {
 		}
 	};
 
-	gulp.task( 'test-editor', [ 'build:js:cjs' ], tasks.testEditor );
+	gulp.task( 'test-node:coverage', [ 'build:js:cjs' ], tasks.prepareCoverage );
+
+	if ( options.coverage ) {
+		gulp.task( 'test-node', [ 'build:js:cjs', 'test-node:coverage' ], tasks.testInNode );
+	} else {
+		gulp.task( 'test-node', [ 'build:js:cjs' ], tasks.testInNode );
+	}
 
 	return tasks;
 };
