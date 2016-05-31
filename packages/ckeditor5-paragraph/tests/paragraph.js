40def5c8c7fa9aca640f8a09d2f0a7bfdb0b5ea4
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-05-31 14:48:07 +0200

    Updated tests after recent changes in the core.

--- packages/ckeditor5-paragraph/tests/paragraph.js
+++ packages/ckeditor5-paragraph/tests/paragraph.js
@@ -6,23 +6,22 @@
 'use strict';
 
 import Paragraph from '/ckeditor5/paragraph/paragraph.js';
-import Editor from '/ckeditor5/editor.js';
-import StandardCreator from '/ckeditor5/creator/standardcreator.js';
+import VirtualTestEditor from '/tests/ckeditor5/_utils/virtualtesteditor.js';
 import { getData } from '/tests/engine/_utils/model.js';
 
 describe( 'Paragraph feature', () => {
-	let editor, document;
+	let editor, doc;
 
 	beforeEach( () => {
-		editor = new Editor( null, {
-			creator: StandardCreator,
-			features: [ Paragraph ]
-		} );
+		return VirtualTestEditor.create( {
+				features: [ Paragraph ]
+			} )
+			.then( newEditor => {
+				editor = newEditor;
+				doc = editor.document;
 
-		return editor.init().then( () => {
-			document = editor.document;
-			document.createRoot( 'main' );
-		} );
+				doc.createRoot();
+			} );
 	} );
 
 	it( 'should be loaded', () => {
@@ -30,29 +29,29 @@ describe( 'Paragraph feature', () => {
 	} );
 
 	it( 'should set proper schema rules', () => {
-		expect( document.schema.hasItem( 'paragraph' ) ).to.be.true;
-		expect( document.schema.check( { name: 'paragraph', inside: '$root' } ) ).to.be.true;
-		expect( document.schema.check( { name: '$inline', inside: 'paragraph' } ) ).to.be.true;
+		expect( doc.schema.hasItem( 'paragraph' ) ).to.be.true;
+		expect( doc.schema.check( { name: 'paragraph', inside: '$root' } ) ).to.be.true;
+		expect( doc.schema.check( { name: '$inline', inside: 'paragraph' } ) ).to.be.true;
 	} );
 
 	it( 'should convert paragraph', () => {
 		editor.setData( '<p>foobar</p>' );
 
-		expect( getData( document, { withoutSelection: true } ) ).to.equal( '<paragraph>foobar</paragraph>' );
+		expect( getData( doc, { withoutSelection: true } ) ).to.equal( '<paragraph>foobar</paragraph>' );
 		expect( editor.getData() ).to.equal( '<p>foobar</p>' );
 	} );
 
 	it( 'should convert paragraph only', () => {
 		editor.setData( '<p>foo<b>baz</b>bar</p>' );
 
-		expect( getData( document, { withoutSelection: true } ) ).to.equal( '<paragraph>foobazbar</paragraph>' );
+		expect( getData( doc, { withoutSelection: true } ) ).to.equal( '<paragraph>foobazbar</paragraph>' );
 		expect( editor.getData() ).to.equal( '<p>foobazbar</p>' );
 	} );
 
 	it( 'should convert multiple paragraphs', () => {
 		editor.setData( '<p>foo</p><p>baz</p>' );
 
-		expect( getData( document, { withoutSelection: true } ) ).to.equal( '<paragraph>foo</paragraph><paragraph>baz</paragraph>' );
+		expect( getData( doc, { withoutSelection: true } ) ).to.equal( '<paragraph>foo</paragraph><paragraph>baz</paragraph>' );
 		expect( editor.getData() ).to.equal( '<p>foo</p><p>baz</p>' );
 	} );
 } );
