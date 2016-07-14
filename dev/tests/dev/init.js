add4e22380dedb0990faabe6a497b96598406a41
Oskar Wrobel <kontakt@oskarwrobel.pl>
2016-07-14 09:32:39 +0200

    Changed const to lower case.

--- dev/tests/dev/init.js
+++ dev/tests/dev/init.js
@@ -18,16 +18,16 @@ describe( 'dev-init', () => {
 	it( 'should get all ckedtior5- dependencies and execute dev-install on them', () => {
 		const getDependenciesSpy = sinon.spy( ckeditor5Dirs, 'getDependencies' );
 		const installSpy = sinon.spy();
-		const JSON = {
+		const json = {
 			dependencies: {
 				'ckeditor5-core': 'ckeditor/ckeditor5-code',
 				'non-ckeditor-plugin': '^2.0.0',
 				'ckeditor5-plugin-devtest': 'ckeditor/ckeditor5-plugin-devtest'
 			}
 		};
-		const deps = JSON.dependencies;
+		const deps = json.dependencies;
 
-		initTask( installSpy, ckeditor5Path, JSON, workspaceRoot );
+		initTask( installSpy, ckeditor5Path, json, workspaceRoot );
 
 		getDependenciesSpy.restore();
 
@@ -41,16 +41,16 @@ describe( 'dev-init', () => {
 	it( 'should not call dev-install if no ckedtior5- dependencies', () => {
 		const getDependenciesSpy = sinon.spy( ckeditor5Dirs, 'getDependencies' );
 		const installSpy = sinon.spy();
-		const JSON = {
+		const json = {
 			dependencies: {}
 		};
 
-		initTask( installSpy, ckeditor5Path, JSON, workspaceRoot );
+		initTask( installSpy, ckeditor5Path, json, workspaceRoot );
 
 		getDependenciesSpy.restore();
 
 		sinon.assert.calledOnce( getDependenciesSpy );
-		sinon.assert.calledWithExactly( getDependenciesSpy, JSON.dependencies );
+		sinon.assert.calledWithExactly( getDependenciesSpy, json.dependencies );
 		sinon.assert.notCalled( installSpy );
 	} );
 } );
