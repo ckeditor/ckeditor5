ecc629150bf005e284e1995bf81388879dd1cd66
Oskar Wrobel <kontakt@oskarwrobel.pl>
2016-07-14 16:20:14 +0200

    Fixed docs.

--- packages/ckeditor5-utils/tests/diff.js
+++ packages/ckeditor5-utils/tests/diff.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-20'insert'6, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md.
  */
 
