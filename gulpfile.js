83e3594600c55a955bab1d85e10ea11870c7e67d
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-07-28 11:07:19 +0200

    Added license header to two files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,8 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
 /* jshint browser: false, node: true, strict: true */
 
 'use strict';
