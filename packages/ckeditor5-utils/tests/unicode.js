a511ca149f60979a0be0561d227fd56ad73a1366
Szymon Cofalik <s.cofalik@cksource.com>
2016-08-03 11:52:18 +0200

    Added: unicode utils.

---
+++ packages/ckeditor5-utils/tests/unicode.js
@@ -0,0 +1,85 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import { isSurrogateHalf, isCombiningMark, isInsideSurrogatePair, isInsideCombinedSymbol } from '/ckeditor5/utils/unicode.js';
+
+describe( 'utils', () => {
+	describe( 'unicode', () => {
+		describe( 'isSurrogateHalf', () => {
+			it( 'should return true if given character is a surrogate half', () => {
+				// Half of pile of poo.
+				expect( isSurrogateHalf( '\uD83D' ) ).to.be.true;
+			} );
+
+			it( 'should return false if given character is not a surrogate half', () => {
+				expect( isSurrogateHalf( 'a' ) ).to.be.false;
+				expect( isSurrogateHalf( '𨭎' ) ).to.be.false;
+			} );
+
+			it( 'should return false for falsy values', () => {
+				expect( isSurrogateHalf( null ) ).to.be.false;
+				expect( isSurrogateHalf( undefined ) ).to.be.false;
+				expect( isSurrogateHalf( false ) ).to.be.false;
+				expect( isSurrogateHalf( 0 ) ).to.be.false;
+			} );
+		} );
+
+		describe( 'isCombiningMark', () => {
+			it( 'should return true if given character is a combining mark', () => {
+				expect( isCombiningMark( '̣' ) ).to.be.true;
+			} );
+
+			it( 'should return false if given character is not a combining mark', () => {
+				expect( isCombiningMark( 'a' ) ).to.be.false;
+				expect( isCombiningMark( 'q̣' ) ).to.be.false;
+			} );
+
+			it( 'should return false for falsy values', () => {
+				expect( isCombiningMark( null ) ).to.be.false;
+				expect( isCombiningMark( undefined ) ).to.be.false;
+				expect( isCombiningMark( false ) ).to.be.false;
+				expect( isCombiningMark( 0 ) ).to.be.false;
+			} );
+		} );
+
+		describe( 'isInsideSurrogatePair', () => {
+			const testString = 'a𨭎b';
+
+			it( 'should return true if given offset in a string is inside a surrogate pair', () => {
+				expect( isInsideSurrogatePair( testString, 2 ) ).to.be.true;
+			} );
+
+			it( 'should return false if given offset in a string is not inside a surrogate pair', () => {
+				expect( isInsideSurrogatePair( testString, 1 ) ).to.be.false;
+				expect( isInsideSurrogatePair( testString, 3 ) ).to.be.false;
+			} );
+
+			it( 'should return false if given offset in a string is at the string beginning or end', () => {
+				expect( isInsideSurrogatePair( testString, 0 ) ).to.be.false;
+				expect( isInsideSurrogatePair( testString, 4 ) ).to.be.false;
+			} );
+		} );
+
+		describe( 'isInsideCombinedSymbol', () => {
+			const testString = 'aa̻̐ͩbb';
+
+			it( 'should return true if given offset in a string is inside a symbol with combining mark', () => {
+				expect( isInsideCombinedSymbol( testString, 2 ) ).to.be.true;
+				expect( isInsideCombinedSymbol( testString, 3 ) ).to.be.true;
+				expect( isInsideCombinedSymbol( testString, 4 ) ).to.be.true;
+			} );
+
+			it( 'should return false if given offset in a string is not inside a symbol with combining mark', () => {
+				expect( isInsideCombinedSymbol( testString, 1 ) ).to.be.false;
+				expect( isInsideCombinedSymbol( testString, 5 ) ).to.be.false;
+			} );
+
+			it( 'should return false if given offset in a string is at the string beginning or end', () => {
+				expect( isInsideCombinedSymbol( testString, 0 ) ).to.be.false;
+				expect( isInsideCombinedSymbol( testString, 6 ) ).to.be.false;
+			} );
+		} );
+	} );
+} );
