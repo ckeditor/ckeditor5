0384c58d9a79c2c7d32ff224e1c9a40973e40410
Maksymilian Barnaś <max.barnas@gmail.com>
2016-08-19 11:42:38 +0200

    Update tests with `throwCKEditorError` check.

--- packages/ckeditor5-core/tests/editor/editor-base.js
+++ packages/ckeditor5-core/tests/editor/editor-base.js
@@ -8,7 +8,6 @@
 import Editor from '/ckeditor5/core/editor/editor.js';
 import Command from '/ckeditor5/core/command/command.js';
 import Locale from '/ckeditor5/utils/locale.js';
-import CKEditorError from '/ckeditor5/utils/ckeditorerror.js';
 
 describe( 'Editor', () => {
 	describe( 'locale', () => {
@@ -70,7 +69,7 @@ describe( 'Editor', () => {
 
 			expect( () => {
 				editor.execute( 'command' );
-			} ).to.throw( CKEditorError, /^editor-command-not-found:/ );
+			} ).to.throwCKEditorError( /^editor-command-not-found:/ );
 		} );
 	} );
 } );
