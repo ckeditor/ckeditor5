0875e94169616ae3aa2f7241ad1df7042e71c74e
Maksymilian Barnaś <max.barnas@gmail.com>
2016-08-30 16:47:14 +0200

    Cleaned up tests.

--- packages/ckeditor5-utils/tests/normalizehtml.js
+++ packages/ckeditor5-utils/tests/normalizehtml.js
@@ -11,49 +11,49 @@ describe( 'utils', () => {
 			let actual = '<a style="border:1px;" class="" href="file://"></a>';
 			let expected = '<a class="" href="file://" style="border:1px;"></a>';
 
-			expect( normalizeHtml( actual ) ).to.be.equal( expected );
+			expect( normalizeHtml( actual ) ).to.equal( expected );
 		} );
 
 		it( 'should normalize styles', () => {
 			let actual = '<a style="border:1px"></a>';
 			let expected = '<a style="border:1px;"></a>';
 
-			expect( normalizeHtml( actual ) ).to.be.equal( expected );
+			expect( normalizeHtml( actual ) ).to.equal( expected );
 		} );
 
 		it( 'should lowercase attributes', () => {
 			let actual = '<A CLASS="" HREF="file://" STYLE="border:1px;"></A>';
 			let expected = '<a class="" href="file://" style="border:1px;"></a>';
 
-			expect( normalizeHtml( actual ) ).to.be.equal( expected );
+			expect( normalizeHtml( actual ) ).to.equal( expected );
 		} );
 
 		it( 'should trim whitespace', () => {
 			let actual = '<a class="  " href="file://"      style="border:  1px"></a>';
 			let expected = '<a class="" href="file://" style="border:1px;"></a>';
 
-			expect( normalizeHtml( actual ) ).to.be.equal( expected );
+			expect( normalizeHtml( actual ) ).to.equal( expected );
 		} );
 
 		it( 'should remove empty style attribute', () => {
 			let actual = '<a style=""></a>';
 			let expected = '<a></a>';
 
-			expect( normalizeHtml( actual ) ).to.be.equal( expected );
+			expect( normalizeHtml( actual ) ).to.equal( expected );
 		} );
 
 		it( 'should leave empty class attribute', () => {
 			let actual = '<p class=""></p>';
 			let expected = '<p class=""></p>';
 
-			expect( normalizeHtml( actual ) ).to.be.equal( expected );
+			expect( normalizeHtml( actual ) ).to.equal( expected );
 		} );
 
 		it( 'should not sort attribute value', () => {
-			let actual = '<a class="a b"></a>';
-			let expected = '<a class="b a"></a>';
+			let actual = '<a class="b c a"></a>';
+			let expected = actual;
 
-			expect( normalizeHtml( actual ) ).to.be.not.equal( expected );
+			expect( normalizeHtml( actual ) ).to.equal( expected );
 		} );
 	} );
 } );
