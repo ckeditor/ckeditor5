a01d2753870f8cfad49b9ca3e1023d426799458d
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-09-01 16:51:35 +0200

    Added missing manual test (see https://github.com/ckeditor/ckeditor5-engine/issues/575#issuecomment-244097959).

---
+++ packages/ckeditor5-enter/tests/manual/enter.js
@@ -0,0 +1,19 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals console:false, document, window */
+
+import ClassicEditor from '/ckeditor5/editor-classic/classic.js';
+
+ClassicEditor.create( document.querySelector( '#editor' ), {
+	features: [ 'enter', 'typing', 'headings' ],
+	toolbar: [ 'headings' ]
+} )
+.then( editor => {
+	window.editor = editor;
+} )
+.catch( err => {
+	console.error( err.stack );
+} );
---
+++ packages/ckeditor5-enter/tests/manual/enter.md
@@ -0,0 +1,13 @@
+@bender-ui: collapsed
+
+## Enter key
+
+Test the <kbd>Enter</kbd>kbd> key support.
+
+### Notes:
+
+* Enter at the end of a heading should create a new paragraph.
+* In the middle of a heading should split it.
+* <kbd>Shift+Enter</kbd> should have <kbd>Enter</kbd> behavior.
+* The selection should always be moved to the newly created block.
+* Select all + <kbd>Enter</kbd> should leave an empty paragraph.
