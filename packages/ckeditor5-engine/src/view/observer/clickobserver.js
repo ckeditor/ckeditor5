c832d6497263151813373e0bb7487d2e3d729592
Oskar Wrobel <kontakt@oskarwrobel.pl>
2016-09-08 13:00:05 +0200

    Added more docs to ClickObserver.

--- packages/ckeditor5-engine/src/view/observer/clickobserver.js
+++ packages/ckeditor5-engine/src/view/observer/clickobserver.js
@@ -8,8 +8,8 @@ import DomEventObserver from './domeventobserver.js';
 /**
  * {@link engine.view.Document#click Click} event observer.
  *
- * Note that this observer is not attached by the {@link engine.view.Document} and is not available by default.
- * To make it available it needs to be added to {@link engine.view.Document} by {@link engine.view.Document#addObserver} method.
+ * Note that this observer is not available by default. To make it available it needs to be added to {@link engine.view.Document}
+ * by a {@link engine.view.Document#addObserver} method.
  *
  * @memberOf engine.view.observer
  * @extends engine.view.observer.DomEventObserver
@@ -25,3 +25,16 @@ export default class ClickObserver extends DomEventObserver {
 		this.fire( domEvent.type, domEvent );
 	}
 }
+
+/**
+ * Fired when one of the editables has been clicked.
+ *
+ * Introduced by {@link engine.view.observer.ClickObserver}.
+ *
+ * Note that this event is not available by default. To make it available {@link engine.view.observer.ClickObserver} needs to be added
+ * to {@link engine.view.Document} by a {@link engine.view.Document#addObserver} method.
+ *
+ * @see engine.view.observer.ClickObserver
+ * @event engine.view.Document#click
+ * @param {engine.view.observer.DomEventData} data Event data.
+ */
