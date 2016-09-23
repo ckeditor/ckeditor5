09d142c8601dffefe6379ee4814d97dba486b54f
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-09-23 11:00:43 +0200

    Changed package name. See https://github.com/ckeditor/ckeditor5-headings/issues/8.

--- packages/ckeditor5-typing/tests/tickets/40/1.js
+++ packages/ckeditor5-typing/tests/tickets/40/1.js
@@ -8,7 +8,7 @@
 import ClassicEditor from '/ckeditor5/editor-classic/classic.js';
 
 ClassicEditor.create( document.querySelector( '#editor' ), {
-	features: [ 'enter', 'typing', 'headings' ],
+	features: [ 'enter', 'typing', 'heading' ],
 	toolbar: [ 'headings' ]
 } )
 .then( editor => {
