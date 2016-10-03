e6df76693575df61164f5726ca7417b170c1f9c9
Maksymilian Barnaś <max.barnas@gmail.com>
2016-10-03 20:17:34 +0200

    Fixed ckeditor5-dev-lint require path.

--- packages/ckeditor5-autoformat/gulpfile.js
+++ packages/ckeditor5-autoformat/gulpfile.js
@@ -19,7 +19,7 @@ const config = {
 	]
 };
 
-const ckeditor5Lint = require( 'ckeditor5-dev-lint' )( config );
+const ckeditor5Lint = require( '@ckeditor/ckeditor5-dev-lint' )( config );
 
 gulp.task( 'lint', ckeditor5Lint.lint );
 gulp.task( 'lint-staged', ckeditor5Lint.lintStaged );
