bc29cf6712c04744ac495096c187da2ff8b2426d
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-10-17 18:04:00 +0200

    Docs: corrected param description.

--- packages/ckeditor5-engine/src/view/observer/mutationobserver.js
+++ packages/ckeditor5-engine/src/view/observer/mutationobserver.js
@@ -238,7 +238,7 @@ export default class MutationObserver extends Observer {
  * Array of mutations.
  * For mutated texts it will be {@link engine.view.Document~MutatatedText} and for mutated elements it will be
  * {@link engine.view.Document~MutatatedElement}. You can recognize the type based on the `type` property.
- * @param {engine.view.Selection|null} View selection that is a result of converting DOM selection to view. Keep in
+ * @param {engine.view.Selection|null} viewSelection View selection that is a result of converting DOM selection to view. Keep in
  * mind that the DOM selection is already "updated", meaning that it already acknowledges changes done in mutation.
  */
 
