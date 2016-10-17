13e9a24e6a35aacef16e70e54948be92b1b41f21
Kamil Piechaczek <pomek@pomek.pl>
2016-10-17 10:51:48 +0200

    Tests: DOM will be built directly in scripts instead of HTML files.

--- packages/ckeditor5-utils/tests/dom/getdatafromelement.js
+++ packages/ckeditor5-utils/tests/dom/getdatafromelement.js
@@ -9,8 +9,23 @@
 import getDataFromElement from '/ckeditor5/utils/dom/getdatafromelement.js';
 
 describe( 'getDataFromElement', () => {
+	let divEl;
+
+	beforeEach( () => {
+		divEl = document.createElement( 'div' );
+		divEl.innerHTML = `<textarea id="getData-textarea">&lt;b&gt;foo&lt;/b&gt;</textarea>
+<div id="getData-div"><b>foo</b></div>
+<template id="getData-template"><b>foo</b></template>`;
+
+		document.body.appendChild( divEl );
+	} );
+
+	afterEach( () => {
+		divEl.parentElement.removeChild( divEl );
+	} );
+
 	[ 'textarea', 'template', 'div' ].forEach( ( elementName ) => {
-		it( 'should return the content of a ' + elementName, function() {
+		it( 'should return the content of a ' + elementName, () => {
 			const data = getDataFromElement( document.getElementById( 'getData-' + elementName ) );
 			expect( data ).to.equal( '<b>foo</b>' );
 		} );
