4e81268000f0d71713d1db714a58d6742056c4f0
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-10-20 11:07:21 +0200

    Tests: corrected a manual test to not throw when there's still no selection.

--- packages/ckeditor5-engine/tests/tickets/462/1.js
+++ packages/ckeditor5-engine/tests/tickets/462/1.js
@@ -17,10 +17,13 @@ ClassicEditor.create( document.querySelector( '#editor' ), {
 	setInterval( () => {
 		console.clear();
 
+		const domSelection = document.getSelection();
+		const selectionExists = domSelection && domSelection.anchorNode;
+
 		console.log( editor.editing.view.getDomRoot().innerHTML.replace( /\u200b/g, '@' ) );
-		console.log( 'selection.hasAttribute( italic )', editor.document.selection.hasAttribute( 'italic' ) );
-		console.log( 'selection.hasAttribute( bold )', editor.document.selection.hasAttribute( 'bold' ) );
-		console.log( 'selection anchor\'s parentNode', document.getSelection().anchorNode.parentNode );
+		console.log( 'selection.hasAttribute( italic ):', editor.document.selection.hasAttribute( 'italic' ) );
+		console.log( 'selection.hasAttribute( bold ):', editor.document.selection.hasAttribute( 'bold' ) );
+		console.log( 'selection anchor\'s parentNode:', selectionExists ? domSelection.anchorNode.parentNode : 'no DOM selection' );
 	}, 2000 );
 } )
 .catch( err => {
