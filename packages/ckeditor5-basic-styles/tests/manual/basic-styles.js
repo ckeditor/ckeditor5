fc718d10d570bc64cb4a5e6375938bdd27b4e8a7
Kamil Piechaczek <pomek@pomek.pl>
2016-10-20 10:44:56 +0200

    Tests: using class constructors instead of string names (config.features).

--- packages/ckeditor5-basic-styles/tests/manual/basic-styles.js
+++ packages/ckeditor5-basic-styles/tests/manual/basic-styles.js
@@ -6,9 +6,15 @@
 /* globals console:false, window, document */
 
 import ClassicEditor from '/ckeditor5/editor-classic/classic.js';
+import Enter from '/ckeditor5/enter/enter.js';
+import Typing from '/ckeditor5/typing/typing.js';
+import Paragraph from '/ckeditor5/paragraph/paragraph.js';
+import Undo from '/ckeditor5/undo/undo.js';
+import Bold from '/ckeditor5/basic-styles/bold.js';
+import Italic from '/ckeditor5/basic-styles/italic.js';
 
 ClassicEditor.create( document.querySelector( '#editor' ), {
-	features: [ 'enter', 'typing', 'paragraph', 'undo', 'basic-styles/bold', 'basic-styles/italic' ],
+	features: [ Enter, Typing, Paragraph, Undo, Bold, Italic ],
 	toolbar: [ 'bold', 'italic', 'undo', 'redo' ]
 } )
 .then( editor => {
