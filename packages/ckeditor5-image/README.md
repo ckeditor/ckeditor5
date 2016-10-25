dd95a481b486c7113c35812ab07f68719ef97409
Szymon Kupś <szymonkups@gmail.com>
2016-10-25 14:05:10 +0200

    Initial commit for ckeditor5-image.

---
+++ packages/ckeditor5-image/README.md
@@ -0,0 +1,10 @@
+CKEditor 5 Image Feature
+========================================
+
+[![devDependency Status](https://david-dm.org/ckeditor/ckeditor5-image/dev-status.svg)](https://david-dm.org/ckeditor/ckeditor5-image?type=dev)
+
+Image feature for CKEditor 5. More information about the project can be found at the following URL: <https://github.com/ckeditor/ckeditor5-image>.
+
+## License
+
+Licensed under the GPL, LGPL and MPL licenses, at your choice. For full details about the license, please check the `LICENSE.md` file.
---
+++ packages/ckeditor5-image/gulpfile.js
@@ -0,0 +1,26 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* jshint browser: false, node: true, strict: true */
+
+'use strict';
+
+const gulp = require( 'gulp' );
+
+const config = {
+	ROOT_DIR: '.',
+	WORKSPACE_DIR: '..',
+
+	// Files ignored by jshint and jscs tasks. Files from .gitignore will be added automatically during tasks execution.
+	IGNORED_FILES: [
+		'src/lib/**'
+	]
+};
+
+const ckeditor5Lint = require( '@ckeditor/ckeditor5-dev-lint' )( config );
+
+gulp.task( 'lint', ckeditor5Lint.lint );
+gulp.task( 'lint-staged', ckeditor5Lint.lintStaged );
+gulp.task( 'pre-commit', [ 'lint-staged' ] );
