3a5449340da8ed37bd3e235f971f85c22769809f
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-10-26 15:00:37 +0200

    Handle the most important case when Blink replaces even single space with nbsps.

--- packages/ckeditor5-clipboard/src/utils/normalizeclipboarddata.js
+++ packages/ckeditor5-clipboard/src/utils/normalizeclipboarddata.js
@@ -11,5 +11,13 @@
  */
 export default function normalizeClipboardData( data ) {
 	return data
-		.replace( /<span class="Apple-converted-space">(\s+)<\/span>/g, '$1' );
+		.replace( /<span class="Apple-converted-space">(\s+)<\/span>/g, ( fullMatch, spaces ) => {
+			// Handle the most popular and problematic case when even a single space becomes an nbsp;.
+			// Decode those to normal spaces. Read more in https://github.com/ckeditor/ckeditor5-clipboard/issues/2.
+			if ( spaces.length == 1 ) {
+				return ' ';
+			}
+
+			return spaces;
+		} );
 }
