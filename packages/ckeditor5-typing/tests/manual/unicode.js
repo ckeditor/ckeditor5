e3b1c6858cfd7d24aba52fd30c7ebd94d253c2c2
Szymon Cofalik <s.cofalik@cksource.com>
2016-10-26 11:40:52 +0200

    Tests: provided manual test for unicode characters.

---
+++ packages/ckeditor5-typing/tests/manual/unicode.js
@@ -0,0 +1,19 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals console, window, document */
+
+import ClassicEditor from '/ckeditor5/editor-classic/classic.js';
+
+ClassicEditor.create( document.querySelector( '#editor' ), {
+	features: [ 'enter', 'typing', 'paragraph' ],
+	toolbar: []
+} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
