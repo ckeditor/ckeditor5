4dab6e3e1d0958c2c976d6118f928354de1a47bf
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-10-28 15:35:34 +0200

    Fix the manual test to handle headings.

--- packages/ckeditor5-undo/tests/manual/undo.js
+++ packages/ckeditor5-undo/tests/manual/undo.js
@@ -9,13 +9,14 @@ import ClassicEditor from '/ckeditor5/editor-classic/classic.js';
 import Enter from '/ckeditor5/enter/enter.js';
 import Typing from '/ckeditor5/typing/typing.js';
 import Paragraph from '/ckeditor5/paragraph/paragraph.js';
+import Heading from '/ckeditor5/heading/heading.js';
 import Undo from '/ckeditor5/undo/undo.js';
 import Bold from '/ckeditor5/basic-styles/bold.js';
 import Italic from '/ckeditor5/basic-styles/italic.js';
 
 ClassicEditor.create( document.querySelector( '#editor' ), {
-	features: [ Enter, Typing, Paragraph, Undo, Bold, Italic ],
-	toolbar: [ 'bold', 'italic', 'undo', 'redo' ]
+	features: [ Enter, Typing, Paragraph, Heading, Undo, Bold, Italic ],
+	toolbar: [ 'headings', 'bold', 'italic', 'undo', 'redo' ]
 } )
 .then( editor => {
 	window.editor = editor;
