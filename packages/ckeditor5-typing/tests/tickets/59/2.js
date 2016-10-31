0fa13eea736a7edade46a0487500800874c03b92
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-10-31 15:06:11 +0100

    Added one more automated test.

--- packages/ckeditor5-typing/tests/tickets/59/2.js
+++ packages/ckeditor5-typing/tests/tickets/59/2.js
@@ -29,11 +29,28 @@ describe( 'Bug #59', () => {
 
 	it( 'editor does not blow up when deleting last styled character', () => {
 		editor.document.enqueueChanges( () => {
+			editor.editing.view.getDomRoot().focus();
 			setData( editor.document, '<paragraph><$text bold="true">foo</$text> x <$text bold="true">bar</$text>.[]</paragraph>' );
 		} );
 
 		while ( editor.document.selection.anchor.offset > 0 ) {
 			editor.execute( 'delete' );
+		}
+
+		expect( editor.getData() ).to.equal( '<p>&nbsp;</p>' );
+	} );
+
+	// This is something that came to my mind after I worked on ckeditor/ckeditor5-engine#659.
+	// Toggling bold at the end creates a lot of weird cases so it's interesting to see if it works... and it didn't back then.
+	it( 'editor does not blow up when deleting last styled character, forcing bold switch', () => {
+		editor.document.enqueueChanges( () => {
+			editor.editing.view.getDomRoot().focus();
+			setData( editor.document, '<paragraph><$text bold="true">foo</$text> x <$text bold="true">bar</$text>.[]</paragraph>' );
+		} );
+
+		while ( editor.document.selection.anchor.offset > 0 ) {
+			editor.execute( 'delete' );
+			editor.execute( 'bold' );
 		}
 
 		expect( editor.getData() ).to.equal( '<p>&nbsp;</p>' );
