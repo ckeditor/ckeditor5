6aabd98c33663de1cfbea021be091060da5a7597
Aleksander Nowodziński <oleq@Aleksanders-MBP.local>
2016-11-04 13:03:54 +0100

    Renamed and refactored createTestUIController -> createTestUIView.

--- packages/ckeditor5-ui/tests/_utils/utils.js
+++ packages/ckeditor5-ui/tests/_utils/utils.js
@@ -6,7 +6,6 @@
 /* globals document */
 
 import View from 'ckeditor5/ui/view.js';
-import Controller from 'ckeditor5/ui/controller.js';
 
 /**
  * Test utils for CKEditor UI.
@@ -21,35 +20,39 @@ const utils = {
 	 * Usage:
 	 *
 	 *		// Get the controller.
-	 *		const controller = testUtils.createTestUIController();
+	 *		const controller = testUtils.createTestUIView();
 	 *
 	 *		// Then use it to organize and initialize children.
 	 *		controller.add( 'some-collection', childControllerInstance );
 	 *
-	 * @param {Object} regions An object literal with `regionName: DOM Selector pairs`.
+	 * @param {Object} regions An object literal with `regionName: [DOM Selector|callback]` pairs.
 	 * See {@link ui.View#register}.
 	 */
-	createTestUIController( regions ) {
+	createTestUIView( regions ) {
 		const TestUIView = class extends View {
 			constructor() {
 				super();
 
 				this.element = document.body;
 
-				for ( let r in regions ) {
-					this.register( r, regions[ r ] );
+				for ( let name in regions ) {
+					const regionCollection = this[ name ] = this.createCollection();
+					const callbackOrSelector = regions[ name ];
+
+					regionCollection.setParent(
+						typeof callbackOrSelector == 'string' ?
+								document.querySelector( callbackOrSelector )
+							:
+								callbackOrSelector( this.element )
+					);
 				}
 			}
 		};
 
-		const controller = new Controller( null, new TestUIView() );
+		const view = new TestUIView();
 
-		for ( let r in regions ) {
-			controller.addCollection( r );
-		}
-
-		return controller.init().then( () => {
-			return controller;
+		return view.init().then( () => {
+			return view;
 		} );
 	}
 };
