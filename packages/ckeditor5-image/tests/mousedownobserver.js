76e38aa9ef23d40c326159e7149a036e2f039279
Szymon Kupś <szymonkups@gmail.com>
2016-11-10 15:32:02 +0100

    Added tests to MousedownObserver.

---
+++ packages/ckeditor5-image/tests/mousedownobserver.js
@@ -0,0 +1,38 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals document */
+/* bender-tags: view, browser-only */
+
+import MouseDownObserver from 'ckeditor5/image/mousedownobserver.js';
+import ViewDocument from 'ckeditor5/engine/view/document.js';
+
+describe( 'MouseDownObserver', () => {
+	let viewDocument, observer;
+
+	beforeEach( () => {
+		viewDocument = new ViewDocument();
+		observer = viewDocument.addObserver( MouseDownObserver );
+	} );
+
+	it( 'should define domEventType', () => {
+		expect( observer.domEventType ).to.equal( 'mousedown' );
+	} );
+
+	describe( 'onDomEvent', () => {
+		it( 'should fire mousedown with the right event data', () => {
+			const spy = sinon.spy();
+
+			viewDocument.on( 'mousedown', spy );
+
+			observer.onDomEvent( { type: 'mousedown', target: document.body } );
+
+			expect( spy.calledOnce ).to.be.true;
+
+			const data = spy.args[ 0 ][ 1 ];
+			expect( data.domTarget ).to.equal( document.body );
+		} );
+	} );
+} );
