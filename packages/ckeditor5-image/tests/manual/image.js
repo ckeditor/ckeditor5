9518fabc280136580e918b297eacedd636dc1966
Szymon Kupś <szymonkups@gmail.com>
2016-11-10 10:45:13 +0100

    Updated manual test.

--- packages/ckeditor5-image/tests/manual/image.js
+++ packages/ckeditor5-image/tests/manual/image.js
@@ -3,19 +3,21 @@
  * For licensing, see LICENSE.md.
  */
 
-/* global document, console */
+/* global document, console, window */
 
 import ClassicEditor from '/ckeditor5/editor-classic/classic.js';
 import EnterFeature from '/ckeditor5/enter/enter.js';
 import TypingFeature from '/ckeditor5/typing/typing.js';
 import ParagraphFeature from '/ckeditor5/paragraph/paragraph.js';
-import UndoFeature from '/ckeditor5/undo/undo.js';
 import HeadingFeature from '/ckeditor5/heading/heading.js';
 import ImageFeature from '/ckeditor5/image/image.js';
 
 ClassicEditor.create( document.querySelector( '#editor' ), {
-	features: [ EnterFeature, TypingFeature, ParagraphFeature, UndoFeature, HeadingFeature, ImageFeature ],
+	features: [ EnterFeature, TypingFeature, ParagraphFeature, HeadingFeature, ImageFeature ],
 	toolbar: [ 'headings' ]
+} )
+.then( editor => {
+	window.editor = editor;
 } )
 .catch( err => {
 	console.error( err.stack );
--- packages/ckeditor5-image/tests/manual/image.md
+++ packages/ckeditor5-image/tests/manual/image.md
@@ -2,3 +2,7 @@
 @bender-tags: view
 
 ## Image feature
+
+ - Two images with CKEditor logo should be loaded.
+ - Hovering over image should apply yellow outline.
+ - Clicking on image should apply blue outline which should not change when hovering over.
