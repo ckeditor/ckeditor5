7400d40149ae5680587842c4282273575d8f889c
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-11-15 16:01:18 +0100

    Aligned engine usage to its changes.

--- packages/ckeditor5-typing/tests/deletecommand.js
+++ packages/ckeditor5-typing/tests/deletecommand.js
@@ -67,7 +67,7 @@ describe( 'DeleteCommand', () => {
 		it( 'does not try to delete when selection is at the boundary', () => {
 			const spy = sinon.spy();
 
-			doc.composer.on( 'deleteContents', spy );
+			editor.data.on( 'deleteContent', spy );
 			setData( doc, '<p>[]foo</p>' );
 
 			editor.execute( 'delete' );
@@ -79,7 +79,7 @@ describe( 'DeleteCommand', () => {
 		it( 'passes options to modifySelection', () => {
 			const spy = sinon.spy();
 
-			doc.composer.on( 'modifySelection', spy );
+			editor.data.on( 'modifySelection', spy );
 			setData( doc, '<p>foo[]bar</p>' );
 
 			editor.commands.get( 'delete' ).direction = 'forward';
