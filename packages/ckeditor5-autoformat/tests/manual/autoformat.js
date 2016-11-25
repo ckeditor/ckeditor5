798fa436a9d29e73748da81fe697acf024b124c3
Kamil Piechaczek <pomek@pomek.pl>
2016-11-25 15:03:37 +0100

    Removed first slash from imports and fixed HTML in manual tests.

--- packages/ckeditor5-autoformat/tests/manual/autoformat.js
+++ packages/ckeditor5-autoformat/tests/manual/autoformat.js
@@ -5,16 +5,16 @@
 
 /* globals console, window, document */
 
-import ClassicEditor from '/ckeditor5/editor-classic/classic.js';
-import Autoformat from '/ckeditor5/autoformat/autoformat.js';
-import Enter from '/ckeditor5/enter/enter.js';
-import List from '/ckeditor5/list/list.js';
-import Typing from '/ckeditor5/typing/typing.js';
-import Heading from '/ckeditor5/heading/heading.js';
-import Paragraph from '/ckeditor5/paragraph/paragraph.js';
-import Undo from '/ckeditor5/undo/undo.js';
-import Bold from '/ckeditor5/basic-styles/bold.js';
-import Italic from '/ckeditor5/basic-styles/italic.js';
+import ClassicEditor from 'ckeditor5/editor-classic/classic.js';
+import Autoformat from 'ckeditor5/autoformat/autoformat.js';
+import Enter from 'ckeditor5/enter/enter.js';
+import List from 'ckeditor5/list/list.js';
+import Typing from 'ckeditor5/typing/typing.js';
+import Heading from 'ckeditor5/heading/heading.js';
+import Paragraph from 'ckeditor5/paragraph/paragraph.js';
+import Undo from 'ckeditor5/undo/undo.js';
+import Bold from 'ckeditor5/basic-styles/bold.js';
+import Italic from 'ckeditor5/basic-styles/italic.js';
 
 ClassicEditor.create( document.querySelector( '#editor' ), {
 	plugins: [ Enter, Typing, Paragraph, Undo, Bold, Italic, Heading, List, Autoformat ],
