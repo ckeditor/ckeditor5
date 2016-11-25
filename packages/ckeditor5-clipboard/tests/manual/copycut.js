f172e091164d4e5fde7ba08fef554711e25a5531
Kamil Piechaczek <pomek@pomek.pl>
2016-11-25 15:04:09 +0100

    Removed first slash from imports and fixed HTML in manual tests.

--- packages/ckeditor5-clipboard/tests/manual/copycut.js
+++ packages/ckeditor5-clipboard/tests/manual/copycut.js
@@ -5,19 +5,19 @@
 
 /* globals console, window, document */
 
-import ClassicEditor from '/ckeditor5/editor-classic/classic.js';
-import Typing from '/ckeditor5/typing/typing.js';
-import Paragraph from '/ckeditor5/paragraph/paragraph.js';
-import Undo from '/ckeditor5/undo/undo.js';
-import Enter from '/ckeditor5/enter/enter.js';
-import Clipboard from '/ckeditor5/clipboard/clipboard.js';
-import Link from '/ckeditor5/link/link.js';
-import List from '/ckeditor5/list/list.js';
-import Heading from '/ckeditor5/heading/heading.js';
-import Bold from '/ckeditor5/basic-styles/bold.js';
-import Italic from '/ckeditor5/basic-styles/italic.js';
+import ClassicEditor from 'ckeditor5/editor-classic/classic.js';
+import Typing from 'ckeditor5/typing/typing.js';
+import Paragraph from 'ckeditor5/paragraph/paragraph.js';
+import Undo from 'ckeditor5/undo/undo.js';
+import Enter from 'ckeditor5/enter/enter.js';
+import Clipboard from 'ckeditor5/clipboard/clipboard.js';
+import Link from 'ckeditor5/link/link.js';
+import List from 'ckeditor5/list/list.js';
+import Heading from 'ckeditor5/heading/heading.js';
+import Bold from 'ckeditor5/basic-styles/bold.js';
+import Italic from 'ckeditor5/basic-styles/italic.js';
 
-import { stringify as stringifyView } from '/ckeditor5/engine/dev-utils/view.js';
+import { stringify as stringifyView } from 'ckeditor5/engine/dev-utils/view.js';
 
 ClassicEditor.create( document.querySelector( '#editor' ), {
 	plugins: [
