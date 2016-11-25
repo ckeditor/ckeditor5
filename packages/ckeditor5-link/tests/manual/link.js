2a3f186f80d1b82e9568bd6695b69f226d15884d
Kamil Piechaczek <pomek@pomek.pl>
2016-11-25 15:05:55 +0100

    Removed first slash from imports and fixed HTML in manual tests.

--- packages/ckeditor5-link/tests/manual/link.js
+++ packages/ckeditor5-link/tests/manual/link.js
@@ -5,12 +5,12 @@
 
 /* globals console:false, window, document */
 
-import ClassicEditor from '/ckeditor5/editor-classic/classic.js';
-import Enter from '/ckeditor5/enter/enter.js';
-import Typing from '/ckeditor5/typing/typing.js';
-import Link from '/ckeditor5/link/link.js';
-import Paragraph from '/ckeditor5/paragraph/paragraph.js';
-import Undo from '/ckeditor5/undo/undo.js';
+import ClassicEditor from 'ckeditor5/editor-classic/classic.js';
+import Enter from 'ckeditor5/enter/enter.js';
+import Typing from 'ckeditor5/typing/typing.js';
+import Link from 'ckeditor5/link/link.js';
+import Paragraph from 'ckeditor5/paragraph/paragraph.js';
+import Undo from 'ckeditor5/undo/undo.js';
 
 ClassicEditor.create( document.querySelector( '#editor' ), {
 	plugins: [ Link, Typing, Paragraph, Undo, Enter ],
