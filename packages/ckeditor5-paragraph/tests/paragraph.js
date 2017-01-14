eb2731719b8cf9a4d8e71789b6b1ea8de386ab43
Piotrek Koszuliński <pkoszulinski@gmail.com>
2017-01-15 00:50:31 +0100

    Updated imports to use scoped packages.

--- packages/ckeditor5-paragraph/tests/paragraph.js
+++ packages/ckeditor5-paragraph/tests/paragraph.js
@@ -3,19 +3,19 @@
  * For licensing, see LICENSE.md.
  */
 
-import Paragraph from 'ckeditor5-paragraph/src/paragraph';
-import VirtualTestEditor from 'ckeditor5-core/tests/_utils/virtualtesteditor';
+import Paragraph from '../src/paragraph';
+import VirtualTestEditor from '@ckeditor/ckeditor5-core/tests/_utils/virtualtesteditor';
 import {
 	getData as getModelData,
 	setData as setModelData,
 	stringify as stringifyModel
-} from 'ckeditor5-engine/src/dev-utils/model';
-import { getData as getViewData } from 'ckeditor5-engine/src/dev-utils/view';
+} from '@ckeditor/ckeditor5-engine/src/dev-utils/model';
+import { getData as getViewData } from '@ckeditor/ckeditor5-engine/src/dev-utils/view';
 
-import buildViewConverter from 'ckeditor5-engine/src/conversion/buildviewconverter';
+import buildViewConverter from '@ckeditor/ckeditor5-engine/src/conversion/buildviewconverter';
 
-import ModelDocumentFragment from 'ckeditor5-engine/src/model/documentfragment';
-import ModelText from 'ckeditor5-engine/src/model/text';
+import ModelDocumentFragment from '@ckeditor/ckeditor5-engine/src/model/documentfragment';
+import ModelText from '@ckeditor/ckeditor5-engine/src/model/text';
 
 describe( 'Paragraph feature', () => {
 	let editor, doc;
