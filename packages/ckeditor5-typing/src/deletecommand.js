77318d8127dde4d5c4cc5ed3427a5c60e1a054df
Krzysztof Krztoń <k.krzton@cksource.com>
2017-03-06 13:40:00 +0100

    Use typing.undoStep config in DeleteCommand.

--- packages/ckeditor5-typing/src/deletecommand.js
+++ packages/ckeditor5-typing/src/deletecommand.js
@@ -45,7 +45,7 @@ export default class DeleteCommand extends Command {
 		 * @private
 		 * @member {typing.ChangeBuffer} #buffer
 		 */
-		this._buffer = new ChangeBuffer( editor.document, editor.config.get( 'undo.step' ) );
+		this._buffer = new ChangeBuffer( editor.document, editor.config.get( 'typing.undoStep' ) );
 	}
 
 	/**
