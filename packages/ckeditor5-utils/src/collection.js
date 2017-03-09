3d6d5eec1ebdc4cfe622b607058b72ec39dd5329
Maciej <jodator@gmail.com>
2017-03-09 15:51:14 +0100

    Docs: Remove superfluous items from Collection's constructor jsdoc.
    Docs: Remove superfluous items from Collection's constructor jsdoc.

--- packages/ckeditor5-utils/src/collection.js
+++ packages/ckeditor5-utils/src/collection.js
@@ -28,7 +28,6 @@ export default class Collection {
 	/**
 	 * Creates a new Collection instance.
 	 *
-	 * @param {Iterable} [items] Items to be added to the collection.
 	 * @param {Object} options The options object.
 	 * @param {String} [options.idProperty='id'] The name of the property which is considered to identify an item.
 	 */
