deac8288739bcd66d17f57a900205d95b69bdcba
Maciej Bukowski <ma2ciek@gmail.com>
2017-03-15 14:22:02 +0100

    Added test for the iterator's item consumption.

--- packages/ckeditor5-utils/tests/first.js
+++ packages/ckeditor5-utils/tests/first.js
@@ -8,15 +8,26 @@ import first from '../src/first';
 describe( 'utils', () => {
 	describe( 'first', () => {
 		it( 'should return first item', () => {
-			const iterator = [ 11, 22 ][ Symbol.iterator ]();
+			const collection = [ 11, 22 ];
+			const iterator = collection[ Symbol.iterator ]();
 
 			expect( first( iterator ) ).to.equal( 11 );
 		} );
 
 		it( 'should return null if iterator is empty', () => {
-			const iterator = [][ Symbol.iterator ]();
+			const collection = [];
+			const iterator = collection[ Symbol.iterator ]();
 
 			expect( first( iterator ) ).to.be.null;
+		} );
+
+		it( 'should consume the iterating item', () => {
+			const collection = [ 11, 22 ];
+			const iterator = collection[ Symbol.iterator ]();
+
+			first( iterator );
+
+			expect( iterator.next().value ).to.equal( 22 );
 		} );
 	} );
 } );
