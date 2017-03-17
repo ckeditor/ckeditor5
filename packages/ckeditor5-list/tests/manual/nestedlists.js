8b9e309c8ccb372929b17c231815cb3c69f4aac6
Szymon Cofalik <s.cofalik@cksource.com>
2017-03-17 10:15:26 +0100

    Tests: updated description of manual test for flat lists. Added manual test for nested lists.

---
+++ packages/ckeditor5-list/tests/manual/nestedlists.js
@@ -0,0 +1,27 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals console, window, document */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classic';
+import Enter from '@ckeditor/ckeditor5-enter/src/enter';
+import Typing from '@ckeditor/ckeditor5-typing/src/typing';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Undo from '@ckeditor/ckeditor5-undo/src/undo';
+import List from '../../src/list';
+
+ClassicEditor.create( document.querySelector( '#editor' ), {
+	plugins: [ Enter, Typing, Heading, Paragraph, Undo, List ],
+	toolbar: [ 'headings', 'bulletedList', 'numberedList', 'undo', 'redo' ]
+} )
+.then( editor => {
+	window.editor = editor;
+	window.modelRoot = editor.document.getRoot();
+	window.viewRoot = editor.editing.view.getRoot();
+} )
+.catch( err => {
+	console.error( err.stack );
+} );
