26f1b1a6885162b262e37081d4109c3a58cb945b
Kamil Piechaczek <pomek@pomek.pl>
2017-03-17 11:25:13 +0100

    Added build for the latest changes in all packages.

--- packages/ckeditor5-build-classic/ckeditor.js
+++ packages/ckeditor5-build-classic/ckeditor.js
@@ -2,7 +2,7 @@
  * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md.
  */
- 
+
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classic';
 import ArticlePlugin from '@ckeditor/ckeditor5-presets/src/article';
 
