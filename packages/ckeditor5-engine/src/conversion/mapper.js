022407e3541ebe3cb5fe99137695ec9100eaabb2
Szymon Cofalik <s.cofalik@cksource.com>
2017-03-21 16:46:50 +0100

    Docs: updated docs for Mapper events.

--- packages/ckeditor5-engine/src/conversion/mapper.js
+++ packages/ckeditor5-engine/src/conversion/mapper.js
@@ -471,9 +471,6 @@ mix( Mapper, EmitterMixin );
  * @event modelToViewPosition
  * @param {Object} data Data pipeline object that can store and pass data between callbacks. The callback should add
  * `viewPosition` value to that object with calculated {@link module:engine/view/position~Position view position}.
- * @param {module:engine/model/position~Position} data.modelPosition Model position to be mapped.
- * @param {module:engine/view/position~Position} data.viewPosition View position that is a result of mapping
- * `modelPosition` using `Mapper` default algorithm.
  * @param {module:engine/conversion/mapper~Mapper} data.mapper Mapper instance that fired the event.
  */
 
@@ -497,8 +494,5 @@ mix( Mapper, EmitterMixin );
  * @event viewToModelPosition
  * @param {Object} data Data pipeline object that can store and pass data between callbacks. The callback should add
  * `modelPosition` value to that object with calculated {@link module:engine/model/position~Position model position}.
- * @param {module:engine/view/position~Position} data.viewPosition View position to be mapped.
- * @param {module:engine/model/position~Position} data.modelPosition Model position that is a result of mapping
- * `viewPosition` using `Mapper` default algorithm.
  * @param {module:engine/conversion/mapper~Mapper} data.mapper Mapper instance that fired the event.
  */
