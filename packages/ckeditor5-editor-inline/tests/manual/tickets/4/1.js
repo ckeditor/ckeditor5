e00d8bde0f2b7e5a81dc5ab287a06208020d7453
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2017-04-10 12:40:23 +0200

    Added manual test for the issue.

---
+++ packages/ckeditor5-editor-inline/tests/manual/tickets/4/1.js
@@ -0,0 +1,20 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals console:false, document, window */
+
+import InlineEditor from '../../../../src/inline';
+import ArticlePreset from '@ckeditor/ckeditor5-presets/src/article';
+
+InlineEditor.create( document.querySelector( '#editor' ), {
+	plugins: [ ArticlePreset ],
+	toolbar: [ 'headings', 'bold', 'italic', 'link', 'unlink', 'bulletedList', 'numberedList', 'blockQuote', 'undo', 'redo' ]
+} )
+.then( editor => {
+	window.editor = editor;
+} )
+.catch( err => {
+	console.error( err.stack );
+} );
