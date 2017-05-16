58dbf0b8b49bce7d40e10dd463946290bad4fc86
Piotrek Koszuliński <pkoszulinski@gmail.com>
2017-05-16 20:30:41 +0200

    Internal: Standardized linter usage.

--- packages/ckeditor5-engine/gulpfile.js
+++ packages/ckeditor5-engine/gulpfile.js
@@ -9,7 +9,14 @@
 
 const gulp = require( 'gulp' );
 const ckeditor5Lint = require( '@ckeditor/ckeditor5-dev-lint' );
+const options = {
+	// Files ignored by `gulp lint` task.
+	// Files from .gitignore will be added automatically during task execution.
+	ignoredFiles: [
+		'src/lib/**'
+	]
+};
 
-gulp.task( 'lint', () => ckeditor5Lint.lint() );
-gulp.task( 'lint-staged', () => ckeditor5Lint.lintStaged() );
+gulp.task( 'lint', () => ckeditor5Lint.lint( options ) );
+gulp.task( 'lint-staged', () => ckeditor5Lint.lintStaged( options ) );
 gulp.task( 'pre-commit', [ 'lint-staged' ] );
