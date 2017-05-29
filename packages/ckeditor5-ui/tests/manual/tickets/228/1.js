4ed9757830b314960261f9d6cee8d70db6743ed0
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2017-05-29 14:43:56 +0200

    Tests: Added a manual test for #228.

---
+++ packages/ckeditor5-ui/tests/manual/tickets/228/1.js
@@ -0,0 +1,15 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals document, console:false */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classic';
+import ArticlePresets from '@ckeditor/ckeditor5-presets/src/article';
+
+ClassicEditor.create( document.querySelector( '#editor' ), {
+	plugins: [ ArticlePresets ],
+	toolbar: [ 'headings' ],
+} )
+.catch( err => console.error( err.stack ) );
