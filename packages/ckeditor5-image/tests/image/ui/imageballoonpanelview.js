3cb7753630a6daa056aca1acba853f4d6d7a4071
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2017-07-04 11:00:27 +0200

    Aligned Image package to the synchronous UI API.

--- packages/ckeditor5-image/tests/image/ui/imageballoonpanelview.js
+++ packages/ckeditor5-image/tests/image/ui/imageballoonpanelview.js
@@ -41,25 +41,15 @@ describe( 'ImageBalloonPanel', () => {
 		return editor.destroy();
 	} );
 
-	it( 'init method should return a promise', () => {
-		const panel = new ImageBalloonPanel( editor );
-		const promise = panel.init();
-
-		expect( promise ).to.be.instanceof( Promise );
-
-		return promise;
-	} );
-
 	it( 'should add element to editor\'s focus tracker after init', () => {
 		const spy = sinon.spy( editor.ui.focusTracker, 'add' );
 		const panel = new ImageBalloonPanel( editor );
 
 		sinon.assert.notCalled( spy );
 
-		return panel.init().then( () => {
-			sinon.assert.calledOnce( spy );
-			sinon.assert.calledWithExactly( spy, panel.element );
-		} );
+		panel.init();
+		sinon.assert.calledOnce( spy );
+		sinon.assert.calledWithExactly( spy, panel.element );
 	} );
 
 	it( 'should detach panel when focus is removed', () => {
