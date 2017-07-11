2c40100e38eb859891ecacce1131375e0d00e36e
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2017-07-11 15:28:32 +0200

    Integrated ImageToolbar with ImageBalloon.

--- packages/ckeditor5-image/src/imagetoolbar.js
+++ packages/ckeditor5-image/src/imagetoolbar.js
@@ -10,13 +10,18 @@
 import Template from '@ckeditor/ckeditor5-ui/src/template';
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import ToolbarView from '@ckeditor/ckeditor5-ui/src/toolbar/toolbarview';
+import ImageBalloon from './image/ui/imageballoon';
 import { isImageWidget } from './image/utils';
-import ImageBalloonPanel from './image/ui/imageballoonpanelview';
+
+const balloonClassName = 'ck-toolbar-container ck-editor-toolbar-container';
 
 /**
- * Image toolbar class. Creates image toolbar placed inside balloon panel that is showed when image widget is selected.
+ * The image toolbar class. Creates an image toolbar that shows up when image widget is selected.
+ *
  * Toolbar components are created using editor's {@link module:ui/componentfactory~ComponentFactory ComponentFactory}
  * based on {@link module:core/editor/editor~Editor#config configuration} stored under `image.toolbar`.
+ *
+ * The toolbar uses {@link module:image/image/ui/imageballoon~ImageBalloon}.
  *
  * @extends module:core/plugin~Plugin
  */
@@ -24,23 +29,15 @@ export default class ImageToolbar extends Plugin {
 	/**
 	 * @inheritDoc
 	 */
-	static get pluginName() {
-		return 'ImageToolbar';
+	static get requires() {
+		return [ ImageBalloon ];
 	}
 
 	/**
 	 * @inheritDoc
 	 */
-	constructor( editor ) {
-		super( editor );
-
-		/**
-		 * When set to `true`, toolbar will be repositioned and showed on each render event and focus change.
-		 * Set to `false` to temporary disable the image toolbar.
-		 *
-		 * @member {Boolean}
-		 */
-		this.isEnabled = true;
+	static get pluginName() {
+		return 'ImageToolbar';
 	}
 
 	/**
@@ -55,65 +52,59 @@ export default class ImageToolbar extends Plugin {
 			return;
 		}
 
-		const panel = this._panel = new ImageBalloonPanel( editor );
-		const toolbar = new ToolbarView();
+		/**
+		 * A `ToolbarView` instance used to display the buttons specific for image
+		 * editing.
+		 *
+		 * @protected
+		 * @type {module:ui/toolbar/toolbarview~ToolbarView}
+		 */
+		this._toolbar = new ToolbarView();
 
 		// Add CSS class to the toolbar.
-		Template.extend( toolbar.template, {
+		Template.extend( this._toolbar.template, {
 			attributes: {
 				class: 'ck-editor-toolbar'
 			}
 		} );
 
-		// Add CSS class to the panel.
-		Template.extend( panel.template, {
-			attributes: {
-				class: [
-					'ck-toolbar-container',
-					'ck-editor-toolbar-container'
-				]
-			}
-		} );
-
-		// Add toolbar to balloon panel.
-		panel.content.add( toolbar );
-
 		// Add buttons to the toolbar.
-		toolbar.fillFromConfig( toolbarConfig, editor.ui.componentFactory );
-
-		// Add balloon panel to editor's UI.
-		editor.ui.view.body.add( panel );
+		this._toolbar.fillFromConfig( toolbarConfig, editor.ui.componentFactory );
 
 		// Show balloon panel each time image widget is selected.
 		this.listenTo( this.editor.editing.view, 'render', () => {
-			if ( this.isEnabled ) {
-				this.show();
-			}
+			this._checkIsVisible();
 		}, { priority: 'low' } );
 
 		// There is no render method after focus is back in editor, we need to check if balloon panel should be visible.
 		this.listenTo( editor.ui.focusTracker, 'change:isFocused', ( evt, name, is, was ) => {
-			if ( !was && is && this.isEnabled ) {
-				this.show();
+			if ( !was && is ) {
+				this._checkIsVisible();
 			}
 		} );
 	}
 
 	/**
-	 * Shows the toolbar.
+	 * Checks whether the toolbar should show up or hide depending on the
+	 * current selection.
+	 *
+	 * @protected
 	 */
-	show() {
-		const selectedElement = this.editor.editing.view.selection.getSelectedElement();
+	_checkIsVisible() {
+		const editingView = this.editor.editing.view;
+		const selectedElement = editingView.selection.getSelectedElement();
+		const balloon = this.editor.plugins.get( 'ImageBalloon' );
+		const toolbar = this._toolbar;
 
 		if ( selectedElement && isImageWidget( selectedElement ) ) {
-			this._panel.attach();
+			if ( !balloon.hasView( toolbar ) ) {
+				balloon.add( {
+					view: toolbar,
+					balloonClassName
+				} );
+			}
+		} else if ( balloon.hasView( toolbar ) ) {
+			balloon.remove( toolbar );
 		}
-	}
-
-	/**
-	 * Hides the toolbar.
-	 */
-	hide() {
-		this._panel.detach();
 	}
 }
