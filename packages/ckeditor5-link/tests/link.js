45aa7b26c1588930a69f780edf72b4eeafc358d1
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2017-07-24 16:19:45 +0200

    Internal: The clickOutsideHandler helper should use mousedown instead of mouseup event (see ckeditor/ckeditor5-ui#281).

--- packages/ckeditor5-link/tests/link.js
+++ packages/ckeditor5-link/tests/link.js
@@ -568,7 +568,7 @@ describe( 'Link', () => {
 			const spy = testUtils.sinon.spy( linkFeature, '_hidePanel' );
 
 			linkFeature._showPanel( true );
-			document.body.dispatchEvent( new Event( 'mouseup', { bubbles: true } ) );
+			document.body.dispatchEvent( new Event( 'mousedown', { bubbles: true } ) );
 
 			sinon.assert.calledWithExactly( spy );
 		} );
@@ -577,7 +577,7 @@ describe( 'Link', () => {
 			const spy = testUtils.sinon.spy( linkFeature, '_hidePanel' );
 
 			linkFeature._showPanel( true );
-			balloon.view.element.dispatchEvent( new Event( 'mouseup', { bubbles: true } ) );
+			balloon.view.element.dispatchEvent( new Event( 'mousedown', { bubbles: true } ) );
 
 			sinon.assert.notCalled( spy );
 		} );
