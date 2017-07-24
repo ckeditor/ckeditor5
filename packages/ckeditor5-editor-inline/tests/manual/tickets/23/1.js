b4bab96314a5290c7807577a49ccb7c44a77e74c
Piotrek Koszuliński <pkoszulinski@gmail.com>
2017-07-24 11:24:29 +0200

    Fixed  config name in manual test.

--- packages/ckeditor5-editor-inline/tests/manual/tickets/23/1.js
+++ packages/ckeditor5-editor-inline/tests/manual/tickets/23/1.js
@@ -18,7 +18,7 @@ InlineEditor.create( document.querySelector( '#editor' ), {
 	plugins: [ Enter, Typing, Paragraph, Undo, Heading, Bold, Italic ],
 	toolbar: {
 		items: [ 'headings', 'bold', 'italic', 'undo', 'redo' ],
-		verticalOffset: 100
+		viewportTopOffset: 100
 	}
 } )
 .then( newEditor => {
