b9bd7314df798403e78e341122c9c6d15cf3f122
Piotrek Koszuliński <pkoszulinski@gmail.com>
2017-08-09 12:52:39 +0200

    Code style: Aligned code style to new ESLint version.

--- packages/ckeditor5-core/tests/manual/readonly.js
+++ packages/ckeditor5-core/tests/manual/readonly.js
@@ -10,25 +10,26 @@ import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
 import ArticlePreset from '@ckeditor/ckeditor5-presets/src/article';
 import ContextualToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/contextual/contextualtoolbar';
 
-ClassicEditor.create( document.querySelector( '#editor' ), {
-	plugins: [ ArticlePreset, ContextualToolbar ],
-	toolbar: [ 'headings', 'bold', 'italic', 'link', 'unlink', 'bulletedList', 'numberedList', 'blockQuote', 'undo', 'redo' ],
-	image: {
-		toolbar: [ 'imageStyleFull', 'imageStyleSide', '|', 'imageTextAlternative' ],
-	},
-	contextualToolbar: [ 'bold', 'italic', 'link' ]
-} )
-.then( editor => {
-	window.editor = editor;
+ClassicEditor
+	.create( document.querySelector( '#editor' ), {
+		plugins: [ ArticlePreset, ContextualToolbar ],
+		toolbar: [ 'headings', 'bold', 'italic', 'link', 'unlink', 'bulletedList', 'numberedList', 'blockQuote', 'undo', 'redo' ],
+		image: {
+			toolbar: [ 'imageStyleFull', 'imageStyleSide', '|', 'imageTextAlternative' ],
+		},
+		contextualToolbar: [ 'bold', 'italic', 'link' ]
+	} )
+	.then( editor => {
+		window.editor = editor;
 
-	const button = document.querySelector( '#read-only' );
+		const button = document.querySelector( '#read-only' );
 
-	button.addEventListener( 'click', () => {
-		editor.isReadOnly = !editor.isReadOnly;
-		button.textContent = editor.isReadOnly ? 'Turn off read-only mode' : 'Turn on read-only mode';
+		button.addEventListener( 'click', () => {
+			editor.isReadOnly = !editor.isReadOnly;
+			button.textContent = editor.isReadOnly ? 'Turn off read-only mode' : 'Turn on read-only mode';
+		} );
+	} )
+	.catch( err => {
+		console.error( err.stack );
 	} );
-} )
-.catch( err => {
-	console.error( err.stack );
-} );
 
