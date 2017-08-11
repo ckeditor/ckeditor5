1557bb167ec2dc01ca26248bd491420c77a84569
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2017-08-11 12:41:46 +0200

    Tests: Added tests for the selectionRootObtainer helper.

--- packages/ckeditor5-ui/src/selectionrootobtainer.js
+++ packages/ckeditor5-ui/src/selectionrootobtainer.js
@@ -7,10 +7,18 @@
  * @module ui/selectionrootobtainer
  */
 
+/**
+ * Returns a function, which obtains the farthest DOM
+ * {@link module:engine/view/rooteditableelement~RootEditableElement}
+ * of the {@link module:engine/view/document~Document#selection}.
+ *
+ * @param {module:core/editor/editor~Editor} editor Editor instance.
+ * @returns {Function}
+ */
 export default function selectionRootObtainer( editor ) {
 	return () => {
-		const viewDocument = editor.editing.view;
+		const view = editor.editing.view;
 
-		return viewDocument.domConverter.mapViewToDom( viewDocument.selection.editableElement.root );
+		return view.domConverter.mapViewToDom( view.selection.editableElement.root );
 	};
 }
