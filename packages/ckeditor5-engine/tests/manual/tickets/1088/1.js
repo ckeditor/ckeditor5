145dec8a40ccb9d02238cc015619027aa552fbd6
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-08-30 21:38:22 +0200

    Added manual test for filtering attributes.

---
+++ packages/ckeditor5-engine/tests/manual/tickets/1088/1.js
@@ -0,0 +1,29 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals console, window, document */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import ArticlePreset from '@ckeditor/ckeditor5-presets/src/article';
+
+ClassicEditor
+	.create( document.querySelector( '#editor' ), {
+		plugins: [ ArticlePreset ],
+		toolbar: [ 'headings', 'undo', 'redo' ]
+	} )
+	.then( editor => {
+		window.editor = editor;
+
+		const schema = editor.document.schema;
+
+		schema.disallow( { name: '$text', attributes: [ 'linkHref' ], inside: 'heading1' } );
+		schema.disallow( { name: '$text', attributes: [ 'italic' ], inside: 'heading2' } );
+		schema.disallow( { name: '$text', attributes: [ 'italic', 'linkHref' ], inside: 'heading3' } );
+		schema.disallow( { name: '$text', attributes: [ 'bold' ], inside: 'paragraph' } );
+		schema.disallow( { name: '$text', attributes: [ 'bold', 'linkHref' ], inside: 'blockQuote listItem' } );
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
