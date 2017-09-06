ea65e1395b835bd98d6f342677d02bdcb651cdaa
Szymon Kupś <szymonkups@gmail.com>
2017-09-06 11:57:20 +0200

    Added ImageCaption plugin to issue's manual test and temporary note describing external issue.

--- packages/ckeditor5-image/tests/manual/tickets/127/1.js
+++ packages/ckeditor5-image/tests/manual/tickets/127/1.js
@@ -12,13 +12,14 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Image from '../../../../src/image';
+import ImageCaption from '../../../../src/imagecaption';
 import Undo from '@ckeditor/ckeditor5-undo/src/undo';
 import ContextualToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/contextual/contextualtoolbar';
 import ImageToolbar from '../../../../src/imagetoolbar';
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
-		plugins: [ Enter, Typing, Paragraph, Link, Bold, Image, Undo, ImageToolbar, ContextualToolbar ],
+		plugins: [ Enter, Typing, Paragraph, Link, Bold, Image, Undo, ImageToolbar, ContextualToolbar, ImageCaption ],
 		toolbar: [ 'bold', 'undo', 'redo' ],
 	} )
 	.then( editor => {
