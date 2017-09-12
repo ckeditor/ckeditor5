7dd308eefb57e1bc65ab5eb0cbace5db70b81471
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-09-12 20:02:05 +0200

    Changed StickyToolbarView to StickyPanelView.

---
+++ packages/ckeditor5-ui/tests/manual/panel/sticky/stickypanelview.js
@@ -0,0 +1,30 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import testUtils from '../../../_utils/utils';
+import StickyPanelView from '../../../../src/panel/sticky/stickypanelview';
+
+import '@ckeditor/ckeditor5-theme-lark/theme/theme.scss';
+
+const ui = testUtils.createTestUIView( {
+	stickyToTheTop: '.ck-sticky_to-the-top .ck-editor__top',
+	stickyToTheBox: '.ck-sticky_to-the-box .ck-editor__top'
+} );
+
+createStickyPanel( ui.stickyToTheTop );
+const stickyToTheBoxTPanel = createStickyPanel( ui.stickyToTheBox );
+
+stickyToTheBoxTPanel.viewportTopOffset = 100;
+
+function createStickyPanel( collection ) {
+	const panel = new StickyPanelView();
+
+	panel.limiterElement = collection._parentElement.parentNode;
+
+	collection.add( panel );
+	panel.isActive = true;
+
+	return panel;
+}
