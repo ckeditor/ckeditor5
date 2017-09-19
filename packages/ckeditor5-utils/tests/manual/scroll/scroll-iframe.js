6d7b613833f6ef0648eb5ef317dddf6835b3ba35
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2017-09-19 10:52:12 +0200

    Tests: Created manual test for scrollViewportToShowTarget in an iframe.

---
+++ packages/ckeditor5-utils/tests/manual/scroll/scroll-iframe.js
@@ -0,0 +1,14 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* global window, document */
+
+import { scrollViewportToShowTarget } from '@ckeditor/ckeditor5-utils/src/dom/scroll';
+
+document.getElementById( 'scroll' ).addEventListener( 'click', () => {
+	const target = window.frames[ 0 ].document.querySelector( '#target' );
+
+	scrollViewportToShowTarget( { target } );
+} );
