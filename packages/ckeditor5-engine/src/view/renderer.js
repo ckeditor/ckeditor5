4f2128f257e549e3c98880df8981330d0962bb1d
Szymon Kupś <szymonkups@gmail.com>
2017-10-19 17:24:43 +0200

    Blocking inline fillers on elements with contenteditable=false.

--- packages/ckeditor5-engine/src/view/renderer.js
+++ packages/ckeditor5-engine/src/view/renderer.js
@@ -381,6 +381,11 @@ export default class Renderer {
 			return false;
 		}
 
+		// Block adding inline filler inside elements with contenteditable=false.
+		if ( _isNotEditable( selectionParent ) ) {
+			return false;
+		}
+
 		// We have block filler, we do not need inline one.
 		if ( selectionOffset === selectionParent.getFillerOffset() ) {
 			return false;
@@ -708,3 +713,18 @@ export default class Renderer {
 }
 
 mix( Renderer, ObservableMixin );
+
+// Checks if contents inside provided element are not editable.
+//
+// @private
+// @param {module:engine/view/element~Element} element
+// @returns {Boolean}
+function _isNotEditable( element ) {
+	if ( element.getAttribute( 'contenteditable' ) === false ) {
+		return true;
+	}
+
+	const parent = element.findAncestor( element => element.hasAttribute( 'contenteditable' ) );
+
+	return parent && !parent.getAttribute( 'contenteditable' );
+}
