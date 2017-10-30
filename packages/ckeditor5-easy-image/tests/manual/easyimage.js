a0e3e6265025468931bbf19a8679b98d0e7b9c6b
Maciej Bukowski <ma2ciek@gmail.com>
2017-10-30 16:28:58 +0100

    Updated import.

--- packages/ckeditor5-easy-image/tests/manual/easyimage.js
+++ packages/ckeditor5-easy-image/tests/manual/easyimage.js
@@ -10,7 +10,7 @@ import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
 import EasyImage from '../../src/easyimage';
 import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
 
-import { TOKEN_URL } from '@ckeditor/ckeditor5-cloudservices/tests/_utils/csconfig';
+import { TOKEN_URL } from '@ckeditor/ckeditor5-cloudservices/tests/_utils/cloudservices-config';
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
