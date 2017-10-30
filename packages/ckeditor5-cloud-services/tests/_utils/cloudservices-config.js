45b912857ddc8e438a9bc3a8f100127e5b55934c
Maciej Bukowski <ma2ciek@gmail.com>
2017-10-30 15:36:52 +0100

    Renamed cloudserviecs config.

---
+++ packages/ckeditor5-cloud-services/tests/_utils/cloudservices-config.js
@@ -0,0 +1,12 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+// WARNING: The URLs below should not be used for any other purpose than Easy Image plugin development.
+// Images uploaded using the testing token service may be deleted automatically at any moment.
+// If you would like to try the Easy Image service, please wait until the official launch of Easy Image and sign up for a free trial.
+// Images uploaded during the free trial will not be deleted for the whole trial period and additionally the trial service can be converted
+// into a subscription at any moment, allowing you to preserve all uploaded images.
+
+export const TOKEN_URL = 'https://j2sns7jmy0.execute-api.eu-central-1.amazonaws.com/prod/token-new';
