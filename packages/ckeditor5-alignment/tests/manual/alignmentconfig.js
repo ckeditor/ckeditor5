94c9153f7541aed40b873a376ea510283561396b
Maciej Gołaszewski <jodator@jodator.net>
2017-11-02 18:57:36 +0100

    Tests: Add manual tests.

---
+++ packages/ckeditor5-alignment/tests/manual/alignmentconfig.js
@@ -0,0 +1,26 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals console, window, document */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
+import Alignment from '../../src/alignment';
+
+ClassicEditor
+	.create( document.querySelector( '#editor' ), {
+		plugins: [ ArticlePluginSet, Alignment ],
+		toolbar: [
+			'headings', 'bold', 'italic', 'link', 'alignLeft', 'alignRight', 'alignCenter', 'alignJustify', 'bulletedList', 'numberedList',
+			'blockQuote', 'undo', 'redo'
+		],
+		alignment: { styles: [ 'center', 'justify' ] }
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
---
+++ packages/ckeditor5-alignment/tests/manual/alignmentconfig.md
@@ -0,0 +1,19 @@
+### Loading
+
+1. The data should be loaded with:
+  * four paragraphs each with different text-alignemnt set,
+  * two first paragraphs should be aligned to left,
+  * third paragraph should be centered,
+  * fourth paragraph should be justified,
+  * an image with caption between first and second paragraphs.
+2. Toolbar should have only two buttons for alignment control: center and justify.
+
+### Testing
+
+"Center" alignment button:
+1. should be highlighted only in third paragraph,
+2. should enable/disable center alignment on any paragraph.
+
+"Justify" alignment button should be:
+1. highlighted only in third paragraph,
+2. should enable/disable justify alignment on any paragraph.
