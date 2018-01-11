9cfe0260663c478892f580d33f37d2fe84d2dd18
Oskar Wróbel <kontakt@oskarwrobel.pl>
2018-01-11 14:42:42 +0100

    Introduced attachToform helper.

---
+++ packages/ckeditor5-core/src/editor/utils/attachtoform.js
@@ -0,0 +1,63 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import isFunction from '@ckeditor/ckeditor5-utils/src/lib/lodash/isFunction';
+import CKEditorError from '@ckeditor/ckeditor5-utils/src/ckeditorerror';
+
+/**
+ * @module core/editor/utils
+ */
+
+/**
+ * Checks if editor is initialized on textarea element that belongs to a form. If yes - updates editor's element
+ * contents before submitting the form.
+ *
+ * This helper requires {@link module:core/editor/utils/elementinterface~ElementInterface ElementInterface}.
+ *
+ * @param {module:core/editor/editor} editor Editor instance.
+ */
+export default function attachToForm( editor ) {
+	if ( !isFunction( editor.updateElement ) ) {
+		/**
+		 * {@link module:core/editor/utils/elementinterface~ElementInterface ElementInterface} is required.
+		 *
+		 * @error attachtoform-missing-elementinterface
+		 */
+		throw new CKEditorError( 'attachtoform-missing-elementinterface: Element interface is required.' );
+	}
+
+	const element = editor.element;
+
+	// Only when replacing a textarea which is inside of a form element.
+	if ( element && element.tagName.toLowerCase() === 'textarea' && element.form ) {
+		let originalSubmit;
+		const form = element.form;
+		const onSubmit = () => editor.updateElement();
+
+		// Replace the original form#submit() to call a custom submit function first.
+		// Check if #submit is a function because the form might have an input named "submit".
+		if ( isFunction( form.submit ) ) {
+			originalSubmit = form.submit;
+
+			form.submit = () => {
+				onSubmit();
+				originalSubmit.apply( form );
+			};
+		}
+
+		// Update the replaced textarea with data before each form#submit event.
+		form.addEventListener( 'submit', onSubmit );
+
+		// Remove the submit listener and revert the original submit method on
+		// editor#destroy.
+		editor.on( 'destroy', () => {
+			form.removeEventListener( 'submit', onSubmit );
+
+			if ( originalSubmit ) {
+				form.submit = originalSubmit;
+			}
+		} );
+	}
+}
