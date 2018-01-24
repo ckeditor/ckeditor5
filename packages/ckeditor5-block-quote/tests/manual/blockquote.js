56b60f45d4792b0e7f23f4560af8a03dc1df5527
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2018-01-24 16:06:24 +0100

    Added a separator after the headings dropdown in the configuration of the toolbar.

--- packages/ckeditor5-block-quote/tests/manual/blockquote.js
+++ packages/ckeditor5-block-quote/tests/manual/blockquote.js
@@ -15,7 +15,7 @@ ClassicEditor
 		plugins: [
 			ArticlePluginSet
 		],
-		toolbar: [ 'headings', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', 'undo', 'redo' ]
+		toolbar: [ 'headings', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', 'undo', 'redo' ]
 	} )
 	.then( editor => {
 		window.editor = editor;
