f9392f6a08606321a13d8f7bc1cb2004ef6ce183
Piotrek Koszuliński <pkoszulinski@gmail.com>
2018-01-24 13:17:51 +0100

    Internal: Updated schema use. See ckeditor/ckeditor5-engine#1234.

--- packages/ckeditor5-typing/tests/deletecommand.js
+++ packages/ckeditor5-typing/tests/deletecommand.js
@@ -244,12 +244,9 @@ describe( 'DeleteCommand', () => {
 		} );
 
 		it( 'does not replace an element if a paragraph is not allowed in current position', () => {
-			model.schema.on( 'checkChild', ( evt, args ) => {
-				const def = model.schema.getDefinition( args[ 1 ] );
-
-				if ( args[ 0 ].endsWith( '$root' ) && def.name == 'paragraph' ) {
-					evt.stop();
-					evt.return = false;
+			model.schema.addChildCheck( ( ctx, childDef ) => {
+				if ( ctx.endsWith( '$root' ) && childDef.name == 'paragraph' ) {
+					return false;
 				}
 			} );
 
