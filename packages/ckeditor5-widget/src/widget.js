634153853412b8fb3da14f4af8fd52fbbbe12d30
Szymon Cofalik <s.cofalik@cksource.com>
2018-02-20 11:06:30 +0100

    Changed: Aligned to changes in ckeditor5-engine.

--- packages/ckeditor5-widget/src/widget.js
+++ packages/ckeditor5-widget/src/widget.js
@@ -55,7 +55,7 @@ export default class Widget extends Plugin {
 
 		// Model to view selection converter.
 		// Converts selection placed over widget element to fake selection
-		this.editor.editing.downcastDispatcher.on( 'selection', ( evt, data, consumable, conversionApi ) => {
+		this.editor.editing.downcastDispatcher.on( 'selection', ( evt, data, conversionApi ) => {
 			// Remove selected class from previously selected widgets.
 			this._clearPreviouslySelectedWidgets( conversionApi.writer );
 
