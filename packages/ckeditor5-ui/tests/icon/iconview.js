b1e9955ff764276740bbd32e7c0a2931b58122dd
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2018-02-22 13:26:44 +0100

    Implemented IconView#fillColor observable which fill with the color all the child path.ck-icon__fill.

--- packages/ckeditor5-ui/tests/icon/iconview.js
+++ packages/ckeditor5-ui/tests/icon/iconview.js
@@ -4,6 +4,7 @@
  */
 
 import IconView from '../../src/icon/iconview';
+import normalizeHtml from '@ckeditor/ckeditor5-utils/tests/_utils/normalizehtml';
 
 describe( 'IconView', () => {
 	let view;
@@ -20,6 +21,10 @@ describe( 'IconView', () => {
 
 		it( 'sets #viewBox', () => {
 			expect( view.viewBox ).to.equal( '0 0 20 20' );
+		} );
+
+		it( 'sets #fillColor', () => {
+			expect( view.fillColor ).to.equal( '' );
 		} );
 
 		it( 'creates element from template', () => {
@@ -64,6 +69,51 @@ describe( 'IconView', () => {
 
 				view.content = '<svg version="1.1" viewBox="10 20 30 40" xmlns="http://www.w3.org/2000/svg"><g id="test"></g></svg>';
 				expect( view.viewBox ).to.equal( '10 20 30 40' );
+			} );
+		} );
+
+		describe( 'fill color', () => {
+			it( 'should be set intially based on view#fillColor', () => {
+				view.fillColor = 'red';
+
+				view.content = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg">' +
+					'<path class="ck-icon__fill"></path><path></path><path class="ck-icon__fill"></path></svg>';
+
+				expect( normalizeHtml( view.element.innerHTML ) )
+					.to.equal( '<path class="ck-icon__fill" style="fill:red"></path>' +
+						'<path></path>' +
+						'<path class="ck-icon__fill" style="fill:red"></path>' );
+			} );
+
+			it( 'should react to changes in view#fillColor', () => {
+				view.content = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg">' +
+					'<path class="ck-icon__fill"></path><path></path><path class="ck-icon__fill"></path></svg>';
+
+				expect( normalizeHtml( view.element.innerHTML ) )
+					.to.equal( '<path class="ck-icon__fill"></path><path></path><path class="ck-icon__fill"></path>' );
+
+				view.fillColor = 'red';
+				expect( normalizeHtml( view.element.innerHTML ) )
+					.to.equal( '<path class="ck-icon__fill" style="fill:red"></path>' +
+						'<path></path>' +
+						'<path class="ck-icon__fill" style="fill:red"></path>' );
+			} );
+
+			it( 'should react to changes in view#content', () => {
+				view.content = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg">' +
+					'<path class="ck-icon__fill"></path><path></path><path class="ck-icon__fill"></path></svg>';
+				view.fillColor = 'red';
+
+				expect( normalizeHtml( view.element.innerHTML ) )
+					.to.equal( '<path class="ck-icon__fill" style="fill:red"></path>' +
+						'<path></path>' +
+						'<path class="ck-icon__fill" style="fill:red"></path>' );
+
+				view.content = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg">' +
+					'<path></path><path class="ck-icon__fill"></path></svg>';
+
+				expect( normalizeHtml( view.element.innerHTML ) )
+					.to.equal( '<path></path><path class="ck-icon__fill" style="fill:red"></path>' );
 			} );
 		} );
 	} );
