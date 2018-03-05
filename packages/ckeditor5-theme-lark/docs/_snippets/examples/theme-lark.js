899abe1fc5f6ec76cbecf7ed16d677f9d4e4a52b
Maciej Gołaszewski <jodator@jodator.net>
2018-03-05 17:18:22 +0100

    Other: Renamed 'headings' ui component to 'heading'.

--- packages/ckeditor5-theme-lark/docs/_snippets/examples/theme-lark.js
+++ packages/ckeditor5-theme-lark/docs/_snippets/examples/theme-lark.js
@@ -15,7 +15,7 @@ ClassicEditor
 	.create( document.querySelector( '#snippet-classic-editor' ), {
 		plugins: [ ArticlePluginSet, EasyImage ],
 		toolbar: {
-			items: [ 'headings', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', 'undo', 'redo' ],
+			items: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', 'undo', 'redo' ],
 			viewportTopOffset: 60
 		},
 		image: {
