398bbcc03c64653ee5522309364dd58485784610
Maciej Gołaszewski <jodator@jodator.net>
2018-03-05 17:18:09 +0100

    Other: Renamed 'headings' ui component to 'heading'.

--- packages/ckeditor5-editor-balloon/tests/manual/ballooneditor.js
+++ packages/ckeditor5-editor-balloon/tests/manual/ballooneditor.js
@@ -21,7 +21,7 @@ function initEditors() {
 		BalloonEditor
 			.create( document.querySelector( selector ), {
 				plugins: [ ArticlePluginSet ],
-				toolbar: [ 'headings', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+				toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
 				image: {
 					toolbar: [ 'imageStyleFull', 'imageStyleSide', '|', 'imageTextAlternative' ]
 				}
