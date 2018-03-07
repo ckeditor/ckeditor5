4d8ff15de944a88123991c28a81fee67924a6645
Piotrek Koszuliński <pkoszulinski@gmail.com>
2018-03-07 20:49:33 +0100

    Internal: Updated CS configs after uploadUrl default value has been removed. See ckeditor/ckeditor5-cloudservices#12.

--- packages/ckeditor5-markdown-gfm/docs/_snippets/features/markdown.js
+++ packages/ckeditor5-markdown-gfm/docs/_snippets/features/markdown.js
@@ -9,7 +9,7 @@ import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
 
 import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import { TOKEN_URL } from '@ckeditor/ckeditor5-cloudservices/tests/_utils/cloudservices-config';
+import { CS_CONFIG } from '@ckeditor/ckeditor5-cloudservices/tests/_utils/cloudservices-config';
 
 import GFMDataProcessor from '@ckeditor/ckeditor5-markdown-gfm/src/gfmdataprocessor';
 
@@ -24,9 +24,7 @@ ClassicEditor
 		image: {
 			toolbar: [ 'imageStyleFull', 'imageStyleSide', '|', 'imageTextAlternative' ]
 		},
-		cloudServices: {
-			tokenUrl: TOKEN_URL
-		}
+		cloudServices: CS_CONFIG
 	} )
 	.then( editor => {
 		window.editor = editor;
