84f25cf775ed6b5d390dbcf2103c2da4d9006d57
Maciej Gołaszewski <jodator@jodator.net>
2018-03-09 12:30:18 +0100

    Other: Unify `getPostCssConfig()` from @ckeditor/ckeditor5-dev-utils package.

--- packages/ckeditor5-build-balloon/webpack.config.js
+++ packages/ckeditor5-build-balloon/webpack.config.js
@@ -9,8 +9,7 @@
 
 const path = require( 'path' );
 const webpack = require( 'webpack' );
-const { bundler } = require( '@ckeditor/ckeditor5-dev-utils' );
-const { getPostCssConfig } = require( '@ckeditor/ckeditor5-dev-utils' ).styles;
+const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
 const BabiliPlugin = require( 'babel-minify-webpack-plugin' );
 const buildConfig = require( './build-config' );
@@ -60,7 +59,7 @@ module.exports = {
 					},
 					{
 						loader: 'postcss-loader',
-						options: getPostCssConfig( {
+						options: styles.getPostCssConfig( {
 							themeImporter: {
 								themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
 							},
