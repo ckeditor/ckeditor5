ca4c87444b5849a909b3f071bdc162b73f0ed281
Travis CI User <travis@example.org>
2018-03-26 13:31:56 +0000

    Revision: https://github.com/ckeditor/ckeditor5-autoformat/commit/8e4414c47fb78497cc338d977702c5dd73f49b95

--- docs/_snippets/examples/inline-editor.js
+++ docs/_snippets/examples/inline-editor.js
@@ -6,7 +6,7 @@
 /* globals console, window, document */
 
 import InlineEditor from '@ckeditor/ckeditor5-build-inline/src/ckeditor';
-import { CS_CONFIG } from '@ckeditor/ckeditor5-cloudservices/tests/_utils/cloudservices-config';
+import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud-services-config';
 
 const inlineInjectElements = document.querySelectorAll( '#snippet-inline-editor [data-inline-inject]' );
 
