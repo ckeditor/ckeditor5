2b495c630f58e50f9981a0da6dbe5467d9f53a19
Oskar Wróbel <kontakt@oskarwrobel.pl>
2018-05-14 09:42:39 +0200

    Improved code style.

--- packages/ckeditor5-image/src/imageupload/imageuploadprogress.js
+++ packages/ckeditor5-image/src/imageupload/imageuploadprogress.js
@@ -7,6 +7,8 @@
  * @module image/imageupload/imageuploadprogress
  */
 
+/* globals setTimeout */
+
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import FileRepository from '@ckeditor/ckeditor5-upload/src/filerepository';
 import uploadingPlaceholder from '../../theme/icons/image_placeholder.svg';
