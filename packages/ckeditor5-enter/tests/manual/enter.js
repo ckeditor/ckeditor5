7fe3cc6b39d6de7c4e9abbeb5ae801dcd60f8a81
Kamil Piechaczek <pomek@pomek.pl>
2018-05-15 15:13:30 +0200

    Updated the MT.

--- packages/ckeditor5-enter/tests/manual/enter.js
+++ packages/ckeditor5-enter/tests/manual/enter.js
@@ -6,7 +6,7 @@
 /* globals console:false, document, window */
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import Enter from '../../src/enter';
+import Essentials from '../../src/essentials';
 import Typing from '@ckeditor/ckeditor5-typing/src/typing';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Undo from '@ckeditor/ckeditor5-undo/src/undo';
@@ -15,7 +15,7 @@ import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
-		plugins: [ Enter, Typing, Heading, Undo, Bold, Italic ],
+		plugins: [ Essentials, Typing, Heading, Undo, Bold, Italic ],
 		toolbar: [ 'heading', '|', 'bold', 'italic', 'undo', 'redo' ]
 	} )
 	.then( editor => {
