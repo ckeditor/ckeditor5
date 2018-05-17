9da9afea226e8175ef17105d4e17763d9ceea4cf
Szymon Kupś <szymonkups@gmail.com>
2018-05-17 23:08:00 +0200

    Fixed typo.

--- packages/ckeditor5-editor-inline/src/inlineeditor.js
+++ packages/ckeditor5-editor-inline/src/inlineeditor.js
@@ -140,7 +140,7 @@ export default class InlineEditor extends Editor {
 	 *		import ...
 	 *
 	 *		InlineEditor
-	 *			.create( '<p>Hello world!</p>, {
+	 *			.create( '<p>Hello world!</p>', {
 	 *				plugins: [ Essentials, Bold, Italic, ... ],
 	 *				toolbar: [ 'bold', 'italic', ... ]
 	 *			} )
