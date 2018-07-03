2f2c7cfb83351ee48f2d50aa3fea2c3a4f90eb57
Kamil Piechaczek <pomek@pomek.pl>
2018-07-03 08:33:02 +0200

    Used better variable name.

--- packages/ckeditor5-editor-classic/src/classiceditor.js
+++ packages/ckeditor5-editor-classic/src/classiceditor.js
@@ -201,9 +201,11 @@ export default class ClassicEditor extends Editor {
 					} )
 					.then( () => editor.editing.view.attachDomRoot( editor.ui.view.editableElement ) )
 					.then( () => {
-						const data = isElement( sourceElementOrData ) ? getDataFromElement( sourceElementOrData ) : sourceElementOrData;
+						const initialData = isElement( sourceElementOrData ) ?
+							getDataFromElement( sourceElementOrData ) :
+							sourceElementOrData;
 
-						return editor.data.init( data );
+						return editor.data.init( initialData );
 					} )
 					.then( () => {
 						editor.fire( 'dataReady' );
