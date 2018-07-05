148bd041ba23bc49670fd243ca9266091d871ea0
Piotrek Koszuliński <pkoszulinski@gmail.com>
2018-07-05 15:38:57 +0200

    Improved comments.

--- packages/ckeditor5-build-classic/webpack.config.js
+++ packages/ckeditor5-build-classic/webpack.config.js
@@ -30,13 +30,13 @@ module.exports = {
 
 	optimization: {
 		minimizer: [
-			// Use the newest version of UglifyJsWebpackPlugin plugin that fixes `inline` optimization bug.
+			// Use the newest version of UglifyJsWebpackPlugin that fixes the `inline` optimization bug.
 			// See https://github.com/webpack-contrib/uglifyjs-webpack-plugin/issues/264.
 			new UglifyJsWebpackPlugin( {
 				sourceMap: true,
 				uglifyOptions: {
 					output: {
-						// Preserve license comments starting with the exclamation mark (!).
+						// Preserve CKEditor 5 license comments.
 						comments: /^!/
 					}
 				}
