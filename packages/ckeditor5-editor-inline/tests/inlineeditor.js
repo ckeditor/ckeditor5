622bbb7790106be547b078191a0b08e2a35ccef7
Maciej Gołaszewski <jodator@jodator.net>
2018-07-09 16:59:40 +0200

    Tests: Properly use sinon sandbox in tests.

--- packages/ckeditor5-editor-inline/tests/inlineeditor.js
+++ packages/ckeditor5-editor-inline/tests/inlineeditor.js
@@ -23,10 +23,10 @@ import RootElement from '@ckeditor/ckeditor5-engine/src/model/rootelement';
 
 import testUtils from '@ckeditor/ckeditor5-core/tests/_utils/utils';
 
-testUtils.createSinonSandbox();
-
 describe( 'InlineEditor', () => {
 	let editor, editorElement;
+
+	testUtils.createSinonSandbox();
 
 	beforeEach( () => {
 		editorElement = document.createElement( 'div' );
