c26daca7b4c19f6ca2b1be816a074e46cf85fb96
Travis CI User <travis@example.org>
2018-07-16 10:42:47 +0000

    Revision: https://github.com/ckeditor/ckeditor5-theme-lark/commit/15eba1ddbe5a1756e4c7fd9cb236e961f3ecc07d

---
+++ docs/_snippets/builds/saving-data/build-autosave-source.js
@@ -0,0 +1,14 @@
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals window */
+
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic/src/ckeditor';
+
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave';
+
+ClassicEditor.builtinPlugins.push( Autosave );
+
+window.ClassicEditor = ClassicEditor;
