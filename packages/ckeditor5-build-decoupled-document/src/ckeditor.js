e8b5aaa768833fe1944cd089a91a23fe7fc75c10
Piotrek Koszuliński <pkoszulinski@gmail.com>
2018-07-18 00:01:08 +0200

    Internal: Further builds simplifications plus some comments.

--- packages/ckeditor5-build-decoupled-document/src/ckeditor.js
+++ packages/ckeditor5-build-decoupled-document/src/ckeditor.js
@@ -3,61 +3,65 @@
  * For licensing, see LICENSE.md.
  */
 
+// The editor creator to use.
 import DecoupledEditorBase from '@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor';
-import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
-import AlignmentPlugin from '@ckeditor/ckeditor5-alignment/src/alignment';
-import FontsizePlugin from '@ckeditor/ckeditor5-font/src/fontsize';
-import FontfamilyPlugin from '@ckeditor/ckeditor5-font/src/fontfamily';
-import HighlightPlugin from '@ckeditor/ckeditor5-highlight/src/highlight';
-import UploadAdapterPlugin from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-import AutoformatPlugin from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import StrikethroughPlugin from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
-import UnderlinePlugin from '@ckeditor/ckeditor5-basic-styles/src/underline';
-import BlockQuotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import EasyImagePlugin from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading';
-import ImagePlugin from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaptionPlugin from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStylePlugin from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbarPlugin from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUploadPlugin from '@ckeditor/ckeditor5-image/src/imageupload';
-import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
-import ListPlugin from '@ckeditor/ckeditor5-list/src/list';
-import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import TablePlugin from '@ckeditor/ckeditor5-table/src/table';
-import TabletoolbarPlugin from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
+import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
 export default class DecoupledEditor extends DecoupledEditorBase {}
 
+// Plugins to include in the build.
 DecoupledEditor.builtinPlugins = [
-	EssentialsPlugin,
-	AlignmentPlugin,
-	FontsizePlugin,
-	FontfamilyPlugin,
-	HighlightPlugin,
-	UploadAdapterPlugin,
-	AutoformatPlugin,
-	BoldPlugin,
-	ItalicPlugin,
-	StrikethroughPlugin,
-	UnderlinePlugin,
-	BlockQuotePlugin,
-	EasyImagePlugin,
-	HeadingPlugin,
-	ImagePlugin,
-	ImageCaptionPlugin,
-	ImageStylePlugin,
-	ImageToolbarPlugin,
-	ImageUploadPlugin,
-	LinkPlugin,
-	ListPlugin,
-	ParagraphPlugin,
-	TablePlugin,
-	TabletoolbarPlugin
+	Essentials,
+	Alignment,
+	FontSize,
+	FontFamily,
+	Highlight,
+	UploadAdapter,
+	Autoformat,
+	Bold,
+	Italic,
+	Strikethrough,
+	Underline,
+	BlockQuote,
+	EasyImage,
+	Heading,
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	Link,
+	List,
+	Paragraph,
+	Table,
+	TableToolbar
 ];
 
+// Editor configuration.
 DecoupledEditor.defaultConfig = {
 	toolbar: {
 		items: [
@@ -107,5 +111,6 @@ DecoupledEditor.defaultConfig = {
 			'mergeTableCells'
 		]
 	},
+	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
