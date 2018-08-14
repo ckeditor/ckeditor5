d76c10780a1c307db5f214f8715edf8caabb9296
Oskar Wróbel <kontakt@oskarwrobel.pl>
2018-08-14 13:07:37 +0200

    Added comment to the node attributes serialization.

--- packages/ckeditor5-engine/src/model/node.js
+++ packages/ckeditor5-engine/src/model/node.js
@@ -380,6 +380,8 @@ export default class Node {
 	toJSON() {
 		const json = {};
 
+		// Serializes attributes to the object.
+		// attributes = { a: 'foo', b: 1, c: true }.
 		if ( this._attrs.size ) {
 			json.attributes = Array.from( this._attrs ).reduce( ( result, attr ) => {
 				result[ attr[ 0 ] ] = attr[ 1 ];
