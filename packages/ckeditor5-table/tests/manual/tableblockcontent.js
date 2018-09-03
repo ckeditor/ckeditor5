c50b679d564ec5b3294415d98e7745199dfd1820
Maciej Gołaszewski <jodator@jodator.net>
2018-09-03 13:17:11 +0200

    Remove image from block content manual test.

--- packages/ckeditor5-table/tests/manual/tableblockcontent.js
+++ packages/ckeditor5-table/tests/manual/tableblockcontent.js
@@ -10,20 +10,14 @@ import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articleplugi
 import Table from '../../src/table';
 import TableToolbar from '../../src/tabletoolbar';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import Image from '@ckeditor/ckeditor5-image/src/image';
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
-		plugins: [ ArticlePluginSet, Table, TableToolbar, Alignment, Image, ImageCaption, ImageStyle ],
+		plugins: [ ArticlePluginSet, Table, TableToolbar, Alignment ],
 		toolbar: [
 			'heading', '|', 'insertTable', '|', 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote',
 			'alignment', '|', 'undo', 'redo'
 		],
-		image: {
-			toolbar: [ 'imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative' ]
-		},
 		table: {
 			toolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
 		}
