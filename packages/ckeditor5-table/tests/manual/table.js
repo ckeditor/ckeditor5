d72e65fbf7c34d67f8738cb1ac121f8583a2b5e1
Maciej Gołaszewski <jodator@jodator.net>
2018-09-14 15:49:00 +0200

    Revert "Tests: Simplify table manual test data and add model output."
    This reverts commit aa398a90

--- packages/ckeditor5-table/tests/manual/table.js
+++ packages/ckeditor5-table/tests/manual/table.js
@@ -3,14 +3,13 @@
  * For licensing, see LICENSE.md.
  */
 
-/* globals console, window, document, global */
+/* globals console, window, document */
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
 import Table from '../../src/table';
 import TableToolbar from '../../src/tabletoolbar';
 import TableSelection from '../../src/tableselection';
-import { getData } from '@ckeditor/ckeditor5-engine/src/dev-utils/model';
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
@@ -24,37 +23,7 @@ ClassicEditor
 	} )
 	.then( editor => {
 		window.editor = editor;
-		editor.model.document.on( 'change', () => {
-			printModelContents( editor );
-		} );
-
-		printModelContents( editor );
 	} )
 	.catch( err => {
 		console.error( err.stack );
 	} );
-
-const modelDiv = global.document.querySelector( '#model' );
-
-function printModelContents( editor ) {
-	modelDiv.innerHTML = formatTable( getData( editor.model ) )
-		.replace( /</g, '&lt;' )
-		.replace( />/g, '&gt;' )
-		.replace( /\n/g, '<br>' )
-		.replace( /\[/g, '<strong>[' )
-		.replace( /]/g, ']</strong>' );
-}
-
-function formatTable( tableString ) {
-	return tableString
-		.replace( /<table>/g, '\n<table>' )
-		.replace( /<tableRow>/g, '\n<tableRow>\n    ' )
-		.replace( /<thead>/g, '\n<thead>\n    ' )
-		.replace( /<tbody>/g, '\n<tbody>\n    ' )
-		.replace( /<tr>/g, '\n<tr>\n    ' )
-		.replace( /<\/tableRow>/g, '\n</tableRow>' )
-		.replace( /<\/thead>/g, '\n</thead>' )
-		.replace( /<\/tbody>/g, '\n</tbody>' )
-		.replace( /<\/tr>/g, '\n</tr>' )
-		.replace( /<\/table>/g, '\n</table>' );
-}
