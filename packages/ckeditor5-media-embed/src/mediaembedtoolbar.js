0e356789b40e443fd5cbf5c5ec649b6a682aeb34
Maciej Bukowski <ma2ciek@gmail.com>
2018-09-19 17:46:17 +0200

    Improved API docs.

--- packages/ckeditor5-media-embed/src/mediaembedtoolbar.js
+++ packages/ckeditor5-media-embed/src/mediaembedtoolbar.js
@@ -2,14 +2,19 @@
  * Copyright (c) 2016 - 2018, CKSource - Frederico Knabben. All rights reserved.
  */
 
+/**
+ * @module media-embed/mediaembedtoolbar
+ */
+
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import WidgetToolbarRepository from '@ckeditor/ckeditor5-widget/src/widgettoolbarrepository';;
+import WidgetToolbarRepository from '@ckeditor/ckeditor5-widget/src/widgettoolbarrepository';
 import { isMediaWidgetSelected } from './utils';
 
 /**
- * The media embed comment toolbar class. Creates a toolbar for media embed that shows up when the media widget is selected.
+ * The media embed toolbar plugin. It creates a toolbar for media embed that shows up when the media element is selected.
  *
- * The toolbar uses the {@link module:ui/panel/balloon/contextualballoon~ContextualBalloon}.
+ * Instances of toolbar components (e.g. buttons) are created based on the
+ * {@link module:media-embed/mediaembedtoolbar~MediaEmbedConfig#toolbar `media.toolbar` configuration option}.
  *
  * @extends module:core/plugin~Plugin
  */
@@ -41,3 +46,12 @@ export default class MediaEmbedToolbar extends Plugin {
 		} );
 	}
 }
+
+/**
+ * Items to be placed in the media embed toolbar.
+ * This option requires adding {@link module:media-embed/mediaembedtoolbar~MediaEmbedToolbar} to the plugin list.
+ *
+ * Read more about configuring toolbar in {@link module:core/editor/editorconfig~EditorConfig#toolbar}.
+ *
+ * @member {Array.<String>} module:media-embed/mediaembedtoolbar~MediaEmbedConfig#toolbar
+ */
