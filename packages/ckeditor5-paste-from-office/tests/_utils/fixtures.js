e01354a5494a974f4a7524b97a931c0063b5f99d
Krzysztof Krztoń <k.krzton@cksource.com>
2018-10-26 13:52:03 +0200

    Tests: Reorganized image test directory.

--- packages/ckeditor5-paste-from-office/tests/_utils/fixtures.js
+++ packages/ckeditor5-paste-from-office/tests/_utils/fixtures.js
@@ -5,7 +5,7 @@
 
 // Import fixtures.
 import { fixtures as basicStyles, browserFixtures as basicStylesBrowser } from '../_data/basic-styles/index.js';
-import { fixtures as image, browserFixtures as imageBrowser } from '../_data/images/image/index.js';
+import { fixtures as image, browserFixtures as imageBrowser } from '../_data/image/index.js';
 import { fixtures as link, browserFixtures as linkBrowser } from '../_data/link/index.js';
 import { fixtures as list, browserFixtures as listBrowser } from '../_data/list/index.js';
 import { fixtures as spacing, browserFixtures as spacingBrowser } from '../_data/spacing/index.js';
