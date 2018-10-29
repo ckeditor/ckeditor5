d35bf50ec053fd43722107e79fed7c1e2fb469c4
Damian Konopka <d.konopka@cksource.com>
2018-10-29 14:16:58 +0100

    Docs: Fixed `viewportTopOffset` of sticky toolbar. [skip ci]

--- packages/ckeditor5-image/docs/_snippets/features/image-style.js
+++ packages/ckeditor5-image/docs/_snippets/features/image-style.js
@@ -10,7 +10,7 @@ import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud
 ClassicEditor
 	.create( document.querySelector( '#snippet-image-style' ), {
 		toolbar: {
-			viewportTopOffset: 60
+			viewportTopOffset: 100
 		},
 		cloudServices: CS_CONFIG
 	} )
