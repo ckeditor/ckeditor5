83d9593d060c3183a45dfddef6109863131e10de
Piotrek Koszuliński <pkoszulinski@gmail.com>
2018-11-01 12:21:55 +0100

    Conversion to a slot.

--- tests/manual/article.js
+++ tests/manual/article.js
@@ -3,15 +3,100 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-/* globals console, window, document */
+/* globals console, window, document, setInterval */
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
 
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+
+import {
+	// downcastElementToElement,
+	insertElement
+} from '@ckeditor/ckeditor5-engine/src/conversion/downcast-converters';
+
+import ViewPosition from '@ckeditor/ckeditor5-engine/src/view/position';
+
+import { setData, getData } from '@ckeditor/ckeditor5-engine/src/dev-utils/model';
+
+class Container extends Plugin {
+	init() {
+		const editor = this.editor;
+
+		editor.model.schema.register( 'container', {
+			allowIn: '$root',
+			isObject: true
+		} );
+
+		editor.model.schema.extend( '$block', {
+			allowIn: 'container'
+		} );
+
+		// # Step 1.
+		//
+		// Our starting point – simple converter which creates a phantom <div.slot> which is not bound to
+		// anything in the model. What we need to do now is to teach the mapper that the inside of
+		// <container> is mapped with the inside of <div.slot>. And that's where the fun starts...
+		// editor.conversion.for( 'editingDowncast' ).add(
+		// 	downcastElementToElement( {
+		// 		model: 'container',
+		// 		view: ( modelElement, viewWriter ) => {
+		// 			const viewContainer = viewWriter.createContainerElement( 'div', { class: 'container' } );
+		// 			const viewSlot = viewWriter.createContainerElement( 'div', { class: 'slot' } );
+
+		// 			viewWriter.insert( ViewPosition.createAt( viewContainer, 0 ), viewSlot );
+
+		// 			return viewContainer;
+		// 		}
+		// 	} )
+		// );
+
+		// # Step 2.
+		//
+		// Let's try to bind <container> with <div.slot> directly. This will leave one part of the old mapping
+		// (div.container -> container). So, in total, we'll have this:
+		//
+		// * container -> div.slot (new)
+		// * div.slot -> container (new)
+		// * div.container -> container (old)
+		editor.conversion.for( 'editingDowncast' ).add(
+			dispatcher => {
+				const insertViewElement = insertElement( ( modelElement, viewWriter ) => {
+					const viewContainer = viewWriter.createContainerElement( 'div', { class: 'container' } );
+					const viewSlot = viewWriter.createContainerElement( 'div', { class: 'slot' } );
+
+					viewWriter.insert( ViewPosition.createAt( viewContainer, 0 ), viewSlot );
+
+					return viewContainer;
+				} );
+
+				dispatcher.on( 'insert:container', ( evt, data, conversionApi ) => {
+					insertViewElement( evt, data, conversionApi );
+
+					// Use the existing "old" mapping created by `insertViewElement()`.
+					const viewContainer = conversionApi.mapper.toViewElement( data.item );
+					const viewSlot = viewContainer.getChild( 0 );
+
+					conversionApi.mapper.bindElements( data.item, viewSlot );
+				} );
+			}
+		);
+
+		// # Step 3.
+		//
+		// Actually, an alternative solution could be to try controlling only positions, without
+		// controlling element binding. To that, you can listen to Mapper#modelToViewPosition and
+		// Mapper#viewToModelPosition events and override the default behaviour. This approach
+		// may also be used as a complementary solution to the one used in "Step 2." –
+		// it will allow fixing some remaining position mapping issues if some are found.
+		// For now, I haven't found anything that would break.
+	}
+}
+
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
-		plugins: [ ArticlePluginSet ],
+		plugins: [ ArticlePluginSet, Container ],
 		toolbar: [
 			'heading',
 			'|',
@@ -43,6 +128,17 @@ ClassicEditor
 	} )
 	.then( editor => {
 		window.editor = editor;
+
+		setData(
+			editor.model,
+			'<paragraph>A</paragraph>' +
+			'<container><paragraph>1</paragraph><paragraph>2</paragraph><paragraph>3</paragraph></container>' +
+			'<paragraph>B</paragraph>'
+		);
+
+		setInterval( () => {
+			console.log( getData( editor.model ) );
+		}, 3000 );
 	} )
 	.catch( err => {
 		console.error( err.stack );
