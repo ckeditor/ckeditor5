2c5ea149ae8adfd84a4b634c0ebeaa74a12d3ce6
Krzysztof Krztoń <k.krzton@cksource.com>
2018-11-08 11:55:59 +0100

    Skip text nodes between 'tr' elements during table upcast.

--- packages/ckeditor5-table/src/converters/upcasttable.js
+++ packages/ckeditor5-table/src/converters/upcasttable.js
@@ -180,7 +180,13 @@ function scanTable( viewTable ) {
 				firstTheadElement = tableChild;
 			}
 
-			for ( const tr of Array.from( tableChild.getChildren() ) ) {
+			// There might be some extra empty text nodes between the `tr`s.
+			// Make sure further code operates on `tr`s only. (#145)
+			const trs = Array.from( tableChild.getChildren() ).filter( el => {
+				return el.is( 'element', 'tr' );
+			} );
+
+			for ( const tr of trs ) {
 				// This <tr> is a child of a first <thead> element.
 				if ( tr.parent.name === 'thead' && tr.parent === firstTheadElement ) {
 					tableMeta.headingRows++;
