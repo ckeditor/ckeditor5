3cc0dc776b5ca5028c84946ff493ca7f3d6020c5
Piotrek Koszuliński <pkoszulinski@gmail.com>
2018-11-14 16:13:51 +0100

    Feature: Introduced the Paste From Office feature.

--- packages/ckeditor5-build-decoupled-document/src/ckeditor.js
+++ packages/ckeditor5-build-decoupled-document/src/ckeditor.js
@@ -29,6 +29,7 @@ import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
@@ -59,6 +60,7 @@ DecoupledEditor.builtinPlugins = [
 	List,
 	MediaEmbed,
 	Paragraph,
+	PasteFromOffice,
 	Table,
 	TableToolbar
 ];
