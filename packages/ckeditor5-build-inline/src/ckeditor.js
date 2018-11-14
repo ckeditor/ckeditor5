9958d8bf35daac8644ae98d66ecf8c8b5741afd8
Piotrek Koszuliński <pkoszulinski@gmail.com>
2018-11-14 16:13:51 +0100

    Feature: Introduced the Paste From Office feature.

--- packages/ckeditor5-build-inline/src/ckeditor.js
+++ packages/ckeditor5-build-inline/src/ckeditor.js
@@ -23,6 +23,7 @@ import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
@@ -47,6 +48,7 @@ InlineEditor.builtinPlugins = [
 	List,
 	MediaEmbed,
 	Paragraph,
+	PasteFromOffice,
 	Table,
 	TableToolbar
 ];
