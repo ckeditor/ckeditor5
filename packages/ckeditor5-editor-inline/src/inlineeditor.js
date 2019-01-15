ceac4214f83edb6ebc9aa5e76de6ea2b82f37165
Maciej Gołaszewski <jodator@jodator.net>
2019-01-15 12:03:06 +0100

    Improve editor destructuring process.

--- packages/ckeditor5-editor-inline/src/inlineeditor.js
+++ packages/ckeditor5-editor-inline/src/inlineeditor.js
@@ -95,11 +95,13 @@ export default class InlineEditor extends Editor {
 		const data = this.getData();
 
 		this.ui.destroy();
+		this.ui = null;
 
 		return super.destroy()
 			.then( () => {
 				if ( this.sourceElement ) {
 					setDataInElement( this.sourceElement, data );
+					this.sourceElement = null;
 				}
 			} );
 	}
