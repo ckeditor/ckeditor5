821669592e58b56d5d96760d5570918b25c0df12
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-01-21 11:10:40 +0100

    Renamed the attachPlaceholder() helper to addPlaceholder().

--- packages/ckeditor5-editor-classic/src/classiceditorui.js
+++ packages/ckeditor5-editor-classic/src/classiceditorui.js
@@ -10,7 +10,7 @@
 import EditorUI from '@ckeditor/ckeditor5-core/src/editor/editorui';
 import enableToolbarKeyboardFocus from '@ckeditor/ckeditor5-ui/src/toolbar/enabletoolbarkeyboardfocus';
 import normalizeToolbarConfig from '@ckeditor/ckeditor5-ui/src/toolbar/normalizetoolbarconfig';
-import { attachPlaceholder, getRootPlaceholderElement } from '@ckeditor/ckeditor5-engine/src/view/placeholder';
+import { addPlaceholder, getRootPlaceholderElement } from '@ckeditor/ckeditor5-engine/src/view/placeholder';
 import ElementReplacer from '@ckeditor/ckeditor5-utils/src/elementreplacer';
 
 /**
@@ -118,7 +118,7 @@ export default class ClassicEditorUI extends EditorUI {
 			if ( placeholderText ) {
 				const placeholderElement = getRootPlaceholderElement( editingRoot );
 
-				attachPlaceholder( editingView, placeholderElement, placeholderText );
+				addPlaceholder( editingView, placeholderElement, placeholderText );
 			}
 		} );
 
