3dea0ff2fec2332da605f01eb2e08b5ae9250c12
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-01-21 11:00:54 +0100

    Refactoring: Renamed getPlaceholderElement to getRootPlaceholderElement.

--- packages/ckeditor5-editor-balloon/src/ballooneditorui.js
+++ packages/ckeditor5-editor-balloon/src/ballooneditorui.js
@@ -9,7 +9,7 @@
 
 import EditorUI from '@ckeditor/ckeditor5-core/src/editor/editorui';
 import enableToolbarKeyboardFocus from '@ckeditor/ckeditor5-ui/src/toolbar/enabletoolbarkeyboardfocus';
-import { attachPlaceholder, getPlaceholderElement } from '@ckeditor/ckeditor5-engine/src/view/placeholder';
+import { attachPlaceholder, getRootPlaceholderElement } from '@ckeditor/ckeditor5-engine/src/view/placeholder';
 
 /**
  * The balloon editor UI class.
@@ -105,7 +105,7 @@ export default class BalloonEditorUI extends EditorUI {
 			const placeholderText = editor.config.get( 'placeholder' ) || editor.sourceElement.getAttribute( 'placeholder' );
 
 			if ( placeholderText ) {
-				const placeholderElement = getPlaceholderElement( editingRoot );
+				const placeholderElement = getRootPlaceholderElement( editingRoot );
 
 				attachPlaceholder( editingView, placeholderElement, placeholderText );
 			}
