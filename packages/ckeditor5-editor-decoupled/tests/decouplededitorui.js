ab1eacb5cdb0aeb41cb85d8c0cc49cb740def32e
Piotr Jasiun <p.jasiun@cksource.com>
2019-01-22 17:21:14 +0100

    Tests: Update tests after getEditableElement simplification.

--- packages/ckeditor5-editor-decoupled/tests/decouplededitorui.js
+++ packages/ckeditor5-editor-decoupled/tests/decouplededitorui.js
@@ -167,8 +167,8 @@ describe( 'DecoupledEditorUI', () => {
 			expect( ui.getEditableElement( 'main' ) ).to.equal( view.editable.element );
 		} );
 
-		it( 'returns null if editable with the given name is absent', () => {
-			expect( ui.getEditableElement( 'absent' ) ).to.null;
+		it( 'returns undefined if editable with the given name is absent', () => {
+			expect( ui.getEditableElement( 'absent' ) ).to.be.undefined;
 		} );
 	} );
 } );
