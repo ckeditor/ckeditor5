3cb05eac33c43b5fa8f123191493a5b03e230698
Kamil Piechaczek <pomek@pomek.pl>
2019-01-23 10:26:27 +0100

    Internal: Bumped the year. [skip ci]

--- packages/ckeditor5-media-embed/tests/mediaembedtoolbar.js
+++ packages/ckeditor5-media-embed/tests/mediaembedtoolbar.js
@@ -1,5 +1,6 @@
 /**
- * Copyright (c) 2016 - 2017, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
  */
 
 /* global document */
