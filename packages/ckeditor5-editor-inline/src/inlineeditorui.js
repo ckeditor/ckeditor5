9011e7a880eb2abc3cb2104d32a4f5b2791372f9
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-01-28 13:54:31 +0100

    Aligned to the new placeholder helpers API in the engine.

--- packages/ckeditor5-editor-inline/src/inlineeditorui.js
+++ packages/ckeditor5-editor-inline/src/inlineeditorui.js
@@ -10,7 +10,7 @@
 import EditorUI from '@ckeditor/ckeditor5-core/src/editor/editorui';
 import enableToolbarKeyboardFocus from '@ckeditor/ckeditor5-ui/src/toolbar/enabletoolbarkeyboardfocus';
 import normalizeToolbarConfig from '@ckeditor/ckeditor5-ui/src/toolbar/normalizetoolbarconfig';
-import { addPlaceholder, getRootPlaceholderElement } from '@ckeditor/ckeditor5-engine/src/view/placeholder';
+import { enablePlaceholder } from '@ckeditor/ckeditor5-engine/src/view/placeholder';
 
 /**
  * The inline editor UI class.
@@ -165,9 +165,12 @@ export default class InlineEditorUI extends EditorUI {
 		const placeholderText = editor.config.get( 'placeholder' ) || editor.sourceElement.getAttribute( 'placeholder' );
 
 		if ( placeholderText ) {
-			const placeholderElement = getRootPlaceholderElement( editingRoot );
-
-			addPlaceholder( editingView, placeholderElement, placeholderText );
+			enablePlaceholder( {
+				view: editingView,
+				element: editingRoot,
+				text: placeholderText,
+				isDirectHost: false
+			} );
 		}
 	}
 }
