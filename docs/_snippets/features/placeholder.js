8a4e359abc352d98a0bf614a2c15eeda9d4a8b88
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-01-30 16:20:11 +0100

    Docs: Created Editor placeholder guide.

---
+++ docs/_snippets/features/placeholder.js
@@ -0,0 +1,19 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* globals console, window, document */
+
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic/src/ckeditor';
+
+ClassicEditor
+	.create( document.querySelector( '#snippet-placeholder' ), {
+		placeholder: 'Type the content here!'
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
