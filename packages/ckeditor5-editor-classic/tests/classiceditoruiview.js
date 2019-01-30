81668f8d1697c9f89cf199bb082f750d9014f3d2
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-01-30 11:44:36 +0100

    Tests: Used createRoot helper in the UIView tests.

--- packages/ckeditor5-editor-classic/tests/classiceditoruiview.js
+++ packages/ckeditor5-editor-classic/tests/classiceditoruiview.js
@@ -5,11 +5,11 @@
 
 import ClassicEditorUIView from '../src/classiceditoruiview';
 import EditingView from '@ckeditor/ckeditor5-engine/src/view/view';
-import ViewRootEditableElement from '@ckeditor/ckeditor5-engine/src/view/rooteditableelement';
 import StickyPanelView from '@ckeditor/ckeditor5-ui/src/panel/sticky/stickypanelview';
 import ToolbarView from '@ckeditor/ckeditor5-ui/src/toolbar/toolbarview';
 import InlineEditableUIView from '@ckeditor/ckeditor5-ui/src/editableui/inline/inlineeditableuiview';
 import Locale from '@ckeditor/ckeditor5-utils/src/locale';
+import createRoot from '@ckeditor/ckeditor5-engine/tests/view/_utils/createroot.js';
 
 import testUtils from '@ckeditor/ckeditor5-core/tests/_utils/utils';
 
@@ -20,7 +20,8 @@ describe( 'ClassicEditorUIView', () => {
 
 	beforeEach( () => {
 		locale = new Locale( 'en' );
-		setUpEditingView();
+		editingView = new EditingView();
+		editingViewRoot = createRoot( editingView.document );
 		view = new ClassicEditorUIView( locale, editingView );
 		view.editable.name = editingViewRoot.rootName;
 		view.render();
@@ -73,11 +74,4 @@ describe( 'ClassicEditorUIView', () => {
 			} );
 		} );
 	} );
-
-	function setUpEditingView() {
-		editingView = new EditingView();
-		editingViewRoot = new ViewRootEditableElement( 'div' );
-		editingViewRoot._document = editingView.document;
-		editingView.document.roots.add( editingViewRoot );
-	}
 } );
