498023545d3158d4d0b8c7740267f2444c8f1ce5
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-01-30 11:44:36 +0100

    Tests: Used createRoot helper in the UIView tests.

--- packages/ckeditor5-editor-decoupled/tests/decouplededitoruiview.js
+++ packages/ckeditor5-editor-decoupled/tests/decouplededitoruiview.js
@@ -7,10 +7,10 @@
 
 import DecoupledEditorUIView from '../src/decouplededitoruiview';
 import EditingView from '@ckeditor/ckeditor5-engine/src/view/view';
-import ViewRootEditableElement from '@ckeditor/ckeditor5-engine/src/view/rooteditableelement';
 import ToolbarView from '@ckeditor/ckeditor5-ui/src/toolbar/toolbarview';
 import InlineEditableUIView from '@ckeditor/ckeditor5-ui/src/editableui/inline/inlineeditableuiview';
 import Locale from '@ckeditor/ckeditor5-utils/src/locale';
+import createRoot from '@ckeditor/ckeditor5-engine/tests/view/_utils/createroot.js';
 
 import testUtils from '@ckeditor/ckeditor5-core/tests/_utils/utils';
 
@@ -21,7 +21,8 @@ describe( 'DecoupledEditorUIView', () => {
 
 	beforeEach( () => {
 		locale = new Locale( 'en' );
-		setUpEditingView();
+		editingView = new EditingView();
+		editingViewRoot = createRoot( editingView.document );
 		view = new DecoupledEditorUIView( locale, editingView );
 		view.editable.name = editingViewRoot.rootName;
 	} );
@@ -130,11 +131,4 @@ describe( 'DecoupledEditorUIView', () => {
 			view.editable.element.remove();
 		} );
 	} );
-
-	function setUpEditingView() {
-		editingView = new EditingView();
-		editingViewRoot = new ViewRootEditableElement( 'div' );
-		editingViewRoot._document = editingView.document;
-		editingView.document.roots.add( editingViewRoot );
-	}
 } );
