0cc0418a2fa3780c4a0fb8a59e4d6256ecd96360
Krzysztof Krztoń <k.krzton@cksource.com>
2019-02-15 14:49:50 +0100

    Tests: Added unit and integration tests for pasting nested lists.

--- packages/ckeditor5-paste-from-office/tests/filters/list.js
+++ packages/ckeditor5-paste-from-office/tests/filters/list.js
@@ -81,6 +81,102 @@ describe( 'PasteFromOffice - filters', () => {
 				expect( view.childCount ).to.equal( 0 );
 				expect( stringify( view ) ).to.equal( '' );
 			} );
+
+			describe( 'Nesting', () => {
+				const level1 = 'style="mso-list:l0 level1 lfo0"';
+				const level2 = 'style="mso-list:l0 level2 lfo0"';
+				const level3 = 'style="mso-list:l0 level3 lfo0"';
+				const level4 = 'style="mso-list:l0 level4 lfo0"';
+
+				it( 'handles simple indentation', () => {
+					const html = `<p ${ level1 }>Foo</p><p ${ level2 }>Bar</p><p ${ level3 }>Baz</p>`;
+					const view = htmlDataProcessor.toView( html );
+
+					transformListItemLikeElementsIntoLists( view, '' );
+
+					expect( view.childCount ).to.equal( 1 );
+					expect( stringify( view ) ).to.equal(
+						`<ol><li ${ level1 }>Foo` +
+							`<ol><li ${ level2 }>Bar` +
+								`<ol><li ${ level3 }>Baz</li></ol></li></ol></li></ol>` );
+				} );
+
+				it( 'handles non-linear indentation', () => {
+					const html = `<p ${ level1 }>Foo</p><p ${ level3 }>Bar</p><p ${ level4 }>Baz</p>`;
+					const view = htmlDataProcessor.toView( html );
+
+					transformListItemLikeElementsIntoLists( view, '' );
+
+					expect( view.childCount ).to.equal( 1 );
+					expect( stringify( view ) ).to.equal(
+						`<ol><li ${ level1 }>Foo` +
+							'<ol><li>' +
+								`<ol><li ${ level3 }>Bar` +
+									`<ol><li ${ level4 }>Baz</li></ol></li></ol></li></ol></li></ol>` );
+				} );
+
+				it( 'handles indentation in both directions', () => {
+					const html = `<p ${ level1 }>Foo</p><p ${ level3 }>Bar</p><p ${ level4 }>Baz</p>` +
+						`<p ${ level2 }>Bax</p><p ${ level1 }>123</p>`;
+					const view = htmlDataProcessor.toView( html );
+
+					transformListItemLikeElementsIntoLists( view, '' );
+
+					expect( view.childCount ).to.equal( 1 );
+					expect( stringify( view ) ).to.equal(
+						`<ol><li ${ level1 }>Foo` +
+							'<ol><li>' +
+								`<ol><li ${ level3 }>Bar` +
+									`<ol><li ${ level4 }>Baz</li></ol>` +
+								'</li></ol>' +
+							`</li><li ${ level2 }>Bax</li></ol>` +
+						`</li><li ${ level1 }>123</li></ol>` );
+				} );
+
+				it( 'handles different list styles #1', () => {
+					const html = `<p ${ level1 }>Foo</p><p ${ level2 }>Bar</p><p ${ level3 }>Baz</p>`;
+					const view = htmlDataProcessor.toView( html );
+
+					transformListItemLikeElementsIntoLists( view, '@list l0:level1 { mso-level-number-format: bullet; }' +
+						'@list l0:level3 { mso-level-number-format: bullet; }' );
+
+					expect( view.childCount ).to.equal( 1 );
+					expect( stringify( view ) ).to.equal(
+						`<ul><li ${ level1 }>Foo` +
+							`<ol><li ${ level2 }>Bar` +
+								`<ul><li ${ level3 }>Baz</li></ul></li></ol></li></ul>` );
+				} );
+
+				it( 'handles different list styles #2', () => {
+					const html = `<p ${ level1 }>Foo</p><p ${ level3 }>Bar</p><p ${ level2 }>Baz</p>`;
+					const view = htmlDataProcessor.toView( html );
+
+					transformListItemLikeElementsIntoLists( view, '@list l0:level1 { mso-level-number-format: bullet; }' +
+						'@list l0:level2 { mso-level-number-format: bullet; }' );
+
+					expect( view.childCount ).to.equal( 1 );
+					expect( stringify( view ) ).to.equal(
+						`<ul><li ${ level1 }>Foo` +
+							'<ul><li>' +
+								`<ol><li ${ level3 }>Bar</li></ol>` +
+							`</li><li ${ level2 }>Baz</li></ul>` +
+						'</li></ul>' );
+				} );
+
+				it( 'handles indentation in the first list element', () => {
+					const html = `<p ${ level2 }>Foo</p><p ${ level1 }>Bar</p><p ${ level2 }>Baz</p>`;
+					const view = htmlDataProcessor.toView( html );
+
+					transformListItemLikeElementsIntoLists( view, '' );
+
+					expect( view.childCount ).to.equal( 1 );
+					expect( stringify( view ) ).to.equal(
+						'<ol><li>' +
+							`<ol><li ${ level2 }>Foo</li></ol>` +
+						`</li><li ${ level1 }>Bar` +
+							`<ol><li ${ level2 }>Baz</li></ol></li></ol>` );
+				} );
+			} );
 		} );
 	} );
 
