b7572c0c26a83184c51983af7b528dcdf2f4e131
Kamil Piechaczek <pomek@pomek.pl>
2019-02-21 09:05:01 +0100

    Docs: Added missing CS config in the editor placeholder example. Closes #1561.

--- docs/_snippets/features/placeholder.js
+++ docs/_snippets/features/placeholder.js
@@ -6,9 +6,11 @@
 /* globals console, window, document */
 
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic/src/ckeditor';
+import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud-services-config';
 
 ClassicEditor
 	.create( document.querySelector( '#snippet-placeholder' ), {
+		cloudServices: CS_CONFIG,
 		placeholder: 'Type the content here!'
 	} )
 	.then( editor => {
