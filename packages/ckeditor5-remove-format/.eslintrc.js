c3c3d528a64ce1bb363f1dc97b8958694d27a4bc
Marek Lewandowski <m.lewandowski@cksource.com>
2019-03-21 10:40:27 +0100

    Internal: Initialized the remove format feature.

---
+++ packages/ckeditor5-remove-format/.eslintrc.js
@@ -0,0 +1,12 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* eslint-env node */
+
+'use strict';
+
+module.exports = {
+	extends: 'ckeditor5'
+};
