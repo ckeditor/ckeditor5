7869e4db8030dc1a19e1e9943d3b5c41506eb380
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-03-26 16:02:49 +0100

    Added the ColorGrid and ColorTile components with their tests and styles.

---
+++ packages/ckeditor5-ui/tests/colorgrid/colortile.js
@@ -0,0 +1,30 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import ColorTile from '../../src/colorgrid/colortile';
+import ButtonView from '../../src/button/buttonview';
+
+describe( 'ColorTile', () => {
+	it( 'inherits from ButtonView', () => {
+		expect( new ColorTile() ).to.be.instanceOf( ButtonView );
+	} );
+
+	it( 'has proper attributes and classes', () => {
+		const colorTile = new ColorTile();
+		colorTile.render();
+
+		expect( colorTile.color ).to.be.undefined;
+		expect( colorTile.hasBorder ).to.be.undefined;
+
+		colorTile.set( 'color', 'green' );
+		expect( colorTile.color ).to.equal( 'green' );
+		expect( colorTile.element.style.backgroundColor ).to.equal( 'green' );
+		expect( colorTile.element.classList.contains( 'ck-color-grid__tile' ) ).to.be.true;
+		expect( colorTile.element.classList.contains( 'ck-color-table__color-tile_bordered' ) ).to.be.false;
+
+		colorTile.set( 'hasBorder', true );
+		expect( colorTile.element.classList.contains( 'ck-color-table__color-tile_bordered' ) ).to.be.true;
+	} );
+} );
