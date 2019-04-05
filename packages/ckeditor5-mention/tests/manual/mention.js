4cd1a98a1d33560a01fb24f20804b0e657de26ec
Maciej Gołaszewski <jodator@jodator.net>
2019-04-05 11:13:26 +0200

    Add required marker configuration option to the feeds in manual tests.

--- packages/ckeditor5-mention/tests/manual/mention.js
+++ packages/ckeditor5-mention/tests/manual/mention.js
@@ -27,13 +27,14 @@ ClassicEditor
 		mention: {
 			feeds: [
 				{
-					feed: [ 'Barney', 'Lily', 'Marshall', 'Robin', 'Ted' ]
+					marker: '@',
+					feed: [ '@Barney', '@Lily', '@Marshall', '@Robin', '@Ted' ]
 				},
 				{
 					marker: '#',
 					feed: [
-						'a01', 'a02', 'a03', 'a04', 'a05', 'a06', 'a07', 'a08', 'a09', 'a10',
-						'a11', 'a12', 'a13', 'a14', 'a15', 'a16', 'a17', 'a18', 'a19', 'a20'
+						'#a01', '#a02', '#a03', '#a04', '#a05', '#a06', '#a07', '#a08', '#a09', '#a10',
+						'#a11', '#a12', '#a13', '#a14', '#a15', '#a16', '#a17', '#a18', '#a19', '#a20'
 					]
 				}
 			]
