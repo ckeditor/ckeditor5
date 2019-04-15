4e625ed0a75812eccf6eef3028cf5287eded4e60
Kamil Piechaczek <pomek@pomek.pl>
2019-04-15 12:22:57 +0200

    Internal: Updated the license header. See ckeditor/ckeditor5#1557. [skip ci]

--- packages/ckeditor5-cloud-services/tests/_utils/tokenmock.js
+++ packages/ckeditor5-cloud-services/tests/_utils/tokenmock.js
@@ -1,6 +1,6 @@
 /**
  * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 import Token from '@ckeditor/ckeditor-cloud-services-core/src/token/token';
