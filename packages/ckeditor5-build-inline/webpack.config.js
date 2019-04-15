6e0fedb52d6409b8c09e5c4f6ec32e26dd0245bd
Kamil Piechaczek <pomek@pomek.pl>
2019-04-15 12:22:53 +0200

    Internal: Updated the license header. See ckeditor/ckeditor5#1557. [skip ci]

--- packages/ckeditor5-build-inline/webpack.config.js
+++ packages/ckeditor5-build-inline/webpack.config.js
@@ -1,6 +1,6 @@
 /**
  * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 'use strict';
