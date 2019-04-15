1ff513d5972a644b0e8cef38ee15332449830e43
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-04-15 16:42:05 +0200

    Docs [WIP]: Extended the mention demo in the feature guide.

--- packages/ckeditor5-mention/docs/_snippets/features/build-mention-source.js
+++ packages/ckeditor5-mention/docs/_snippets/features/build-mention-source.js
@@ -6,8 +6,10 @@
 /* globals window */
 
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic/src/ckeditor';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import Mention from '@ckeditor/ckeditor5-mention/src/mention';
 
-ClassicEditor.builtinPlugins.push( Mention );
+ClassicEditor.builtinPlugins.push( Underline, Strikethrough, Mention );
 
 window.ClassicEditor = ClassicEditor;
