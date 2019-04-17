08539cb07ebf80eba1bfe71462fa266675964a9f
Mateusz Samsel <m.samsel@cksource.com>
2019-04-17 17:35:03 +0200

    Update manual tests to use manual link decorators.

--- packages/ckeditor5-link/tests/manual/linkdecorator.js
+++ packages/ckeditor5-link/tests/manual/linkdecorator.js
@@ -12,14 +12,43 @@ import Link from '../../src/link';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Undo from '@ckeditor/ckeditor5-undo/src/undo';
 import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard';
+import { getData as getModelData } from '@ckeditor/ckeditor5-engine/src/dev-utils/model';
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
 		plugins: [ Link, Typing, Paragraph, Clipboard, Undo, Enter ],
-		toolbar: [ 'link', 'undo', 'redo' ]
+		toolbar: [ 'link', 'undo', 'redo' ],
+		link: {
+			decorators: [
+				{
+					mode: 'manual',
+					label: 'Open in new window',
+					attributes: {
+						target: '_blank',
+						rel: 'noopener noreferrer'
+					}
+				},
+				{
+					mode: 'manual',
+					label: 'downloadable',
+					attributes: {
+						download: 'download'
+					}
+				},
+				{
+					mode: 'manual',
+					label: 'gallery',
+					attributes: {
+						class: 'gallery'
+					}
+				}
+			]
+		}
 	} )
 	.then( editor => {
+		window.getModelData = getModelData;
 		window.editor = editor;
+		window.model = editor.model;
 	} )
 	.catch( err => {
 		console.error( err.stack );
