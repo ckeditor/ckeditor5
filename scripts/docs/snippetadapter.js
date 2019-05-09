c2127f38c94c0c93af96025c0dd60bd000e24b72
Kamil Piechaczek <pomek@users.noreply.github.com>
2019-05-09 11:10:16 +0200

    An empty snippet element will not be added to DOM.

--- scripts/docs/snippetadapter.js
+++ scripts/docs/snippetadapter.js
@@ -163,14 +163,10 @@ module.exports = function snippetAdapter( snippets, options, umbertoHelpers ) {
 
 					let snippetHTML = fs.readFileSync( snippetData.snippetSources.html ).toString();
 
-					const elementClasses = [ 'live-snippet' ];
-
-					if ( !snippetHTML.trim() ) {
-						elementClasses.push( 'live-snippet--empty' );
+					if ( snippetHTML.trim() ) {
+						snippetHTML = snippetHTML.replace( /%BASE_PATH%/g, snippetData.basePath );
+						snippetHTML = `<div class="live-snippet">${ snippetHTML }</div>`;
 					}
-
-					snippetHTML = snippetHTML.replace( /%BASE_PATH%/g, snippetData.basePath );
-					snippetHTML = `<div class="${ elementClasses.join( ' ' ) }">${ snippetHTML }</div>`;
 
 					content = content.replace( getSnippetPlaceholder( snippetData.snippetName ), snippetHTML );
 
