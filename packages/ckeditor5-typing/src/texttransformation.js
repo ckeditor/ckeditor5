5f8d375e4dad31e5e1a530942e69402713b9f3ec
Maciej Gołaszewski <jodator@jodator.net>
2019-05-14 17:44:55 +0200

    Add proof of concept text transformations.

--- packages/ckeditor5-typing/src/texttransformation.js
+++ packages/ckeditor5-typing/src/texttransformation.js
@@ -8,6 +8,7 @@
  */
 
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import TextWatcher from '@ckeditor/ckeditor5-mention/src/textwatcher';
 
 /**
  * The text transformation plugin.
@@ -22,5 +23,57 @@ export default class TextTransformation extends Plugin {
 	 */
 	static get pluginName() {
 		return 'TextTransformation';
+	}
+
+	init() {
+		const editor = this.editor;
+		const model = editor.model;
+
+		const transformations = [
+			// TODO: not nice definition - needs RegExp escaping.
+			{ in: '\\(c\\)$', out: '©' },
+			{ in: '\\(tm\\)$', out: '™' },
+			{ in: '\\.\\.\\.$', out: '…' },
+			{ in: '1/2', out: '½' },
+			{ in: '<=', out: '≤' },
+			// TODO: not nice - needs special order.
+			{ in: ' --- $', out: ' — ' },
+			// TODO: not nice - needs spaces.
+			{ in: ' -- $', out: ' – ' },
+			// TODO: add language rules
+			// TODO: add secondary support
+			{ in: '"([a-zA-Z -]+)"$', out: '„$1”' }
+		];
+
+		for ( const transformation of transformations ) {
+			const regExp = new RegExp( transformation.in );
+
+			// setup text watcher
+			const watcher = new TextWatcher( editor, text => regExp.test( text ), text => {
+				const match = text.match( regExp );
+
+				return {
+					text: match[ 0 ],
+					groups: match
+				};
+			} );
+
+			watcher.on( 'matched', ( evt, data ) => {
+				const selection = editor.model.document.selection;
+				const focus = selection.focus;
+				const message = data.matched.text;
+				const textToReplaceLength = message.length;
+
+				const textToInsert = message.replace( regExp, transformation.out );
+
+				// TODO: use model.insertContent()
+				model.enqueueChange( model.createBatch(), writer => {
+					const replaceRange = writer.createRange( focus.getShiftedBy( -textToReplaceLength ), focus );
+
+					writer.remove( replaceRange );
+					writer.insertText( textToInsert, selection.getAttributes(), selection.focus );
+				} );
+			} );
+		}
 	}
 }
