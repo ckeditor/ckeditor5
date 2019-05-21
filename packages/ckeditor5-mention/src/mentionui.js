5ae3c7e8972402fde1f45653b3f208b01399b006
Maciej Gołaszewski <jodator@jodator.net>
2019-05-21 10:39:06 +0200

    Hide panel on matched text inside other mention when UI is still waiting for a feed.

--- packages/ckeditor5-mention/src/mentionui.js
+++ packages/ckeditor5-mention/src/mentionui.js
@@ -296,6 +296,8 @@ export default class MentionUI extends Plugin {
 			const nodeBefore = focus.nodeBefore;
 
 			if ( hasMention || nodeBefore && nodeBefore.is( 'text' ) && nodeBefore.hasAttribute( 'mention' ) ) {
+				this._hideUIAndRemoveMarker();
+
 				return;
 			}
 
