79c34bad81be3257cb295ebac19d76417a517472
Mateusz Samsel <m.samsel@cksource.com>
2019-06-07 16:39:26 +0200

    Remove translation of a label.

--- packages/ckeditor5-link/src/ui/linkformview.js
+++ packages/ckeditor5-link/src/ui/linkformview.js
@@ -250,14 +250,13 @@ export default class LinkFormView extends View {
 	 */
 	_createCustomAttributesView() {
 		const switches = this.createCollection();
-		const t = this.locale.t;
 
 		switches.bindTo( this.customAttributes ).using( item => {
 			const switchButton = new SwitchButtonView( this.locale );
 
 			switchButton.set( {
 				name: item.id,
-				label: t( item.label ),
+				label: item.label,
 				withText: true
 			} );
 
