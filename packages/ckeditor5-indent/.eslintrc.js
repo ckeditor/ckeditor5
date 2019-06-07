6aa7bf3cf7c5c45dfee4cda5d5ebcb308404ab7d
Maciej Gołaszewski <jodator@jodator.net>
2019-06-07 12:05:01 +0200

    Initial commit.

---
+++ packages/ckeditor5-indent/.eslintrc.js
@@ -0,0 +1,12 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* eslint-env node */
+
+'use strict';
+
+module.exports = {
+	extends: 'ckeditor5'
+};
