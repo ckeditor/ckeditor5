150ce09a2e1b0e3f4d7c804e1225e69f07df03fb
Kamil Piechaczek <pomek@users.noreply.github.com>
2019-06-12 14:34:27 +0200

    Added docs page, simplified demo snippet.

--- packages/ckeditor5-upload/docs/_snippets/features/base64-upload.js
+++ packages/ckeditor5-upload/docs/_snippets/features/base64-upload.js
@@ -5,48 +5,16 @@
 
 /* globals console, window, document */
 
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic/src/ckeditor';
 import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/base64uploadadapter';
+
+ClassicEditor.builtinPlugins.push( Base64UploadAdapter );
 
 ClassicEditor
 	.create( document.querySelector( '#snippet-image-base64-upload' ), {
-		plugins: [
-			Essentials,
-			UploadAdapter,
-			Base64UploadAdapter,
-			Bold,
-			Italic,
-			Heading,
-			Image,
-			ImageCaption,
-			ImageStyle,
-			ImageToolbar,
-			ImageUpload,
-			Paragraph,
-			PasteFromOffice
-		],
 		toolbar: {
-			items: [
-				'heading', '|', 'bold', 'italic', 'imageUpload', 'undo', 'redo'
-			],
 			viewportTopOffset: window.getViewportTopOffsetConfig()
-		},
-		image: {
-			toolbar: [ 'imageTextAlternative', '|', 'imageStyle:alignLeft', 'imageStyle:full', 'imageStyle:alignRight' ],
-			styles: [ 'full', 'alignLeft', 'alignRight' ]
-		},
+		}
 	} )
 	.then( editor => {
 		window.editor = editor;
---
+++ packages/ckeditor5-upload/docs/features/base64-upload-adapter.md
@@ -0,0 +1,13 @@
+---
+category: features-image-upload
+menu-title: Base64 Upload Adapter
+order: 40
+---
+
+# Base64 Upload Adapter
+
+{@link features/base64-upload-adapter Base64 Upload Adapter}
+
+## Demo
+
+{@snippet features/base64-upload}
