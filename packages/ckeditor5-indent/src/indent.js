c4a2e2ea7a69bf57a80bcd791a58d7d8559e9753
Maciej Gołaszewski <jodator@jodator.net>
2019-06-26 15:53:13 +0200

    Move Indent* classes from core to intend plugin.

---
+++ packages/ckeditor5-indent/src/indent.js
@@ -0,0 +1,51 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module core/indent/indent
+ */
+
+import Plugin from '../plugin';
+
+import IndentEditing from './indentediting';
+import IndentUI from './indentui';
+
+/**
+ * The indent feature.
+ *
+ * This plugin acts as a single entry point plugin for other features that implement indenting of elements like lists or paragraphs.
+ *
+ * The compatible features are:
+ 
+ * - the {@link module:list/list~List} or {@link module:list/listediting~ListEditing} feature for list indentation
+ * * the {@link module:list/list~List} or {@link module:list/listediting~ListEditing} feature for list indentation
+ *
+ * This is a "glue" plugin which loads the following plugins:
+ *
+ * * The {@link module:core/indent/indentediting~IndentEditing indent editing feature} and
+ * * The {@link module:core/indent/indentui~IndentUI indent UI feature}.
+ *
+ * The dependent plugins register the `'indent'` and `'outdent'` commands and it introduce the `'indent'` and `'outdent'` buttons
+ * which allow to increase or decrease text indentation of supported elements.
+ *
+ * **Note**: In order the commands and buttons to work at least one of compatible features is required.
+ *
+ * @extends module:core/plugin~Plugin
+ */
+export default class Indent extends Plugin {
+	/**
+	 * @inheritDoc
+	 */
+	static get pluginName() {
+		return 'Indent';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	static get requires() {
+		return [ IndentEditing, IndentUI ];
+	}
+}
---
+++ packages/ckeditor5-indent/src/indentui.js
@@ -0,0 +1,69 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module core/indent/indentui
+ */
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+
+import Plugin from '../plugin';
+
+/**
+ * The indent feature.
+ *
+ * This plugin registers the `'indent'` and `'outdent'` buttons.
+ *
+ * **Note**: In order the commands to work at least one of compatible features is required. Read more in
+ * {@link module:core/indent/indent~Indent indent feature} api docs.
+ *
+ * @extends module:core/plugin~Plugin
+ */
+export default class IndentUI extends Plugin {
+	/**
+	 * @inheritDoc
+	 */
+	static get pluginName() {
+		return 'IndentUI';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	init() {
+		const editor = this.editor;
+		const t = editor.t;
+
+		this._defineButton( 'indent', t( 'Increase indent' ) );
+		this._defineButton( 'outdent', t( 'Decrease indent' ) );
+	}
+
+	/**
+	 * Defines an UI button.
+	 *
+	 * @param {String} commandName
+	 * @param {String} label
+	 * @private
+	 */
+	_defineButton( commandName, label ) {
+		const editor = this.editor;
+
+		editor.ui.componentFactory.add( commandName, locale => {
+			const command = editor.commands.get( commandName );
+			const view = new ButtonView( locale );
+
+			view.set( {
+				label,
+				withText: true,
+				tooltip: true
+			} );
+
+			view.bind( 'isOn', 'isEnabled' ).to( command, 'value', 'isEnabled' );
+
+			this.listenTo( view, 'execute', () => editor.execute( commandName ) );
+
+			return view;
+		} );
+	}
+}
