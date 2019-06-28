4558b04c00c3209f790101f25a176d41f8dfa2f6
Szymon Cofalik <s.cofalik@cksource.com>
2019-06-28 12:54:19 +0200

    Feature: Added `view.Document#event:beforeinput`.

--- packages/ckeditor5-engine/src/view/view.js
+++ packages/ckeditor5-engine/src/view/view.js
@@ -21,6 +21,7 @@ import FakeSelectionObserver from './observer/fakeselectionobserver';
 import SelectionObserver from './observer/selectionobserver';
 import FocusObserver from './observer/focusobserver';
 import CompositionObserver from './observer/compositionobserver';
+import InputObserver from './observer/inputobserver';
 
 import ObservableMixin from '@ckeditor/ckeditor5-utils/src/observablemixin';
 import log from '@ckeditor/ckeditor5-utils/src/log';
@@ -172,6 +173,7 @@ export default class View {
 		this.addObserver( KeyObserver );
 		this.addObserver( FakeSelectionObserver );
 		this.addObserver( CompositionObserver );
+		this.addObserver( InputObserver );
 
 		// Inject quirks handlers.
 		injectQuirksHandling( this );
