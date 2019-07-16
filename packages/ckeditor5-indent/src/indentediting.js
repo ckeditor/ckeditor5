8856f0d1649cab7113059aa87c8e248692989bc9
Marek Lewandowski <mlewand@users.noreply.github.com>
2019-07-16 14:59:39 +0200

    Internal: accepted language corrections.
    Co-Authored-By: Anna Tomanek <a.tomanek@cksource.com>

--- packages/ckeditor5-indent/src/indentediting.js
+++ packages/ckeditor5-indent/src/indentediting.js
@@ -15,7 +15,7 @@ import MultiCommand from '@ckeditor/ckeditor5-core/src/multicommand';
  *
  * This plugin registers the `'indent'` and `'outdent'` commands.
  *
- * **Note**: In order the commands to work at least one of compatible features is required. Read more in
+ * **Note**: In order for the commands to work, at least one of the compatible features is required. Read more in:
  * {@link module:indent/indent~Indent indent feature} API docs.
  *
  * @extends module:core/plugin~Plugin
