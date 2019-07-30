56ce65429c8258da20cd452a04d5180dbc9e6132
Mateusz Samsel <m.samsel@cksource.com>
2019-07-30 11:53:37 +0200

    Add unit test for mixed lists.

--- packages/ckeditor5-paste-from-office/tests/_data/paste-from-google-docs/lists/index.js
+++ packages/ckeditor5-paste-from-office/tests/_data/paste-from-google-docs/lists/index.js
@@ -6,16 +6,22 @@
 import nestedOrderedList from './nested-ordered-lists/input.html';
 import nestedOrderedListNormalized from './nested-ordered-lists/normalized.html';
 import nestedOrderedListModel from './nested-ordered-lists/model.html';
+import mixedList from './mixed-list/input.html';
+import mixedListNormalized from './mixed-list/normalized.html';
+import mixedListModel from './mixed-list/model.html';
 
 export const fixtures = {
 	input: {
-		nestedOrderedList
+		nestedOrderedList,
+		mixedList
 	},
 	normalized: {
-		nestedOrderedList: nestedOrderedListNormalized
+		nestedOrderedList: nestedOrderedListNormalized,
+		mixedList: mixedListNormalized
 	},
 	model: {
-		nestedOrderedList: nestedOrderedListModel
+		nestedOrderedList: nestedOrderedListModel,
+		mixedList: mixedListModel
 	}
 };
 
