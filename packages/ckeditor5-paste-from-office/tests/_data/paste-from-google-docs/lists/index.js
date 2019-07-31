35a8b5716f7aa441d9d84711a28cae5e7bf1b61d
Mateusz Samsel <m.samsel@cksource.com>
2019-07-31 15:24:20 +0200

    Add new automatic test for repeatedly nested lists.

--- packages/ckeditor5-paste-from-office/tests/_data/paste-from-google-docs/lists/index.js
+++ packages/ckeditor5-paste-from-office/tests/_data/paste-from-google-docs/lists/index.js
@@ -6,22 +6,30 @@
 import nestedOrderedList from './nested-ordered-lists/input.html';
 import nestedOrderedListNormalized from './nested-ordered-lists/normalized.html';
 import nestedOrderedListModel from './nested-ordered-lists/model.html';
+
 import mixedList from './mixed-list/input.html';
 import mixedListNormalized from './mixed-list/normalized.html';
 import mixedListModel from './mixed-list/model.html';
 
+import repeatedlyNestedList from './repeatedly-nested-list/input.html';
+import repeatedlyNestedListNormalized from './repeatedly-nested-list/normalized.html';
+import repeatedlyNestedListModel from './repeatedly-nested-list/model.html';
+
 export const fixtures = {
 	input: {
 		nestedOrderedList,
-		mixedList
+		mixedList,
+		repeatedlyNestedList
 	},
 	normalized: {
 		nestedOrderedList: nestedOrderedListNormalized,
-		mixedList: mixedListNormalized
+		mixedList: mixedListNormalized,
+		repeatedlyNestedList: repeatedlyNestedListNormalized
 	},
 	model: {
 		nestedOrderedList: nestedOrderedListModel,
-		mixedList: mixedListModel
+		mixedList: mixedListModel,
+		repeatedlyNestedList: repeatedlyNestedListModel
 	}
 };
 
