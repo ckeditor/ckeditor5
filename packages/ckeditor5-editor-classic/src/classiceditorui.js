fc860665769b5fd1304816f9f77e1263c567d6d9
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-08-06 17:28:54 +0200

    Used the new toolbar grouping API in the UI.

--- packages/ckeditor5-editor-classic/src/classiceditorui.js
+++ packages/ckeditor5-editor-classic/src/classiceditorui.js
@@ -148,17 +148,8 @@ export default class ClassicEditorUI extends EditorUI {
 			view.stickyPanel.viewportTopOffset = this._toolbarConfig.viewportTopOffset;
 		}
 
+		view.toolbar.isGrouping = true;
 		view.toolbar.fillFromConfig( this._toolbarConfig.items, this.componentFactory );
-
-		editor.on( 'ready', () => {
-			// This feature should not be enabled earlier because for the algorithm to work,
-			// the toolbar must be visible in DOM (i.e. only after #uiReady).
-			// But that's not all: the editor content must also be ready because loading it into
-			// editable can make the vertical website scrollbar to show up (it is usually styled
-			// differently in the website and in the editor; it can "grow" a lot). And that would
-			// reduce the horizontal space available for toolbar items. In other words, later is better.
-			view.toolbar.enableWrappedItemsGroupping();
-		} );
 
 		enableToolbarKeyboardFocus( {
 			origin: editingView,
