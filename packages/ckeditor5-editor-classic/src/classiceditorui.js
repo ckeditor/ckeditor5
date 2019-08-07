2d96e9e3e79343a3ef5f85cff7a0fbb44d26429f
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-08-07 13:20:01 +0200

    Code refactoring.

--- packages/ckeditor5-editor-classic/src/classiceditorui.js
+++ packages/ckeditor5-editor-classic/src/classiceditorui.js
@@ -148,7 +148,7 @@ export default class ClassicEditorUI extends EditorUI {
 			view.stickyPanel.viewportTopOffset = this._toolbarConfig.viewportTopOffset;
 		}
 
-		view.toolbar.isGrouping = true;
+		view.toolbar.shouldGroupWhenFull = true;
 		view.toolbar.fillFromConfig( this._toolbarConfig.items, this.componentFactory );
 
 		enableToolbarKeyboardFocus( {
