c3c5284e246f2e65caf8ff55046c9f4c32631a00
Marek Lewandowski <m.lewandowski@cksource.com>
2019-08-14 14:42:32 +0200

    Internal: update-stable-branches.sh script with the master version.

--- scripts/update-stable-branches.sh
+++ scripts/update-stable-branches.sh
@@ -13,25 +13,13 @@ then
 	# Update the `stable` branch in the `ckeditor5` repository.
 	git checkout stable && git merge master && git checkout master
 
-	# Add `stable` branches in all repos which don't have them yet.
-	mrgit exec 'git checkout -b stable 2> /dev/null && git push origin stable && git checkout master'
+	# Add `#stable` branches in all repos which don't have them yet.
+	mgit exec 'git checkout -b stable 2> /dev/null && git push origin stable && git checkout master'
 
-	# Update all `stable` branches in all packages.
-	mrgit exec 'git checkout stable && git pull origin stable && git merge master && git checkout master'
+	# Update all `#stable` branches in all packages.
+	mgit exec 'git checkout stable && git pull origin stable && git merge master && git checkout master'
 
-	# Make sure that `mrgit.json` for `stable` and `master` branches is correct.
-	# `stable` branch.
-	git checkout stable && \
-	node ./scripts/release/update-mrgit-branches stable && \
-	git commit -a -m "Internal: Use stable branches. [skip ci]"
-
-	# `master` branch.
-	git checkout master && \
-	git merge stable && \
-	node ./scripts/release/update-mrgit-branches master && \
-	git commit -a -m "Internal: Use master branches. [skip ci]"
-
-	# Push the `stable` branches.
+	# Push the `#stable` branches.
 	git push origin stable master && \
 	mrgit exec 'git push origin stable'
 
