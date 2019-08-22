57bde0801873dc43a320e2c7b2f8cdbcc3ba440c
Maciej Gołaszewski <jodator@jodator.net>
2019-08-22 15:12:11 +0200

    Revert table manual test.

--- packages/ckeditor5-table/tests/manual/tableblockcontent.js
+++ packages/ckeditor5-table/tests/manual/tableblockcontent.js
@@ -8,16 +8,13 @@
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud-services-config';
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
-		cloudServices: CS_CONFIG,
-		plugins: [ ArticlePluginSet, Alignment, EasyImage ],
+		plugins: [ ArticlePluginSet, Alignment ],
 		toolbar: [
 			'heading', '|', 'insertTable', '|', 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote',
-			'alignment', '|', 'imageUpload', 'undo', 'redo'
+			'alignment', '|', 'undo', 'redo'
 		],
 		image: {
 			toolbar: [ 'imageStyle:full', 'imageStyle:side' ]
