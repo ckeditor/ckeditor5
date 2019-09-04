119af4a042f2346d8a2f6770be2e50417fc0f62a
Kamil Piechaczek <pomek@users.noreply.github.com>
2019-09-04 09:17:18 +0200

    Docs: Adding ckeditor* packages to the script.

--- scripts/docs/buildapi.js
+++ scripts/docs/buildapi.js
@@ -16,8 +16,8 @@ module.exports = function buildApiDocs() {
 		.build( {
 			readmePath: path.join( process.cwd(), 'README.md' ),
 			sourceFiles: [
-				process.cwd() + '/packages/ckeditor5-*/src/**/*.@(js|jsdoc)',
-				'!' + process.cwd() + '/packages/ckeditor5-*/src/lib/**/*.js',
+				process.cwd() + '/packages/@(ckeditor|ckeditor5)-*/src/**/*.@(js|jsdoc)',
+				'!' + process.cwd() + '/packages/@(ckeditor|ckeditor5)-*/src/lib/**/*.js',
 				'!' + process.cwd() + '/packages/ckeditor5-build-*/src/**/*.js'
 			],
 			validateOnly: process.argv.includes( '--validate-only' )
