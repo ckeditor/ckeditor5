40d55422a71168142b80648fc3affa5a98198294
Kamil Piechaczek <pomek@users.noreply.github.com>
2019-09-05 16:51:25 +0200

    Improved "getRealImportPath()" script.

--- scripts/docs/getrealimportpath.js
+++ scripts/docs/getrealimportpath.js
@@ -6,5 +6,9 @@
 /* eslint-env node */
 
 module.exports = function getRealImportPath( modulePath ) {
+	if ( modulePath.includes( 'ckeditor-' ) ) {
+		return modulePath.replace( /^([^/]+)\//, '@ckeditor/$1/src/' );
+	}
+
 	return modulePath.replace( /^([^/]+)\//, '@ckeditor/ckeditor5-$1/src/' );
 };
