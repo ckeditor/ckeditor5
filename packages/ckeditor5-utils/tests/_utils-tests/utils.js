caa1d91e1da788b323c8822dcf93b58b5b4d4a03
Maciej Gołaszewski <jodator@jodator.net>
2019-09-09 09:07:41 +0200

    Fix imports in test utils tests.

--- packages/ckeditor5-utils/tests/_utils-tests/utils.js
+++ packages/ckeditor5-utils/tests/_utils-tests/utils.js
@@ -3,7 +3,7 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-import { AssertionError } from 'chai';
+import AssertionError from 'assertion-error';
 import testUtils from '@ckeditor/ckeditor5-core/tests/_utils/utils';
 import ObservableMixin from '../../src/observablemixin';
 import EmitterMixin from '../../src/emittermixin';
