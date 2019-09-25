acb229e8083b3dc0f7c55c4ff60e8ea31a8f08bc
Maciej Gołaszewski <jodator@jodator.net>
2019-09-25 13:27:43 +0200

    Add manual tests for table styles.

---
+++ packages/ckeditor5-table/tests/manual/tablestyles.js
@@ -0,0 +1,33 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* globals console, window, document */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
+
+const sourceElement = document.querySelector( '#editor' );
+const clonedSource = sourceElement.cloneNode( true );
+
+document.querySelector( '#cloned-source' ).append( ...clonedSource.childNodes );
+
+ClassicEditor
+	.create( sourceElement, {
+		plugins: [ ArticlePluginSet ],
+		toolbar: [
+			'heading', '|', 'insertTable', '|', 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote', 'undo', 'redo'
+		],
+		table: {
+			contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ],
+			tableToolbar: [ 'bold', 'italic' ]
+		}
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
+
