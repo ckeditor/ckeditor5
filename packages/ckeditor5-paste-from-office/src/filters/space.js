8ffc1b8501476c10ef8e10b655c0be2e64377586
amrita-syn <amrita.yadav@synapsica.com>
2019-10-22 14:23:53 +0530

    Lint fix.

--- packages/ckeditor5-paste-from-office/src/filters/space.js
+++ packages/ckeditor5-paste-from-office/src/filters/space.js
@@ -41,10 +41,10 @@ export function normalizeSpacerunSpans( htmlDocument ) {
 	htmlDocument.querySelectorAll( 'span[style*=spacerun]' ).forEach( el => {
 		// Use `el.childNodes[ 0 ].data.length` instead of `el.innerText.length`. For `el.innerText.length` which
 		// contains spaces mixed with `&nbsp;` Edge browser returns incorrect length.
-		const innerTextLength = (el.childNodes &&
+		const innerTextLength = ( el.childNodes &&
 			el.childNodes[ 0 ] &&
 			el.childNodes[ 0 ].data &&
-			el.childNodes[ 0 ].data.length) || 0;
+			el.childNodes[ 0 ].data.length ) || 0;
 
 		el.innerHTML = Array( innerTextLength + 1 ).join( '\u00A0 ' ).substr( 0, innerTextLength );
 	} );
