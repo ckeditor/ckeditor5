95955b82610465912a630d29d4d7d36a7b0084e0
Oskar Wróbel <kontakt@oskarwrobel.pl>
2019-10-22 09:50:09 +0200

    Introduced very basic manual test.

---
+++ packages/ckeditor5-code-block/tests/manual/codeblock.js
@@ -0,0 +1,22 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* global document, console, window */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
+
+ClassicEditor
+	.create( document.querySelector( '#editor' ), {
+		plugins: [ CodeBlock, ArticlePluginSet ],
+		toolbar: [ 'heading', '|', 'bold', 'italic', 'blockQuote', 'codeBlock', 'undo', 'redo', 'insertTable' ]
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
