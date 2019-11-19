42d987cafcc89281f9102e2b35972ef5cd432133
Kamil Piechaczek <pomek@users.noreply.github.com>
2019-11-19 15:01:23 +0100

    Added some special characters.

--- packages/ckeditor5-special-characters/src/specialcharactersarrows.js
+++ packages/ckeditor5-special-characters/src/specialcharactersarrows.js
@@ -12,25 +12,24 @@ import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 export default class SpecialCharactersArrows extends Plugin {
 	init() {
 		this.editor.plugins.get( 'SpecialCharacters' ).addItems( 'Arrows', [
-			{
-				title: 'arrow left',
-				character: '←'
-			},
-			{
-				title: 'arrow right',
-				character: '→'
-			}
-		] );
-
-		this.editor.plugins.get( 'SpecialCharacters' ).addItems( 'Arrows', [
-			{
-				title: 'arrow up',
-				character: '↑'
-			},
-			{
-				title: 'arrow down',
-				character: '↓'
-			}
+			{ title: 'leftwards double arrow', character: '⇐' },
+			{ title: 'rightwards double arrow', character: '⇒' },
+			{ title: 'upwards double arrow', character: '⇑' },
+			{ title: 'downwards double arrow', character: '⇓' },
+			{ title: 'leftwards dashed arrow', character: '⇠' },
+			{ title: 'rightwards dashed arrow', character: '⇢' },
+			{ title: 'upwards dashed arrow', character: '⇡' },
+			{ title: 'downwards dashed arrow', character: '⇣' },
+			{ title: 'leftwards arrow to bar', character: '⇤' },
+			{ title: 'rightwards arrow to bar', character: '⇥' },
+			{ title: 'upwards arrow to bar', character: '⤒' },
+			{ title: 'downwards arrow to bar', character: '⤓' },
+			{ title: 'up down arrow with base', character: '↨' },
+			{ title: 'back with leftwards arrow above', character: '🔙' },
+			{ title: 'end with leftwards arrow above', character: '🔚' },
+			{ title: 'on with exclamation mark with left right arrow above', character: '🔛' },
+			{ title: 'soon with rightwards arrow above', character: '🔜' },
+			{ title: 'top with upwards arrow above', character: '🔝' }
 		] );
 	}
 }
--- packages/ckeditor5-special-characters/src/specialcharactersmathematical.js
+++ packages/ckeditor5-special-characters/src/specialcharactersmathematical.js
@@ -12,25 +12,40 @@ import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 export default class SpecialCharactersMathematical extends Plugin {
 	init() {
 		this.editor.plugins.get( 'SpecialCharacters' ).addItems( 'Mathematical', [
-			{
-				title: 'precedes',
-				character: '≺'
-			},
-			{
-				title: 'succeeds',
-				character: '≻'
-			}
-		] );
-
-		this.editor.plugins.get( 'SpecialCharacters' ).addItems( 'Mathematical', [
-			{
-				title: 'precedes or equal to',
-				character: '≼'
-			},
-			{
-				title: 'succeeds or equal to',
-				character: '≽'
-			}
+			{ title: 'greek small letter alpha', character: 'α' },
+			{ title: 'greek small letter beta', character: 'β' },
+			{ title: 'greek small letter delta', character: 'δ' },
+			{ title: 'greek small letter epsilon', character: 'ε' },
+			{ title: 'greek small letter theta', character: 'θ' },
+			{ title: 'greek small letter lamda', character: 'λ' },
+			{ title: 'greek small letter mu', character: 'μ' },
+			{ title: 'greek small letter pi', character: 'π' },
+			{ title: 'greek small letter phi', character: 'φ' },
+			{ title: 'greek small letter psi', character: 'ψ' },
+			{ title: 'greek capital letter omega', character: 'Ω' },
+			{ title: 'precedes', character: '≺' },
+			{ title: 'succeeds', character: '≻' },
+			{ title: 'precedes or equal to', character: '≼' },
+			{ title: 'succeeds or equal to', character: '≽' },
+			{ title: 'double precedes', character: '⪻' },
+			{ title: 'double succeeds', character: '⪼' },
+			{ title: 'less-than', character: '<' },
+			{ title: 'greater-than', character: '>' },
+			{ title: 'less-than or equal to', character: '≤' },
+			{ title: 'greater-than or equal to', character: '≥' },
+			{ title: 'equals colon', character: '≕' },
+			{ title: 'double colon equal', character: '⩴' },
+			{ title: 'identical to', character: '≡' },
+			{ title: 'not identical to', character: '≢' },
+			{ title: 'almost equal to', character: '≈' },
+			{ title: 'not almost equal to', character: '≉' },
+			{ title: 'almost equal or equal to', character: '≊' },
+			{ title: 'triple tilde', character: '≋' },
+			{ title: 'true', character: '⊨' },
+			{ title: 'not true', character: '⊭' },
+			{ title: 'for all', character: '∀' },
+			{ title: 'complement', character: '∁' },
+			{ title: 'there exists', character: '∃' }
 		] );
 	}
 }
