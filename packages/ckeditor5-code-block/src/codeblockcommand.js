7a39a25e4f6fd707c7cfa76806fe53e075817281
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-11-19 15:24:04 +0100

    Update src/codeblockcommand.js
    Co-Authored-By: Maciej <jodator@jodator.net>

--- packages/ckeditor5-code-block/src/codeblockcommand.js
+++ packages/ckeditor5-code-block/src/codeblockcommand.js
@@ -46,7 +46,7 @@ export default class CodeBlockCommand extends Command {
 		const editor = this.editor;
 		const model = editor.model;
 		const selection = model.document.selection;
-		const firstLanguageInConfig = editor.config.get( 'codeBlock' ).languages[ 0 ];
+		const firstLanguageInConfig = editor.config.get( 'codeBlock.languages' ).shift();
 
 		const blocks = Array.from( selection.getSelectedBlocks() );
 		const value = ( options.forceValue === undefined ) ? !this.value : options.forceValue;
