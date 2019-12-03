715ebc9f1434586ced44597968a00bcc28ad8943
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-12-03 14:03:20 +0100

    Minor improvements to error messages used in manual tests and a sample. [skip ci]

--- packages/ckeditor5-build-balloon/tests/manual/ckeditor.js
+++ packages/ckeditor5-build-balloon/tests/manual/ckeditor.js
@@ -12,5 +12,5 @@ BalloonEditor.create( document.querySelector( '#editor' ) )
 		window.editor = editor;
 	} )
 	.catch( error => {
-		console.error( 'There was a problem with initializing the editor', error );
+		console.error( 'There was a problem initializing the editor.', error );
 	} );
