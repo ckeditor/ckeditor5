1fde736ffa6cabb4d55f797e20d5f5c830ec344f
Marek Lewandowski <m.lewandowski@cksource.com>
2020-01-27 10:57:33 +0100

    Tests: Added a manual performance for initializing multiple editors at once.

---
+++ tests/manual/performance/multipleeditors.js
@@ -0,0 +1,77 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* globals console, window, document */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
+import { loadPerformanceData } from '../../_utils/utils';
+
+const editorCount = 6;
+
+loadPerformanceData()
+	.then( fixtures => {
+		const editorsWrapper = document.getElementById( 'editors-wrapper' );
+		const buttons = document.querySelectorAll( '#test-controls button' );
+
+		for ( let i = 0; i < editorCount; i++ ) {
+			const editorElement = document.createElement( 'div' );
+			editorElement.setAttribute( 'id', `editor_${ i }` );
+			editorsWrapper.appendChild( editorElement );
+		}
+
+		for ( const button of buttons ) {
+			const fixtureName = button.getAttribute( 'data-file-name' );
+			const content = fixtures[ fixtureName ];
+
+			button.addEventListener( 'click', function() {
+				for ( let i = 0; i < editorCount; i++ ) {
+					const editorElement = document.querySelector( `#editor_${ i }` );
+					editorElement.innerHTML = content;
+
+					ClassicEditor
+						.create( editorElement, {
+							plugins: [ ArticlePluginSet ],
+							toolbar: [
+								'heading',
+								'|',
+								'bold',
+								'italic',
+								'link',
+								'bulletedList',
+								'numberedList',
+								'|',
+								'outdent',
+								'indent',
+								'|',
+								'blockQuote',
+								'insertTable',
+								'mediaEmbed',
+								'undo',
+								'redo'
+							],
+							image: {
+								toolbar: [ 'imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative' ]
+							},
+							table: {
+								contentToolbar: [
+									'tableColumn',
+									'tableRow',
+									'mergeTableCells'
+								]
+							}
+						} )
+						.then( editor => {
+							window.editor = editor;
+						} )
+						.catch( err => {
+							console.error( err.stack );
+						} );
+				}
+			} );
+
+			button.disabled = false;
+		}
+	} );
---
+++ tests/manual/performance/multipleeditors.md
@@ -0,0 +1,7 @@
+# Performance: multiple editors
+
+1. Begin performance recording in devtools.
+1. Use any button to initialize multiple editors with a given content.
+1. Stop performance recording.
+
+**Note:** You should execute this test only once. Refresh the page if you want to repeat it.
