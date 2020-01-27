eb4e958ee81058e80088a11f0de05d41e3283897
Maciej Gołaszewski <jodator@jodator.net>
2020-01-27 17:23:11 +0100

    Update license header.

--- packages/ckeditor5-table/tests/manual/tableselection.js
+++ packages/ckeditor5-table/tests/manual/tableselection.js
@@ -1,6 +1,6 @@
 /**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md.
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 /* globals console, window, document, global */
