bcfed402fcde0afac297784ae0f99f2e99031507
Szymon Cofalik <s.cofalik@cksource.com>
2020-01-27 15:57:57 +0100

    Tests: Rewritten tests for `CloudServices` using `Context`.

--- packages/ckeditor5-cloud-services/tests/cloudservices.js
+++ packages/ckeditor5-cloud-services/tests/cloudservices.js
@@ -6,6 +6,7 @@
 /* global document */
 
 import CloudServices from '../src/cloudservices';
+import Context from '@ckeditor/ckeditor5-core/src/context';
 import ClassicTestEditor from '@ckeditor/ckeditor5-core/tests/_utils/classictesteditor';
 import TokenMock from './_utils/tokenmock';
 
@@ -27,6 +28,26 @@ describe( 'CloudServices', () => {
 
 	describe( 'init()', () => {
 		it( 'should expose its properties based on config', () => {
+			return Context
+				.create( {
+					plugins: [ CloudServices ],
+					cloudServices: {
+						tokenUrl: 'http://token-endpoint',
+						additionalOption: 'some-value'
+					}
+				} )
+				.then( editor => {
+					const cloudServicesPlugin = editor.plugins.get( CloudServices );
+
+					expect( cloudServicesPlugin ).to.be.instanceOf( CloudServices );
+					expect( cloudServicesPlugin.tokenUrl ).to.equal( 'http://token-endpoint' );
+					expect( cloudServicesPlugin.additionalOption ).to.equal( 'some-value' );
+
+					return editor.destroy();
+				} );
+		} );
+
+		it( 'should work as an editor plugin', () => {
 			return ClassicTestEditor
 				.create( element, {
 					plugins: [ CloudServices ],
@@ -47,38 +68,27 @@ describe( 'CloudServices', () => {
 		} );
 
 		it( 'should be able to get by its plugin name', () => {
-			return ClassicTestEditor
-				.create( element, {
-					plugins: [ CloudServices ]
-				} )
-				.then( editor => {
-					const cloudServicesPlugin = editor.plugins.get( 'CloudServices' );
-					expect( cloudServicesPlugin ).to.be.instanceOf( CloudServices );
-				} );
+			return Context.create( { plugins: [ CloudServices ] } ).then( editor => {
+				const cloudServicesPlugin = editor.plugins.get( 'CloudServices' );
+				expect( cloudServicesPlugin ).to.be.instanceOf( CloudServices );
+			} );
 		} );
 
 		it( 'should not throw an error when no config is provided', () => {
-			return ClassicTestEditor
-				.create( element, {
-					plugins: [ CloudServices ]
-				} );
+			return Context.create( { plugins: [ CloudServices ] } );
 		} );
 
 		it( 'should not expose any default uploadUrl', () => {
-			return ClassicTestEditor
-				.create( element, {
-					plugins: [ CloudServices ]
-				} )
-				.then( editor => {
-					const cloudServicesPlugin = editor.plugins.get( CloudServices );
+			return Context.create( { plugins: [ CloudServices ] } ).then( editor => {
+				const cloudServicesPlugin = editor.plugins.get( CloudServices );
 
-					expect( cloudServicesPlugin.uploadUrl ).to.be.undefined;
-				} );
+				expect( cloudServicesPlugin.uploadUrl ).to.be.undefined;
+			} );
 		} );
 
 		it( 'should use provided uploadUrl', () => {
-			return ClassicTestEditor
-				.create( element, {
+			return Context
+				.create( {
 					plugins: [ CloudServices ],
 					cloudServices: {
 						uploadUrl: 'https://some-upload-url/'
@@ -94,8 +104,8 @@ describe( 'CloudServices', () => {
 		it( 'should provide token if tokenUrl is provided', () => {
 			CloudServices.Token.initialToken = 'initial-token';
 
-			return ClassicTestEditor
-				.create( element, {
+			return Context
+				.create( {
 					plugins: [ CloudServices ],
 					cloudServices: {
 						tokenUrl: 'http://token-endpoint',
@@ -113,16 +123,11 @@ describe( 'CloudServices', () => {
 		it( 'should not provide token if tokenUrl is not provided', () => {
 			CloudServices.Token.initialToken = 'initial-token';
 
-			return ClassicTestEditor
-				.create( element, {
-					plugins: [ CloudServices ],
-					cloudServices: {}
-				} )
-				.then( editor => {
-					const cloudServicesPlugin = editor.plugins.get( CloudServices );
+			return Context.create( { plugins: [ CloudServices ] } ).then( editor => {
+				const cloudServicesPlugin = editor.plugins.get( CloudServices );
 
-					expect( cloudServicesPlugin.token ).to.equal( null );
-				} );
+				expect( cloudServicesPlugin.token ).to.equal( null );
+			} );
 		} );
 	} );
 } );
