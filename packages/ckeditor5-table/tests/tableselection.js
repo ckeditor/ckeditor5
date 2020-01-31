b94dd98f4eec4b340f17d20a7ee47a3ed650ce82
Maciej Gołaszewski <jodator@jodator.net>
2020-01-31 17:07:53 +0100

    Rename test method to better describe its role.

--- packages/ckeditor5-table/tests/tableselection.js
+++ packages/ckeditor5-table/tests/tableselection.js
@@ -357,7 +357,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			] ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 0, 0 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 0, 0 );
 
 			view.document.fire( 'mousedown', domEvtDataStub );
 
@@ -380,7 +380,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			] ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 0, 0 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 0, 0 );
 			view.document.fire( 'mousedown', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -388,7 +388,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			] ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 0, 1 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 0, 1 );
 			view.document.fire( 'mousemove', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -408,7 +408,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			] ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 0, 0 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 0, 0 );
 			view.document.fire( 'mousedown', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -416,7 +416,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			] ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 1, 1 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 1, 1 );
 			view.document.fire( 'mousemove', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -436,7 +436,7 @@ describe( 'table selection', () => {
 				[ '10', '[]11' ]
 			] ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 1, 1 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 1, 1 );
 			view.document.fire( 'mousedown', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -444,7 +444,7 @@ describe( 'table selection', () => {
 				[ '10', '[]11' ]
 			] ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 0, 0 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 0, 0 );
 			view.document.fire( 'mousemove', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -465,10 +465,10 @@ describe( 'table selection', () => {
 				[ '20', '21', '22' ]
 			] ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 0, 0 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 0, 0 );
 			view.document.fire( 'mousedown', domEvtDataStub );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 2, 2 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 2, 2 );
 			view.document.fire( 'mousemove', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -495,7 +495,7 @@ describe( 'table selection', () => {
 				]
 			], { asWidget: true } ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 1, 1 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 1, 1 );
 			view.document.fire( 'mousemove', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -529,7 +529,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			] ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 0, 0 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 0, 0 );
 			view.document.fire( 'mousedown', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -537,7 +537,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			] ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 0, 1 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 0, 1 );
 			view.document.fire( 'mousemove', domEvtDataStub );
 
 			view.document.fire( 'mouseup', domEvtDataStub );
@@ -552,7 +552,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			], { asWidget: true } ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 1, 1 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 1, 1 );
 			view.document.fire( 'mousemove', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -567,7 +567,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			] ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 0, 0 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 0, 0 );
 			view.document.fire( 'mousedown', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -575,7 +575,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			] ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 0, 1 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 0, 1 );
 			view.document.fire( 'mousemove', domEvtDataStub );
 
 			view.document.fire( 'mouseleave', domEvtDataStub );
@@ -590,7 +590,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			], { asWidget: true } ) );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 1, 1 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 1, 1 );
 			view.document.fire( 'mousemove', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -605,7 +605,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			] ) + '<paragraph>foo</paragraph>' );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 0, 0 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 0, 0 );
 			view.document.fire( 'mousedown', domEvtDataStub );
 
 			assertEqualMarkup( getModelData( model ), modelTable( [
@@ -613,7 +613,7 @@ describe( 'table selection', () => {
 				[ '10', '11' ]
 			] ) + '<paragraph>foo</paragraph>' );
 
-			selectTableCell( domEvtDataStub, view, 0, 0, 0, 1 );
+			markTableCellAsSelectedInEvent( domEvtDataStub, view, 0, 0, 0, 1 );
 			view.document.fire( 'mousemove', domEvtDataStub );
 
 			domEvtDataStub.target = view.document.getRoot().getChild( 1 );
@@ -679,7 +679,7 @@ describe( 'table selection', () => {
 	}
 } );
 
-function selectTableCell( domEvtDataStub, view, tableIndex, sectionIndex, rowInSectionIndex, tableCellIndex ) {
+function markTableCellAsSelectedInEvent( domEvtDataStub, view, tableIndex, sectionIndex, rowInSectionIndex, tableCellIndex ) {
 	domEvtDataStub.target = view.document.getRoot()
 		.getChild( tableIndex )
 		.getChild( 1 ) // Table is second in widget
