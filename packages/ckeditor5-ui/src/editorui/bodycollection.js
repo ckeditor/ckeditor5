143938772f18812a6a77815a9a390de629ae4a5d
Anna Tomanek <a.tomanek@cksource.com>
2020-02-07 12:50:12 +0100

    Docs: Body collection API docs corrected. [skip ci]

--- packages/ckeditor5-ui/src/editorui/bodycollection.js
+++ packages/ckeditor5-ui/src/editorui/bodycollection.js
@@ -20,13 +20,13 @@ import createElement from '@ckeditor/ckeditor5-utils/src/dom/createelement';
  *
  * The body collection is available in the {@link module:ui/editorui/editoruiview~EditorUIView#body `editor.ui.view.body`} property.
  * Any plugin can add a {@link module:ui/view~View view} to this collection.
- * Those views will render in a container placed directly in the `<body>` element.
+ * These views will render in a container placed directly in the `<body>` element.
  * The editor will detach and destroy this collection when the editor will be {@link module:core/editor/editor~Editor#destroy destroyed}.
  *
  * If you need to control the life cycle of the body collection on your own, you can create your own instance of this class.
  *
  * A body collection will render itself automatically in the DOM body element as soon as you call {@link ~BodyCollection#attachToDom}.
- * If you create multiple body collections this class will create a special wrapper element in the DOM to limit the number of
+ * If you create multiple body collections, this class will create a special wrapper element in the DOM to limit the number of
  * elements created directly in the body and remove it when the last body collection will be
  * {@link ~BodyCollection#detachFromDom detached}.
  *
@@ -69,7 +69,7 @@ export default class BodyCollection extends ViewCollection {
 	}
 
 	/**
-	 * Detach the collection from the DOM structure. Use this method when you do not need to use the body collection
+	 * Detaches the collection from the DOM structure. Use this method when you do not need to use the body collection
 	 * anymore to clean-up the DOM structure.
 	 */
 	detachFromDom() {
