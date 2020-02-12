fc00327501d36d530a4a932210dfc79b727914cb
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-02-12 16:54:30 +0100

    Removed trailing commas in the manual test.

--- packages/ckeditor5-table/tests/manual/tableproperties.js
+++ packages/ckeditor5-table/tests/manual/tableproperties.js
@@ -27,8 +27,8 @@ ClassicEditor
 		],
 		table: {
 			contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties' ],
-			tableToolbar: [ 'bold', 'italic' ],
-		},
+			tableToolbar: [ 'bold', 'italic' ]
+		}
 	} )
 	.then( editor => {
 		window.editor = editor;
