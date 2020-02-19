f2d3b7d1c37ce1cb9338d1e230f8b7b42e30e048
Maciej Gołaszewski <jodator@jodator.net>
2020-02-19 15:42:04 +0100

    Add missing StylesProcessor instance.

--- packages/ckeditor5-editor-inline/src/inlineeditor.js
+++ packages/ckeditor5-editor-inline/src/inlineeditor.js
@@ -64,7 +64,7 @@ export default class InlineEditor extends Editor {
 	constructor( sourceElementOrData, config ) {
 		super( config );
 
-		this.data.processor = new HtmlDataProcessor();
+		this.data.processor = new HtmlDataProcessor( this.stylesProcessor );
 
 		this.model.document.createRoot();
 
