ff2b5c06e8e43fc3dfcaed4e23d9099f8ad1d440
Kamil Piechaczek <pomek@users.noreply.github.com>
2020-02-20 13:55:37 +0100

    Aligned to changes in Editor class.

--- packages/ckeditor5-watchdog/tests/editorwatchdog.js
+++ packages/ckeditor5-watchdog/tests/editorwatchdog.js
@@ -937,7 +937,7 @@ describe( 'EditorWatchdog', () => {
 				constructor( sourceElements, config ) {
 					super( config );
 
-					this.data.processor = new HtmlDataProcessor( this.stylesProcessor );
+					this.data.processor = new HtmlDataProcessor( this.data.stylesProcessor );
 
 					// Create a root for each source element.
 					for ( const rootName of Object.keys( sourceElements ) ) {
