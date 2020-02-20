96b60c5cd36bb3710bc4d5670593eaf1d38c583a
Kamil Piechaczek <pomek@users.noreply.github.com>
2020-02-20 13:55:30 +0100

    Aligned to changes in Editor class.

--- packages/ckeditor5-editor-inline/src/inlineeditor.js
+++ packages/ckeditor5-editor-inline/src/inlineeditor.js
@@ -64,7 +64,7 @@ export default class InlineEditor extends Editor {
 	constructor( sourceElementOrData, config ) {
 		super( config );
 
-		this.data.processor = new HtmlDataProcessor( this.stylesProcessor );
+		this.data.processor = new HtmlDataProcessor( this.data.stylesProcessor );
 
 		this.model.document.createRoot();
 
