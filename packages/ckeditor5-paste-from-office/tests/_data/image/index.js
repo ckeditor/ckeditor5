dc2b6c63118c5ef79ca8ed415a7eb63dbbb630f4
Maciej Gołaszewski <jodator@jodator.net>
2020-02-21 13:07:26 +0100

    Fix dangling comma.

--- packages/ckeditor5-paste-from-office/tests/_data/image/index.js
+++ packages/ckeditor5-paste-from-office/tests/_data/image/index.js
@@ -156,7 +156,7 @@ export const browserFixtures = {
 			adjacentGroups: adjacentGroupsRtfChrome,
 			onlineOffline: onlineOfflineRtfChrome,
 			shapesOnlineOffline: shapesOnlineOfflineRtfChrome
-		},
+		}
 
 	},
 
