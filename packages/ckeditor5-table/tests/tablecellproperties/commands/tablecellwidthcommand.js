0d907895600a3b52984d98e0c708c883ac6f73b6
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-02-27 14:31:00 +0100

    Update tests/tablecellproperties/commands/tablecellwidthcommand.js

--- packages/ckeditor5-table/tests/tablecellproperties/commands/tablecellwidthcommand.js
+++ packages/ckeditor5-table/tests/tablecellproperties/commands/tablecellwidthcommand.js
@@ -143,7 +143,7 @@ describe( 'table cell properties', () => {
 						expect( command.value ).to.be.undefined;
 					} );
 
-					it( 'should be set if all table cell has the same width property value', () => {
+					it( 'should be set if all table cells have the same "width" property value', () => {
 						setData( model, modelTable( [
 							[
 								{ contents: '00', isSelected: true, width: '100px' },
