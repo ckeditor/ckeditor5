d65d2cd7b291d646c4c03166900541c5cc292113
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-02-27 14:39:19 +0100

    Update tests/tablecellproperties/commands/tablecellborderstylecommand.js

--- packages/ckeditor5-table/tests/tablecellproperties/commands/tablecellborderstylecommand.js
+++ packages/ckeditor5-table/tests/tablecellproperties/commands/tablecellborderstylecommand.js
@@ -123,7 +123,7 @@ describe( 'table cell properties', () => {
 				} );
 
 				describe( 'multi-cell selection', () => {
-					it( 'should be undefined if no table cell has a borderStyle property', () => {
+					it( 'should be undefined if no table cells have the "borderStyle" property', () => {
 						setData( model, modelTable( [
 							[
 								{ contents: '00', isSelected: true },
