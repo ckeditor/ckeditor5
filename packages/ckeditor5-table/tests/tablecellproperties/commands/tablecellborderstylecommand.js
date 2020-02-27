e7f91119fe5a3eea618fdc1f10b24e760626dfa7
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-02-27 14:27:24 +0100

    Update tests/tablecellproperties/commands/tablecellborderstylecommand.js

--- packages/ckeditor5-table/tests/tablecellproperties/commands/tablecellborderstylecommand.js
+++ packages/ckeditor5-table/tests/tablecellproperties/commands/tablecellborderstylecommand.js
@@ -138,7 +138,7 @@ describe( 'table cell properties', () => {
 						expect( command.value ).to.be.undefined;
 					} );
 
-					it( 'should be undefined if only some table cell has a borderStyle property', () => {
+					it( 'should be undefined if only some table cells have the "borderStyle" property', () => {
 						setData( model, modelTable( [
 							[
 								{ contents: '00', isSelected: true, borderStyle: 'solid' },
