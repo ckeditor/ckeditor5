e727e8bf00a3cb71efef44e90b3a0479599cc5b3
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-02-27 14:31:30 +0100

    Update tests/tablecellproperties/commands/tablecellpaddingcommand.js

--- packages/ckeditor5-table/tests/tablecellproperties/commands/tablecellpaddingcommand.js
+++ packages/ckeditor5-table/tests/tablecellproperties/commands/tablecellpaddingcommand.js
@@ -168,7 +168,7 @@ describe( 'table cell properties', () => {
 						expect( command.value ).to.be.undefined;
 					} );
 
-					it( 'should be set if all table cell has the same padding property value', () => {
+					it( 'should be set if all table cells have the same "padding" property value', () => {
 						setData( model, modelTable( [
 							[
 								{ contents: '00', isSelected: true, padding: '2em' },
