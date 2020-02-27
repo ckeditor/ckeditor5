c4695bbdc6b8edc633160accd4d71ed5409c7986
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-02-27 14:34:55 +0100

    Update tests/tablecellproperties/commands/tablecellpaddingcommand.js

--- packages/ckeditor5-table/tests/tablecellproperties/commands/tablecellpaddingcommand.js
+++ packages/ckeditor5-table/tests/tablecellproperties/commands/tablecellpaddingcommand.js
@@ -320,7 +320,7 @@ describe( 'table cell properties', () => {
 						] ) );
 					} );
 
-					it( 'should remove padding from a selected table cell if no value is passed', () => {
+					it( 'should remove "padding" from selected table cells if no value is passed', () => {
 						setData( model, modelTable( [
 							[ { contents: '00', isSelected: true, padding: '25px' }, '01' ],
 							[ '10', { contents: '11', isSelected: true, padding: '25px' } ]
