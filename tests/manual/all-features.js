0f0a23d98af278749471d790a10619ef236d8308
Piotrek Koszuliński <pkoszulinski@gmail.com>
2020-03-03 23:05:46 +0100

    Minor improvements.

--- tests/manual/all-features.js
+++ tests/manual/all-features.js
@@ -31,31 +31,9 @@ import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Mention from '@ckeditor/ckeditor5-mention/src/mention';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 
 import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud-services-config';
-
-const COLOR_PALETTE = [
-	{ color: 'hsl(0, 0%, 0%)', label: 'Black' },
-	{ color: 'hsl(0, 0%, 30%)', label: 'Dim grey' },
-	{ color: 'hsl(0, 0%, 60%)', label: 'Grey' },
-	{ color: 'hsl(0, 0%, 90%)', label: 'Light grey' },
-	{ color: 'hsl(0, 0%, 100%)', label: 'White', hasBorder: true },
-	{ color: 'hsl(0, 100%, 89%)', label: 'Pink' },
-	{ color: 'hsl(0, 75%, 60%)', label: 'Red' },
-	{ color: 'hsl(60, 75%, 60%)', label: 'Yellow' },
-	{ color: 'hsl(27, 100%, 85%)', label: 'Light Orange' },
-	{ color: 'hsl(30, 75%, 60%)', label: 'Orange' },
-	{ color: 'hsl(90, 75%, 60%)', label: 'Light green' },
-	{ color: 'hsl(120, 75%, 60%)', label: 'Green' },
-	{ color: 'hsl(150, 75%, 60%)', label: 'Aquamarine' },
-	{ color: 'hsl(120, 100%, 25%)', label: 'Dark green' },
-	{ color: 'hsl(180, 75%, 60%)', label: 'Turquoise' },
-	{ color: 'hsl(180, 52%, 58%)', label: 'Light Aqua', },
-	{ color: 'hsl(180, 97%, 31%)', label: 'Aqua' },
-	{ color: 'hsl(210, 75%, 60%)', label: 'Light blue' },
-	{ color: 'hsl(240, 75%, 60%)', label: 'Blue' },
-	{ color: 'hsl(270, 75%, 60%)', label: 'Purple' }
-];
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
@@ -65,7 +43,7 @@ ClassicEditor
 			Code, Underline, Strikethrough, Superscript, Subscript,
 			Highlight, FontColor, FontBackgroundColor, FontFamily, FontSize,
 			IndentBlock, WordCount, EasyImage,
-			TodoList, PageBreak, HorizontalLine, Mention, RemoveFormat
+			TodoList, PageBreak, HorizontalLine, Mention, RemoveFormat, TextTransformation
 		],
 		toolbar: [
 			'heading',
@@ -86,15 +64,7 @@ ClassicEditor
 		],
 		cloudServices: CS_CONFIG,
 		table: {
-			contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties' ],
-			tableProperties: {
-				borderColors: COLOR_PALETTE,
-				backgroundColors: COLOR_PALETTE
-			},
-			tableCellProperties: {
-				borderColors: COLOR_PALETTE,
-				backgroundColors: COLOR_PALETTE
-			}
+			contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties' ]
 		},
 		image: {
 			styles: [
@@ -109,7 +79,12 @@ ClassicEditor
 			feeds: [
 				{
 					marker: '@',
-					feed: [ '@Barney', '@Lily', '@Marshall', '@Robin', '@Ted' ],
+					feed: [
+						'@apple', '@bears', '@brownie', '@cake', '@cake', '@candy', '@canes', '@chocolate', '@cookie', '@cotton', '@cream',
+						'@cupcake', '@danish', '@donut', '@dragée', '@fruitcake', '@gingerbread', '@gummi', '@ice', '@jelly-o',
+						'@liquorice', '@macaroon', '@marzipan', '@oat', '@pie', '@plum', '@pudding', '@sesame', '@snaps', '@soufflé',
+						'@sugar', '@sweet', '@topping', '@wafer'
+					],
 					minimumCharacters: 1
 				}
 			]
