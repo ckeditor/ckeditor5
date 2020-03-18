e58099a6a0150f28b585cbef8bb7363c2ee73eb5
Kamil Piechaczek <pomek@users.noreply.github.com>
2020-03-18 13:54:52 +0100

    Added data-cke attribute to style selector.

--- packages/ckeditor5-build-inline/webpack.config.js
+++ packages/ckeditor5-build-inline/webpack.config.js
@@ -69,7 +69,10 @@ module.exports = {
 					{
 						loader: 'style-loader',
 						options: {
-							injectType: 'singletonStyleTag'
+							injectType: 'singletonStyleTag',
+							attributes: {
+								'data-cke': true
+							}
 						}
 					},
 					{
