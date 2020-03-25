302bddb0fa6091287241690d98bb6818dbced9c3
panr <radoslaw.koziel@gmail.com>
2020-03-25 16:21:20 +0100

    Fix docs references

--- packages/ckeditor5-media-embed/src/mediaembedui.js
+++ packages/ckeditor5-media-embed/src/mediaembedui.js
@@ -78,7 +78,7 @@ export default class MediaEmbedUI extends Plugin {
 		// invisible form/input cannot be focused/selected.
 		button.on( 'open', () => {
 			// Make sure that each time the panel shows up, the URL field remains in sync with the value of
-			// the command. If the user typed in the input, then canceled (`urlInputView#field#value` stays
+			// the command. If the user typed in the input, then canceled (`urlInputView#fieldView#value` stays
 			// unaltered) and re-opened it without changing the value of the media command (e.g. because they
 			// didn't change the selection), they would see the old value instead of the actual value of the
 			// command.
