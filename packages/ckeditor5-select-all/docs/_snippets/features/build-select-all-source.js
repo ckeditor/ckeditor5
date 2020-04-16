dce3e51f46a4b3854c0c6d6d70d2db4855714658
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-04-16 16:08:27 +0200

    Docs: Added the feature guide.

---
+++ packages/ckeditor5-select-all/docs/_snippets/features/build-select-all-source.js
@@ -0,0 +1,16 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* globals window */
+
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic/src/ckeditor';
+
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+
+ClassicEditor.builtinPlugins.push( Bold, Italic, Underline );
+
+window.ClassicEditor = ClassicEditor;
