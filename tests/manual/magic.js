7f296ab6eeb652231b23d7af42eae4b5acafadd5
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-04-28 17:05:24 +0200

    PoC

--- tests/manual/magic.js
+++ tests/manual/magic.js
@@ -3,10 +3,11 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-/* globals console, window, document */
+/* globals console, window, document, IntersectionObserver */
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
+import TreeWalker from '@ckeditor/ckeditor5-engine/src/view/treewalker';
 
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import indexOf from '@ckeditor/ckeditor5-utils/src/dom/indexof';
@@ -16,40 +17,81 @@ class AccessTools extends Plugin {
 		this._initMagicBlock();
 	}
 
-	_initMagicBlock() {
+	_getViewElements() {
 		const editor = this.editor;
 		const editingView = editor.editing.view;
 		const viewRoot = editingView.document.getRoot();
 
-		editingView.document.registerPostFixer( writer => {
-			// Cloning so we're sure we can make modifications as we iterate (without affecting the loop).
-			let children = Array.from( viewRoot.getChildren() );
+		const walker = new TreeWalker( {
+			startPosition: editingView.createPositionAt( viewRoot, 0 ),
+			ignoreElementEnd: true
+		} );
 
-			for ( const child of children ) {
-				if ( child.is( 'uiElement' ) && child.hasClass( 'magic-block' ) ) {
-					if ( !isMagicBlockStillNeeded( child ) ) {
-						writer.remove( child );
+		return [ ...walker ]
+			.map( walkerValue => {
+				const item = walkerValue.item;
+
+				if ( !item.is( 'element' ) ) {
+					return;
+				}
+
+				return item;
+			} )
+			.filter( element => element );
+	}
+
+	_initMagicBlock() {
+		const editor = this.editor;
+		const editingView = editor.editing.view;
+		const intersectionObserver = new IntersectionObserver( onIntersectionChange );
+
+		function onIntersectionChange( entries ) {
+			entries.forEach( entry => {
+				if ( entry.intersectionRatio > 0 ) {
+					// entry.target.style = 'outline: 1px solid red';
+				} else {
+					// entry.target.style = 'outline: none';
+				}
+			} );
+		}
+
+		editingView.document.registerPostFixer( writer => {
+			let elements = this._getViewElements();
+
+			for ( const element of elements ) {
+				if ( element.is( 'uiElement' ) && element.hasClass( 'magic-block' ) ) {
+					if ( !isMagicBlockStillNeeded( element ) ) {
+						writer.remove( element );
 
 						continue;
 					}
 				}
 			}
 
-			children = Array.from( viewRoot.getChildren() );
+			elements = this._getViewElements();
 
-			for ( const child of children ) {
-				if ( !child.is( 'containerElement' ) ) {
+			for ( const element of elements ) {
+				if ( !element.is( 'containerElement' ) ) {
 					continue;
 				}
 
-				if ( !child.previousSibling && isWidget( child ) ) {
-					insertMagicBlockAt( writer, writer.createPositionBefore( child ) );
+				if ( !element.previousSibling && isWidget( element ) ) {
+					insertMagicBlockAt( writer, writer.createPositionBefore( element ) );
 				}
 
-				if ( isWidget( child ) && ( !child.nextSibling || isWidget( child.nextSibling ) ) ) {
-					insertMagicBlockAt( writer, writer.createPositionAfter( child ) );
+				if ( isWidget( element ) && ( !element.nextSibling || isWidget( element.nextSibling ) ) ) {
+					insertMagicBlockAt( writer, writer.createPositionAfter( element ) );
 				}
 			}
+		} );
+
+		editingView.on( 'render', () => {
+			intersectionObserver.disconnect();
+
+			this._getViewElements()
+				.forEach( element => {
+					intersectionObserver.observe( editingView.domConverter.mapViewToDom( element ) );
+				} );
 		} );
 
 		editor.ui.on( 'ready', () => {
@@ -66,6 +108,8 @@ class AccessTools extends Plugin {
 						writer.insert( paragraph, modelPosition );
 						writer.setSelection( paragraph, 0 );
 					} );
+
+					editingView.scrollToTheSelection();
 				}
 			} );
 		} );
