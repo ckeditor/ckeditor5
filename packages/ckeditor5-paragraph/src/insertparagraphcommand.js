b7f0fffbec8ace6a2b8b0ee6d7ccc2d5cb99b02f
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-05-21 13:54:43 +0200

    Made the InsertParagraphCommand use a model position instead of element and before/after.

--- packages/ckeditor5-paragraph/src/insertparagraphcommand.js
+++ packages/ckeditor5-paragraph/src/insertparagraphcommand.js
@@ -10,12 +10,12 @@
 import Command from '@ckeditor/ckeditor5-core/src/command';
 
 /**
- * The insert paragraph command. It insert a new paragraph next to the provided model element.
+ * The insert paragraph command. It inserts a new paragraph at a specific
+ * {@link module:engine/model/position~Position document position}.
  *
- *		// Insert a new paragraph before the first element in the document root.
+ *		// Insert a new paragraph before an element in the document.
  *		editor.execute( 'insertParagraph', {
- *			element: editor.model.document.getRoot().getChild( 0 ),
- *			position: 'before'
+ *			position: editor.model.createPositionBefore( element )
  *		} );
  *
  * **Note**: This command moves the selection to the inserted paragraph.
@@ -27,34 +27,23 @@ export default class InsertParagraphCommand extends Command {
 	 * Executes the command.
 	 *
 	 * @param {Object} options Options for the executed command.
-	 * @param {module:engine/model/element~Element} options.element The model element next to which
-	 * the new paragraph will be inserted.
-	 * @param {'before'|'after'} options.position The position relative to the passed `element` where
+	 * @param {module:engine/model/position~Position} options.position The model position at which
 	 * the new paragraph will be inserted.
 	 * @fires execute
 	 */
 	execute( options ) {
-		const editor = this.editor;
 		const model = this.editor.model;
-		let modelPosition;
 
-		if ( options.position === 'before' ) {
-			modelPosition = model.createPositionBefore( options.element );
-		} else {
-			modelPosition = model.createPositionAfter( options.element );
-		}
-
-		if ( !model.schema.checkChild( modelPosition, 'paragraph' ) ) {
+		if ( !model.schema.checkChild( options.position, 'paragraph' ) ) {
 			return;
 		}
 
-		editor.model.change( writer => {
+		model.change( writer => {
 			const paragraph = writer.createElement( 'paragraph' );
-			const selection = writer.createSelection( paragraph, 'in' );
 
-			editor.model.insertContent( paragraph, modelPosition );
+			model.insertContent( paragraph, options.position );
 
-			writer.setSelection( selection );
+			writer.setSelection( paragraph, 'in' );
 		} );
 	}
 }
