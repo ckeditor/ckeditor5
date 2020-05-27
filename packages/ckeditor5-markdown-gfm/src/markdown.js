41491015a72c3ddfaf98355ddba16097f6664dd4
fredck <f.knabben@cksource.com>
2020-05-27 15:32:54 +0200

    Introduced the Markdown plugin.

---
+++ packages/ckeditor5-markdown-gfm/src/markdown.js
@@ -0,0 +1,35 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module markdown-gfm/markdown
+ */
+
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import GFMDataProcessor from './gfmdataprocessor';
+
+/**
+ * The GitHub Flavored Markdown (GFM) plugin.
+ *
+ * For a detailed overview, check the {@glink features/markdown Markdown feature documentation}.
+ *
+ * @extends module:core/plugin~Plugin
+ */
+export default class Markdown extends Plugin {
+	/**
+	 * @inheritDoc
+	 */
+	constructor( editor ) {
+		super( editor );
+		editor.data.processor = new GFMDataProcessor();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	static get pluginName() {
+		return 'Markdown';
+	}
+}
