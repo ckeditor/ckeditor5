7778ddc2925941ee5f3794091589c4d098374679
panr <radoslaw.koziel@gmail.com>
2020-06-03 17:09:07 +0200

    Add manual test for link protocol.

---
+++ packages/ckeditor5-link/tests/manual/protocol.js
@@ -0,0 +1,49 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* globals console:false, window, document */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Enter from '@ckeditor/ckeditor5-enter/src/enter';
+import Typing from '@ckeditor/ckeditor5-typing/src/typing';
+import Link from '../../src/link';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Undo from '@ckeditor/ckeditor5-undo/src/undo';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+
+ClassicEditor
+	.create( document.querySelector( '#editor' ), {
+		plugins: [ Link, Typing, Paragraph, Undo, Enter, Superscript ],
+		toolbar: [ 'link', 'undo', 'redo' ],
+		link: {
+			addTargetToExternalLinks: true,
+			defaultProtocol: 'http://'
+		}
+	} )
+	.then( editor => {
+		window.editor = editor;
+
+		const LinkUIPlugin = editor.plugins.get( 'LinkUI' );
+		const formView = LinkUIPlugin.formView;
+		const defaultProtocol = editor.config.get( 'link.defaultProtocol' );
+
+		document.getElementById( 'default-protocol' ).innerText = defaultProtocol;
+
+		document.querySelectorAll( '#protocol-settings input[type="radio"]' ).forEach( radio => {
+			radio.checked = radio.value === defaultProtocol;
+
+			radio.addEventListener( 'click', ( {
+				target: { value: protocol }
+			} ) => {
+				editor.config.set( 'link.defaultProtocol', protocol );
+
+				// Change input placeholder just for manual test's case to provide more dynamic behavior.
+				formView.urlInputView.fieldView.placeholder = protocol + 'example.com';
+			} );
+		} );
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
---
+++ packages/ckeditor5-link/tests/manual/protocol.md
@@ -0,0 +1,8 @@
+## Link protocol
+
+This test checks whether:
+- `config.link.defaultProtocol` applies.
+- user is able to dynamically change the link protocol (just as an extended example of usage).
+- the plugin dynamically change link protocol to `mailto:` when email address was detected.
+
+When you dynamically change the value of the link protocol, the form input placeholder should change as well.
