26c95ae963f4f8ba10e989b196974f6a34bff9a4
Kuba Niegowski <j.niegowski@cksource.com>
2020-06-03 20:28:03 +0200

    Added Writer#cloneElement().

--- packages/ckeditor5-table/src/tableclipboard.js
+++ packages/ckeditor5-table/src/tableclipboard.js
@@ -295,7 +295,7 @@ function replaceSelectedCellsWithPasted( pastedTable, pastedDimensions, selected
 
 		// Clone cell to insert (to duplicate its attributes and children).
 		// Cloning is required to support repeating pasted table content when inserting to a bigger selection.
-		const cellToInsert = pastedCell._clone( true );
+		const cellToInsert = writer.cloneElement( pastedCell );
 
 		// Trim the cell if it's row/col-spans would exceed selection area.
 		trimTableCellIfNeeded( cellToInsert, row, column, lastRowOfSelection, lastColumnOfSelection, writer );
