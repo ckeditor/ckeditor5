992f967d4342e8f64d81b5494e9f133002054b6d
Szymon Cofalik <s.cofalik@cksource.com>
2020-07-10 13:29:43 +0200

    Other: Prevented from adding markers with names containing "," character.

--- packages/ckeditor5-engine/src/model/markercollection.js
+++ packages/ckeditor5-engine/src/model/markercollection.js
@@ -92,6 +92,11 @@ export default class MarkerCollection {
 	 */
 	_set( markerOrName, range, managedUsingOperations = false, affectsData = false ) {
 		const markerName = markerOrName instanceof Marker ? markerOrName.name : markerOrName;
+
+		if ( markerName.includes( ',' ) ) {
+			throw new CKEditorError( 'markercollection-incorrect-marker-name: Marker name cannot contain "," character.', this );
+		}
+
 		const oldMarker = this._markers.get( markerName );
 
 		if ( oldMarker ) {
