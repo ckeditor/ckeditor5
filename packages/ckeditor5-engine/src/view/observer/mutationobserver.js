e694001462b960aee8bb62cc7e4753c84f6cd5bc
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-07-13 11:46:50 +0200

    Added RawElement support in the MutationObserver.

--- packages/ckeditor5-engine/src/view/observer/mutationobserver.js
+++ packages/ckeditor5-engine/src/view/observer/mutationobserver.js
@@ -150,8 +150,8 @@ export default class MutationObserver extends Observer {
 			if ( mutation.type === 'childList' ) {
 				const element = domConverter.mapDomToView( mutation.target );
 
-				// Do not collect mutations from UIElements.
-				if ( element && element.is( 'uiElement' ) ) {
+				// Do not collect mutations from UIElements and RawElements.
+				if ( element && ( element.is( 'uiElement' ) || element.is( 'rawElement' ) ) ) {
 					continue;
 				}
 
@@ -165,8 +165,8 @@ export default class MutationObserver extends Observer {
 		for ( const mutation of domMutations ) {
 			const element = domConverter.mapDomToView( mutation.target );
 
-			// Do not collect mutations from UIElements.
-			if ( element && element.is( 'uiElement' ) ) {
+			// Do not collect mutations from UIElements and RawElements.
+			if ( element && ( element.is( 'uiElement' ) || element.is( 'rawElement' ) ) ) {
 				continue;
 			}
 
