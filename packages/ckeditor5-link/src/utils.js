a426403fe765278c9f4968634763134521de91be
Kamil Piechaczek <pomek@pomek.pl>
2020-07-16 14:50:02 +0200

    Support for decorators when linking images.

--- packages/ckeditor5-link/src/utils.js
+++ packages/ckeditor5-link/src/utils.js
@@ -119,3 +119,18 @@ export function normalizeDecorators( decorators ) {
 
 	return retArray;
 }
+
+/**
+ * Returns `true` if the specified `element` is an image and it can be linked (the element allows having the `linkHref` attribute).
+ *
+ * @params {module:engine/model/element~Element|null} element
+ * @params {module:engine/model/schema~Schema} schema
+ * @returns {Boolean}
+ */
+export function isImageAllowed( element, schema ) {
+	if ( !element ) {
+		return false;
+	}
+
+	return element.is( 'image' ) && schema.checkAttribute( 'image', 'linkHref' );
+}
