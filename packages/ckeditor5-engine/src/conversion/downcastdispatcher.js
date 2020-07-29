bcc1366a5e46eba6dfc23a669ccb860d2c7b0e1d
Bartek Biedrzycki <68123541+godai78@users.noreply.github.com>
2020-07-29 12:34:08 +0200

    Grammar.
    Co-authored-by: Anna Tomanek <a.tomanek@cksource.com>

--- packages/ckeditor5-engine/src/conversion/downcastdispatcher.js
+++ packages/ckeditor5-engine/src/conversion/downcastdispatcher.js
@@ -44,7 +44,7 @@ import { extend } from 'lodash-es';
  * downcast dispatcher generates {@link module:engine/conversion/modelconsumable~ModelConsumable consumables}.
  * These are used to have control over which changes have already been consumed. It is useful when some converters
  * overwrite others or convert multiple changes (for example, it converts an insertion of an element and also converts that
- * element's attributes during insertion).
+ * element's attributes during the insertion).
  *
  * Additionally, `DowncastDispatcher` fires events for {@link module:engine/model/markercollection~Marker marker} changes:
  *
