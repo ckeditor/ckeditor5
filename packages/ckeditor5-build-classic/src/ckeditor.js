4eed60478c58cc05a0758b68868973722ec65a63
Maciej Gołaszewski <jodator@jodator.net>
2020-08-27 10:01:56 +0200

    Move POC code to the build-classic.

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -29,6 +29,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import Info from './ckx/info.ckx';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -56,7 +57,9 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	// POC: Add ckx-capable plugin
+	Info
 ];
 
 // Editor configuration.
