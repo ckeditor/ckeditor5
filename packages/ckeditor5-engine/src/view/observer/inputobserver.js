b380f7c68c6636fb0d2f2e27b8cedd8ea273673a
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-09-08 14:47:29 +0200

    Improved the InputObserver by adding some useful input event data properties.

--- packages/ckeditor5-engine/src/view/observer/inputobserver.js
+++ packages/ckeditor5-engine/src/view/observer/inputobserver.js
@@ -8,11 +8,13 @@
  */
 
 import DomEventObserver from './domeventobserver';
+import DataTransfer from '../datatransfer';
 
 /**
  * Observer for events connected with data input.
  *
- * Note that this observer is attached by the {@link module:engine/view/view~View} and is available by default.
+ * **Note**: This observer is attached by {@link module:engine/view/view~View} and available by default in all
+ * editor instances.
  *
  * @extends module:engine/view/observer/domeventobserver~DomEventObserver
  */
@@ -24,21 +26,98 @@ export default class InputObserver extends DomEventObserver {
 	}
 
 	onDomEvent( domEvent ) {
-		this.fire( domEvent.type, domEvent );
+		const domTargetRanges = Array.from( domEvent.getTargetRanges() );
+		const view = this.view;
+
+		let dataTransfer = null;
+		let data = null;
+
+		if ( domEvent.dataTransfer ) {
+			dataTransfer = new DataTransfer( domEvent.dataTransfer );
+		}
+
+		if ( domEvent.data ) {
+			data = domEvent.data;
+		} else if ( dataTransfer ) {
+			data = dataTransfer.getData( 'text/plain' );
+		}
+
+		this.fire( domEvent.type, domEvent, {
+			data,
+			dataTransfer,
+			domTargetRanges,
+			targetRanges: domTargetRanges.map( domRange => view.domConverter.domRangeToView( domRange ) ),
+			inputType: domEvent.inputType
+		} );
 	}
 }
 
 /**
- * Fired before browser inputs (or deletes) some data.
+ * Fired before the web browser inputs, deletes, or formats some data.
  *
- * This event is available only on browsers which support DOM `beforeinput` event.
+ * **Note**: This event is available only in browsers which support the DOM [`beforeinput`](https://www.w3.org/TR/input-events-2/) event.
  *
- * Introduced by {@link module:engine/view/observer/inputobserver~InputObserver}.
- *
- * Note that because {@link module:engine/view/observer/inputobserver~InputObserver} is attached by the
- * {@link module:engine/view/view~View} this event is available by default.
+ * This event is introduced by {@link module:engine/view/observer/inputobserver~InputObserver} and available
+ * by default in all editor instances (attached by {@link module:engine/view/view~View}).
  *
  * @see module:engine/view/observer/inputobserver~InputObserver
  * @event module:engine/view/document~Document#event:beforeinput
- * @param {module:engine/view/observer/domeventdata~DomEventData} data Event data.
+ * @param {module:engine/view/observer/inputobserver~InputEventData} data Event data containing detailed information about the event.
+ */
+
+/**
+ * The value of the {@link module:engine/view/document~Document#event:beforeinput} event.
+ *
+ * @class module:engine/view/observer/inputobserver~InputEventData
+ * @extends module:engine/view/observer/domeventdata~DomEventData
+ */
+
+/**
+ * The data transfer instance of the input event. Corresponds to native `InputEvent#dataTransfer`.
+ *
+ * The value is `null` when no `dataTransfer` was passed along with the input event.
+ *
+ * @readonly
+ * @member {module:engine/view/datatransfer~DataTransfer|null} module:engine/view/observer/inputobserver~InputEventData#dataTransfer
+ */
+
+/**
+ * An array of target ranges passed along with the input event. Corresponds to the output of native `InputEvent#getTargetRanges()`.
+ *
+ * @readonly
+ * @member {Array.<Range>} module:engine/view/observer/inputobserver~InputEventData#domTargetRanges
+ */
+
+/**
+ * The type of the input event (e.g. "insertText" or "deleteWordBackward"). Corresponds to native `InputEvent#inputType`.
+ *
+ * @readonly
+ * @member {String} module:engine/view/observer/inputobserver~InputEventData#inputType
+ */
+
+/**
+ * Editing view ranges corresponding to
+ * {@link module:engine/view/observer/inputobserver~InputEventData#domTargetRanges DOM ranges} passed along with the input event.
+ *
+ * @readonly
+ * @member {Array.<module:engine/view/range~Range>} module:engine/view/observer/inputobserver~InputEventData#targetRanges
+ */
+
+/**
+ * A unified text data passed along with the input event. Depending on:
+ *
+ * * the web browser and input events implementation (for instance [Level 1](https://www.w3.org/TR/input-events-1/) or
+ * [Level 2](https://www.w3.org/TR/input-events-2/)),
+ * * {@link module:engine/view/observer/inputobserver~InputEventData#inputType input type}
+ *
+ * text data is sometimes passed in the `data` and sometimes in the `dataTransfer` property.
+ *
+ * * If `InputEvent#data` was set, this property reflects its value.
+ * * If `InputEvent#data` is unavailable, this property contains the `'text/plain'` data from
+ * {@link module:engine/view/observer/inputobserver~InputEventData#dataTransfer}.
+ * * If the event ({@link module:engine/view/observer/inputobserver~InputEventData#inputType input type})
+ * provides no data whatsoever, this property is `null`.
+ *
+ * @readonly
+ * @member {String|null} module:engine/view/observer/inputobserver~InputEventData#data
  */
--- packages/ckeditor5-engine/tests/view/observer/inputobserver.js
+++ packages/ckeditor5-engine/tests/view/observer/inputobserver.js
@@ -5,48 +5,192 @@
 
 import InputObserver from '../../../src/view/observer/inputobserver';
 import View from '../../../src/view/view';
-import env from '@ckeditor/ckeditor5-utils/src/env';
+import createViewRoot from '../_utils/createroot';
+import global from '@ckeditor/ckeditor5-utils/src/dom/global';
 import { StylesProcessor } from '../../../src/view/stylesmap';
+import DataTransfer from '../../../src/view/datatransfer';
 
 describe( 'InputObserver', () => {
-	const oldEnvIsAndroid = env.isAndroid;
-
-	let view, viewDocument, observer;
-
-	before( () => {
-		env.isAndroid = true;
-	} );
+	let domEditable, view, viewRoot, viewDocument, observer, evtData, beforeInputSpy;
 
 	beforeEach( () => {
 		view = new View( new StylesProcessor() );
 		viewDocument = view.document;
+		domEditable = global.document.createElement( 'div' );
+		viewRoot = createViewRoot( viewDocument );
+
+		view.attachDomRoot( domEditable );
+
+		// <p>foo</p>
+		view.change( writer => {
+			const paragraph = writer.createContainerElement( 'p' );
+			const text = writer.createText( 'foo' );
+
+			writer.insert( writer.createPositionAt( paragraph, 0 ), text );
+			writer.insert( writer.createPositionAt( viewRoot, 0 ), paragraph );
+		} );
+
 		observer = view.getObserver( InputObserver );
+
+		beforeInputSpy = sinon.spy();
+		viewDocument.on( 'beforeinput', beforeInputSpy );
 	} );
 
 	afterEach( () => {
 		view.destroy();
-	} );
-
-	after( () => {
-		env.isAndroid = oldEnvIsAndroid;
+		domEditable.remove();
 	} );
 
 	it( 'should define domEventType', () => {
 		expect( observer.domEventType ).to.contains( 'beforeinput' );
 	} );
 
-	it( 'should fire beforeinput with dom event data', () => {
-		const spy = sinon.spy();
+	it( 'should fire the #beforeinput once for every DOM event', () => {
+		fireMockNativeBeforeInput();
 
-		viewDocument.on( 'beforeinput', spy );
-
-		const domEvtData = {
-			type: 'beforeinput'
-		};
-
-		observer.onDomEvent( domEvtData );
-
-		expect( spy.calledOnce ).to.be.true;
-		expect( spy.args[ 0 ][ 1 ].domEvent ).to.equal( domEvtData );
+		sinon.assert.calledOnce( beforeInputSpy );
 	} );
+
+	describe( 'event data', () => {
+		it( 'should contain #eventType', () => {
+			fireMockNativeBeforeInput( {
+				inputType: 'foo'
+			} );
+
+			expect( evtData.inputType ).to.equal( 'foo' );
+		} );
+
+		describe( '#dataTransfer', () => {
+			it( 'should be an instance of DataTransfer if the DOM event passes data transfer', () => {
+				const dataTransferMock = sinon.stub();
+
+				dataTransferMock.withArgs( 'foo/bar' ).returns( 'baz' );
+
+				fireMockNativeBeforeInput( {
+					dataTransfer: {
+						getData: dataTransferMock
+					}
+				} );
+
+				expect( evtData.dataTransfer ).to.be.instanceOf( DataTransfer );
+				expect( evtData.dataTransfer.getData( 'foo/bar' ) ).to.equal( 'baz' );
+			} );
+
+			it( 'should be "null" if no data transfer was provided by the DOM event', () => {
+				fireMockNativeBeforeInput();
+
+				expect( evtData.dataTransfer ).to.be.null;
+			} );
+		} );
+
+		describe( '#domTargetRanges', () => {
+			it( 'should be an array of native DOM ranges', () => {
+				const domRange1 = global.document.createRange();
+				const domRange2 = global.document.createRange();
+
+				// [<p>foo</p>]
+				domRange1.selectNodeContents( domEditable );
+				// <p>[fo]o</p>
+				domRange2.setStart( domEditable.firstChild.firstChild, 0 );
+				domRange2.setEnd( domEditable.firstChild.firstChild, 2 );
+
+				fireMockNativeBeforeInput( {
+					getTargetRanges: () => [ domRange1, domRange2 ]
+				} );
+
+				expect( evtData.domTargetRanges ).to.have.ordered.members( [ domRange1, domRange2 ] );
+			} );
+
+			it( 'should be an empty array if there are no native DOM ranges', () => {
+				fireMockNativeBeforeInput( {
+					getTargetRanges: () => []
+				} );
+
+				expect( evtData.domTargetRanges ).to.be.empty;
+			} );
+		} );
+
+		describe( '#targetRanges', () => {
+			it( 'should be an empty array if there are no native DOM ranges', () => {
+				fireMockNativeBeforeInput( {
+					getTargetRanges: () => []
+				} );
+
+				expect( evtData.targetRanges ).to.be.empty;
+			} );
+
+			it( 'should provide editing view ranges corresponding to DOM ranges passed along with the DOM event', () => {
+				const domRange1 = global.document.createRange();
+				const domRange2 = global.document.createRange();
+
+				// [<p>foo</p>]
+				domRange1.selectNodeContents( domEditable );
+				// <p>[fo]o</p>
+				domRange2.setStart( domEditable.firstChild.firstChild, 0 );
+				domRange2.setEnd( domEditable.firstChild.firstChild, 2 );
+
+				fireMockNativeBeforeInput( {
+					getTargetRanges: () => [ domRange1, domRange2 ]
+				} );
+
+				expect( evtData.targetRanges ).to.have.length( 2 );
+
+				const viewRange1 = evtData.targetRanges[ 0 ];
+				const viewRange2 = evtData.targetRanges[ 1 ];
+
+				expect( viewRange1.start.parent ).to.equal( viewRoot );
+				expect( viewRange1.start.offset ).to.equal( 0 );
+				expect( viewRange1.end.parent ).to.equal( viewRoot );
+				expect( viewRange1.end.offset ).to.equal( 1 );
+
+				expect( viewRange2.start.parent ).to.equal( viewRoot.getChild( 0 ).getChild( 0 ) );
+				expect( viewRange2.start.offset ).to.equal( 0 );
+				expect( viewRange2.end.parent ).to.equal( viewRoot.getChild( 0 ).getChild( 0 ) );
+				expect( viewRange2.end.offset ).to.equal( 2 );
+			} );
+		} );
+
+		describe( '#data', () => {
+			it( 'should reflect InputEvent#data (when available)', () => {
+				fireMockNativeBeforeInput( {
+					data: 'foo'
+				} );
+
+				expect( evtData.data ).to.equal( 'foo' );
+			} );
+
+			it( 'should attempt to use text/plain from #dataTransfer if InputEvent#data is unavailable', () => {
+				const dataTransferMock = sinon.stub();
+
+				dataTransferMock.withArgs( 'text/plain' ).returns( 'bar' );
+
+				fireMockNativeBeforeInput( {
+					data: null,
+					dataTransfer: {
+						getData: dataTransferMock
+					}
+				} );
+
+				expect( evtData.data ).to.equal( 'bar' );
+			} );
+
+			it( 'should be "null" if both InputEvent#data and InputEvent#dataTransfer are unavailable', () => {
+				fireMockNativeBeforeInput( {
+					data: null,
+					dataTransfer: null
+				} );
+
+				expect( evtData.data ).to.be.null;
+			} );
+		} );
+	} );
+
+	function fireMockNativeBeforeInput( domEvtMock ) {
+		observer.onDomEvent( Object.assign( {
+			type: 'beforeinput',
+			getTargetRanges: () => []
+		}, domEvtMock ) );
+
+		evtData = beforeInputSpy.firstCall.args[ 1 ];
+	}
 } );
