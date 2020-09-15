e6cde9743678d545f8b5e77d4e149103670ed6aa
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-09-15 13:21:58 +0200

    Integrated EnterObserver with the beforeinput event.

--- packages/ckeditor5-enter/src/enterobserver.js
+++ packages/ckeditor5-enter/src/enterobserver.js
@@ -10,6 +10,7 @@
 import Observer from '@ckeditor/ckeditor5-engine/src/view/observer/observer';
 import DomEventData from '@ckeditor/ckeditor5-engine/src/view/observer/domeventdata';
 import { keyCodes } from '@ckeditor/ckeditor5-utils/src/keyboard';
+import env from '@ckeditor/ckeditor5-utils/src/env';
 
 /**
  * Enter observer introduces the {@link module:engine/view/document~Document#event:enter} event.
@@ -20,25 +21,75 @@ export default class EnterObserver extends Observer {
 	constructor( view ) {
 		super( view );
 
-		const doc = this.document;
+		// Use the beforeinput DOM event to handle enter when supported by the browser.
+		// Fall back to the keydown event if beforeinput is not supported by the browser.
+		if ( env.features.isInputEventsLevel1Supported ) {
+			this._enableBeforeInputBasedObserver();
+		} else {
+			this._enableKeyEventsBasedObserver();
+		}
+	}
 
-		doc.on( 'keydown', ( evt, data ) => {
-			if ( this.isEnabled && data.keyCode == keyCodes.enter ) {
-				// Save the event object to check later if it was stopped or not.
-				let event;
-				doc.once( 'enter', evt => ( event = evt ), { priority: 'highest' } );
+	/**
+	 * TODO
+	 */
+	_enableBeforeInputBasedObserver() {
+		const viewDocument = this.view.document;
 
-				doc.fire( 'enter', new DomEventData( doc, data.domEvent, {
-					isSoft: data.shiftKey
-				} ) );
+		viewDocument.on( 'beforeinput', ( evt, data ) => {
+			if ( !this.isEnabled ) {
+				return;
+			}
 
-				// Stop `keydown` event if `enter` event was stopped.
-				// https://github.com/ckeditor/ckeditor5/issues/753
-				if ( event && event.stop.called ) {
-					evt.stop();
-				}
+			const domEvent = data.domEvent;
+			const { inputType } = domEvent;
+
+			if ( inputType === 'insertParagraph' ) {
+				this._fireEnterEvent( domEvent, evt.stop );
+				data.preventDefault();
+			} else if ( inputType === 'insertLineBreak' ) {
+				this._fireEnterEvent( domEvent, evt.stop, true );
+				data.preventDefault();
 			}
 		} );
+	}
+
+	/**
+	 * TODO
+	 */
+	_enableKeyEventsBasedObserver() {
+		const viewDocument = this.view.document;
+
+		viewDocument.on( 'keydown', ( evt, data ) => {
+			if ( !this.isEnabled ) {
+				return;
+			}
+
+			const { domEvent, shiftKey, keyCode } = data;
+
+			if ( keyCode === keyCodes.enter ) {
+				this._fireEnterEvent( domEvent, evt.stop, shiftKey );
+			}
+		} );
+	}
+
+	/**
+	 * TODO
+	 */
+	_fireEnterEvent( domEvent, stop, isSoft ) {
+		const viewDocument = this.view.document;
+
+		// Save the event object to check later if it was stopped or not.
+		let event;
+
+		viewDocument.once( 'enter', evt => ( event = evt ), { priority: Number.POSITIVE_INFINITY } );
+		viewDocument.fire( 'enter', new DomEventData( viewDocument, domEvent, { isSoft } ) );
+
+		// Stop `keydown` event if `enter` event was stopped.
+		// https://github.com/ckeditor/ckeditor5/issues/753
+		if ( event && event.stop.called ) {
+			stop();
+		}
 	}
 
 	/**
