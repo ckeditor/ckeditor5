49fa2e44ed074712444857dfe3d456185bc04d33
Kamil Piechaczek <pomek@users.noreply.github.com>
2020-10-06 12:18:17 +0200

    Improved the snippet adapter.

--- scripts/docs/snippetadapter.js
+++ scripts/docs/snippetadapter.js
@@ -393,7 +393,10 @@ function getWebpackConfig( snippets, config ) {
 		// Configure the paths so building CKEditor 5 snippets work even if the script
 		// is triggered from a directory outside ckeditor5 (e.g. multi-project case).
 		resolve: {
-			modules: getModuleResolvePaths()
+			modules: [
+				...getPackageDependenciesPaths(),
+				...getModuleResolvePaths()
+			]
 		},
 
 		resolveLoader: {
@@ -469,6 +472,21 @@ function getModuleResolvePaths() {
 		path.resolve( __dirname, '..', '..', 'node_modules' ),
 		'node_modules'
 	];
+}
+
+/**
+ * Returns an array that contains paths to packages' dependencies.
+ * The snippet adapter should use packages' dependencies instead of the documentation builder dependencies.
+ *
+ * See #7916.
+ *
+ * @returns {Array.<String>}
+ */
+function getPackageDependenciesPaths() {
+	const packagesDirectory = path.resolve( __dirname, '..', '..', 'packages' );
+
+	return fs.readdirSync( packagesDirectory )
+		.map( directory => path.join( packagesDirectory, directory, 'node_modules' ) );
 }
 
 /**
