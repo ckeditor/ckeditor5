ce9cf804d1a141d93deac1e089062b1aa6ffafe8
Paweł Kwaśnik <kwach2000@gmail.com>
2020-10-07 17:41:27 +0200

    Add the list style plugin to ckeditor5-build-decoupled-document

--- packages/ckeditor5-build-decoupled-document/src/ckeditor.js
+++ packages/ckeditor5-build-decoupled-document/src/ckeditor.js
@@ -37,6 +37,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import ListStyle from '@ckeditor/ckeditor5-list/src/liststyle';
 
 export default class DecoupledEditor extends DecoupledEditorBase {}
 
@@ -72,7 +73,8 @@ DecoupledEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	ListStyle
 ];
 
 // Editor configuration.
