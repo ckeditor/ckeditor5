f426f9fe876acad22f553d8568227282d4f715bc
Maciej Gołaszewski <jodator@jodator.net>
2020-10-22 13:18:49 +0200

    Refactor element reconversion sample.

--- packages/ckeditor5-engine/docs/_snippets/framework/element-reconversion-demo.js
+++ packages/ckeditor5-engine/docs/_snippets/framework/element-reconversion-demo.js
@@ -17,9 +17,70 @@ const getTypeFromViewElement = viewElement => {
 	return 'info';
 };
 
+const upcastInfoBox = ( viewElement, { writer } ) => {
+	const complexInfoBox = writer.createElement( 'complexInfoBox' );
+
+	const type = getTypeFromViewElement( viewElement );
+
+	writer.setAttribute( 'infoBoxType', type, complexInfoBox );
+	writer.setAttribute( 'infoBoxURL', 'https://ckeditor.com', complexInfoBox );
+
+	return complexInfoBox;
+};
+
+const downcastInfoBox = ( modelElement, { writer, consumable, mapper } ) => {
+	const complexInfoBoxView = writer.createContainerElement( 'div', { class: 'info-box' } );
+
+	const type = modelElement.getAttribute( 'infoBoxType' ) || 'info';
+
+	writer.addClass( `info-box-${ type }`, complexInfoBoxView );
+
+	for ( const child of modelElement.getChildren() ) {
+		const childView = writer.createContainerElement( 'div' );
+
+		if ( child.is( 'element', 'complexInfoBoxTitle' ) ) {
+			writer.addClass( 'info-box-title', childView );
+		} else {
+			writer.addClass( 'info-box-content', childView );
+		}
+
+		consumable.consume( child, 'insert' );
+		mapper.bindElements( child, childView );
+		writer.insert( writer.createPositionAt( complexInfoBoxView, 'end' ), childView );
+	}
+
+	return complexInfoBoxView;
+};
+
 class ComplexInfoBox {
 	constructor( editor ) {
-		// Schema definition.
+		this._defineSchema( editor );
+		this._defineConversion( editor );
+	}
+
+	_defineConversion( editor ) {
+		editor.conversion.for( 'upcast' )
+			.elementToElement( {
+				view: { name: 'div', classes: [ 'info-box' ] },
+				model: upcastInfoBox
+			} )
+			.elementToElement( {
+				view: { name: 'div', classes: [ 'info-box-title' ] },
+				model: 'complexInfoBoxTitle'
+			} )
+			.elementToElement( {
+				view: { name: 'div', classes: [ 'info-box-content' ] },
+				model: 'complexInfoBoxContent'
+			} );
+
+		// The downcast conversion must be split as you need a widget in the editing pipeline.
+		editor.conversion.for( 'downcast' ).elementToElement( {
+			model: 'complexInfoBox',
+			view: downcastInfoBox
+		} );
+	}
+
+	_defineSchema( editor ) {
 		editor.model.schema.register( 'complexInfoBox', {
 			allowWhere: '$block',
 			allowContentOf: '$root',
@@ -37,67 +98,6 @@ class ComplexInfoBox {
 			isLimit: true,
 			allowIn: 'complexInfoBox',
 			allowContentOf: '$root'
-		} );
-
-		// Upcast converter.
-		editor.conversion.for( 'upcast' )
-			.elementToElement( {
-				view: {
-					name: 'div',
-					classes: [ 'info-box' ]
-				},
-				model( viewElement, { writer } ) {
-					const complexInfoBox = writer.createElement( 'complexInfoBox' );
-
-					const type = getTypeFromViewElement( viewElement );
-
-					writer.setAttribute( 'infoBoxType', type, complexInfoBox );
-					writer.setAttribute( 'infoBoxURL', 'https://ckeditor.com', complexInfoBox );
-
-					return complexInfoBox;
-				}
-			} )
-			.elementToElement( {
-				view: {
-					name: 'div',
-					classes: [ 'info-box-title' ]
-				},
-				model: 'complexInfoBoxTitle'
-			} )
-			.elementToElement( {
-				view: {
-					name: 'div',
-					classes: [ 'info-box-content' ]
-				},
-				model: 'complexInfoBoxContent'
-			} );
-
-		// The downcast conversion must be split as you need a widget in the editing pipeline.
-		editor.conversion.for( 'downcast' ).elementToElement( {
-			model: 'complexInfoBox',
-			view( modelElement, { writer, consumable, mapper } ) {
-				const complexInfoBoxView = writer.createContainerElement( 'div', { class: 'info-box' } );
-
-				const type = modelElement.getAttribute( 'infoBoxType' ) || 'info';
-
-				writer.addClass( `info-box-${ type }`, complexInfoBoxView );
-
-				for ( const child of modelElement.getChildren() ) {
-					const childView = writer.createContainerElement( 'div' );
-
-					if ( child.is( 'element', 'complexInfoBoxTitle' ) ) {
-						writer.addClass( 'info-box-title', childView );
-					} else {
-						writer.addClass( 'info-box-content', childView );
-					}
-
-					consumable.consume( child, 'insert' );
-					mapper.bindElements( child, childView );
-					writer.insert( writer.createPositionAt( complexInfoBoxView, 'end' ), childView );
-				}
-
-				return complexInfoBoxView;
-			}
 		} );
 	}
 }
@@ -117,7 +117,7 @@ ClassicEditor
 			]
 		},
 		toolbar: {
-			items: [ 'paragraph', 'heading1', 'heading2', '|', '|', 'bold', 'italic', '|' ],
+			items: [ 'heading', '|', 'bold', 'italic' ],
 			viewportTopOffset: window.getViewportTopOffsetConfig()
 		}
 	} )
