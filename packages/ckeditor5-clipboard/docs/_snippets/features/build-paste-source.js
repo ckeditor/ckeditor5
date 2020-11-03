3f710203271fc6df88d198b232674222c7f0378a
Anna Tomanek <a.tomanek@cksource.com>
2020-11-03 13:15:00 +0100

    Image resize, superscript, subscript removed from the snippet configuration.

--- packages/ckeditor5-clipboard/docs/_snippets/features/build-paste-source.js
+++ packages/ckeditor5-clipboard/docs/_snippets/features/build-paste-source.js
@@ -7,19 +7,13 @@
 
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic/src/ckeditor';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
-import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
-import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import Font from '@ckeditor/ckeditor5-font/src/font';
-import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 
 ClassicEditor.builtinPlugins.push(
 	Strikethrough,
-	Subscript,
-	Superscript,
 	Underline,
-	Font,
-	ImageResize
+	Font
 );
 
 window.ClassicEditor = ClassicEditor;
