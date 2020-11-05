b4782bb8bc3d890b549df70aa0274e91055094be
Anna Tomanek <a.tomanek@cksource.com>
2020-11-05 09:47:42 +0100

    Fixed heading buttons demos to make them consistent with the default example.

--- packages/ckeditor5-heading/docs/_snippets/features/heading-buttons.js
+++ packages/ckeditor5-heading/docs/_snippets/features/heading-buttons.js
@@ -9,9 +9,9 @@ ClassicEditor
 		heading: {
 			options: [
 				{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-				{ model: 'heading1', view: 'h2', title: 'Heading 1', class: 'ck-heading_heading1' },
-				{ model: 'heading2', view: 'h3', title: 'Heading 2', class: 'ck-heading_heading2' },
-				{ model: 'heading3', view: 'h4', title: 'Heading 3', class: 'ck-heading_heading3' }
+				{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+				{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+				{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' }
 			]
 		}
 	} )
