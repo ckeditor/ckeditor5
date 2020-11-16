e603aad4a21cd10a97f5eeeda6811a15d3295aa3
Paweł Kwaśnik <kwach2000@gmail.com>
2020-11-16 15:03:51 +0100

    Deactivate save button when there are no changes.

--- packages/ckeditor5-html-embed/src/htmlembedediting.js
+++ packages/ckeditor5-html-embed/src/htmlembedediting.js
@@ -167,8 +167,6 @@ export default class HtmlEmbedEditing extends Plugin {
 						if ( newValue !== state.getRawHtmlValue() ) {
 							editor.execute( 'updateHtmlEmbed', newValue );
 							editor.editing.view.focus();
-						} else {
-							this.cancel();
 						}
 					},
 					cancel() {
