69459e357be8fab3e8128bb4ffeca7d984ba880d
Maciej Gołaszewski <jodator@jodator.net>
2020-11-24 15:00:01 +0100

    DLLify ckeditor5-utils.

---
+++ packages/ckeditor5-utils/index.js
@@ -0,0 +1,3 @@
+export { default as EmitterMixin } from './src/emittermixin';
+export { default as ObservableMixin } from './src/observablemixin';
+export { default as CKEditorError, logError, logWarning } from './src/ckeditorerror';
