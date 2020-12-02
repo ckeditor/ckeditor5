109b5bdee87c35e5f212e44cc7e8b58c371e2d7f
Maciej Gołaszewski <jodator@jodator.net>
2020-12-02 08:33:15 +0100

    Expose public-public API in ckeditor5-core.

---
+++ packages/ckeditor5-core/core.js
@@ -0,0 +1,13 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module core
+ */
+
+export { default as Plugin } from './src/plugin';
+export { default as Command } from './src/command';
+export { default as Editor } from './src/editor/editor';
+
--- packages/ckeditor5-dll/src/core.js
+++ packages/ckeditor5-dll/src/core.js
@@ -1,3 +1,1 @@
-export { default as Plugin } from '@ckeditor/ckeditor5-core/src/plugin';
-export { default as Command } from '@ckeditor/ckeditor5-core/src/command';
-export { default as Editor } from '@ckeditor/ckeditor5-core/src/editor/editor';
+export * from '@ckeditor/ckeditor5-core/core';
