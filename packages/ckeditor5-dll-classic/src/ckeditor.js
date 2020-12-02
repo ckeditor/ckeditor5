701af7347aa04c058d023b5828cb27945f31a450
Maciej Gołaszewski <jodator@jodator.net>
2020-12-02 08:59:19 +0100

    Add basic-styles DLL enabled build to the DLL sample.

--- packages/ckeditor5-dll-classic/src/ckeditor.js
+++ packages/ckeditor5-dll-classic/src/ckeditor.js
@@ -48,38 +48,8 @@ ClassicEditor.builtinPlugins = [
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'indent',
-			'outdent',
-			'|',
-			'imageUpload',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
 			'undo',
 			'redo'
-		]
-	},
-	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
