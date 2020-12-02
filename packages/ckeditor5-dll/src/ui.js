f73a6ffb2074bcf5d371ada30ac58949cb1d6b80
Maciej Gołaszewski <jodator@jodator.net>
2020-12-02 08:33:38 +0100

    Expose public-public API in ckeditor5-ui.

--- packages/ckeditor5-dll/src/ui.js
+++ packages/ckeditor5-dll/src/ui.js
@@ -1,1 +1,1 @@
-export { default as ButtonView } from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+export * from '@ckeditor/ckeditor5-ui/ui';
---
+++ packages/ckeditor5-ui/ui.js
@@ -0,0 +1,1 @@
+export { default as ButtonView } from './src/button/buttonview';
