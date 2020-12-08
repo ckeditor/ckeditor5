0be8e2a19d550273fb3f9c78935469b2153dc454
Kamil Piechaczek <pomek@users.noreply.github.com>
2020-12-08 10:14:45 +0100

    Added webpack config and updated package.json for feature packages.

--- packages/ckeditor5-table/webpack.config.js
+++ packages/ckeditor5-table/webpack.config.js
@@ -3,64 +3,13 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
+'use strict';
+
 /* eslint-env node */
 
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
+const { builds } = require( '@ckeditor/ckeditor5-dev-utils' );
 
-module.exports = {
-	mode: 'development',
-	optimization: {
-		minimize: false,
-		moduleIds: 'named'
-	},
-	entry: {
-		path: path.resolve( __dirname, 'table.js' )
-	},
-	output: {
-		path: path.resolve( __dirname, 'build' ),
-		filename: 'table.js',
-		library: [ 'CKEditor5', 'Table' ],
-		libraryTarget: 'umd',
-		libraryExport: 'default'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svg$/,
-				use: [ 'raw-loader' ]
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{
-						loader: 'style-loader',
-						options: {
-							injectType: 'singletonStyleTag',
-							attributes: {
-								'data-cke': true
-							}
-						}
-					},
-					{
-						loader: 'postcss-loader',
-						options: styles.getPostCssConfig( {
-							themeImporter: {
-								themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-							},
-							minify: true
-						} )
-					}
-				]
-			}
-		]
-	},
-	plugins: [
-		new webpack.DllReferencePlugin( {
-			manifest: require( '../../build/ckeditor5-dll.manifest.json' ),
-			scope: 'ckeditor5/src'
-		} )
-	]
-};
-
+module.exports = builds.getPluginWebpackConfig( {
+	themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' ),
+	packagePath: __dirname
+} );
