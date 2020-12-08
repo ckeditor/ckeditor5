b33b9b667629392d4bdbebec3ef070c58bf95fd6
godai78 <b.biedrzycki@cksource.com>
2020-12-08 08:19:17 +0100

    Docs: changing spellcheck language.

--- docs/_snippets/features/wproofreader.js
+++ docs/_snippets/features/wproofreader.js
@@ -17,6 +17,7 @@ ClassicEditor
 		plugins: [ ArticlePluginSet, EasyImage, WProofreader ],
 		wproofreader: {
 			serviceId: '1:Eebp63-lWHbt2-ASpHy4-AYUpy2-fo3mk4-sKrza1-NsuXy4-I1XZC2-0u2F54-aqYWd1-l3Qf14-umd',
+			lang: 'en_AI',
 			srcUrl: 'https://svc.webspellchecker.net/spellcheck31/wscbundle/wscbundle.js'
 		},
 		cloudServices: CS_CONFIG,
@@ -40,6 +41,7 @@ ClassicEditor
 				'undo',
 				'redo'
 			],
+			shouldNotGroupWhenFull: true,
 			viewportTopOffset: window.getViewportTopOffsetConfig()
 		},
 		image: {
