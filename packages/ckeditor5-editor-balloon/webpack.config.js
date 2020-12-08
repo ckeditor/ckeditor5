0d34a8d3066ac2b0eab9eb8497083509e3ef342b
Kamil Piechaczek <pomek@users.noreply.github.com>
2020-12-08 11:28:00 +0100

    Aligned to the changed in ckeditor5-dev.

--- packages/ckeditor5-editor-balloon/webpack.config.js
+++ packages/ckeditor5-editor-balloon/webpack.config.js
@@ -9,7 +9,7 @@
 
 const { builds } = require( '@ckeditor/ckeditor5-dev-utils' );
 
-module.exports = builds.getPluginWebpackConfig( {
+module.exports = builds.getDLLPluginWebpackConfig( {
 	themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' ),
 	packagePath: __dirname,
 	manifestPath: require.resolve( 'ckeditor5/build/ckeditor5-dll.manifest.json' )
