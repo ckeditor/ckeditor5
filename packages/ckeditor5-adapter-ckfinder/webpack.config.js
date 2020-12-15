822269cd6043cf5f00e460b2efc018f6ecfc69e8
Kamil Piechaczek <pomek@users.noreply.github.com>
2020-12-15 08:34:20 +0100

    s/getDLLPluginWebpackConfig/getDllPluginWebpackConfig.

--- packages/ckeditor5-adapter-ckfinder/webpack.config.js
+++ packages/ckeditor5-adapter-ckfinder/webpack.config.js
@@ -9,7 +9,7 @@
 
 const { builds } = require( '@ckeditor/ckeditor5-dev-utils' );
 
-module.exports = builds.getDLLPluginWebpackConfig( {
+module.exports = builds.getDllPluginWebpackConfig( {
 	themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' ),
 	packagePath: __dirname,
 	manifestPath: require.resolve( 'ckeditor5/build/ckeditor5-dll.manifest.json' )
