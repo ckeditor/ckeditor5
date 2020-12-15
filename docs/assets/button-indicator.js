6988dea95fd2313da0cbee1574aa61d4592ceaa2
Paweł Kwaśnik <kwach2000@gmail.com>
2020-12-16 00:24:22 +0100

    Changed close button in button indicator tooltip.

--- docs/assets/button-indicator.js
+++ docs/assets/button-indicator.js
@@ -4,14 +4,9 @@ function addItemIndicator( item, text ) {
 		return;
 	}
 
-	/* eslint max-len: ["error", 140, { "ignoreComments": true, "ignoreTemplateLiterals": true }] */
 	const content = `
 		<div class="tippy-content__message">${ text }</div>
-		<button class="ck ck-button tippy-content__close-button ck-off" >
-			<svg class="ck ck-icon ck-button__icon" viewBox="0 0 20 20">
-				<path d="M11.591 10.177l4.243 4.242a1 1 0 0 1-1.415 1.415l-4.242-4.243-4.243 4.243a1 1 0 0 1-1.414-1.415l4.243-4.242L4.52 5.934A1 1 0 0 1 5.934 4.52l4.243 4.243 4.242-4.243a1 1 0 1 1 1.415 1.414l-4.243 4.243z"></path>
-			</svg>
-		</button>
+		<button class="ck ck-button tippy-content__close-button ck-off"></button>
 	`;
 
 	// eslint-disable-next-line no-undef
