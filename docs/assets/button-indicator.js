80bb7d6e61253b5bcfca6f4bb050c5f76a874671
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-12-16 14:49:09 +0100

    Code refactoring in button-indicator.

--- docs/assets/button-indicator.js
+++ docs/assets/button-indicator.js
@@ -1,22 +1,23 @@
+/* global window, document */
+
 /**
- * Attach to any HTML element tooltip with its description.
+ * Attaches a tooltip with a description to any DOM node element.
  *
- * @param {HTMLElement} [item] HTML element.
- * @param {String} [text] Item description to be shown in the tooltip.
+ * @param {HTMLElement} [domNode] DOM node.
+ * @param {String} [text] The description to be shown in the tooltip.
  */
-// eslint-disable-next-line no-unused-vars
-function addItemIndicator( item, text ) {
-	if ( !item ) {
+window.addItemIndicator = function( domNode, text ) {
+	if ( !domNode ) {
 		return;
 	}
 
 	const content = `
 		<div class="tippy-content__message">${ text }</div>
-		<button class="ck ck-button tippy-content__close-button ck-off" />
+		<button class="ck ck-button tippy-content__close-button ck-off" title="Close"></button>
 	`;
 
 	// eslint-disable-next-line no-undef
-	const tooltip = tippy( item, {
+	const tooltip = tippy( domNode, {
 		content,
 		theme: 'light-border',
 		placement: 'bottom',
@@ -28,17 +29,17 @@ function addItemIndicator( item, text ) {
 		interactive: true,
 		touch: 'hold',
 		zIndex: 1,
-		// eslint-disable-next-line no-undef
 		appendTo: () => document.body
 	} );
 
 	// eslint-disable-next-line no-undef
 	const closeButton = tooltip.popper.querySelector( '.tippy-content__close-button' );
+
 	closeButton.addEventListener( 'click', () => {
 		tooltip.hide();
 	} );
 
-	item.addEventListener( 'click', () => {
+	domNode.addEventListener( 'click', () => {
 		tooltip.hide();
 	} );
-}
+};
