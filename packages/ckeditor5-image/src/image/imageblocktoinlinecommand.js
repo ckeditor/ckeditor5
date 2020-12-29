9147c491c223508faa4ab73c2d480edcd42ec90e
Paweł Kwaśnik <kwach2000@gmail.com>
2020-12-29 12:50:38 +0100

    Added tests.

--- packages/ckeditor5-image/src/image/imageblocktoinlinecommand.js
+++ packages/ckeditor5-image/src/image/imageblocktoinlinecommand.js
@@ -43,6 +43,11 @@ export default class ImageBlockToInlineCommand extends Command {
 
 			writer.append( imageInlineElement, paragraph );
 			model.insertContent( paragraph, position );
+
+			if ( imageInlineElement.parent ) {
+				writer.setSelection( imageInlineElement, 'on' );
+			}
+
 			writer.remove( imageElement );
 		} );
 	}
---
+++ packages/ckeditor5-image/tests/image/imageblocktoinlinecommand.js
@@ -0,0 +1,107 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import VirtualTestEditor from '@ckeditor/ckeditor5-core/tests/_utils/virtualtesteditor';
+import { setData as setModelData, getData as getModelData } from '@ckeditor/ckeditor5-engine/src/dev-utils/model';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
+import Image from '../../src/image/imageediting';
+import ImageBlockToInlineCommand from '../../src/image/imageblocktoinlinecommand';
+
+describe( 'ImageBlockToInlineCommand', () => {
+	let editor, command, model;
+
+	beforeEach( () => {
+		return VirtualTestEditor
+			.create( {
+				plugins: [ Image, Paragraph ]
+			} )
+			.then( newEditor => {
+				editor = newEditor;
+				model = editor.model;
+
+				command = new ImageBlockToInlineCommand( editor );
+
+				const schema = model.schema;
+				schema.extend( 'image', { allowAttributes: 'uploadId' } );
+			} );
+	} );
+
+	afterEach( () => {
+		return editor.destroy();
+	} );
+
+	describe( 'isEnabled', () => {
+		it( 'should be false when the selection directly in the root', () => {
+			model.enqueueChange( 'transparent', () => {
+				setModelData( model, '[]' );
+
+				command.refresh();
+				expect( command.isEnabled ).to.be.false;
+			} );
+		} );
+
+		it( 'should be false when the selection is in empty block', () => {
+			setModelData( model, '<paragraph>[]</paragraph>' );
+
+			expect( command.isEnabled ).to.be.false;
+		} );
+
+		it( 'should be false when the selection directly in a paragraph', () => {
+			setModelData( model, '<paragraph>foo[]</paragraph>' );
+			expect( command.isEnabled ).to.be.false;
+		} );
+
+		it( 'should be false when the selection directly in a block', () => {
+			model.schema.register( 'block', { inheritAllFrom: '$block' } );
+			model.schema.extend( '$text', { allowIn: 'block' } );
+			editor.conversion.for( 'downcast' ).elementToElement( { model: 'block', view: 'block' } );
+
+			setModelData( model, '<block>foo[]</block>' );
+			expect( command.isEnabled ).to.be.false;
+		} );
+
+		it( 'should be true when the selection is an block image', () => {
+			setModelData( model, '[<image></image>]' );
+			expect( command.isEnabled ).to.be.true;
+		} );
+
+		it( 'should be false when the selection is an inline image', () => {
+			setModelData( model, '<paragraph>[<imageInline></imageInline>]</paragraph>' );
+			expect( command.isEnabled ).to.be.false;
+		} );
+
+		it( 'should be false when the selection is inside other image', () => {
+			model.schema.register( 'caption', {
+				allowIn: 'image',
+				allowContentOf: '$block',
+				isLimit: true
+			} );
+			editor.conversion.for( 'downcast' ).elementToElement( { model: 'caption', view: 'figcaption' } );
+			setModelData( model, '<image><caption>[]</caption></image>' );
+			expect( command.isEnabled ).to.be.false;
+		} );
+
+		it( 'should be false when the selection is on other object', () => {
+			model.schema.register( 'object', { isObject: true, allowIn: '$root' } );
+			editor.conversion.for( 'downcast' ).elementToElement( { model: 'object', view: 'object' } );
+			setModelData( model, '[<object></object>]' );
+
+			expect( command.isEnabled ).to.be.false;
+		} );
+	} );
+
+	describe( 'execute()', () => {
+		it( 'should convert block image to inline image', () => {
+			const imgSrc = 'foo/bar.jpg';
+
+			setModelData( model, `[<image src="${ imgSrc }"></image>]` );
+
+			command.execute();
+
+			expect( getModelData( model ) ).to.equal( `<paragraph>[<imageInline src="${ imgSrc }"></imageInline>]</paragraph>` );
+		} );
+	} );
+} );
