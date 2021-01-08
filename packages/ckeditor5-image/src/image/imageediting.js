a24e72060d9fed48ee38dc105cede6660dacc600
Paweł Kwaśnik <kwach2000@gmail.com>
2021-01-08 17:18:43 +0100

    Update packages/ckeditor5-image/src/image/imageediting.js
    Co-authored-by: Aleksander Nowodzinski <a.nowodzinski@cksource.com>

--- packages/ckeditor5-image/src/image/imageediting.js
+++ packages/ckeditor5-image/src/image/imageediting.js
@@ -162,8 +162,7 @@ export function createImageViewElement( writer, imageType ) {
 	return container;
 }
 
-// {@link module:engine/view/matcher~Matcher} pattern. Returns function which checks if a given element is `<img>` element that is placed
-// inside the element of a provided type.
+// A function returning a {@link module:engine/view/matcher~Matcher} callback for a particular type of View images. 
 //
 // @param {String} parentName
 // @returns {Function}
