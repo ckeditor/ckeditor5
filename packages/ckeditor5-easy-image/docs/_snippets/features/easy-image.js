08fd7a2176211a31391e613ed483c506636aebb1
godai78 <b.biedrzycki@cksource.com>
2021-01-08 11:06:40 +0100

    Docs: toolbar indicators: image upload.

--- packages/ckeditor5-easy-image/docs/_snippets/features/easy-image.js
+++ packages/ckeditor5-easy-image/docs/_snippets/features/easy-image.js
@@ -16,6 +16,12 @@ ClassicEditor
 	} )
 	.then( editor => {
 		window.editor = editor;
+
+		window.attachTourBalloon( {
+			target: window.findToolbarItem( editor.ui.view.toolbar,
+				item => item.buttonView && item.buttonView.label && item.buttonView.label === 'Insert image' ),
+			text: 'Click to insert an image.'
+		} );
 	} )
 	.catch( err => {
 		console.error( err.stack );
