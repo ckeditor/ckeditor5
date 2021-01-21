ea6bebb88a3f409d32526dd498e5a2d47f6330df
Piotrek Koszuliński <pkoszulinski@gmail.com>
2021-01-21 14:56:28 +0100

    Removed tests for packages that were removed from the DLL.

--- tests/dll.js
+++ tests/dll.js
@@ -3,48 +3,36 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-import * as ballooneditorBase from '@ckeditor/ckeditor5-editor-balloon/ballooneditor';
-import * as ballooneditorDLL from '../src/ballooneditor';
-import * as classiceditorBase from '@ckeditor/ckeditor5-editor-classic/classiceditor';
-import * as classiceditorDLL from '../src/classiceditor';
-import * as clipboardBase from '@ckeditor/ckeditor5-clipboard/clipboard';
+import * as clipboardBase from '@ckeditor/ckeditor5-clipboard';
 import * as clipboardDLL from '../src/clipboard';
-import * as coreBase from '@ckeditor/ckeditor5-core/core';
+import * as coreBase from '@ckeditor/ckeditor5-core';
 import * as coreDLL from '../src/core';
-import * as decouplededitorBase from '@ckeditor/ckeditor5-editor-decoupled/decouplededitor';
-import * as decouplededitorDLL from '../src/decouplededitor';
-import * as engineBase from '@ckeditor/ckeditor5-engine/engine';
+import * as engineBase from '@ckeditor/ckeditor5-engine';
 import * as engineDLL from '../src/engine';
-import * as enterBase from '@ckeditor/ckeditor5-enter/enter';
+import * as enterBase from '@ckeditor/ckeditor5-enter';
 import * as enterDLL from '../src/enter';
-import * as inlineeditorBase from '@ckeditor/ckeditor5-editor-inline/inlineeditor';
-import * as inlineeditorDLL from '../src/inlineeditor';
-import * as paragraphBase from '@ckeditor/ckeditor5-paragraph/paragraph';
+import * as paragraphBase from '@ckeditor/ckeditor5-paragraph';
 import * as paragraphDLL from '../src/paragraph';
-import * as selectallBase from '@ckeditor/ckeditor5-select-all/selectall';
-import * as selectallDLL from '../src/selectall';
-import * as typingBase from '@ckeditor/ckeditor5-typing/typing';
+import * as selectallBase from '@ckeditor/ckeditor5-select-all';
+import * as selectallDLL from '../src/select-all';
+import * as typingBase from '@ckeditor/ckeditor5-typing';
 import * as typingDLL from '../src/typing';
-import * as uiBase from '@ckeditor/ckeditor5-ui/ui';
+import * as uiBase from '@ckeditor/ckeditor5-ui';
 import * as uiDLL from '../src/ui';
-import * as undoBase from '@ckeditor/ckeditor5-undo/undo';
+import * as undoBase from '@ckeditor/ckeditor5-undo';
 import * as undoDLL from '../src/undo';
-import * as uploadBase from '@ckeditor/ckeditor5-upload/upload';
+import * as uploadBase from '@ckeditor/ckeditor5-upload';
 import * as uploadDLL from '../src/upload';
-import * as utilsBase from '@ckeditor/ckeditor5-utils/utils';
+import * as utilsBase from '@ckeditor/ckeditor5-utils';
 import * as utilsDLL from '../src/utils';
-import * as widgetBase from '@ckeditor/ckeditor5-widget/widget';
+import * as widgetBase from '@ckeditor/ckeditor5-widget';
 import * as widgetDLL from '../src/widget';
 
 describe( 'CKEditor DLL', () => {
-	testEqualExports( 'ballooneditor', ballooneditorBase, ballooneditorDLL );
-	testEqualExports( 'classiceditor', classiceditorBase, classiceditorDLL );
 	testEqualExports( 'clipboard', clipboardBase, clipboardDLL );
 	testEqualExports( 'core', coreBase, coreDLL );
-	testEqualExports( 'decouplededitor', decouplededitorBase, decouplededitorDLL );
 	testEqualExports( 'engine', engineBase, engineDLL );
 	testEqualExports( 'enter', enterBase, enterDLL );
-	testEqualExports( 'inlineeditor', inlineeditorBase, inlineeditorDLL );
 	testEqualExports( 'paragraph', paragraphBase, paragraphDLL );
 	testEqualExports( 'selectall', selectallBase, selectallDLL );
 	testEqualExports( 'typing', typingBase, typingDLL );
