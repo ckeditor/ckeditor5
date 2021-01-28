d9f72e66a2820a4f039224b2a64f47859d7097ba
Kuba Niegowski <j.niegowski@cksource.com>
2021-01-28 13:52:31 +0100

    Simplified downcast conversion for image caption.

--- packages/ckeditor5-image/src/imagecaption/imagecaptionediting.js
+++ packages/ckeditor5-image/src/imagecaption/imagecaptionediting.js
@@ -10,7 +10,7 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import { isImage } from '../image/utils';
 import ImageCaptionToggleCommand from './imagecaptiontogglecommand';
-import { createCaptionElement, getCaptionFromImage, matchImageCaption } from './utils';
+import { createCaptionElement, matchImageCaption } from './utils';
 
 /**
  * The image caption engine plugin.
@@ -35,8 +35,6 @@ export default class ImageCaptionEditing extends Plugin {
 		const editor = this.editor;
 		const view = editor.editing.view;
 		const schema = editor.model.schema;
-		const data = editor.data;
-		const editing = editor.editing;
 		const t = editor.t;
 
 		/**
@@ -70,110 +68,26 @@ export default class ImageCaptionEditing extends Plugin {
 			model: 'caption'
 		} );
 
-		// Model to view converter for the data pipeline.
-		const createCaptionForData = writer => writer.createContainerElement( 'figcaption' );
-		data.downcastDispatcher.on( 'insert:caption', captionModelToView( createCaptionForData, false ) );
+		editor.conversion.for( 'dataDowncast' ).elementToElement( {
+			model: 'caption',
+			view: ( modelElement, { writer } ) => {
+				if ( !isImage( modelElement.parent ) ) {
+					return null;
+				}
+
+				return writer.createContainerElement( 'figcaption' );
+			}
+		} );
 
 		editor.conversion.for( 'editingDowncast' ).elementToElement( {
 			model: 'caption',
 			view: ( modelElement, { writer } ) => {
 				if ( !isImage( modelElement.parent ) ) {
-
+					return null;
 				}
 
 				return createCaptionElement( view, writer, t( 'Enter image caption' ) );
 			}
 		} );
 	}
-}
-
-// Creates a converter that converts image caption model element to view element.
-//
-// @private
-// @param {Function} elementCreator
-// @param {Boolean} [hide=true] When set to `false` view element will not be inserted when it's empty.
-// @returns {Function}
-function captionModelToView( elementCreator, shouldInsertWhenEmpty = true ) {
-	return ( evt, data, conversionApi ) => {
-		const captionElement = data.item;
-
-		if ( !isImage( captionElement.parent ) ) {
-			return;
-		}
-
-		if ( !conversionApi.consumable.consume( data.item, 'insert' ) ) {
-			return;
-		}
-
-		const viewImage = conversionApi.mapper.toViewElement( data.range.start.parent );
-		const viewCaption = elementCreator( conversionApi.writer );
-		const viewWriter = conversionApi.writer;
-
-		// Hide if empty.
-		if ( !captionElement.childCount ) {
-			viewWriter.addClass( 'ck-hidden', viewCaption );
-		}
-
-		insertViewCaptionAndBind( viewCaption, data.item, viewImage, conversionApi );
-	};
-}
-
-// Inserts `viewCaption` at the end of `viewImage` and binds it to `modelCaption`.
-//
-// @private
-// @param {module:engine/view/containerelement~ContainerElement} viewCaption
-// @param {module:engine/model/element~Element} modelCaption
-// @param {module:engine/view/containerelement~ContainerElement} viewImage
-// @param {module:engine/conversion/downcastdispatcher~DowncastConversionApi} conversionApi
-function insertViewCaptionAndBind( viewCaption, modelCaption, viewImage, conversionApi ) {
-	const viewPosition = conversionApi.writer.createPositionAt( viewImage, 'end' );
-
-	conversionApi.writer.insert( viewPosition, viewCaption );
-	conversionApi.mapper.bindElements( modelCaption, viewCaption );
-}
-
-// Checks if the provided node or one of its ancestors is a caption element, and returns it.
-//
-// @private
-// @param {module:engine/model/node~Node} node
-// @returns {module:engine/model/element~Element|null}
-function getParentCaption( node ) {
-	const ancestors = node.getAncestors( { includeSelf: true } );
-	const caption = ancestors.find( ancestor => ancestor.name == 'caption' );
-
-	if ( caption && caption.parent && caption.parent.name == 'image' ) {
-		return caption;
-	}
-
-	return null;
-}
-
-// Hides a given caption in the view if it is empty.
-//
-// @private
-// @param {module:engine/view/containerelement~ContainerElement} caption
-// @param {module:engine/view/downcastwriter~DowncastWriter} viewWriter
-// @returns {Boolean} Returns `true` if the view was modified.
-function hideCaptionIfEmpty( caption, viewWriter ) {
-	if ( !caption.childCount && !caption.hasClass( 'ck-hidden' ) ) {
-		viewWriter.addClass( 'ck-hidden', caption );
-		return true;
-	}
-
-	return false;
-}
-
-// Shows the caption.
-//
-// @private
-// @param {module:engine/view/containerelement~ContainerElement} caption
-// @param {module:engine/view/downcastwriter~DowncastWriter} viewWriter
-// @returns {Boolean} Returns `true` if the view was modified.
-function showCaption( caption, viewWriter ) {
-	if ( caption.hasClass( 'ck-hidden' ) ) {
-		viewWriter.removeClass( 'ck-hidden', caption );
-		return true;
-	}
-
-	return false;
 }
