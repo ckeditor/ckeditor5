08c601244474cca70f35c65faef9619660fbc6fc
Kuba Niegowski <j.niegowski@cksource.com>
2021-02-05 10:28:05 +0100

    Code clean.

--- packages/ckeditor5-engine/src/model/operation/transform.js
+++ packages/ckeditor5-engine/src/model/operation/transform.js
@@ -2126,11 +2126,7 @@ setTransformation( SplitOperation, MoveOperation, ( a, b, context ) => {
 	// After split:
 	// <paragraph>A</paragraph><paragraph>d</paragraph><paragraph>Xbcyz</paragraph>
 	//
-	if (
-		a.splitPosition.hasSameParentAs( b.sourcePosition ) &&
-		rangeToMove.containsPosition( a.splitPosition ) &&
-		!a.graveyardPosition
-	) {
+	if ( a.splitPosition.hasSameParentAs( b.sourcePosition ) && rangeToMove.containsPosition( a.splitPosition ) && !a.graveyardPosition ) {
 		const howManyRemoved = b.howMany - ( a.splitPosition.offset - b.sourcePosition.offset );
 		a.howMany -= howManyRemoved;
 
