87325aafc1d44a191b93b6c934fd154d55678a76
limengning <micheal_li@foxmail.com>
2021-02-08 11:58:44 +0800

    Remove duplicate plugin.

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -4,42 +4,40 @@
  */
 
 // The editor creator to use.
-import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
-import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
-import Base64UploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter";
-import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
-import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
-import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
-import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
-import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
-import EasyImage from "@ckeditor/ckeditor5-easy-image/src/easyimage";
-import Heading from "@ckeditor/ckeditor5-heading/src/heading";
-import Image from "@ckeditor/ckeditor5-image/src/image";
-import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
-import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
-import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
-import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
-import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize";
-import Indent from "@ckeditor/ckeditor5-indent/src/indent";
-import Link from "@ckeditor/ckeditor5-link/src/link";
-import List from "@ckeditor/ckeditor5-list/src/list";
-import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
-import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
-import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
-import Table from "@ckeditor/ckeditor5-table/src/table";
-import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
-import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
-import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
-import RemoveFormat from "@ckeditor/ckeditor5-remove-format/src/removeformat";
-import HtmlEmbed from "@ckeditor/ckeditor5-html-embed/src/htmlembed";
-import Font from "@ckeditor/ckeditor5-font/src/font";
-import WordCount from "@ckeditor/ckeditor5-word-count/src/wordcount";
-import SelectAll from "@ckeditor/ckeditor5-select-all/src/selectall";
-import FileUpload from "./ckeditor5-file-upload/fileupload";
-import SimpleFileUploadAdapter from "./ckeditor5-file-upload/src/simplefileuploadadapter";
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-
+import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
+import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed';
+import Font from '@ckeditor/ckeditor5-font/src/font';
+import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount';
+import SelectAll from '@ckeditor/ckeditor5-select-all/src/selectall';
+import FileUpload from './ckeditor5-file-upload/fileupload';
+import SimpleFileUploadAdapter from './ckeditor5-file-upload/src/simplefileuploadadapter';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -76,87 +74,86 @@ ClassicEditor.builtinPlugins = [
 	WordCount,
 	SelectAll,
 	FileUpload,
-	SimpleFileUploadAdapter,
-	PasteFromOffice,
+	SimpleFileUploadAdapter
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
-			"heading",
-			"|",
-			"alignment",
-			"bold",
-			"italic",
-			"link",
-			"removeFormat",
-			"|",
-			"bulletedList",
-			"numberedList",
-			"indent",
-			"outdent",
-			"|",
-			"imageUpload",
-			"fileUpload",
-			"blockQuote",
-			"insertTable",
-			"htmlEmbed",
-			"mediaEmbed",
-			"undo",
-			"redo",
-			"|",
-			"fontColor",
-			"fontBackgroundColor",
-		],
+			'heading',
+			'|',
+			'alignment',
+			'bold',
+			'italic',
+			'link',
+			'removeFormat',
+			'|',
+			'bulletedList',
+			'numberedList',
+			'indent',
+			'outdent',
+			'|',
+			'imageUpload',
+			'fileUpload',
+			'blockQuote',
+			'insertTable',
+			'htmlEmbed',
+			'mediaEmbed',
+			'undo',
+			'redo',
+			'|',
+			'fontColor',
+			'fontBackgroundColor'
+		]
 	},
 	image: {
 		toolbar: [
-			"imageStyle:full",
-			"imageStyle:side",
-			"|",
-			"imageTextAlternative",
-		],
+			'imageStyle:full',
+			'imageStyle:side',
+			'|',
+			'imageTextAlternative'
+		]
 	},
 	table: {
-		contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+		contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: "zh-cn",
+	language: 'zh-cn',
 	heading: {
 		options: [
 			{
-				model: "paragraph",
-				title: "正文",
-				class: "ck-heading_paragraph",
+				model: 'paragraph',
+				title: '正文',
+				class: 'ck-heading_paragraph'
 			},
 			{
-				model: "heading1",
-				view: "h1",
-				title: "标题 1",
-				class: "ck-heading_heading1",
+				model: 'heading1',
+				view: 'h1',
+				title: '标题 1',
+				class: 'ck-heading_heading1'
 			},
 			{
-				model: "heading2",
-				view: "h2",
-				title: "标题 2",
-				class: "ck-heading_heading2",
+				model: 'heading2',
+				view: 'h2',
+				title: '标题 2',
+				class: 'ck-heading_heading2'
 			},
 			{
-				model: "heading3",
-				view: "h3",
-				title: "标题 3",
-				class: "ck-heading_heading3",
+				model: 'heading3',
+				view: 'h3',
+				title: '标题 3',
+				class: 'ck-heading_heading3'
 			},
 			{
-				model: "heading4",
-				view: "h4",
-				title: "标题 4",
-				class: "ck-heading_heading4",
-			},
-		],
+				model: 'heading4',
+				view: 'h4',
+				title: '标题 4',
+				class: 'ck-heading_heading4'
+			}
+		]
 	},
 	simpleFileUpload: {
-		fileTypes: [".pdf", ".doc", ".docx", ".xls", ".xlsx"],
-	},
+		fileTypes: [ '.pdf', '.doc', '.docx', '.xls', '.xlsx' ]
+	}
 };
