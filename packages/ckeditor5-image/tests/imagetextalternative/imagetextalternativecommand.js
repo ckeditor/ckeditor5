10aa872920d0826410f742cbc5a8f28891aa0232
Paweł Kwaśnik <kwach2000@gmail.com>
2021-02-18 12:10:59 +0100

    Added tests.

--- packages/ckeditor5-image/tests/imagetextalternative/imagetextalternativecommand.js
+++ packages/ckeditor5-image/tests/imagetextalternative/imagetextalternativecommand.js
@@ -24,17 +24,18 @@ describe( 'ImageTextAlternativeCommand', () => {
 					isBlock: true,
 					alllowAttributes: [ 'alt', 'src' ]
 				} );
+
+				model.schema.register( 'imageInline', {
+					allowWhere: '$text',
+					isObject: true,
+					isInline: true,
+					allowAttributes: [ 'alt', 'src', 'srcset' ]
+				} );
 			} );
 	} );
 
 	it( 'should have false value if no image is selected', () => {
 		setData( model, '[]<p></p>' );
-
-		expect( command.value ).to.be.false;
-	} );
-
-	it( 'should have false value if image without alt is selected', () => {
-		setData( model, '[<image src="image.png"></image>]' );
 
 		expect( command.value ).to.be.false;
 	} );
@@ -45,43 +46,99 @@ describe( 'ImageTextAlternativeCommand', () => {
 		expect( command.isEnabled ).to.be.false;
 	} );
 
-	it( 'should be enabled on image element without alt attribute', () => {
-		setData( model, '[<image src="image.png"></image>]' );
+	describe( 'on block image', () => {
+		it( 'should have false value if image without alt is selected', () => {
+			setData( model, '[<image src="image.png"></image>]' );
 
-		expect( command.isEnabled ).to.be.true;
+			expect( command.value ).to.be.false;
+		} );
+
+		it( 'should be enabled on image element without alt attribute', () => {
+			setData( model, '[<image src="image.png"></image>]' );
+
+			expect( command.isEnabled ).to.be.true;
+		} );
+
+		it( 'should have proper value if on image element with alt attribute', () => {
+			setData( model, '[<image src="image.png" alt="foo bar baz"></image>]' );
+
+			expect( command.value ).to.equal( 'foo bar baz' );
+		} );
+
+		it( 'should set proper alt if executed on image without alt attribute', () => {
+			setData( model, '[<image src="image.png"></image>]' );
+
+			command.execute( { newValue: 'fiz buz' } );
+
+			expect( getData( model ) ).to.equal( '[<image alt="fiz buz" src="image.png"></image>]' );
+		} );
+
+		it( 'should change alt if executed on image with alt attribute', () => {
+			setData( model, '[<image alt="foo bar" src="image.png"></image>]' );
+
+			command.execute( { newValue: 'fiz buz' } );
+
+			expect( getData( model ) ).to.equal( '[<image alt="fiz buz" src="image.png"></image>]' );
+		} );
+
+		it( 'should use parent batch', () => {
+			setData( model, '[<image src="image.png"></image>]' );
+
+			model.change( writer => {
+				expect( writer.batch.operations ).to.length( 0 );
+
+				command.execute( { newValue: 'foo bar' } );
+
+				expect( writer.batch.operations ).to.length.above( 0 );
+			} );
+		} );
 	} );
 
-	it( 'should have proper value if on image element with alt attribute', () => {
-		setData( model, '[<image src="image.png" alt="foo bar baz"></image>]' );
+	describe( 'on inline image', () => {
+		it( 'should have false value if image without alt is selected', () => {
+			setData( model, '<p>[<imageInline src="image.png"></imageInline>]</p>' );
 
-		expect( command.value ).to.equal( 'foo bar baz' );
-	} );
+			expect( command.value ).to.be.false;
+		} );
 
-	it( 'should set proper alt if executed on image without alt attribute', () => {
-		setData( model, '[<image src="image.png"></image>]' );
+		it( 'should be enabled on image element without alt attribute', () => {
+			setData( model, '<p>[<imageInline src="image.png"></imageInline>]</p>' );
 
-		command.execute( { newValue: 'fiz buz' } );
+			expect( command.isEnabled ).to.be.true;
+		} );
 
-		expect( getData( model ) ).to.equal( '[<image alt="fiz buz" src="image.png"></image>]' );
-	} );
+		it( 'should have proper value if on image element with alt attribute', () => {
+			setData( model, '<p>[<imageInline src="image.png" alt="foo bar baz"></imageInline>]</p>' );
 
-	it( 'should change alt if executed on image with alt attribute', () => {
-		setData( model, '[<image alt="foo bar" src="image.png"></image>]' );
+			expect( command.value ).to.equal( 'foo bar baz' );
+		} );
 
-		command.execute( { newValue: 'fiz buz' } );
+		it( 'should set proper alt if executed on image without alt attribute', () => {
+			setData( model, '<p>[<imageInline src="image.png"></imageInline>]</p>' );
 
-		expect( getData( model ) ).to.equal( '[<image alt="fiz buz" src="image.png"></image>]' );
-	} );
+			command.execute( { newValue: 'fiz buz' } );
 
-	it( 'should use parent batch', () => {
-		setData( model, '[<image src="image.png"></image>]' );
+			expect( getData( model ) ).to.equal( '<p>[<imageInline alt="fiz buz" src="image.png"></imageInline>]</p>' );
+		} );
 
-		model.change( writer => {
-			expect( writer.batch.operations ).to.length( 0 );
+		it( 'should change alt if executed on image with alt attribute', () => {
+			setData( model, '<p>[<imageInline alt="foo bar" src="image.png"></imageInline>]</p>' );
 
-			command.execute( { newValue: 'foo bar' } );
+			command.execute( { newValue: 'fiz buz' } );
 
-			expect( writer.batch.operations ).to.length.above( 0 );
+			expect( getData( model ) ).to.equal( '<p>[<imageInline alt="fiz buz" src="image.png"></imageInline>]</p>' );
+		} );
+
+		it( 'should use parent batch', () => {
+			setData( model, '<p>[<imageInline src="image.png"></imageInline>]</p>' );
+
+			model.change( writer => {
+				expect( writer.batch.operations ).to.length( 0 );
+
+				command.execute( { newValue: 'foo bar' } );
+
+				expect( writer.batch.operations ).to.length.above( 0 );
+			} );
 		} );
 	} );
 } );
