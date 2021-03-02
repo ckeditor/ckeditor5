4dade26ead94d5462d7a4add7931428ba0f70d71
Magdalena Chrześcian <m.chrzescian@cksource.com>
2021-03-02 09:53:47 +0100

    Updated the manual test.

--- packages/ckeditor5-image/tests/manual/imagestyle.js
+++ packages/ckeditor5-image/tests/manual/imagestyle.js
@@ -7,30 +7,31 @@
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
-import ImageBlock from '../../src/imageblock';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import ImageStyle from '../../src/imagestyle';
-import ImageToolbar from '../../src/imagetoolbar';
-import ImageCaption from '../../src/imagecaption';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import ImageInsert from '../../src/imageinsert';
 
 ClassicEditor
 	.create( document.querySelector( '#editor-semantic' ), {
 		plugins: [
-			Essentials,
-			ImageBlock,
-			ImageStyle,
-			ImageToolbar,
-			ImageCaption,
-			Paragraph,
-			ImageInsert
+			ArticlePluginSet
 		],
 		toolbar: [
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'link',
+			'bulletedList',
+			'numberedList',
+			'blockQuote',
+			'insertTable',
+			'mediaEmbed',
 			'undo',
 			'redo'
 		],
 		image: {
+			styles: {
+				arrangements: [ 'full', 'side' ],
+				groups: []
+			},
 			toolbar: [ 'imageStyle:full', 'imageStyle:side' ]
 		}
 	} )
@@ -64,7 +65,8 @@ ClassicEditor
 		],
 		image: {
 			styles: {
-				arrangements: [ 'alignLeft', 'alignCenter', 'alignRight' ]
+				arrangements: [ 'alignLeft', 'alignCenter', 'alignRight' ],
+				groups: []
 			},
 			toolbar: [ 'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight' ]
 		}
@@ -97,7 +99,15 @@ ClassicEditor
 			'undo',
 			'redo'
 		],
-		image: getConfig()
+		image: {
+			toolbar: [
+				'imageStyle:inline',
+				'imageStyle:wrapText',
+				'imageStyle:breakText',
+				'|',
+				'imageTextAlternative'
+			]
+		}
 	} )
 	.then( editor => {
 		window.editorInline = editor;
@@ -107,200 +117,3 @@ ClassicEditor
 	.catch( err => {
 		console.error( err.stack );
 	} );
-
-function getConfig( type ) {
-	const configs = {
-		setItemsInGroup: {
-			styles: {
-				groups: [
-					{
-						name: 'inParagraph',
-						items: [ 'alignLeft' ]
-					},
-					'betweenParagraphs'
-				]
-			},
-			toolbar: [
-				'imageStyle:inline',
-				'imageStyle:inParagraph',
-				'imageStyle:betweenParagraphs',
-				'|',
-				'imageTextAlternative'
-			]
-		},
-
-		onlyToolbar: {
-			toolbar: [
-				'imageStyle:inline',
-				'imageStyle:inParagraph',
-				'imageStyle:betweenParagraphs',
-				'|',
-				'imageTextAlternative'
-			]
-		},
-
-		validStyles: {
-			styles: {
-				arrangements: [
-					{
-						name: 'side',
-						title: 'Side image',
-						icon: 'inLineLeft',
-						modelElement: 'imageInline',
-						className: 'image-style-side'
-					},
-					'full'
-				],
-				groups: [
-					{
-						name: 'custom',
-						title: 'Image in paragraph',
-						defaultIcon: 'inLineLeft',
-						items: [ 'side', 'full' ]
-					}
-				]
-			},
-			toolbar: [
-				'imageStyle:inLine',
-				'imageStyle:inParagraph',
-				'imageStyle:betweenParagraphs',
-				'|',
-				'imageTextAlternative'
-			]
-		},
-
-		undeclaredItemInGroup: { // 1
-			styles: {
-				arrangements: [
-					{
-						name: 'side',
-						title: 'Side image',
-						icon: 'inLineLeft',
-						modelElement: 'imageInline',
-						className: 'image-style-side'
-					}
-				],
-				groups: [
-					{
-						name: 'custom',
-						title: 'Image in paragraph',
-						defaultIcon: 'inLineLeft',
-						items: [ 'side', 'full' ]
-					}
-				]
-			},
-			toolbar: [
-				'imageStyle:custom',
-				'imageStyle:full',
-				'|',
-				'imageTextAlternative'
-			]
-		},
-
-		// expected result: toolbarview-item-unavailable
-		undeclaredItemButton: { // 2
-			styles: {
-				arrangements: [
-					{
-						name: 'side',
-						title: 'Side image',
-						icon: 'inLineLeft',
-						modelElement: 'imageInline',
-						className: 'image-style-side'
-					}
-				]
-			},
-			toolbar: [
-				'imageStyle:full',
-				'|',
-				'imageTextAlternative'
-			]
-		},
-
-		// expected result: toolbarview-item-unavailable
-		undeclaredGroup: { // 3
-			styles: {
-				arrangements: [
-					{
-						name: 'side',
-						title: 'Side image',
-						icon: 'inLineLeft',
-						modelElement: 'imageInline',
-						className: 'image-style-side'
-					}
-				],
-				groups: [
-					'inParagraph'
-				]
-			},
-			toolbar: [
-				'imageStyle:inLine',
-				'|',
-				'imageTextAlternative'
-			]
-		},
-
-		// requires removing ImageInline plugin
-		// expected result: image-style-not-supported
-		unsupportedItemInGroup: { // 4
-			styles: {
-				arrangements: [
-					{
-						name: 'side',
-						title: 'Side image',
-						icon: 'inLineLeft',
-						modelElement: 'imageInline',
-						className: 'image-style-side'
-					}
-				],
-				groups: [
-					{
-						name: 'custom',
-						title: 'Image in paragraph',
-						defaultIcon: 'inLineLeft',
-						items: [ 'side' ]
-					}
-				]
-			},
-			toolbar: [
-				'imageStyle:custom',
-				'|',
-				'imageTextAlternative'
-			]
-		},
-
-		// requires removing ImageInline plugin
-		// expected result: image-style-not-supported
-		// expected result: toolbar-item-unavailable
-		arrangementUnsupported: { // 4
-			styles: {
-				arrangements: [
-					{
-						name: 'side',
-						title: 'Side image',
-						icon: 'inLineLeft',
-						modelElement: 'imageInline',
-						className: 'image-style-side'
-					}
-				], groups: []
-			},
-			toolbar: [
-				'imageStyle:side',
-				'|',
-				'imageTextAlternative'
-			]
-		},
-		styleNotFound: {
-			styles: {
-				arrangements: [ 'custom' ]
-			},
-			toolbar: 'imageStyle:custom'
-		}
-	};
-
-	if ( type ) {
-		return configs[ type ];
-	} else {
-		return configs.onlyToolbar;
-	}
-}
