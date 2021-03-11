8df630e15a31a00b7bb3c32702934fd138b4e0d8
Kamil Piechaczek <pomek@users.noreply.github.com>
2021-03-11 20:16:50 +0100

    Will it work now 2?

--- packages/ckeditor5-media-embed/docs/_snippets/features/media-embed.js
+++ packages/ckeditor5-media-embed/docs/_snippets/features/media-embed.js
@@ -28,13 +28,14 @@ ClassicEditor
 		console.error( err.stack );
 	} );
 
-// Due to unknown reasons, Travis cannot load video from Vimeo. Let's ignore this error.
+// For a totally unknown reason, Travis and Vimeo do not like each other and the test fail on CI.
 const metaElement = document.createElement( 'meta' );
 
 metaElement.name = 'x-cke-crawler-ignore-patterns';
 metaElement.content = JSON.stringify( {
+	'request-failure': 'vimeo.com',
 	'response-failure': 'vimeo.com',
-	'console-error': '<svg> attribute preserveAspectRatio'
+	'console-error': [ '<svg> attribute preserveAspectRatio', 'vimeo.com' ]
 } );
 
 document.head.appendChild( metaElement );
