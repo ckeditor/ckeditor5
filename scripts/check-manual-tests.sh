139bb03a14b3f9419df8aa2e255a8763b8b53c7f
Kamil Piechaczek <pomek@users.noreply.github.com>
2021-03-11 18:36:41 +0100

    Break the script if a subcommand failed.

--- scripts/check-manual-tests.sh
+++ scripts/check-manual-tests.sh
@@ -3,6 +3,8 @@
 # @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
 # For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 
+set -e
+
 while getopts f:r:i: flag
 do
     case "${flag}" in
