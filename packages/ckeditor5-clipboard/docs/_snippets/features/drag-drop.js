a47c4136dcbd17a63bf84252519807c1695ed661
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2021-03-16 11:40:13 +0100

    Refined the drag and drop demo.

--- packages/ckeditor5-clipboard/docs/_snippets/features/drag-drop.js
+++ packages/ckeditor5-clipboard/docs/_snippets/features/drag-drop.js
@@ -43,6 +43,8 @@ import { toWidget, viewToModelPositionOutsideModelElement } from '@ckeditor/cked
 
 import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud-services-config';
 
+const contactsContainer = document.querySelector( '.contacts' );
+
 //
 // The H-Card editor plugin.
 //
@@ -265,6 +267,18 @@ ClassicEditor
 	} )
 	.then( editor => {
 		window.editor = editor;
+
+		window.attachTourBalloon( {
+			target: contactsContainer.childNodes[ 2 ],
+			text: 'Drag and drop me in the editor.',
+			editor,
+			tippyOptions: {
+				placement: 'top',
+				onHide: () => {
+					contactsContainer.classList.remove( 'tour-balloon-visible' );
+				}
+			}
+		} );
 	} )
 	.catch( err => {
 		console.error( err.stack );
@@ -273,14 +287,13 @@ ClassicEditor
 //
 // Draggable H-Cards list.
 //
-const contactsContainer = document.querySelector( '#contactList' );
 
 const contacts = [
 	{ name: 'Huckleberry Finn',			tel: '+48 1345 234 235', email: 'h.finn@example.com', avatar: 'hfin' },
 	{ name: 'D\'Artagnan',				tel: '+45 2345 234 235', email: 'dartagnan@example.com', avatar: 'dartagnan' },
-	{ name: 'Phileas Fogg',				tel: '+44 3345 234 235', email: 'p.fogg@example.com', avatar: 'pfog' },
-	{ name: 'Alice',					tel: '+20 4345 234 235', email: 'alice@example.com', avatar: 'alice' },
 	{ name: 'Little Red Riding Hood',	tel: '+45 2345 234 235', email: 'lrrh@example.com', avatar: 'lrrh' },
+	{ name: 'Alice',					tel: '+20 4345 234 235', email: 'alice@example.com', avatar: 'alice' },
+	{ name: 'Phileas Fogg',				tel: '+44 3345 234 235', email: 'p.fogg@example.com', avatar: 'pfog' },
 	{ name: 'Winnetou',					tel: '+44 3345 234 235', email: 'winnetou@example.com', avatar: 'winetou' },
 	{ name: 'Edmond Dantès',			tel: '+20 4345 234 235', email: 'count@example.com', avatar: 'edantes' },
 	{ name: 'Robinson Crusoe',			tel: '+45 2345 234 235', email: 'r.crusoe@example.com', avatar: 'rcrusoe' }
--- packages/ckeditor5-clipboard/docs/features/drag-drop.md
+++ packages/ckeditor5-clipboard/docs/features/drag-drop.md
@@ -12,7 +12,7 @@ The drag and drop feature is implemented by the {@link module:clipboard/dragdrop
 
 ## Demo
 
-The sample below allows you to drag contacts from the list on the right-hand side to the editor on the left-hand side. They are inserted into the editor as custom widgets representing the [h-card microformat](http://microformats.org/wiki/h-card).
+The sample below allows you to drag contacts from the list to the editor content. They are inserted into the editor as custom widgets representing the [h-card microformat](http://microformats.org/wiki/h-card).
 
 {@snippet features/drag-drop}
 
@@ -27,7 +27,7 @@ CKEditor 5 supports dropping images from the file system by the {@link features/
 ## Installation
 
 <info-box info>
-	This feature is required by the clipboard plugin and is enabled by default in all official builds. The installation instructions are for developers interested in building their own custom rich-text editor.
+	This feature is enabled by default in all {@link builds/guides/overview ready-to-use editor builds} (a dependency of the {@link module:clipboard/clipboard~Clipboard} plugin). The installation instructions are for developers interested in building their own custom rich-text editor.
 </info-box>
 
 To add this feature to your rich-text editor, install the [`@ckeditor/ckeditor5-clipboard`](https://www.npmjs.com/package/@ckeditor/ckeditor5-clipboard) package:
