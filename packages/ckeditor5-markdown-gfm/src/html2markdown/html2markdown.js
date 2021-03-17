37ea0916130427a39bf16dcca0590df8cbbd48af
Paweł Smyrek <p.smyrek@cksource.com>
2021-03-17 08:04:30 +0100

    Fixed domain matcher

--- packages/ckeditor5-markdown-gfm/src/html2markdown/html2markdown.js
+++ packages/ckeditor5-markdown-gfm/src/html2markdown/html2markdown.js
@@ -95,7 +95,7 @@ const regex = new RegExp(
 	/\b(?:(?:https?|ftp):\/\/|www\.)/.source +
 
 	// Domain name.
-	/(?![-_])(?:[-_a-z0-9\u00a1-\uffff]{1,63}[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,})/.source +
+	/(?![-_])(?:[-_a-z0-9\u00a1-\uffff]{1,63}\.)+(?:[a-z\u00a1-\uffff]{2,})/.source +
 
 	// The rest.
 	/(?:[^\s<>]*)/.source,
