06c36767b0c83e152d121f5b9ee57c6321b04e77
Szymon Cofalik <s.cofalik@cksource.com>
2021-03-30 11:57:28 +0200

    Internal (page-break): Upcast converter will be less strict when checking if a view element should be converted to the model.

--- packages/ckeditor5-page-break/tests/pagebreakediting.js
+++ packages/ckeditor5-page-break/tests/pagebreakediting.js
@@ -172,34 +172,21 @@ describe( 'PageBreakEditing', () => {
 					.to.equal( '' );
 			} );
 
+			it( 'should convert if inner span is empty', () => {
+				editor.setData(
+					'<div class="page-break" style="page-break-after:always;">' +
+						'<span style="display:none;"></span>' +
+					'</div>'
+				);
+
+				expect( getModelData( model, { withoutSelection: true } ) )
+					.to.equal( '<pageBreak></pageBreak>' );
+			} );
+
 			it( 'should not convert if inner span has wrong styles', () => {
 				editor.setData(
 					'<div class="page-break" style="page-break-after:always;">' +
 						'<span style="display:inline-block;">&nbsp;</span>' +
-					'</div>'
-				);
-
-				expect( getModelData( model, { withoutSelection: true } ) )
-					.to.equal( '' );
-			} );
-
-			it( 'should not convert if inner span has any children', () => {
-				editor.setData(
-					'<div class="page-break" style="page-break-after:always;">' +
-						'<span style="display:none;">' +
-							'<span>Foo</span>' +
-						'</span>' +
-					'</div>'
-				);
-
-				expect( getModelData( model, { withoutSelection: true } ) )
-					.to.equal( '' );
-			} );
-
-			it( 'should not convert if inner span has text', () => {
-				editor.setData(
-					'<div class="page-break" style="page-break-after:always;">' +
-						'<span style="display:none;">Foo</span>' +
 					'</div>'
 				);
 
@@ -235,26 +222,6 @@ describe( 'PageBreakEditing', () => {
 
 				expect( getModelData( model, { withoutSelection: true } ) )
 					.to.equal( '<pageBreak></pageBreak><section><$text foo="true">Foo</$text></section>' );
-			} );
-
-			it( 'should not convert if inner span has no children', () => {
-				editor.setData( '<div class="page-break" style="page-break-after:always;"><span style="display:none;"></span></div>' );
-
-				expect( getModelData( model, { withoutSelection: true } ) )
-					.to.equal( '' );
-			} );
-
-			it( 'should not convert if inner span has other element as a child', () => {
-				editor.setData(
-					'<div class="page-break" style="page-break-after:always;">' +
-						'<span style="display:none;">' +
-							'<span></span>' +
-						'</span>' +
-					'</div>'
-				);
-
-				expect( getModelData( model, { withoutSelection: true } ) )
-					.to.equal( '' );
 			} );
 		} );
 	} );
