ea7616dde3616be972ff36a30e564be0d5ee868b
Kuba Niegowski <j.niegowski@cksource.com>
2021-04-20 13:24:17 +0200

    Code refactor.

--- packages/ckeditor5-engine/src/view/styles/border.js
+++ packages/ckeditor5-engine/src/view/styles/border.js
@@ -99,7 +99,7 @@ export function addBorderRules( stylesProcessor ) {
 	stylesProcessor.setReducer( 'border-right', getBorderPositionReducer( 'right' ) );
 	stylesProcessor.setReducer( 'border-bottom', getBorderPositionReducer( 'bottom' ) );
 	stylesProcessor.setReducer( 'border-left', getBorderPositionReducer( 'left' ) );
-	stylesProcessor.setReducer( 'border', borderReducer() );
+	stylesProcessor.setReducer( 'border', getBorderReducer() );
 
 	stylesProcessor.setStyleRelation( 'border', [
 		'border-color', 'border-style', 'border-width',
@@ -235,7 +235,7 @@ function normalizeBorderShorthand( string ) {
 	return result;
 }
 
-// The border reducer.
+// The border reducer factory.
 //
 // It tries to produce the most optimal output for the specified styles.
 //
@@ -261,112 +261,56 @@ function normalizeBorderShorthand( string ) {
 // Definitions are merged only if all border selectors have the same values.
 //
 // @returns {Function}
-function borderReducer() {
+function getBorderReducer() {
 	return value => {
-		const topReducer = reduceBorderPosition( extractBorderPosition( value, 'top' ), 'top' );
-		const rightReducer = reduceBorderPosition( extractBorderPosition( value, 'right' ), 'right' );
-		const bottomReducer = reduceBorderPosition( extractBorderPosition( value, 'bottom' ), 'bottom' );
-		const leftReducer = reduceBorderPosition( extractBorderPosition( value, 'left' ), 'left' );
+		const topStyles = extractBorderPosition( value, 'top' );
+		const rightStyles = extractBorderPosition( value, 'right' );
+		const bottomStyles = extractBorderPosition( value, 'bottom' );
+		const leftStyles = extractBorderPosition( value, 'left' );
 
-		if ( shouldMergeAllBordersDefinitions( topReducer, rightReducer, bottomReducer, leftReducer ) ) {
-			return [
-				// All `border-*` reducers define the same value.
-				[ 'border', `${ topReducer[ 0 ][ 1 ] }` ]
-			];
+		const borderStyles = [ topStyles, rightStyles, bottomStyles, leftStyles ];
+
+		const borderStylesByType = {
+			width: getReducedStyleValueForType( borderStyles, 'width' ),
+			style: getReducedStyleValueForType( borderStyles, 'style' ),
+			color: getReducedStyleValueForType( borderStyles, 'color' )
+		};
+
+		// Try reducing to a single `border:` property.
+		const reducedBorderStyle = reduceBorderPosition( borderStylesByType, 'all' );
+
+		if ( reducedBorderStyle.length == 1 ) {
+			return reducedBorderStyle;
 		}
 
-		const results = [];
-		const borderReducers = [ topReducer, rightReducer, bottomReducer, leftReducer ];
+		// Try reducing to `border-style:`, `border-width:`, `border-color:` properties.
+		const reducedStyleTypes = Object.entries( borderStylesByType ).reduce( ( reducedStyleTypes, [ type, value ] ) => {
+			if ( value ) {
+				reducedStyleTypes.push( [ `border-${ type }`, value ] );
 
-		mergeBorderDefinitions( results, /border-[^-]+-style/, 'border-style', borderReducers );
-		mergeBorderDefinitions( results, /border-[^-]+-width/, 'border-width', borderReducers );
-		mergeBorderDefinitions( results, /border-[^-]+-color/, 'border-color', borderReducers );
+				// Remove it from the full set to not include it in the most specific properties later.
+				borderStyles.forEach( style => ( style[ type ] = null ) );
+			}
 
+			return reducedStyleTypes;
+		}, [] );
+
+		// The reduced properties (by type) and all the rest that could not be reduced.
 		return [
-			...results,
-			...topReducer,
-			...rightReducer,
-			...bottomReducer,
-			...leftReducer
+			...reducedStyleTypes,
+			...reduceBorderPosition( topStyles, 'top' ),
+			...reduceBorderPosition( rightStyles, 'right' ),
+			...reduceBorderPosition( bottomStyles, 'bottom' ),
+			...reduceBorderPosition( leftStyles, 'left' )
 		];
 	};
 
-	// Checks whether border properties should be merged into the single `border` property.
-	//
-	// The merge can happen if each `border-*` property returns exactly the same value.
-	//
-	// @param {Array} topReducer `border-top-*` definitions.
-	// @param {Array} rightReducer `border-right-*` definitions.
-	// @param {Array} bottomReducer `border-bottom-*` definitions.
-	// @param {Array} leftReducer `border-left-*` definitions.
-	// @returns {Boolean}
-	function shouldMergeAllBordersDefinitions( topReducer, rightReducer, bottomReducer, leftReducer ) {
-		return topReducer.length === 1 &&
-			rightReducer.length === 1 &&
-			bottomReducer.length === 1 &&
-			leftReducer.length === 1 &&
-			topReducer[ 0 ][ 0 ] === 'border-top' &&
-			rightReducer[ 0 ][ 0 ] === 'border-right' &&
-			bottomReducer[ 0 ][ 0 ] === 'border-bottom' &&
-			leftReducer[ 0 ][ 0 ] === 'border-left' &&
-			areEquals( [
-				topReducer[ 0 ][ 1 ],
-				bottomReducer[ 0 ][ 1 ],
-				bottomReducer[ 0 ][ 1 ],
-				leftReducer[ 0 ][ 1 ]
-			] );
-	}
-
-	// Tries to merge the same values definitions for all borders (top, right, bottom, left) into the single one (border-*).
-	//
-	// The merge is possible if all values for `border-*-(style|color|width)` are identical.
-	//
-	// After merging, the checked CSS definitions are removed from the initial data.
-	//
-	// @param {Array} resultsReducers The resulting array where all reducers are being added.
-	// @param {RegExp} pattern Definitions returned by reducers must match to this pattern.
-	// @param {String} definition The output definition that will be created.
-	// @param {Array} reducers All `border-*-(style|color|width` combinations (the initial data).
-	function mergeBorderDefinitions( resultsReducers, pattern, definition, reducers ) {
-		// The array should contain 4 items that describes the same property (color, width, or style) for each border.
-		const patternReducers = [];
-
-		// The initial data contains definitions for all directions (top, right, bottom, left).
-		for ( const borderReducers of reducers ) {
-			// Find the rule that match to the output `definition`.
-			const item = borderReducers.find( item => item[ 0 ].match( pattern ) );
-
-			if ( item ) {
-				patternReducers.push( item );
-			}
-		}
-
-		// Merge can happen only if we have values for all borders...
-		if ( patternReducers.length !== 4 ) {
-			return;
-		}
-
-		// ...and these values must be identical.
-		if ( !areEquals( patternReducers.map( item => item[ 1 ] ) ) ) {
-			return;
-		}
-
-		// Create the new definition...
-		resultsReducers.push( [ definition, patternReducers[ 0 ][ 1 ] ] );
-
-		// ...and remove parsed entries from the initial array because they
-		// should not be returned to avoid creating improper output.
-		patternReducers.forEach( ( item, index ) => {
-			reducers[ index ].splice( reducers[ index ].indexOf( item ), 1 );
-		} );
-	}
-
-	// Returns true if all specified items are identical. Otherwise, returns false.
-	//
-	// @param {Array.<String>} items
-	// @returns {Boolean}
-	function areEquals( items ) {
-		return new Set( items ).size === 1;
+	// @param {Array.<Object>} styles The array of objects with `style`, `color`, `width` properties.
+	// @param {'width'|'style'|'color'} type
+	function getReducedStyleValueForType( styles, type ) {
+		return styles
+			.map( style => style[ type ] )
+			.reduce( ( result, style ) => result == style ? result : null );
 	}
 }
 
@@ -382,27 +326,34 @@ function getBorderPositionReducer( which ) {
 // Otherwise, the specific definitions will be returned: `border-(width|style|color)-*: [value]`.
 //
 // @param {Object|null} value Styles if defined.
-// @param {'top'|'right'|'bottom'|left|} which The border position.
+// @param {'top'|'right'|'bottom'|'left'|'all'} which The border position.
 // @returns {Array}
 function reduceBorderPosition( value, which ) {
 	const borderTypes = [];
 
-	if ( value && value.width !== undefined ) {
+	if ( value && value.width ) {
 		borderTypes.push( 'width' );
 	}
 
-	if ( value && value.style !== undefined ) {
+	if ( value && value.style ) {
 		borderTypes.push( 'style' );
 	}
 
-	if ( value && value.color !== undefined ) {
+	if ( value && value.color ) {
 		borderTypes.push( 'color' );
 	}
 
-	if ( borderTypes.length === 3 ) {
+	if ( borderTypes.length == 3 ) {
+		const borderValue = borderTypes.map( item => value[ item ] ).join( ' ' );
+
 		return [
-			[ `border-${ which }`, borderTypes.map( item => value[ item ] ).join( ' ' ) ]
+			which == 'all' ? [ 'border', borderValue ] : [ `border-${ which }`, borderValue ]
 		];
+	}
+
+	// We are unable to reduce to a single `border:` property.
+	if ( which == 'all' ) {
+		return [];
 	}
 
 	return borderTypes.map( type => {
