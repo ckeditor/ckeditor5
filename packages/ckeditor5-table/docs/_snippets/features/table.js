eb6b39ce63e93b5a280c49ea44b24b140dd802bf
Maksymilian Barnaś <max.barnas@gmail.com>
2021-04-26 12:42:12 +0200

    Revert table tour ballong removal.

--- packages/ckeditor5-table/docs/_snippets/features/table.js
+++ packages/ckeditor5-table/docs/_snippets/features/table.js
@@ -21,6 +21,16 @@ ClassicEditor
 	} )
 	.then( editor => {
 		window.editor = editor;
+
+		window.attachTourBalloon( {
+			target: window.findToolbarItem( editor.ui.view.toolbar,
+				item => item.buttonView && item.buttonView.label && item.buttonView.label === 'Insert table' ),
+			text: 'Click to create a table.',
+			editor,
+			tippyOptions: {
+				placement: 'bottom-start'
+			}
+		} );
 	} )
 	.catch( err => {
 		console.error( err.stack );
