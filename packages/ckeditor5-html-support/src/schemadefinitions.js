58af624f3a0427f86fd23daf772d57263026a9dd
Jacek Bogdanski <jacekbogd@gmail.com>
2021-06-17 16:35:40 +0200

    Expanded data schema with missing elements.

--- packages/ckeditor5-html-support/src/schemadefinitions.js
+++ packages/ckeditor5-html-support/src/schemadefinitions.js
@@ -1,14 +1,63 @@
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module html-support/schemadefinitions
+ */
+
+// Skipped elements due to HTML deprecation:
+// * noframes (not sure if we should provide support for this element. CKE4 is not supporting frameset and frame,
+//   but it will unpack <frameset><noframes>foobar</noframes></frameset> to <noframes>foobar</noframes>, so there
+//   may be some content loss. Although using noframes as a standalone element seems invalid)
+// * keygen (this one is also empty)
+// * applet (support is limited mostly to old IE)
+// * basefont (this one is also empty)
+// * isindex (basically no support for modern browsers at all)
+// Skipped elements due to lack empty element support:
+// * hr
+// * area
+// * br
+// * command
+// * map
+// * wbr
+// Skipped elements due to complexity:
+// * datalist with option elements used as a data source for input[list] element
+
 export default {
 	block: [
 		// Existing features
-		{ model: 'heading1', view: 'h2' },
-		{ model: 'heading2', view: 'h3' },
-		{ model: 'heading3', view: 'h4' },
-		{ model: 'codeBlock', view: 'pre' },
-		{ model: 'paragraph', view: 'p' },
-		{ model: 'blockQuote', view: 'blockquote' },
-		{ model: 'listItem', view: 'li' },
-		// New features
+		{
+			model: 'heading1',
+			view: 'h2'
+		},
+		{
+			model: 'heading2',
+			view: 'h3'
+		},
+		{
+			model: 'heading3',
+			view: 'h4'
+		},
+		{
+			model: 'codeBlock',
+			view: 'pre'
+		},
+		{
+			model: 'paragraph',
+			view: 'p'
+		},
+		{
+			model: 'blockQuote',
+			view: 'blockquote'
+		},
+		{
+			model: 'listItem',
+			view: 'li'
+		},
+
+		// Compatibility features
 		{
 			model: '$htmlBlock',
 			modelSchema: {
@@ -17,11 +66,299 @@ export default {
 				isBlock: true
 			}
 		},
-		{ model: 'htmlPre', view: 'pre', modelSchema: { inheritAllFrom: '$block' } },
-		{ model: 'htmlArticle', view: 'article', modelSchema: { inheritAllFrom: '$htmlBlock' } },
-		{ model: 'htmlSection', view: 'section', modelSchema: { inheritAllFrom: '$htmlBlock' } },
-		{ model: 'htmlForm', view: 'form', modelSchema: { inheritAllFrom: '$htmlBlock' } },
-		{ model: 'htmlDetails', view: 'details', modelSchema: { inheritAllFrom: '$htmlBlock' } },
+		{
+			model: 'htmlParagraph',
+			view: 'p',
+			modelSchema: {
+				inheritAllFrom: '$block'
+			}
+		},
+		{
+			model: 'htmlBlockQuote',
+			view: 'blockquote',
+			modelSchema: {
+				inheritAllFrom: '$htmlBlock'
+			}
+		},
+		{
+			model: 'htmlTable',
+			view: 'table',
+			modelSchema: {
+				allowIn: [ '$htmlBlock', '$root' ],
+				isBlock: true
+			}
+		},
+		{
+			model: 'htmlTableBody',
+			view: 'tbody',
+			modelSchema: {
+				allowIn: 'htmlTable',
+				isBlock: true
+			}
+		},
+		{
+			model: 'htmlTableHead',
+			view: 'thead',
+			modelSchema: {
+				allowIn: 'htmlTable',
+				isBlock: true
+			}
+		},
+		{
+			model: 'htmlCaption',
+			view: 'caption',
+			modelSchema: {
+				allowIn: 'htmlTable',
+				allowChildren: '$text',
+				isBlock: true
+			}
+		},
+		{
+			model: 'htmlTableRow',
+			view: 'tr',
+			modelSchema: {
+				allowIn: [ 'htmlTable', 'htmlTableHead', 'htmlTableBody' ],
+				isBlock: true
+			}
+		},
+		// TODO can also include text.
+		{
+			model: 'htmlTableData',
+			view: 'td',
+			modelSchema: {
+				allowIn: 'tr',
+				allowChildren: [ '$block', '$htmlBlock' ],
+				isBlock: true
+			}
+		},
+		// TODO can also include text.
+		{
+			model: 'htmlTableHeader',
+			view: 'th',
+			modelSchema: {
+				allowIn: 'tr',
+				allowChildren: [ '$block', '$htmlBlock' ],
+				isBlock: true
+			}
+		},
+		// TODO can also include text.
+		{
+			model: 'htmlFigure',
+			view: 'figure',
+			modelSchema: {
+				inheritAllFrom: '$htmlBlock',
+				isBlock: true
+			}
+		},
+		// TODO can also include other block elements.
+		{
+			model: 'htmlFigcaption',
+			view: 'figcaption',
+			modelSchema: {
+				allowIn: 'htmlFigure',
+				allowChildren: '$text',
+				isBlock: true
+			}
+		},
+		// TODO can also include other block elements.
+		{
+			model: 'htmlAddress',
+			view: 'address',
+			modelSchema: {
+				inheritAllFrom: '$block'
+			}
+		},
+		// TODO can also include text.
+		{
+			model: 'htmlAside',
+			view: 'aside',
+			modelSchema: {
+				inheritAllFrom: '$htmlBlock'
+			}
+		},
+		// TODO can also include text.
+		{
+			model: 'htmlMain',
+			view: 'main',
+			modelSchema: {
+				inheritAllFrom: '$htmlBlock'
+			}
+		},
+		// TODO can also include text.
+		{
+			model: 'htmlDetails',
+			view: 'details',
+			modelSchema: {
+				inheritAllFrom: '$htmlBlock'
+			}
+		},
+		// TODO can also include text.
+		{
+			model: 'htmlDiv',
+			view: 'div',
+			modelSchema: {
+				inheritAllFrom: '$htmlBlock'
+			}
+		},
+		// TODO can also include text.
+		{
+			model: 'htmlFieldset',
+			view: 'fieldset',
+			modelSchema: {
+				inheritAllFrom: '$htmlBlock'
+			}
+		},
+		// TODO can also include text.
+		{
+			model: 'htmlHeader',
+			view: 'header',
+			modelSchema: {
+				inheritAllFrom: '$htmlBlock'
+			}
+		},
+		// TODO can also include text.
+		{
+			model: 'htmlFooter',
+			view: 'footer',
+			modelSchema: {
+				inheritAllFrom: '$htmlBlock'
+			}
+		},
+		// TODO can also include text.
+		{
+			model: 'htmlForm',
+			view: 'form',
+			modelSchema: {
+				inheritAllFrom: '$htmlBlock'
+			}
+		},
+		{
+			model: 'htmlHeadingGroup',
+			view: 'hgroup',
+			modelSchema: {
+				allowChildren: [
+					'htmlHeading1',
+					'htmlHeading2',
+					'htmlHeading3',
+					'htmlHeading4',
+					'htmlHeading5',
+					'htmlHeading6',
+					'heading1',
+					'heading2',
+					'heading3'
+				],
+				isBlock: true
+			}
+		},
+		{
+			model: 'htmlHeading1',
+			view: 'h1',
+			modelSchema: {
+				inheritAllFrom: '$block'
+			}
+		},
+		{
+			model: 'htmlHeading2',
+			view: 'h2',
+			modelSchema: {
+				inheritAllFrom: '$block'
+			}
+		},
+		{
+			model: 'htmlHeading3',
+			view: 'h3',
+			modelSchema: {
+				inheritAllFrom: '$block'
+			}
+		},
+		{
+			model: 'htmlHeading4',
+			view: 'h4',
+			modelSchema: {
+				inheritAllFrom: '$block'
+			}
+		},
+		{
+			model: 'htmlHeading5',
+			view: 'h5',
+			modelSchema: {
+				inheritAllFrom: '$block'
+			}
+		},
+		{
+			model: 'htmlHeading6',
+			view: 'h6',
+			modelSchema: {
+				inheritAllFrom: '$block'
+			}
+		},
+		{
+			model: '$htmlList',
+			modelSchema: {
+				allowIn: [ '$htmlBlock', 'htmlListItem' ],
+				allowChildren: 'htmlListItem',
+				isBlock: true
+			}
+		},
+		{
+			model: 'htmlDirectory',
+			view: 'dir',
+			modelSchema: {
+				inheritAllFrom: '$htmlList'
+			}
+		},
+		{
+			model: 'htmlMenu',
+			view: 'menu',
+			modelSchema: {
+				inheritAllFrom: '$htmlList'
+			}
+		},
+		{
+			model: 'htmlUnorderedList',
+			view: 'ul',
+			modelSchema: {
+				inheritAllFrom: '$htmlList'
+			}
+		},
+		{
+			model: 'htmlOrderedList',
+			view: 'ol',
+			modelSchema: {
+				inheritAllFrom: '$htmlList'
+			}
+		},
+		// TODO can also include other block elements.
+		{
+			model: 'htmlListItem',
+			view: 'li',
+			modelSchema: {
+				allowChildren: '$text',
+				allowIn: '$htmlList',
+				isBlock: true
+			}
+		},
+		{
+			model: 'htmlPre',
+			view: 'pre',
+			modelSchema: {
+				inheritAllFrom: '$block'
+			}
+		},
+		{
+			model: 'htmlArticle',
+			view: 'article',
+			modelSchema: {
+				inheritAllFrom: '$htmlBlock'
+			}
+		},
+		{
+			model: 'htmlSection',
+			view: 'section',
+			modelSchema: {
+				inheritAllFrom: '$htmlBlock'
+			}
+		},
 		{
 			model: 'htmlSumary',
 			view: 'summary',
@@ -68,18 +405,233 @@ export default {
 		}
 	],
 	inline: [
-		{ model: 'htmlA', view: 'a', priority: 5 },
-		{ model: 'htmlStrong', view: 'strong' },
-		{ model: 'htmlI', view: 'i' },
-		{ model: 'htmlS', view: 's' },
-		{ model: 'htmlU', view: 'u' },
-		{ model: 'htmlSub', view: 'sub' },
-		{ model: 'htmlSup', view: 'sup' },
-		{ model: 'htmlCode', view: 'code' },
-		{ model: 'htmlMark', view: 'mark' },
-		{ model: 'htmlSpan', view: 'span', attributeProperties: { copyOnEnter: true } },
-		{ model: 'htmlCite', view: 'cite', attributeProperties: { copyOnEnter: true } },
-		{ model: 'htmlLabel', view: 'label', attributeProperties: { copyOnEnter: true } },
+		{
+			model: 'htmlCenter',
+			view: 'center',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlAcronym',
+			view: 'acronym',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlTeletypeText',
+			view: 'tt',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlFont',
+			view: 'font',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlTime',
+			view: 'time',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlVar',
+			view: 'var',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlBig',
+			view: 'big',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlSmall',
+			view: 'small',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlSample',
+			view: 'samp',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlQuote',
+			view: 'q',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlOutput',
+			view: 'output',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlKeyboardInput',
+			view: 'kbd',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlBdi',
+			view: 'bdi',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlBdo',
+			view: 'bdo',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlAbbreviation',
+			view: 'abbr',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlLink',
+			view: 'a',
+			priority: 5,
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlStrong',
+			view: 'strong',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlBold',
+			view: 'b',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlItalic',
+			view: 'i',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlEmphasis',
+			view: 'em',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlStrike',
+			view: 's',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		// TODO According to HTML-spec can behave as div-like element, althouth CKE4 only handles it as an inline element.
+		{
+			model: 'htmlDeleted',
+			view: 'del',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		// TODO According to HTML-spec can behave as div-like element, althouth CKE4 only handles it as an inline element.
+		{
+			model: 'htmlInserted',
+			view: 'ins',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlUnderline',
+			view: 'u',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlSubscript',
+			view: 'sub',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlSuperscript',
+			view: 'sup',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlCode',
+			view: 'code',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlMark',
+			view: 'mark',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlSpan',
+			view: 'span',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlCite',
+			view: 'cite',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlLabel',
+			view: 'label',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
+		{
+			model: 'htmlDefinition',
+			view: 'dfn',
+			attributeProperties: {
+				copyOnEnter: true
+			}
+		},
 
 		// Objects
 		{
@@ -93,64 +645,110 @@ export default {
 			}
 		},
 		{
-			view: 'object',
 			model: 'htmlObject',
+			view: 'object',
 			isObject: true,
 			modelSchema: {
 				inheritAllFrom: '$htmlObjectInline'
 			}
 		},
 		{
-			view: 'iframe',
 			model: 'htmlIframe',
+			view: 'iframe',
 			isObject: true,
 			modelSchema: {
 				inheritAllFrom: '$htmlObjectInline'
 			}
 		},
 		{
-			view: 'input',
 			model: 'htmlInput',
+			view: 'input',
 			isObject: true,
 			modelSchema: {
 				inheritAllFrom: '$htmlObjectInline'
 			}
 		},
 		{
-			view: 'button',
 			model: 'htmlButton',
+			view: 'button',
 			isObject: true,
 			modelSchema: {
 				inheritAllFrom: '$htmlObjectInline'
 			}
 		},
 		{
-			view: 'textarea',
 			model: 'htmlTextarea',
+			view: 'textarea',
 			isObject: true,
 			modelSchema: {
 				inheritAllFrom: '$htmlObjectInline'
 			}
 		},
 		{
-			view: 'select',
 			model: 'htmlSelect',
+			view: 'select',
 			isObject: true,
 			modelSchema: {
 				inheritAllFrom: '$htmlObjectInline'
 			}
 		},
 		{
-			view: 'video',
 			model: 'htmlVideo',
+			view: 'video',
 			isObject: true,
 			modelSchema: {
 				inheritAllFrom: '$htmlObjectInline'
 			}
 		},
 		{
-			view: 'audio',
+			model: 'htmlEmbed',
+			view: 'embed',
+			isObject: true,
+			modelSchema: {
+				inheritAllFrom: '$htmlObjectInline'
+			}
+		},
+		{
 			model: 'htmlAudio',
+			view: 'audio',
+			isObject: true,
+			modelSchema: {
+				inheritAllFrom: '$htmlObjectInline'
+			}
+		},
+		// TODO Should image be an object?
+		{
+			model: 'htmlImage',
+			view: 'img',
+			isObject: true,
+			modelSchema: {
+				inheritAllFrom: '$htmlObjectInline'
+			}
+		},
+		// TODO Should canvas be an object?
+		{
+			model: 'htmlCanvas',
+			view: 'canvas',
+			isObject: true,
+			modelSchema: {
+				inheritAllFrom: '$htmlObjectInline'
+			}
+		},
+		// TODO it could be probably represented as non-object element, although it has grafical representation,
+		// so probably makes more sense to keep it as an object.
+		{
+			model: 'htmlMeter',
+			view: 'meter',
+			isObject: true,
+			modelSchema: {
+				inheritAllFrom: '$htmlObjectInline'
+			}
+		},
+		// TODO it could be probably represented as non-object element, although it has grafical representation,
+		// so probably makes more sense to keep it as an object.
+		{
+			model: 'htmlProgress',
+			view: 'progress',
 			isObject: true,
 			modelSchema: {
 				inheritAllFrom: '$htmlObjectInline'
