44151a73979ae18159ca53c9dd83c3ef4eb882ed
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2021-06-18 13:19:30 +0200

    Tests: Verified findViewImgElement() helper deep tree search capabilities.

--- packages/ckeditor5-image/tests/imageutils.js
+++ packages/ckeditor5-image/tests/imageutils.js
@@ -730,6 +730,27 @@ describe( 'ImageUtils plugin', () => {
 
 		// figure
 		//   div
+		//      div
+		//         div
+		//            div
+		//              img
+		it( 'finds the the img element deeply nested in a view tree', () => {
+			const divElement1 = writer.createContainerElement( 'div' );
+			const divElement2 = writer.createContainerElement( 'div' );
+			const divElement3 = writer.createContainerElement( 'div' );
+			const divElement4 = writer.createContainerElement( 'div' );
+
+			writer.insert( writer.createPositionAt( element, 0 ), divElement1 );
+			writer.insert( writer.createPositionAt( divElement1, 0 ), divElement2 );
+			writer.insert( writer.createPositionAt( divElement2, 0 ), divElement3 );
+			writer.insert( writer.createPositionAt( divElement3, 0 ), divElement4 );
+			writer.move( writer.createRangeOn( image ), writer.createPositionAt( divElement4, 0 ) );
+
+			expect( imageUtils.findViewImgElement( element ) ).to.equal( image );
+		} );
+
+		// figure
+		//   div
 		//     "Bar"
 		//     img
 		//   "Foo"
