ed0da05e65d9b6bb815f343f9f1c8c47e89988ac
Marek Lewandowski <m.lewandowski@cksource.com>
2021-06-21 13:46:27 +0200

    Added bold plugin to the sample.

--- packages/ckeditor5-find-and-replace/tests/manual/findandreplace.js
+++ packages/ckeditor5-find-and-replace/tests/manual/findandreplace.js
@@ -11,12 +11,13 @@ import FindAndReplace from '../../src/findandreplace';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 
 // Note: We need to load paragraph because we don't have inline editors yet.
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
-		plugins: [ Essentials, Paragraph, FindAndReplace, Highlight ],
-		toolbar: [ 'undo', 'redo', 'findAndReplace', 'highlight' ]
+		plugins: [ Essentials, Paragraph, FindAndReplace, Highlight, Bold ],
+		toolbar: [ 'undo', 'redo', 'highlight', 'bold', 'findAndReplace' ]
 	} )
 	.then( editor => {
 		window.editor = editor;
