d32886cb56a1ede3dbacd1c2ac62ec68b9e4c55d
Marek Lewandowski <m.lewandowski@cksource.com>
2021-06-25 09:20:50 +0200

    Fixed wrong selection after making a single replacement.

--- packages/ckeditor5-find-and-replace/tests/replacecommand.js
+++ packages/ckeditor5-find-and-replace/tests/replacecommand.js
@@ -4,7 +4,7 @@
  */
 
 import ModelTestEditor from '@ckeditor/ckeditor5-core/tests/_utils/modeltesteditor';
-import { setData } from '@ckeditor/ckeditor5-engine/src/dev-utils/model';
+import { setData, getData } from '@ckeditor/ckeditor5-engine/src/dev-utils/model';
 import FindAndReplaceEditing from '../src/findandreplaceediting';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import BoldEditing from '@ckeditor/ckeditor5-basic-styles/src/bold/boldediting';
@@ -52,7 +52,7 @@ describe( 'ReplaceCommand', () => {
 
 	describe( 'execute()', () => {
 		it( 'should replace single search result using text', () => {
-			setData( model, '<paragraph>Foo bar baz</paragraph><paragraph>Foo [bar] baz</paragraph>' );
+			setData( model, '<paragraph>foo foo foo foo</paragraph>' );
 
 			const range = editor.model.document.selection.getFirstRange();
 			const markerId = 'my-marker-id';
@@ -68,6 +68,35 @@ describe( 'ReplaceCommand', () => {
 			} );
 
 			expect( editor.getData() ).to.equal( '<p>Foo bar baz</p><p>Foo new baz</p>' );
+		} );
+
+		it( 'should highlight next match', () => {
+			setData( model, '<paragraph>foo foo foo foo []</paragraph>' );
+
+			const { results } = editor.execute( 'find', 'foo' );
+			editor.execute( 'replace', 'bar', results.get( 0 ) );
+
+			for ( let i = 0; i < results.length; i++ ) {
+				const result = results.get( i );
+
+				result.marker.name = `findResult:${ i }`;
+			}
+
+			for ( const marker of editor.model.markers ) {
+				if ( marker.name.startsWith( 'findResultHighlighted:' ) ) {
+					marker.name = 'findResultHighlighted:x';
+				}
+			}
+
+			expect( getData( editor.model, { convertMarkers: true } ) ).to.equal(
+				'<paragraph>bar <findResult:1:start></findResult:1:start>' +
+					'<findResultHighlighted:x:start></findResultHighlighted:x:start>foo<findResult:1:end></findResult:1:end>' +
+					'<findResultHighlighted:x:end></findResultHighlighted:x:end> ' +
+					'<findResult:2:start>' +
+						'</findResult:2:start>foo<findResult:2:end></findResult:2:end> ' +
+						'<findResult:3:start></findResult:3:start>foo<findResult:3:end></findResult:3:end> ' +
+				'</paragraph>'
+			);
 		} );
 	} );
 } );
