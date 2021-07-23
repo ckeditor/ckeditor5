71c82fef12382f77285ff52748d19d1f92c097a0
Maciej Bukowski <ma2ciek@gmail.com>
2021-07-23 10:17:16 +0200

    Fixed failing tests.

--- packages/ckeditor5-html-support/tests/htmlcomment-integration.js
+++ packages/ckeditor5-html-support/tests/htmlcomment-integration.js
@@ -969,9 +969,7 @@ describe( 'HtmlComment integration', () => {
 
 			editor.execute( 'delete' );
 
-			// Currently, comments inside a deleted content are moved to adjacent paragraph.
-			// See https://github.com/ckeditor/ckeditor5/issues/10119.
-			//
+			// The following output will be also correct:
 			// expect( editor.getData() ).to.equal(
 			// 	'<p>' +
 			// 		'<!-- comment 1 -->' +
@@ -985,7 +983,6 @@ describe( 'HtmlComment integration', () => {
 					'<!-- comment 1 -->' +
 					'Foo' +
 					'<!-- comment 4 -->' +
-					'<!-- comment 3 -->' +
 					'<!-- comment 2 -->' +
 				'</p>'
 			);
@@ -1086,19 +1083,6 @@ describe( 'HtmlComment integration', () => {
 
 			toggleSourceEditingModeButton.fire( 'execute' );
 
-			// Currently, comments at root boundary are not removed after setData() is called with new content.
-			// See https://github.com/ckeditor/ckeditor5/issues/10117.
-			//
-			// expect( editor.getData() ).to.equal(
-			// 	'<!-- comment 1 -->' +
-			// 	'<p>' +
-			// 		'<!-- comment 2 -->' +
-			// 		'Foo' +
-			// 		'<!-- comment 3 -->' +
-			// 	'</p>' +
-			// 	'<!-- comment 4 -->'
-			// );
-
 			expect( editor.getData() ).to.equal(
 				'<!-- comment 1 -->' +
 				'<p>' +
@@ -1106,9 +1090,7 @@ describe( 'HtmlComment integration', () => {
 					'Foo' +
 					'<!-- comment 3 -->' +
 				'</p>' +
-				'<!-- comment 4 -->' +
-				'<!-- comment 2 -->' +
-				'<!-- comment 1 -->'
+				'<!-- comment 4 -->'
 			);
 		} );
 	} );
