ee798d8eede49232f7dd89c23f9806eed8d2e247
Magdalena Chrześcian <m.chrzescian@cksource.com>
2021-07-28 12:08:51 +0200

    Docs: merge from stable - config fix.

--- packages/ckeditor5-html-support/docs/_snippets/features/general-html-support.js
+++ packages/ckeditor5-html-support/docs/_snippets/features/general-html-support.js
@@ -5,9 +5,54 @@
 
 /* globals console, window, document, ClassicEditor, GeneralHtmlSupport */
 
+import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud-services-config.js';
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
+import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport';
+
+import './general-html-support.css';
+
 ClassicEditor
 	.create( document.querySelector( '#snippet-general-html-support' ), {
-		extraPlugins: [ GeneralHtmlSupport ],
+		plugins: [
+			ArticlePluginSet,
+			Code,
+			EasyImage,
+			ImageUpload,
+			CloudServices,
+			SourceEditing,
+			GeneralHtmlSupport
+		],
+		toolbar: {
+			items: [
+				'heading',
+				'|',
+				'bold',
+				'italic',
+				'code',
+				'bulletedList',
+				'numberedList',
+				'|',
+				'outdent',
+				'indent',
+				'|',
+				'blockQuote',
+				'link',
+				'mediaEmbed',
+				'insertTable',
+				'|',
+				'undo',
+				'redo',
+				'|',
+				'sourceEditing'
+			],
+			viewportTopOffset: window.getViewportTopOffsetConfig()
+		},
 		image: {
 			toolbar: [
 				'imageStyle:inline',
@@ -21,6 +66,7 @@ ClassicEditor
 		table: {
 			contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
 		},
+		cloudServices: CS_CONFIG,
 		htmlSupport: {
 			allow: [
 				// Enables <div>, <details>, and <summary> elements with all kind of attributes.
@@ -60,7 +106,10 @@ ClassicEditor
 			target: window.findToolbarItem( editor.ui.view.toolbar,
 				item => item.label && item.label === 'Source' ),
 			text: 'Switch to the source mode to check out the source of the content and play with it.',
-			editor
+			editor,
+			tippyOptions: {
+				placement: 'bottom-end'
+			}
 		} );
 	} )
 	.catch( err => {
