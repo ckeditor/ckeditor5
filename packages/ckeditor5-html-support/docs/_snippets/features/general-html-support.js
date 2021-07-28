f55ea46634ad3aab91193fce2cf789554b52763c
godai78 <b.biedrzycki@cksource.com>
2021-07-28 12:34:26 +0200

    Docs: snippet fixes.

--- packages/ckeditor5-html-support/docs/_snippets/features/general-html-support.js
+++ packages/ckeditor5-html-support/docs/_snippets/features/general-html-support.js
@@ -3,7 +3,7 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-/* globals console, window, document, ClassicEditor, GeneralHtmlSupport */
+/* globals console, window, document */
 
 import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud-services-config.js';
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
