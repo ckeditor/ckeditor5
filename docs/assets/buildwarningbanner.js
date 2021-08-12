603ef08d04010063f64d498f1b7d111a74979e9f
Kamil Piechaczek <pomek@users.noreply.github.com>
2021-08-12 12:43:56 +0200

    Simplified the script.

--- docs/assets/buildwarningbanner.js
+++ docs/assets/buildwarningbanner.js
@@ -3,21 +3,10 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-/* globals window, setTimeout */
+/* globals window */
 
 // Display a warning banner when browsing local or nightly documentation build.
-window.addEventListener( 'DOMContentLoaded', () => {
-	// Um, this is nasty. We need to figure out how to resolve the race between this script and app.js.
-	function waitForUmbertoToLoad() {
-		if ( !window.umberto.showWarningBanner ) {
-			setTimeout( () => waitForUmbertoToLoad(), 100 );
-		} else if ( window.location.host !== 'ckeditor.com' ) {
-			window.umberto.showWarningBanner(
-				`${ window.location.host === 'ckeditor5.github.io' ? 'Nightly' : 'Development' } documentation ahead. ` +
-				'Switch to the <a href="https://ckeditor.com/docs/ckeditor5">stable editor documentation</a>.'
-			);
-		}
-	}
-
-	waitForUmbertoToLoad();
-} );
+window.umberto.showWarningBanner(
+	`${ window.location.host === 'ckeditor5.github.io' ? 'Nightly' : 'Development' } documentation ahead. ` +
+	'Switch to the <a href="https://ckeditor.com/docs/ckeditor5">stable editor documentation</a>.'
+);
