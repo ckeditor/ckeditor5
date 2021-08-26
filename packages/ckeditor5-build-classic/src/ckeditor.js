370b7cdc9d785d47feaf5b2e9c9f7525c589692a
Donald Massé <donald.masse@gmail.com>
2021-08-26 12:47:53 -0400

    Test

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -106,7 +106,7 @@ ClassicEditor.defaultConfig = {
 	localImage: {
 		label: 'Test',
 		execute: () => {
-			alert('Test');
+
 		}
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
