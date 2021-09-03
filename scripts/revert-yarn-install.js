a570db6b4a64d9e84ff9b5e56f12f08c94360bab
Kamil Piechaczek <pomek@users.noreply.github.com>
2021-09-03 09:59:32 +0200

    Externals of externals should be removed too.

--- scripts/revert-yarn-install.js
+++ scripts/revert-yarn-install.js
@@ -24,12 +24,11 @@
 const fs = require( 'fs' );
 const path = require( 'path' );
 
-const CWD = process.cwd();
-const EXTERNAL_PATH = path.join( CWD, 'external' );
+const cwd = process.cwd();
 
 const filesToRemove = [
-	path.join( CWD, 'yarn.lock' ),
-	path.join( CWD, 'package-lock.json' )
+	path.join( cwd, 'yarn.lock' ),
+	path.join( cwd, 'package-lock.json' )
 ];
 
 console.log( 'Removing "lock" files...' );
@@ -40,36 +39,44 @@ for ( const file of filesToRemove ) {
 	}
 }
 
-console.log( 'Removing "node_modules/" directory...' );
-fs.rmdirSync( path.join( CWD, 'node_modules' ), { recursive: true } );
+console.log( 'Removing "./node_modules/" and "./packages/*/node_modules/" directories...' );
+removeNodeModulesInRepository( cwd );
 
-console.log( 'Removing the nested "packages/*/node_modules/" directories...' );
-removeDirectoryInPackages( CWD, 'node_modules' );
-
-console.log( 'Checking whether the "external/" directory exists...' );
-
-if ( fs.existsSync( EXTERNAL_PATH ) ) {
-	console.log( `Removing the nested "external/**/node_modules/" directories...` );
-
-	for ( const externalRepo of getDirectories( EXTERNAL_PATH ) ) {
-		fs.rmdirSync( path.join( externalRepo, 'node_modules' ), { recursive: true } );
-		removeDirectoryInPackages( externalRepo, 'node_modules' );
-	}
-}
+console.log( 'Removing the nested "external/**/node_modules/" directories...' );
+removeDirectoryFromExternalRepositories( cwd );
 
 console.log( 'Done.' );
 
-// Removes a directory specified as `directoryName` in all found packages.
+// Removes the `node_modules/` directory in the `rootDir` and all found packages (packages/*).
 //
-// Substitute of `rm -rf packages/*/node_modules` on Unix environments.
+// Substitute of `rm -rf node_modules/ packages/*/node_modules/` on Unix environments.
 //
 // @param {String} rootDir
 // @param {String} directoryName
-function removeDirectoryInPackages( rootDir, directoryName ) {
+function removeNodeModulesInRepository( rootDir ) {
 	const directoryPath = path.join( rootDir, 'packages' );
 
+	fs.rmdirSync( path.join( rootDir, 'node_modules' ), { recursive: true } );
+
 	for ( const absolutePath of getDirectories( directoryPath ) ) {
-		fs.rmdirSync( path.join( absolutePath, directoryName ), { recursive: true } );
+		fs.rmdirSync( path.join( absolutePath, 'node_modules' ), { recursive: true } );
+	}
+}
+
+// Removes the nested `node_modules/` directory in repositories found in the `external/` directory.
+//
+// @param {String} rootDir
+function removeDirectoryFromExternalRepositories( rootDir ) {
+	const externalPath = path.join( rootDir, 'external' );
+
+	if ( fs.existsSync( externalPath ) ) {
+		for ( const externalRepo of getDirectories( externalPath ) ) {
+			// Remove `node_modules` within the external repository.
+			removeNodeModulesInRepository( externalRepo, 'node_modules' );
+
+			// Remove `rootDir/external/**/node_modules`.
+			removeDirectoryFromExternalRepositories( externalRepo );
+		}
 	}
 }
 
@@ -78,7 +85,11 @@ function removeDirectoryInPackages( rootDir, directoryName ) {
 // @param {String} rootDir
 // @return {Array.<String>}
 function getDirectories( rootDir ) {
+	if ( !fs.existsSync( rootDir ) ) {
+		return [];
+	}
+
 	return fs.readdirSync( rootDir, { withFileTypes: true } )
 		.filter( dirent => dirent.isDirectory() )
-		.map( dirent => path.join( rootDir, dirent.name ) )
+		.map( dirent => path.join( rootDir, dirent.name ) );
 }
