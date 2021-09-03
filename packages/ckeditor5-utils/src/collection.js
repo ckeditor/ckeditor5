a17db16a41ffa2e2d8404e76538a85c474dbb573
Federico Panico <fedemp@users.noreply.github.com>
2021-09-03 16:48:04 -0300

    jsdoc: Utils>Collection initialItemsOrOptions is optional

--- packages/ckeditor5-utils/src/collection.js
+++ packages/ckeditor5-utils/src/collection.js
@@ -56,7 +56,7 @@ export default class Collection {
 	 *		console.log( collection.get( 'George' ) ); // -> { name: 'George' }
 	 *		console.log( collection.get( 'John' ) ); // -> { name: 'John' }
 	 *
-	 * @param {Iterable.<Object>|Object} initialItemsOrOptions The initial items of the collection or
+	 * @param {Iterable.<Object>|Object} [initialItemsOrOptions] The initial items of the collection or
 	 * the options object.
 	 * @param {Object} [options={}] The options object, when the first argument is an array of initial items.
 	 * @param {String} [options.idProperty='id'] The name of the property which is used to identify an item.
