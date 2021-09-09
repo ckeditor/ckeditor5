6ff1d1bd449e89bac95bcd15f1dabd10e67d3d31
Ewelina Doktor-Donocik <e.doktor-donocik@cksource.com>
2021-09-09 11:34:09 +0200

    Fix: ButtonView - editor crashed with keystroke undefined.

---
+++ packages/ckeditor5-ui/tests/manual/tickets/9412/1.js
@@ -0,0 +1,50 @@
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* globals window, document, console:false */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import HeadingButtonsUI from '@ckeditor/ckeditor5-heading/src/headingbuttonsui';
+import ParagraphButtonUI from '@ckeditor/ckeditor5-paragraph/src/paragraphbuttonui';
+import boldIcon from '@ckeditor/ckeditor5-basic-styles/theme/icons/bold.svg';
+import ButtonView from '../../../../src/button/buttonview';
+
+function customButtonView( editor ) {
+	editor.ui.componentFactory.add( 'customButtonView', locale => {
+		const view = new ButtonView( locale );
+		view.set( {
+			label: 'Custom Button',
+			icon: boldIcon,
+			tooltip: true,
+			withKeystroke: true
+		} );
+
+		return view;
+	} );
+}
+
+ClassicEditor
+	.create( document.querySelector( '#editor' ), {
+		plugins: [
+			Essentials,
+			List,
+			Paragraph,
+			Heading,
+			HeadingButtonsUI,
+			ParagraphButtonUI,
+			customButtonView
+		],
+		toolbar: [ 'customButtonView' ]
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
