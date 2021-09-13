2ed4b3153f25ba982594cbf27ee7d966076e78cd
Mateusz Zagórski <m.zagorski@cksource.com>
2021-09-13 13:56:50 +0200

    Fixed focus after escaping dropdown.

--- packages/ckeditor5-find-and-replace/tests/findandreplaceui.js
+++ packages/ckeditor5-find-and-replace/tests/findandreplaceui.js
@@ -122,10 +122,10 @@ describe( 'FindAndReplaceUI', () => {
 			} );
 
 			describe( 'upon dropdown close', () => {
-				it( 'the editing view should be focused', () => {
+				it( 'the form should be focused', () => {
 					dropdown.isOpen = true;
 
-					const spy = sinon.spy( editor.editing.view, 'focus' );
+					const spy = sinon.spy( form, 'focus' );
 
 					dropdown.isOpen = false;
 
