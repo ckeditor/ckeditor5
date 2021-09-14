56a2e55dc2bb56944ce5140e48a88c7b9583b705
Nicolas Bernier <nicolas.a.bernier@gmail.com>
2021-09-14 16:00:06 +0200

    feat: edit classic build

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -29,7 +29,10 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+
+// NEW STUFFS
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -42,7 +45,6 @@ ClassicEditor.builtinPlugins = [
 	Italic,
 	BlockQuote,
 	CKFinder,
-	CloudServices,
 	EasyImage,
 	Heading,
 	Image,
@@ -58,7 +60,10 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	// NEW
+	Alignment,
+	Underline
 ];
 
 // Editor configuration.
@@ -69,6 +74,7 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'bold',
 			'italic',
+			'underline',
 			'link',
 			'bulletedList',
 			'numberedList',
@@ -99,6 +105,44 @@ ClassicEditor.defaultConfig = {
 			'tableColumn',
 			'tableRow',
 			'mergeTableCells'
+		]
+	},
+	heading: {
+		options: [ {
+			model: 'paragraph',
+			title: 'Paragraph',
+			class: 'ck-heading_paragraph'
+		},
+		{
+			model: 'heading1',
+			view: 'h1',
+			title: 'Heading 1',
+			class: 'ck-heading_heading1'
+		},
+		{
+			model: 'heading2',
+			view: 'h2',
+			title: 'Heading 2',
+			class: 'ck-heading_heading2'
+		},
+		{
+			model: 'heading3',
+			view: 'h3',
+			title: 'Heading 3',
+			class: 'ck-heading_heading3'
+		},
+		{
+			model: 'heading4',
+			view: 'h4',
+			title: 'Heading 4',
+			class: 'ck-heading_heading4'
+		},
+		{
+			model: 'heading5',
+			view: 'h5',
+			title: 'Heading 5',
+			class: 'ck-heading_heading5'
+		}
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
