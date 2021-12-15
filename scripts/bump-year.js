533853b7e8261813ba687c4fcbb7a73f48afaf52
Przemyslaw Zan <p.zan@cksource.com>
2021-12-15 18:17:48 +0100

    Replaced bump-year logic with execution of an external function.

--- scripts/bump-year.js
+++ scripts/bump-year.js
@@ -20,65 +20,12 @@ git commit -am "Internal: Bumped the year." && git push
 
 */
 
-const glob = require( 'glob' );
-const minimatch = require( 'minimatch' );
-const fs = require( 'fs' );
-
-const includeDotFiles = {
-	dot: true
-};
-
-glob( '!(build|coverage|node_modules|external)/**', updateYear );
-
-// LICENSE.md, .eslintrc.js, etc.
-glob( '*', includeDotFiles, updateYear );
-
-function updateYear( err, fileNames ) {
-	const filteredFileNames = fileNames.filter( fileName => {
-		// Filter out nested `node_modules`.
-		if ( minimatch( fileName, '**/node_modules/**' ) ) {
-			return false;
-		}
-
-		// Filter out stuff from `src/lib/`.
-		if ( minimatch( fileName, '**/src/lib/**' ) ) {
-			return false;
-		}
-
-		// Filter out builds.
-		if ( minimatch( fileName, '**/ckeditor5-build-*/build/**' ) ) {
-			return false;
-		}
-
-		// Filter out directories.
-		if ( fs.statSync( fileName ).isDirectory() ) {
-			return false;
-		}
-
-		return true;
+require( '@ckeditor/ckeditor5-dev-env' )
+	.bumpYear( {
+		initialYear: '2003',
+		globPatterns: [
+			{ pattern: '*', options: { dot: true } }, // LICENSE.md, .eslintrc.js, etc.
+			{ pattern: '!(build|coverage|node_modules|external)/**' }
+		],
+		cwd: process.cwd()
 	} );
-
-	filteredFileNames.forEach( fileName => {
-		fs.readFile( fileName, ( err, data ) => {
-			data = data.toString();
-
-			const year = new Date().getFullYear();
-			const regexp = /Copyright \(c\) 2003-\d{4}/g;
-			const updatedData = data.replace( regexp, 'Copyright (c) 2003-' + year );
-
-			if ( data == updatedData ) {
-				// License headers are only required in JS files.
-				// Also, the file might have already been updated.
-				if ( fileName.endsWith( '.js' ) && !data.match( regexp ) ) {
-					console.warn( `The file "${ process.cwd() }/${ fileName }" misses a license header.` );
-				}
-			} else {
-				fs.writeFile( fileName, updatedData, err => {
-					if ( err ) {
-						throw err;
-					}
-				} );
-			}
-		} );
-	} );
-}
