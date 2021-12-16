1e1a249b35c36b5aa5fa949a0b0c30aa7a8e4537
Przemyslaw Zan <p.zan@cksource.com>
2021-12-16 21:16:05 +0100

    Updated comment, added new argument.

--- scripts/release/update-package-versions.js
+++ scripts/release/update-package-versions.js
@@ -9,18 +9,24 @@
 
 'use strict';
 
-// Updates `@ckeditor/ckeditor5-*` and `ckeditor5` dependencies in `packages/*` and `release/*` directories to the latest version.
-// Changes in `packages/*` will be committed as well.
-//
-// See https://github.com/cksource/ckeditor5-internal/issues/1123
-//
-// This task must be called before: `npm run release:publish`.
-//
-// Use:
-// npm run release:update-package-versions -- --dry-run
+/*
+Updates `@ckeditor/ckeditor5-*` and `ckeditor5` dependencies in `packages/*` and `release/*` directories to the latest version.
+Changes in `packages/*` will be committed as well.
+
+See https://github.com/cksource/ckeditor5-internal/issues/1123
+
+This task must be called before: `npm run release:publish`.
+
+Use:
+npm run release:update-package-versions -- --dry-run --diff
+Available arguments:
+--dry-run:   Prevents the script from committing, and instead shows list of files that was changed in `packages/*` directory.
+--diff:      Instead of list of files, it displays detailed list of changes from each file. Has no effect without --dry-run.
+*/
 
 require( '@ckeditor/ckeditor5-dev-env' )
 	.updatePackageVersions( {
 		cwd: process.cwd(),
-		dryRun: process.argv.includes( '--dry-run' )
+		dryRun: process.argv.includes( '--dry-run' ),
+		diff: process.argv.includes( '--diff' )
 	} );
