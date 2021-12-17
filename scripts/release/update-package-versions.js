9446f9a9c80001580c61db25e104fdc836f66ef3
przemyslaw-zan <69513154+przemyslaw-zan@users.noreply.github.com>
2021-12-17 11:48:53 +0100

    Change from multi-line comment to single-line comments
    Co-authored-by: Paweł Smyrek <56868128+psmyrek@users.noreply.github.com>

--- scripts/release/update-package-versions.js
+++ scripts/release/update-package-versions.js
@@ -9,20 +9,19 @@
 
 'use strict';
 
-/*
-Updates `@ckeditor/ckeditor5-*` and `ckeditor5` dependencies in `packages/*` and `release/*` directories to the latest version.
-Changes in `packages/*` will be committed as well.
-
-See https://github.com/cksource/ckeditor5-internal/issues/1123
-
-This task must be called before: `npm run release:publish`.
-
-Use:
-npm run release:update-package-versions -- --dry-run --diff
-Available arguments:
---dry-run:   Prevents the script from committing, and instead shows list of files that was changed in `packages/*` directory.
---diff:      Instead of list of files, it displays detailed list of changes from each file. Has no effect without --dry-run.
-*/
+// Updates `@ckeditor/ckeditor5-*` and `ckeditor5` dependencies in `packages/*` and `release/*` directories to the latest version.
+// Changes in `packages/*` will be committed as well.
+//
+// See https://github.com/cksource/ckeditor5-internal/issues/1123.
+//
+// This task must be called before: `npm run release:publish`.
+//
+// Use:
+// npm run release:update-package-versions -- --dry-run --diff
+//
+// Available arguments:
+// --dry-run:   Prevents the script from committing, and instead shows list of files that were changed in `packages/*` directory.
+// --diff:      Instead of list of files, it displays detailed list of changes from each file. Has no effect without --dry-run.
 
 require( '@ckeditor/ckeditor5-dev-env' )
 	.updatePackageVersions( {
