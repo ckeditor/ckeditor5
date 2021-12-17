9f3a8a2fe2cf28e1f0e132c500ec720f43066dc3
Andrzej Stanek <andrzej.stanek415@gmail.com>
2021-12-17 14:52:44 +0100

    Extract isComment() to utils

---
+++ packages/ckeditor5-utils/src/dom/iscomment.js
@@ -0,0 +1,18 @@
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module utils/dom/istext
+ */
+
+/**
+ * Checks if the object is a native DOM Comment node.
+ *
+ * @param {*} obj
+ * @returns {Boolean}
+ */
+export default function isComment( obj ) {
+	return Object.prototype.toString.call( obj ) == '[object Comment]';
+}
