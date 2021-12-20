273a4aaa5cb9edf01d3285a02a4f80fd3eb623ee
Przemyslaw Zan <p.zan@cksource.com>
2021-12-20 22:06:40 +0100

    Specified paths to update

--- scripts/release/update-package-versions.js
+++ scripts/release/update-package-versions.js
@@ -9,6 +9,9 @@
 
 'use strict';
 
+const fs = require( 'fs' );
+const path = require( 'path' );
+
 // Updates `@ckeditor/ckeditor5-*` and `ckeditor5` dependencies in `packages/*` and `release/*` directories to the latest version.
 // Changes in `packages/*` will be committed as well.
 //
@@ -17,15 +20,33 @@
 // This task must be called before: `npm run release:publish`.
 //
 // Use:
-// npm run release:update-package-versions -- --dry-run --diff
+// npm run release:update-package-versions -- --dry-run
 //
 // Available arguments:
-// --dry-run:   Prevents the script from committing, and instead shows list of files that were changed in `packages/*` directory.
-// --diff:      Instead of list of files, it displays detailed list of changes from each file. Has no effect without --dry-run.
+// --dry-run:   Prevents the script from committing, and instead displays detailed list of changes from each file that was changed.
+
+const CKEDITOR5_PATH = path.resolve( __dirname, '..', '..' );
+const CKEDITOR5_INTERNAL_PATH = path.resolve( __dirname, '..', '..', 'external', 'ckeditor5-internal' );
+const COLLABORATION_FEATURES_PATH = path.resolve( __dirname, '..', '..', 'external', 'collaboration-features' );
+
+if ( !fs.existsSync( CKEDITOR5_INTERNAL_PATH ) ) {
+	throw new Error( `The script assumes that the directory "${ CKEDITOR5_INTERNAL_PATH }" exists.` );
+}
+
+if ( !fs.existsSync( COLLABORATION_FEATURES_PATH ) ) {
+	throw new Error( `The script assumes that the directory "${ COLLABORATION_FEATURES_PATH }" exists.` );
+}
 
 require( '@ckeditor/ckeditor5-dev-env' )
 	.updatePackageVersions( {
 		cwd: process.cwd(),
 		dryRun: process.argv.includes( '--dry-run' ),
-		diff: process.argv.includes( '--diff' )
+		pathsToUpdate: [
+			{ path: CKEDITOR5_PATH + '/packages', commit: true },
+			{ path: CKEDITOR5_PATH + '/release', commit: false },
+			{ path: CKEDITOR5_INTERNAL_PATH + '/packages', commit: true },
+			{ path: CKEDITOR5_INTERNAL_PATH + '/release', commit: false },
+			{ path: COLLABORATION_FEATURES_PATH + '/packages', commit: true },
+			{ path: COLLABORATION_FEATURES_PATH + '/release', commit: false }
+		]
 	} );
