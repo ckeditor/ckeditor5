30286f77b39526fce2856b03b9be0ba4cc91d1c7
Kamil Piechaczek <pomek@users.noreply.github.com>
2022-01-13 09:07:41 +0100

    Internal: Removed a dash from the company name.

--- packages/ckeditor5-html-support/tests/manual/htmlcomment.js
+++ packages/ckeditor5-html-support/tests/manual/htmlcomment.js
@@ -1,7 +1,7 @@
 /* global document, window, console */
 
 /**
- * @license Copyright (c) 2003-2022, CKSource - Holding sp. z o.o. All rights reserved.
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
