2b1e4de0da67f746c20a6fa9c7ce039fc263e43e
dawidurbanski <hi@dawidurbanski.com>
2022-02-03 09:32:14 +0100

    Update snippet comments.

--- packages/ckeditor5-engine/docs/_snippets/framework/mini-inspector-structure.js
+++ packages/ckeditor5-engine/docs/_snippets/framework/mini-inspector-structure.js
@@ -26,7 +26,7 @@ function Structure( editor ) {
 
 	editor.conversion.for( 'upcast' ).add( dispatcher => {
 		dispatcher.on( 'element:div', ( evt, data, conversionApi ) => {
-			// Get all the necessary items from the conversion API object
+			// Get all the necessary items from the conversion API object.
 			const {
 				consumable,
 				writer,
@@ -35,54 +35,54 @@ function Structure( editor ) {
 				updateConversionResult
 			} = conversionApi;
 
-			// Get view item from data object
+			// Get view item from data object.
 			const { viewItem } = data;
 
-			// Define elements consumables
+			// Define elements consumables.
 			const wrapper = { name: true, classes: 'wrapper' };
 			const innerWrapper = { name: true, classes: 'inner-wrapper' };
 
-			// Tests if the view element can be consumed
+			// Tests if the view element can be consumed.
 			if ( !consumable.test( viewItem, wrapper ) ) {
 				return;
 			}
 
-			// Check if there is only one child
+			// Check if there is only one child.
 			if ( viewItem.childCount !== 1 ) {
 				return;
 			}
 
-			// Get the first child element
+			// Get the first child element.
 			const firstChildItem = viewItem.getChild( 0 );
 
-			// Check if the first element is a div
+			// Check if the first element is a div.
 			if ( !firstChildItem.is( 'element', 'div' ) ) {
 				return;
 			}
 
-			// Tests if the first child element can be consumed
+			// Tests if the first child element can be consumed.
 			if ( !consumable.test( firstChildItem, innerWrapper ) ) {
 				return;
 			}
 
-			// Create model element
+			// Create model element.
 			const modelElement = writer.createElement( 'myElement' );
 
-			// Insert element on cursor location
+			// Insert element on cursor location.
 			if ( !safeInsert( modelElement, data.modelCursor ) ) {
 				return;
 			}
 
-			// Consume the main outer wrapper element
+			// Consume the main outer wrapper element.
 			consumable.consume( viewItem, wrapper );
-			// Consume the inner wrapper element
+			// Consume the inner wrapper element.
 			consumable.consume( firstChildItem, innerWrapper );
 
-			// Handle children conversion inside inner wrapper element
+			// Handle children conversion inside inner wrapper element.
 			convertChildren( firstChildItem, modelElement );
 
 			// Necessary function call to help setting model range and cursor
-			// for some specific cases when elements being split
+			// for some specific cases when elements being split.
 			updateConversionResult( modelElement, data );
 		} );
 	} );
