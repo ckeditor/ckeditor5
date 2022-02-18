aeaa38d8ba0055d7046158b45820d87f7709a4e1
Andrzej Stanek <catstrategist@gmail.com>
2022-02-18 14:43:54 +0100

    Delete redundant operation in TabObserver and add documentation.

--- packages/ckeditor5-engine/src/view/observer/tabobserver.js
+++ packages/ckeditor5-engine/src/view/observer/tabobserver.js
@@ -8,13 +8,15 @@
  */
 
 import Observer from './observer';
-import DomEventData from './domeventdata';
 import BubblingEventInfo from './bubblingeventinfo';
 
-import { keyCodes, getCode } from '@ckeditor/ckeditor5-utils/src/keyboard';
+import { keyCodes } from '@ckeditor/ckeditor5-utils/src/keyboard';
 
 /**
- * Tab observer introduces the {@link module:engine/view/document~Document#event:tab} event.
+ * Tab observer introduces the {@link module:engine/view/document~Document#event:tab `Document#tab`} event.
+ *
+ * Note that because {@link module:engine/view/observer/tabobserver~TabObserver} is attached by the
+ * {@link module:engine/view/view~View} this event is available by default.
  *
  * @extends module:engine/view/observer/observer~Observer
  */
@@ -38,15 +40,7 @@ export default class TabObserver extends Observer {
 
 			const event = new BubblingEventInfo( doc, 'tab', doc.selection.getFirstRange() );
 
-			doc.fire( event, new DomEventData( doc, data, {
-				altKey: data.altKey,
-				ctrlKey: data.ctrlKey,
-				shiftKey: data.shiftKey,
-				metaKey: data.metaKey,
-				get keystroke() {
-					return getCode( this );
-				}
-			} ) );
+			doc.fire( event, data );
 
 			if ( event.stop.called ) {
 				evt.stop();
