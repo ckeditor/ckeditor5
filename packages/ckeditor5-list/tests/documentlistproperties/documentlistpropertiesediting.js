0d79394219f6537e3933d8dfd3011229d848c0d3
Arkadiusz Filipczak <ark.flp@gmail.com>
2022-02-22 14:41:39 +0100

    Generic post-fixer for DocumentLists (test coverage)

--- packages/ckeditor5-list/tests/documentlistproperties/documentlistpropertiesediting.js
+++ packages/ckeditor5-list/tests/documentlistproperties/documentlistpropertiesediting.js
@@ -6,7 +6,9 @@
 import VirtualTestEditor from '@ckeditor/ckeditor5-core/tests/_utils/virtualtesteditor';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import UndoEditing from '@ckeditor/ckeditor5-undo/src/undoediting';
+import { getData, setData } from '@ckeditor/ckeditor5-engine/src/dev-utils/model';
 import DocumentListPropertiesEditing from '../../src/documentlistproperties/documentlistpropertiesediting';
+import { modelList } from '../documentlist/_utils/utils';
 
 describe( 'DocumentListPropertiesEditing', () => {
 	let editor, model;
@@ -70,6 +72,34 @@ describe( 'DocumentListPropertiesEditing', () => {
 				expect( model.schema.checkAttribute( [ '$root', 'paragraph' ], 'listStart' ) ).to.be.false;
 			} );
 		} );
+
+		it( 'should ensure that all item in a single list have the same `listStyle` attribute', () => {
+			setData( model, modelList( `
+				* 1. {style:circle}
+				* 2.
+				* 3. {style:square}
+				* 4.
+				  # 4.1. {style:default}
+				  # 4.2. {style:upper-roman}
+				  # 4.3. {style:decimal}
+				    # 4.3.1. {style:decimal}
+					# 4.3.2. {style:upper-roman}
+				* 5. {style:disc}
+			` ) );
+
+			expect( getData( model, { withoutSelection: true } ) ).to.equalMarkup( modelList( `
+				* 1. {style:circle}
+				* 2.
+				* 3.
+				* 4.
+				  # 4.1. {style:default}
+				  # 4.2.
+				  # 4.3.
+				    # 4.3.1. {style:decimal}
+				    # 4.3.2.
+				* 5.
+			` ) );
+		} );
 	} );
 
 	describe( 'listReversed', () => {
@@ -101,6 +131,34 @@ describe( 'DocumentListPropertiesEditing', () => {
 				expect( model.schema.checkAttribute( [ '$root', 'paragraph' ], 'listStart' ) ).to.be.false;
 			} );
 		} );
+
+		it( 'should ensure that all item in a single list have the same `listReversed` attribute', () => {
+			setData( model, modelList( `
+				# 1. {reversed:true}
+				# 2.
+				# 3. {reversed:false}
+				# 4.
+				  # 4.1. {reversed:false}
+				  # 4.2. {reversed:true}
+				  # 4.3. {reversed:false}
+				    # 4.3.1. {reversed:true}
+					# 4.3.2. {reversed:false}
+				# 5. {reversed:true}
+			` ) );
+
+			expect( getData( model, { withoutSelection: true } ) ).to.equalMarkup( modelList( `
+				# 1. {reversed:true}
+				# 2.
+				# 3.
+				# 4.
+				  # 4.1. {reversed:false}
+				  # 4.2.
+				  # 4.3.
+				    # 4.3.1. {reversed:true}
+				    # 4.3.2.
+				# 5.
+			` ) );
+		} );
 	} );
 
 	describe( 'listStart', () => {
@@ -131,6 +189,34 @@ describe( 'DocumentListPropertiesEditing', () => {
 			it( 'should not allow set `listStart` on the `paragraph`', () => {
 				expect( model.schema.checkAttribute( [ '$root', 'paragraph' ], 'listStart' ) ).to.be.true;
 			} );
+		} );
+
+		it( 'should ensure that all item in a single list have the same `listStart` attribute', () => {
+			setData( model, modelList( `
+				# 1. {start:2}
+				# 2.
+				# 3. {start:5}
+				# 4.
+				  # 4.1. {start:3}
+				  # 4.2. {start:7}
+				  # 4.3. {start:1}
+				    # 4.3.1. {start:42}
+					# 4.3.2. {start:1}
+				# 5. {start:8}
+			` ) );
+
+			expect( getData( model, { withoutSelection: true } ) ).to.equalMarkup( modelList( `
+				# 1. {start:2}
+				# 2.
+				# 3.
+				# 4.
+				  # 4.1. {start:3}
+				  # 4.2.
+				  # 4.3.
+				    # 4.3.1. {start:42}
+				    # 4.3.2.
+				# 5.
+			` ) );
 		} );
 	} );
 } );
