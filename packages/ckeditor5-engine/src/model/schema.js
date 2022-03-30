7274d3a1c4ccb95a1cb1db360cba9dccd1a574c0
Andrzej Stanek <a.stanek@cksource.com>
2022-03-31 00:31:13 +0200

    Fix API docs.

--- packages/ckeditor5-engine/src/model/schema.js
+++ packages/ckeditor5-engine/src/model/schema.js
@@ -883,7 +883,7 @@ export default class Schema {
 	/**
 	 * Gets attributes of a node that have given property.
 	 *
-	 * @param {<module:engine/model/node~Node>} node Node to get attributes from.
+	 * @param {module:engine/model/node~Node} node Node to get attributes from.
 	 * @param {String} propertyName Name of the property that attribute must have to return it.
 	 * @param {Boolean|Symbol|String|Number|Object|null|undefined} propertyValue Desired value of the property that we want to check.
 	 * When `undefined` attributes will be returned if they have set a given property no matter what the value is. If specified it will
