b65ff249c019a812114e5393a9dadf1ef517b9e5
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2022-04-06 09:02:55 +0200

    Fixed the editor not starting. Improvements to the snippet.

--- packages/ckeditor5-style/docs/_snippets/features/styles.js
+++ packages/ckeditor5-style/docs/_snippets/features/styles.js
@@ -16,19 +16,20 @@ import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
 import Style from '@ckeditor/ckeditor5-style/src/style';
+import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport';
 import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud-services-config';
 
 ClassicEditor
 	.create( document.querySelector( '#snippet-styles' ), {
 		plugins: [
 			ArticlePluginSet, EasyImage, ImageUpload, CloudServices,
-			Code, CodeBlock, Strikethrough, HorizontalLine, Style
+			Code, CodeBlock, Strikethrough, HorizontalLine, GeneralHtmlSupport, Style
 		],
 		toolbar: {
 			items: [
-				'heading',
+				'style',
 				'|',
-				'styleDropdown',
+				'heading',
 				'|',
 				'bold',
 				'italic',
@@ -46,6 +47,80 @@ ClassicEditor
 				'horizontalLine'
 			],
 			shouldNotGroupWhenFull: true
+		},
+		style: {
+			definitions: [
+				{
+					name: 'Red heading',
+					element: 'h2',
+					classes: [ 'red-heading' ]
+				},
+				{
+					name: 'Large heading',
+					element: 'h2',
+					classes: [ 'large-heading' ]
+				},
+				{
+					name: 'Rounded container',
+					element: 'p',
+					classes: [ 'rounded-container' ]
+				},
+				{
+					name: 'Large preview',
+					element: 'p',
+					classes: [ 'large-preview' ]
+				},
+				{
+					name: 'Bold table',
+					element: 'table',
+					classes: [ 'bold-table' ]
+				},
+				{
+					name: 'Facncy table',
+					element: 'table',
+					classes: [ 'fancy-table' ]
+				},
+				{
+					name: 'Colorfull cell',
+					element: 'td',
+					classes: [ 'colorful-cell' ]
+				},
+				{
+					name: 'Vibrant code',
+					element: 'pre',
+					classes: [ 'vibrant-code' ]
+				},
+				{
+					name: 'Marker',
+					element: 'span',
+					classes: [ 'marker' ]
+				},
+				{
+					name: 'Typewriter',
+					element: 'span',
+					classes: [ 'typewriter' ]
+				},
+				{
+					name: 'Deleted text',
+					element: 'span',
+					classes: [ 'deleted' ]
+				},
+				{
+					name: 'Cited work',
+					element: 'span',
+					classes: [ 'cited', 'another-class' ]
+				},
+				{
+					name: 'Small text',
+					element: 'span',
+					classes: [ 'small' ]
+				},
+				{
+					name: 'Very long name of the style',
+					element: 'span',
+					classes: [ 'foo' ]
+				}
+			]
 		},
 		image: {
 			toolbar: [ 'imageStyle:inline', 'imageStyle:block', 'imageStyle:side', '|', 'toggleImageCaption', 'imageTextAlternative' ]
@@ -78,6 +153,16 @@ ClassicEditor
 		setTimeout( () => {
 			outputElement.innerText = editor.getData();
 		}, 500 );
+
+		window.attachTourBalloon( {
+			target: window.findToolbarItem( editor.ui.view.toolbar,
+				item => item.buttonView && item.buttonView.label === 'Styles' ),
+			text: 'Click to apply styles.',
+			editor,
+			tippyOptions: {
+				placement: 'bottom-start'
+			}
+		} );
 	} )
 	.catch( err => {
 		console.error( err.stack );
