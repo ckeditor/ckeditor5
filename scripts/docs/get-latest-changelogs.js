ca6348d4dadee79a84fc40b088f84229a4e643d9
Kamil Piechaczek <pomek@users.noreply.github.com>
2022-04-13 14:04:16 +0200

    Docs: Changed the way how to get all the latest releases.

--- scripts/docs/get-latest-changelogs.js
+++ scripts/docs/get-latest-changelogs.js
@@ -7,9 +7,8 @@
 
 'use strict';
 
-const childProcess = require( 'child_process' );
 const path = require( 'path' );
-const { getChangesForVersion } = require( '@ckeditor/ckeditor5-dev-env/lib/release-tools/utils/changelog' );
+const { getChangesForVersion, getChangelog } = require( '@ckeditor/ckeditor5-dev-env/lib/release-tools/utils/changelog' );
 
 const ROOT_DIRECTORY = path.join( __dirname, '..', '..' );
 
@@ -24,26 +23,18 @@ const VERSIONS_TO_PRINT = 3;
  * @returns {String}
  */
 module.exports = () => {
-	return childProcess
-		// Git does not contain a single command for displaying N last tags.
-		// Hence, we need to adjust the returned output manually.
-		// First, find all available tags.
-		.execSync( 'git tag', {
-			encoding: 'utf8',
-			cwd: ROOT_DIRECTORY
-		} )
-		// Remove the last new line.
-		.trim()
-		// Each line contains a single tag.
-		.split( '\n' )
-		// Reverse the list to start with the latest releases.
-		.reverse()
+	const changes = getChangelog( ROOT_DIRECTORY );
+
+	// Get all releases from the changelog file.
+	return [ ...changes.matchAll( /## \[(?<version>\d+\.\d+\.\d+)\]/g ) ]
+		// Take `version` from matches.
+		.map( match => match.groups.version )
 		// Take three latest.
 		.slice( 0, VERSIONS_TO_PRINT )
 		// And map each version to its changelog entries.
 		.map( version => {
 			// `slice` removes the `v` prefix.
-			const changelog = getChangesForVersion( version.slice( 1 ) )
+			const changelog = getChangesForVersion( version, ROOT_DIRECTORY )
 				// Remove the `ℹ️` character along with its link from breaking change headers.
 				.replace( / \[ℹ️\]\(.+\)$/gm, '' )
 				// Remove `Release highlights` section.
