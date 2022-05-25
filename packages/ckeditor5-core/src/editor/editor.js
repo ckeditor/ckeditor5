1e4f642f9c733a67e71348b97f8fab43839b712a
godai78 <b.biedrzycki@cksource.com>
2022-05-25 09:58:11 +0200

    Docs: post-release review.

--- packages/ckeditor5-core/src/editor/editor.js
+++ packages/ckeditor5-core/src/editor/editor.js
@@ -246,16 +246,16 @@ export default class Editor {
 
 	set isReadOnly( value ) {
 		/**
-		 * {@link #isReadOnly Editor#isReadOnly} property is read-only since version `34.0.0` and can be set only using
+		 * The {@link #isReadOnly Editor#isReadOnly} property is read-only since version `34.0.0` and can be set only using
 		 * {@link #enableReadOnlyMode `Editor#enableReadOnlyMode( lockId )`} and
 		 * {@link #disableReadOnlyMode `Editor#disableReadOnlyMode( lockId )`}.
 		 *
-		 * Before version `34.0.0`:
+		 * Usage before version `34.0.0`:
 		 *
 		 *		editor.isReadOnly = true;
 		 * 		editor.isReadOnly = false;
 		 *
-		 * Since version `34.0.0`:
+		 * Usage since version `34.0.0`:
 		 *
 		 *		editor.enableReadOnlyMode( 'my-feature-id' );
 		 * 		editor.disableReadOnlyMode( 'my-feature-id' );
