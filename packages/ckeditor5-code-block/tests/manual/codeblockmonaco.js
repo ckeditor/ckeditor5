e9f3c72fb263bc80c9a21eb4c1d38672d8d7243c
Dariusz Jarzębski <dumluregn@gmail.com>
2022-06-09 14:39:25 +0200

    Implement monaco in the manual test using unpkg.

--- packages/ckeditor5-code-block/tests/manual/codeblockmonaco.js
+++ packages/ckeditor5-code-block/tests/manual/codeblockmonaco.js
@@ -12,16 +12,16 @@
 // import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 // import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+// import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
-( function() {
-	// create div to avoid needing a HtmlWebpackPlugin template
-	const div = document.createElement( 'div' );
-	div.id = 'root';
-	div.style = 'width:800px; height:600px; border:1px solid #ccc;';
+// ( function() {
+// 	// create div to avoid needing a HtmlWebpackPlugin template
+// 	const div = document.createElement( 'div' );
+// 	div.id = 'root';
+// 	div.style = 'width:800px; height:600px; border:1px solid #ccc;';
 
-	document.body.appendChild( div );
-} )();
+// 	document.body.appendChild( div );
+// } )();
 
 // monaco.editor.create( document.getElementById( 'root' ), {
 // 	value: `const foo = () => 0;`,
