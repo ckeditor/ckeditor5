88fba618551367302aa2b595d6bd9b96e6bab496
Kuba Niegowski <j.niegowski@cksource.com>
2022-06-13 11:28:33 +0200

    Extracted plain content-editable manual test with event logging to a separate test.

--- packages/ckeditor5-typing/tests/manual/beforeinput.js
+++ packages/ckeditor5-typing/tests/manual/beforeinput.js
@@ -3,7 +3,7 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-/* globals CKEditorInspector, console, document */
+/* globals CKEditorInspector, document */
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import BalloonEditor from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
@@ -114,74 +114,4 @@ async function initEditor( editorConstructor, element, name ) {
 	} );
 
 	CKEditorInspector.attach( { [ name ]: editor } );
-}
-
-document.addEventListener( 'beforeinput', logEvent );
-document.addEventListener( 'compositionstart', logEvent );
-document.addEventListener( 'compositionend', logEvent );
-
-document.addEventListener( 'selectionchange', logEvent );
-
-document.addEventListener( 'keydown', logEvent );
-document.addEventListener( 'keyup', logEvent );
-
-function logEvent( evt ) {
-	if ( !document.activeElement || document.activeElement.getAttribute( 'id' ) != 'raw-contenteditable' ) {
-		return;
-	}
-
-	console.group( `%c${ evt.type }`, 'color:red' );
-
-	if ( 'inputType' in evt ) {
-		console.log( `%cinput type:%c "${ evt.inputType }"`, 'font-weight: bold', 'font-weight: default; color: blue' );
-	}
-
-	if ( 'isComposing' in evt ) {
-		console.log( `%cisComposing:%c ${ evt.isComposing }`, 'font-weight: bold', 'font-weight: default; color: green' );
-	}
-
-	if ( 'data' in evt ) {
-		console.log( `%cdata:%c "${ evt.data }"`, 'font-weight: bold', 'font-weight: default; color: red' );
-	}
-
-	if ( 'keyCode' in evt ) {
-		console.log( `%ckeyCode:%c ${ evt.keyCode }`, 'font-weight: bold', 'font-weight: default; color: green' );
-	}
-
-	logTargetRanges( evt );
-	logSelection();
-
-	console.groupEnd();
-}
-
-function logTargetRanges( evt ) {
-	if ( evt.getTargetRanges ) {
-		console.group( '%cevent target ranges:', 'font-weight: bold' );
-		logRanges( evt.getTargetRanges() );
-		console.groupEnd();
-	}
-}
-
-function logSelection() {
-	const selection = document.getSelection();
-	const ranges = [];
-
-	for ( let i = 0; i < selection.rangeCount; i++ ) {
-		ranges.push( selection.getRangeAt( i ) );
-	}
-
-	console.group( '%cselection:', 'font-weight: bold' );
-	logRanges( ranges );
-	console.groupEnd();
-}
-
-function logRanges( ranges ) {
-	if ( !ranges || !ranges.length ) {
-		console.log( '  %cno ranges', 'font-style: italic' );
-	} else {
-		for ( const range of ranges ) {
-			console.log( '  start:', range.startContainer, range.startOffset );
-			console.log( '  end:', range.endContainer, range.endOffset );
-		}
-	}
 }
