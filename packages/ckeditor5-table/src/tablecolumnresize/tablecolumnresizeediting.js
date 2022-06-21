4bf2afb5cd911ecdca34572b7a737558230ee6f8
Marta Motyczynska <m.motyczynska@cksource.com>
2022-06-21 15:30:32 +0200

    Revert "Add listener for drop event."
    This reverts commit 3aa5b279aa2552560dc88720e7e1c946d94727be.

--- packages/ckeditor5-table/src/tablecolumnresize/tablecolumnresizeediting.js
+++ packages/ckeditor5-table/src/tablecolumnresize/tablecolumnresizeediting.js
@@ -454,7 +454,6 @@ export default class TableColumnResizeEditing extends Plugin {
 
 		domEmitter.listenTo( global.window.document, 'mousedown', this._onMouseDownHandler.bind( this ), { priority: 'high' } );
 		domEmitter.listenTo( global.window.document, 'mouseup', this._onMouseUpHandler.bind( this ) );
-		domEmitter.listenTo( global.window.document, 'drop', this._onMouseUpHandler.bind( this ), { useCapture: true } );
 		domEmitter.listenTo(
 			global.window.document, 'mousemove', throttle( this._onMouseMoveThrottledHandler.bind( this ), 50 ), { useCapture: true }
 		);
