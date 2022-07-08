decefcac40af3300ff0dd6c75d56c20f5e4cae03
Kuba Niegowski <j.niegowski@cksource.com>
2022-07-08 17:35:06 +0200

    Added tests.

---
+++ packages/ckeditor5-enter/tests/manual/softbreak-paragraphing.md
@@ -0,0 +1,2 @@
+# The softBreak upcast conversion
+
--- packages/ckeditor5-enter/tests/shiftenter-integration.js
+++ packages/ckeditor5-enter/tests/shiftenter-integration.js
@@ -70,4 +70,149 @@ describe( 'ShiftEnter integration', () => {
 		expect( selection.hasAttribute( 'linkHref' ) ).to.equal( false );
 		expect( selection.hasAttribute( 'bold' ) ).to.equal( false );
 	} );
+
+	describe( 'conversion', () => {
+		it( 'should convert BR inside text in paragraph', () => {
+			editor.setData( '<p>foo<br>bar</p>' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph>foo<softBreak></softBreak>bar</paragraph>'
+			);
+			expect( editor.getData() ).to.equalMarkup(
+				'<p>foo<br>bar</p>'
+			);
+		} );
+
+		it( 'should convert multiple BRs inside text in paragraph', () => {
+			editor.setData( '<p>foo<br><br>bar</p>' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph>foo<softBreak></softBreak><softBreak></softBreak>bar</paragraph>'
+			);
+			expect( editor.getData() ).to.equalMarkup(
+				'<p>foo<br><br>bar</p>'
+			);
+		} );
+
+		it( 'should convert multiple BRs inside text (outside paragraph)', () => {
+			editor.setData( 'foo<br><br>bar' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph>foo<softBreak></softBreak><softBreak></softBreak>bar</paragraph>'
+			);
+			expect( editor.getData() ).to.equalMarkup(
+				'<p>foo<br><br>bar</p>'
+			);
+		} );
+
+		it( 'should convert BR at the beginning of text in paragraph', () => {
+			editor.setData( '<p><br>foo</p>' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph><softBreak></softBreak>foo</paragraph>'
+			);
+			expect( editor.getData() ).to.equalMarkup(
+				'<p><br>foo</p>'
+			);
+		} );
+
+		it( 'should convert BR at the beginning of text (outside paragraph)', () => {
+			editor.setData( '<br>foo' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph><softBreak></softBreak>foo</paragraph>'
+			);
+			expect( editor.getData() ).to.equalMarkup(
+				'<p><br>foo</p>'
+			);
+		} );
+
+		it( 'should convert BR before a paragraph to a paragraph', () => {
+			editor.setData( '<br><p>foo</p>' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph></paragraph><paragraph>foo</paragraph>'
+			);
+			expect( editor.getData() ).to.equalMarkup(
+				'<p>&nbsp;</p><p>foo</p>'
+			);
+		} );
+
+		it( 'should convert BR after a paragraph to a paragraph', () => {
+			editor.setData( '<p>foo</p><br>' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph>foo</paragraph><paragraph></paragraph>'
+			);
+			expect( editor.getData() ).to.equalMarkup(
+				'<p>foo</p><p>&nbsp;</p>'
+			);
+		} );
+
+		it( 'should convert BR between paragraphs to a paragraph', () => {
+			editor.setData( '<p>foo</p><br><p>bar</p>' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph>foo</paragraph><paragraph></paragraph><paragraph>bar</paragraph>'
+			);
+			expect( editor.getData() ).to.equalMarkup(
+				'<p>foo</p><p>&nbsp;</p><p>bar</p>'
+			);
+		} );
+
+		it( 'should convert a single BR', () => {
+			editor.setData( '<br>' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph><softBreak></softBreak></paragraph>'
+			);
+			expect( editor.getData( { trim: 'none' } ) ).to.equalMarkup(
+				'<p><br>&nbsp;</p>'
+			);
+		} );
+
+		it( 'should ignore a BR at the end of a block', () => {
+			editor.setData( '<p>foo<br></p>' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph>foo</paragraph>'
+			);
+			expect( editor.getData( { trim: 'none' } ) ).to.equalMarkup(
+				'<p>foo</p>'
+			);
+		} );
+
+		it( 'should not ignore a BR at the end of a block (wrapped with inline element)', () => {
+			editor.setData( '<p><strong>foo<br></strong></p>' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph><$text bold="true">foo</$text><softBreak></softBreak></paragraph>'
+			);
+			expect( editor.getData( { trim: 'none' } ) ).to.equalMarkup(
+				'<p><strong>foo</strong><br>&nbsp;</p>'
+			);
+		} );
+
+		it( 'should ignore a BR before a block', () => {
+			editor.setData( 'foo<br><p>bar</p>' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph>foo</paragraph><paragraph>bar</paragraph>'
+			);
+			expect( editor.getData( { trim: 'none' } ) ).to.equalMarkup(
+				'<p>foo</p><p>bar</p>'
+			);
+		} );
+
+		it( 'should ignore a BR before a block (with blocks before)', () => {
+			editor.setData( '<p>a</p>foo<br><p>bar</p>' );
+
+			expect( getModelData( model, { withoutSelection: true } ) ).to.equalMarkup(
+				'<paragraph>a</paragraph><paragraph>foo</paragraph><paragraph>bar</paragraph>'
+			);
+			expect( editor.getData( { trim: 'none' } ) ).to.equalMarkup(
+				'<p>a</p><p>foo</p><p>bar</p>'
+			);
+		} );
+	} );
 } );
