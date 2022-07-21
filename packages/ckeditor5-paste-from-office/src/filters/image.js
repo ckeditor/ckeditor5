24904e89379a7d94bd9cf9026054ceec60772e84
yurichi <913463115@qq.com>
2022-07-21 18:25:00 +0800

    feat(ckeditor5-paste-from-office): ckeditor5 31.1.0

--- packages/ckeditor5-paste-from-office/src/filters/image.js
+++ packages/ckeditor5-paste-from-office/src/filters/image.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
