98873f52b4df031853e517a2a8d9f1eb7d3419af
Marta Motyczynska <m.motyczynska@cksource.com>
2022-08-22 15:47:06 +0200

    Tests for arrow right/down in character grid view.

--- packages/ckeditor5-special-characters/tests/ui/charactergridview.js
+++ packages/ckeditor5-special-characters/tests/ui/charactergridview.js
@@ -3,10 +3,13 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
+/* global document */
+
 import CharacterGridView from '../../src/ui/charactergridview';
 import ViewCollection from '@ckeditor/ckeditor5-ui/src/viewcollection';
 import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 import testUtils from '@ckeditor/ckeditor5-core/tests/_utils/utils';
+import { keyCodes } from '@ckeditor/ckeditor5-utils/src/keyboard';
 
 describe( 'CharacterGridView', () => {
 	let view;
@@ -40,6 +43,62 @@ describe( 'CharacterGridView', () => {
 			expect( tilesElement.classList.contains( 'ck-character-grid__tiles' ) ).to.be.true;
 
 			expect( tile.element.parentNode ).to.equal( tilesElement );
+		} );
+
+		describe( 'Focus management across the grid items using arrow keys', () => {
+			let view;
+
+			beforeEach( () => {
+				view = new CharacterGridView();
+
+				createTilesForGrid( view );
+
+				view.render();
+				document.body.appendChild( view.element );
+			} );
+
+			afterEach( () => {
+				view.element.remove();
+				view.destroy();
+			} );
+
+			it( '"arrow right" should focus the next focusable grid item', () => {
+				const keyEvtData = {
+					keyCode: keyCodes.arrowright,
+					preventDefault: sinon.spy(),
+					stopPropagation: sinon.spy()
+				};
+
+				// Mock the first grid item is focused.
+				view.focusTracker.isFocused = true;
+				view.focusTracker.focusedElement = view.tiles.first.element;
+
+				const spy = sinon.spy( view.tiles.get( 1 ), 'focus' );
+
+				view.keystrokes.press( keyEvtData );
+				sinon.assert.calledOnce( keyEvtData.preventDefault );
+				sinon.assert.calledOnce( keyEvtData.stopPropagation );
+				sinon.assert.calledOnce( spy );
+			} );
+
+			it( '"arrow down" should focus the focusable grid item in the second row', () => {
+				const keyEvtData = {
+					keyCode: keyCodes.arrowdown,
+					preventDefault: sinon.spy(),
+					stopPropagation: sinon.spy()
+				};
+
+				// Mock the first grid item is focused.
+				view.focusTracker.isFocused = true;
+				view.focusTracker.focusedElement = view.tiles.first.element;
+
+				const spy = sinon.spy( view.tiles.get( 10 ), 'focus' );
+
+				view.keystrokes.press( keyEvtData );
+				sinon.assert.calledOnce( keyEvtData.preventDefault );
+				sinon.assert.calledOnce( keyEvtData.stopPropagation );
+				sinon.assert.calledOnce( spy );
+			} );
 		} );
 	} );
 
@@ -112,4 +171,10 @@ describe( 'CharacterGridView', () => {
 			} );
 		} );
 	} );
+
+	function createTilesForGrid( gridView ) {
+		for ( let i = 0; i < 11; i++ ) {
+			gridView.tiles.add( gridView.createTile( 'ε', 'foo bar baz' ) );
+		}
+	}
 } );
