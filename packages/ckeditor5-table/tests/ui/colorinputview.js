b9b9e829ccce7f96ae224e882bf946b514306215
Marta Motyczynska <m.motyczynska@cksource.com>
2022-08-23 17:17:59 +0200

    Tests for destroy().

--- packages/ckeditor5-table/tests/ui/colorinputview.js
+++ packages/ckeditor5-table/tests/ui/colorinputview.js
@@ -529,4 +529,22 @@ describe( 'ColorInputView', () => {
 			view.destroy();
 		} );
 	} );
+
+	describe( 'destroy()', () => {
+		it( 'should destroy the FocusTracker instance', () => {
+			const destroySpy = sinon.spy( view.focusTracker, 'destroy' );
+
+			view.destroy();
+
+			sinon.assert.calledOnce( destroySpy );
+		} );
+
+		it( 'should destroy the KeystrokeHandler instance', () => {
+			const destroySpy = sinon.spy( view.keystrokes, 'destroy' );
+
+			view.destroy();
+
+			sinon.assert.calledOnce( destroySpy );
+		} );
+	} );
 } );
