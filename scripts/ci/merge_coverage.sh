8dea18e258bc12d473f490ad04ca0fe955fc138a
Przemyslaw Zan <p.zan@cksource.com>
2022-08-26 18:04:07 +0200

    update

--- scripts/ci/merge_coverage.sh
+++ scripts/ci/merge_coverage.sh
@@ -1,1 +1,1 @@
-curl -k https://coveralls.io/webhook?repo_token=$COVERALLS_REPO_TOKEN -d "payload[build_num]=$BUILD_NUMBER&payload[status]=done"
+curl -k https://coveralls.io/webhook?repo_token=$COVERALLS_REPO_TOKEN -d "payload[build_num]=$TRAVIS_BUILD_NUMBER&payload[status]=done"
