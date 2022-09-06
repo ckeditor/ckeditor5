301e8ff738570c4cff5016a0a251184bb512c590
Kamil Piechaczek <pomek@users.noreply.github.com>
2022-09-06 07:31:55 +0200

    Temporary workaround with importing *.svg files in TS.

--- packages/ckeditor5-core/src/index.ts
+++ packages/ckeditor5-core/src/index.ts
@@ -25,39 +25,39 @@ export { default as secureSourceElement } from './editor/utils/securesourceeleme
 
 export { default as PendingActions } from './pendingactions';
 
-import cancel from './../theme/icons/cancel.svg';
-import caption from './../theme/icons/caption.svg';
-import check from './../theme/icons/check.svg';
-import cog from './../theme/icons/cog.svg';
-import eraser from './../theme/icons/eraser.svg';
-import lowVision from './../theme/icons/low-vision.svg';
-import image from './../theme/icons/image.svg';
+import cancel from '../theme/icons/cancel.svg';
+import caption from '../theme/icons/caption.svg';
+import check from '../theme/icons/check.svg';
+import cog from '../theme/icons/cog.svg';
+import eraser from '../theme/icons/eraser.svg';
+import lowVision from '../theme/icons/low-vision.svg';
+import image from '../theme/icons/image.svg';
 
-import alignBottom from './../theme/icons/align-bottom.svg';
-import alignMiddle from './../theme/icons/align-middle.svg';
-import alignTop from './../theme/icons/align-top.svg';
-import alignLeft from './../theme/icons/align-left.svg';
-import alignCenter from './../theme/icons/align-center.svg';
-import alignRight from './../theme/icons/align-right.svg';
-import alignJustify from './../theme/icons/align-justify.svg';
+import alignBottom from '../theme/icons/align-bottom.svg';
+import alignMiddle from '../theme/icons/align-middle.svg';
+import alignTop from '../theme/icons/align-top.svg';
+import alignLeft from '../theme/icons/align-left.svg';
+import alignCenter from '../theme/icons/align-center.svg';
+import alignRight from '../theme/icons/align-right.svg';
+import alignJustify from '../theme/icons/align-justify.svg';
 
-import objectBlockLeft from './../theme/icons/object-left.svg';
-import objectCenter from './../theme/icons/object-center.svg';
-import objectBlockRight from './../theme/icons/object-right.svg';
-import objectFullWidth from './../theme/icons/object-full-width.svg';
-import objectInline from './../theme/icons/object-inline.svg';
-import objectLeft from './../theme/icons/object-inline-left.svg';
-import objectRight from './../theme/icons/object-inline-right.svg';
+import objectBlockLeft from '../theme/icons/object-left.svg';
+import objectCenter from '../theme/icons/object-center.svg';
+import objectBlockRight from '../theme/icons/object-right.svg';
+import objectFullWidth from '../theme/icons/object-full-width.svg';
+import objectInline from '../theme/icons/object-inline.svg';
+import objectLeft from '../theme/icons/object-inline-left.svg';
+import objectRight from '../theme/icons/object-inline-right.svg';
 
-import objectSizeFull from './../theme/icons/object-size-full.svg';
-import objectSizeLarge from './../theme/icons/object-size-large.svg';
-import objectSizeSmall from './../theme/icons/object-size-small.svg';
-import objectSizeMedium from './../theme/icons/object-size-medium.svg';
+import objectSizeFull from '../theme/icons/object-size-full.svg';
+import objectSizeLarge from '../theme/icons/object-size-large.svg';
+import objectSizeSmall from '../theme/icons/object-size-small.svg';
+import objectSizeMedium from '../theme/icons/object-size-medium.svg';
 
-import pencil from './../theme/icons/pencil.svg';
-import pilcrow from './../theme/icons/pilcrow.svg';
-import quote from './../theme/icons/quote.svg';
-import threeVerticalDots from './../theme/icons/three-vertical-dots.svg';
+import pencil from '../theme/icons/pencil.svg';
+import pilcrow from '../theme/icons/pilcrow.svg';
+import quote from '../theme/icons/quote.svg';
+import threeVerticalDots from '../theme/icons/three-vertical-dots.svg';
 
 export const icons = {
 	cancel,
