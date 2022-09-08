64352ac1b89b0bdaf1dac5198100b426766432b7
Kamil Piechaczek <pomek@users.noreply.github.com>
2022-09-08 09:46:52 +0200

    Simplified the script for checking the structure of manual test directories.

--- scripts/ci/check-directory-structure.js
+++ scripts/ci/check-directory-structure.js
@@ -12,22 +12,20 @@
 const glob = require( 'glob' );
 const path = require( 'path' );
 const { red } = require( './ansi-colors' );
+const ROOT_DIRECTORY = path.join( __dirname, '..', '..' );
 
-// This script ensures that the "manual" test directories are only located in the root
+// This script ensures that the "manual/" test directories are only located in the root
 // of the "tests" directories. Previously, they have been nested deeper, which prevents
 // those tests from being compiled while running tests using the `--files` argument.
-// See: https://github.com/ckeditor/ckeditor5/issues/12251
+// See: https://github.com/ckeditor/ckeditor5/issues/12251.
 
 const manualDirectoriesNotInTestsRoot = new Set(
-	glob.sync( '**/tests/*/**/manual/**/*.js', {
-		ignore: [ '**/node_modules/**' ],
-		cwd: path.join( __dirname, '..', '..' )
-	} ).map( path => path.replace( /[^/]+.js$/, '*.js' ) )
+	glob.sync( 'packages/*/tests/*/**/manual/**/*.@(js|ts|html|md)', { cwd: ROOT_DIRECTORY } )
 );
 
 if ( manualDirectoriesNotInTestsRoot.size ) {
-	console.log( red( 'All "manual" test directories should be directly in the root of the "tests" directory.' ) );
-	console.log( red( 'Following ones do not follow this rule:' ) );
+	console.log( red( 'The "manual/" directory should be stored directly in the "tests/" directory.' ) );
+	console.log( red( 'The following tests do not follow this rule:' ) );
 	console.log( red( [ ...manualDirectoriesNotInTestsRoot ].map( str => ` - ${ str }` ).join( '\n' ) ) );
 
 	process.exit( 1 );
