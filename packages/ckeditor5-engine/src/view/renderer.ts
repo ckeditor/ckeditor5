595a7ddfd9e52f0bc8357d41e5c4385dd7a14f84
Kuba Niegowski <j.niegowski@cksource.com>
2022-10-10 16:54:08 +0200

    Fixed selection updating after fake selection.

--- packages/ckeditor5-engine/src/view/renderer.ts
+++ packages/ckeditor5-engine/src/view/renderer.ts
@@ -900,7 +900,8 @@ export default class Renderer extends Observable {
 		}
 		// There was a fake selection so remove it and update the DOM selection.
 		// This is especially important on Android because otherwise IME will try to compose over the fake selection container.
-		else if ( this._fakeSelectionContainer ) {
+		else if ( this._fakeSelectionContainer && this._fakeSelectionContainer.isConnected ) {
+			console.log( 'remove fake selection container' );
 			this._removeFakeSelection();
 			this._updateDomSelection( domRoot );
 		}
