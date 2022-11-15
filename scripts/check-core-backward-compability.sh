404889a049e8edfa3ff8bb430410daaf5b7f0869
Marcin Panek <m.panek@cksource.com>
2022-11-15 17:04:58 +0100

    Go back to initial branch after backward compatibility finishes

--- scripts/check-core-backward-compability.sh
+++ scripts/check-core-backward-compability.sh
@@ -3,6 +3,9 @@
 # @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
 # For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 
+# Get branch name
+INITIAL_BRANCH=$(git branch --show-current)
+
 # Checkout release branch and update with origin
 git checkout release
 git pull origin release
@@ -20,3 +23,5 @@ cd ../..
 
 # Serve manual for ckeditor5 and run crawler to check for errors
 npx --yes start-server-and-test "yarn manual -f ckeditor5 --no-dll" "http://localhost:8125" "yarn manual:verify"
+
+git checkout $INITIAL_BRANCH
