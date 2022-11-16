a5f31a94973790244380214ca7c027cc20b52239
Kamil Piechaczek <pomek@users.noreply.github.com>
2022-11-16 11:03:04 +0100

    Fixed a script name and improved comments.

--- scripts/check-core-backward-compatibility.sh
+++ scripts/check-core-backward-compatibility.sh
@@ -6,19 +6,19 @@
 # Get branch name.
 INITIAL_BRANCH=$( git branch --show-current )
 
-# Build dlls for core.
+# Prepare CKEditor 5 DLL builds.
 yarn dll:build
 
-# Checkout previous commit.
+# Check out to the latest tag.
 LAST_TAG=$( git tag --sort=committerdate | grep -E '[0-9]' --color=none | tail -1 )
 git checkout $LAST_TAG
 
-# Build dll for an example package.
+# Prepare a DLL build for a single feature.
 cd packages/ckeditor5-basic-styles
 yarn dll:build
 cd ../..
 
-# Serve manual for ckeditor5 and run crawler to check for errors.
+# Compile manual tests and verify whether they work.
 npx --yes start-server-and-test "yarn manual -f ckeditor5/all-features-dll --no-dll" "http://localhost:8125" "yarn manual:verify"
 
 git checkout $INITIAL_BRANCH
