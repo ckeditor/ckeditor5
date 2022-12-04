1f9dfe6e281bd30620afd60b27c56696d97f120b
Szymon Cofalik <s.cofalik@cksource.com>
2022-12-04 15:02:39 +0100

    Minor improvement in docs.

--- packages/ckeditor5-table/src/tablecolumnresize/tablewidthresizecommand.js
+++ packages/ckeditor5-table/src/tablecolumnresize/tablewidthresizecommand.js
@@ -39,8 +39,8 @@ export default class TableWidthResizeCommand extends TablePropertyCommand {
 	 * Changes the `tableWidth` and `columnWidths` attribute values for the given or currently selected table.
 	 *
 	 * @param {Object} options
-	 * @param {String} [options.tableWidth] The new table width. Model attribute gets removed if skipped.
-	 * @param {String} [options.columnWidths] The new table column widths. Model attribute gets removed if skipped.
+	 * @param {String} [options.tableWidth] The new table width. If skipped, the model attribute will be removed.
+	 * @param {String} [options.columnWidths] The new table column widths. If skipped, the model attribute will be removed.
 	 * @param {module:engine/model/element~Element} [options.table] The table that is affected by the resize.
 	 */
 	execute( options = {} ) {
