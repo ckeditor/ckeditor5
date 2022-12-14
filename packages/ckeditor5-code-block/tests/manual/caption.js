896d6ede38f6968ed0dd362d8504ed8d771efc44
Einstrasse <hg9587@naver.com>
2022-12-14 16:21:18 +0900

    codeblock caption manual test cases added

---
+++ packages/ckeditor5-code-block/tests/manual/caption.js
@@ -0,0 +1,34 @@
+
+/* global document, console, window */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import CodeblockCaption from '../../src/codeblockcaption'
+import CodeBlock from '../../src/codeblock';
+import CodeblockToolbar from '../../src/codeblocktoolbar';
+
+ClassicEditor
+	.create( document.querySelector( '#editor' ), {
+		image: { toolbar: [ 'toggleImageCaption', 'imageTextAlternative' ] },
+		plugins: [ Code, CodeBlock, Autoformat, Indent, ArticlePluginSet, CodeblockCaption, CodeblockToolbar ],
+		toolbar: [
+			'heading', '|',
+			'bold', 'italic', 'code', 'blockQuote', 'codeBlock', '|',
+			'outdent', 'indent', '|',
+			'undo', 'redo'
+		],
+        codeblock: {
+            toolbar: [
+                'toggleCodeblockCaption'
+            ]
+        },
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
