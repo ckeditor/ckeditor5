d39403b2bf910b119633e7e6c9cba2bf718b874a
Przemyslaw Zan <p.zan@cksource.com>
2022-12-20 18:37:53 +0100

    Cleanup.

--- packages/ckeditor5-engine/src/index.ts
+++ packages/ckeditor5-engine/src/index.ts
@@ -16,8 +16,8 @@ export { default as DataController, type DataControllerSetEvent } from './contro
 // Conversion.
 export { default as Conversion } from './conversion/conversion';
 export type { DowncastInsertEvent, DowncastSelectionEvent } from './conversion/downcastdispatcher';
-export type { AddHighlightCallback, HighlightDescriptor, RemoveHighlightCallback, AttributeDescriptor } from './conversion/downcasthelpers';
 export type { UpcastElementEvent, UpcastTextEvent } from './conversion/upcastdispatcher';
+export type { AddHighlightCallback, HighlightDescriptor, RemoveHighlightCallback, AttributeDescriptor } from './conversion/downcasthelpers';
 export type { MapperViewToModelPositionEvent } from './conversion/mapper';
 
 // DataProcessor.
