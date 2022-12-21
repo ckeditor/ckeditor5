e6ebb569fdb17a4bde4ea7d5c3d682fa4ea56cdc
Arkadiusz Filipczak <ark.flp@gmail.com>
2022-12-21 10:36:21 +0100

    Added ckeditor5-list/_src.

---
+++ packages/ckeditor5-list/_src/todolist/todolistui.js
@@ -0,0 +1,36 @@
+/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module list/todolist/todolistui
+ */
+
+import { createUIComponent } from '../list/utils';
+import todoListIcon from '../../theme/icons/todolist.svg';
+import { Plugin } from 'ckeditor5/src/core';
+
+/**
+ * The to-do list UI feature. It introduces the `'todoList'` button that
+ * allows to convert elements to and from to-do list items and to indent or outdent them.
+ *
+ * @extends module:core/plugin~Plugin
+ */
+export default class TodoListUI extends Plugin {
+	/**
+	 * @inheritDoc
+	 */
+	static get pluginName() {
+		return 'TodoListUI';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	init() {
+		const t = this.editor.t;
+
+		createUIComponent( this.editor, 'todoList', t( 'To-do List' ), todoListIcon );
+	}
+}
