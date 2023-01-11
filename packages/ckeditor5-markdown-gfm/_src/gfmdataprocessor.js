973cf3e069e3e32cd687297cf83b9bab55b1e7e8
Arkadiusz Filipczak <ark.flp@gmail.com>
2023-01-11 14:38:54 +0100

    Added ckeditor5-markdown-gfm/_src.

---
+++ packages/ckeditor5-markdown-gfm/_src/gfmdataprocessor.js
@@ -0,0 +1,92 @@
+/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module markdown-gfm/gfmdataprocessor
+ */
+
+import { HtmlDataProcessor } from 'ckeditor5/src/engine';
+
+import markdown2html from './markdown2html/markdown2html';
+import html2markdown, { turndownService } from './html2markdown/html2markdown';
+
+/**
+ * This data processor implementation uses GitHub Flavored Markdown as input/output data.
+ *
+ * See the {@glink features/markdown Markdown output} guide to learn more on how to enable it.
+ *
+ * @implements module:engine/dataprocessor/dataprocessor~DataProcessor
+ */
+export default class GFMDataProcessor {
+	/**
+	 * Creates a new instance of the Markdown data processor class.
+	 *
+	 * @param {module:engine/view/document~Document} document
+	 */
+	constructor( document ) {
+		/**
+		 * HTML data processor used to process HTML produced by the Markdown-to-HTML converter and the other way.
+		 *
+		 * @private
+		 * @member {module:engine/dataprocessor/htmldataprocessor~HtmlDataProcessor}
+		 */
+		this._htmlDP = new HtmlDataProcessor( document );
+	}
+
+	/**
+	 * Keeps the specified element in the output as HTML. This is useful if the editor contains
+	 * features producing HTML that is not a part of the Markdown standard.
+	 *
+	 * By default, all HTML tags are removed.
+	 *
+	 * @param element {String} The element name to be kept.
+	 */
+	keepHtml( element ) {
+		turndownService.keep( [ element ] );
+	}
+
+	/**
+	 * Converts the provided Markdown string to a view tree.
+	 *
+	 * @param {String} data A Markdown string.
+	 * @returns {module:engine/view/documentfragment~DocumentFragment} The converted view element.
+	 */
+	toView( data ) {
+		const html = markdown2html( data );
+		return this._htmlDP.toView( html );
+	}
+
+	/**
+	 * Converts the provided {@link module:engine/view/documentfragment~DocumentFragment} to data format &mdash; in this
+	 * case to a Markdown string.
+	 *
+	 * @param {module:engine/view/documentfragment~DocumentFragment} viewFragment
+	 * @returns {String} Markdown string.
+	 */
+	toData( viewFragment ) {
+		const html = this._htmlDP.toData( viewFragment );
+		return html2markdown( html );
+	}
+
+	/**
+	 * Registers a {@link module:engine/view/matcher~MatcherPattern} for view elements whose content should be treated as raw data
+	 * and not processed during the conversion from Markdown to view elements.
+	 *
+	 * The raw data can be later accessed by a
+	 * {@link module:engine/view/element~Element#getCustomProperty custom property of a view element} called `"$rawContent"`.
+	 *
+	 * @param {module:engine/view/matcher~MatcherPattern} pattern The pattern matching all view elements whose content should
+	 * be treated as raw data.
+	 */
+	registerRawContentMatcher( pattern ) {
+		this._htmlDP.registerRawContentMatcher( pattern );
+	}
+
+	/**
+	 * This method does not have any effect on the data processor result. It exists for compatibility with the
+	 * {@link module:engine/dataprocessor/dataprocessor~DataProcessor `DataProcessor` interface}.
+	 */
+	useFillerType() {}
+}
---
+++ packages/ckeditor5-markdown-gfm/_src/markdown2html/markdown2html.js
@@ -0,0 +1,52 @@
+/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module markdown-gfm/markdown2html
+ */
+
+import { marked } from 'marked';
+
+// Overrides.
+marked.use( {
+	tokenizer: {
+		// Disable the autolink rule in the lexer.
+		autolink: () => null,
+		url: () => null
+	},
+	renderer: {
+		checkbox( ...args ) {
+			// Remove bogus space after <input type="checkbox"> because it would be preserved
+			// by DomConverter as it's next to an inline object.
+			return Object.getPrototypeOf( this ).checkbox.call( this, ...args ).trimRight();
+		},
+
+		code( ...args ) {
+			// Since marked v1.2.8, every <code> gets a trailing "\n" whether it originally
+			// ended with one or not (see https://github.com/markedjs/marked/issues/1884 to learn why).
+			// This results in a redundant soft break in the model when loaded into the editor, which
+			// is best prevented at this stage. See https://github.com/ckeditor/ckeditor5/issues/11124.
+			return Object.getPrototypeOf( this ).code.call( this, ...args ).replace( '\n</code>', '</code>' );
+		}
+	}
+} );
+
+/**
+ * Parses markdown string to an HTML.
+ *
+ * @param {String} markdown
+ * @returns {String}
+ */
+export default function markdown2html( markdown ) {
+	return marked.parse( markdown, {
+		gfm: true,
+		breaks: true,
+		tables: true,
+		xhtml: true,
+		headerIds: false
+	} );
+}
+
+export { marked };
