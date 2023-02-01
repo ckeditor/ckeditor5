834a6f96bf65f167148aea59c863b37ac3c5760a
Arkadiusz Filipczak <ark.flp@gmail.com>
2023-02-01 15:57:33 +0100

    Added ckeditor5-ckfinder/_src.

---
+++ packages/ckeditor5-ckfinder/_src/index.js
@@ -0,0 +1,12 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module ckfinder
+ */
+
+export { default as CKFinder } from './ckfinder';
+export { default as CKFinderEditing } from './ckfinderediting';
+export { default as CKFinderUI } from './ckfinderui';
