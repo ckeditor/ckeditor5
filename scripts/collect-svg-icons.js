b4aa4b64ad6c93452489787684d54e81c09be435
Przemyslaw Zan <p.zan@cksource.com>
2023-02-03 21:00:33 +0100

    Implemented review requests.

--- scripts/collect-svg-icons.js
+++ scripts/collect-svg-icons.js
@@ -20,6 +20,8 @@ const ICONS_DIRECTORY = path.join( BUILD_DIRECTORY, 'icons' );
 collectSvgIcons();
 
 async function collectSvgIcons() {
+	logProcess( 'Cleaning the build directory...' );
+
 	if ( !fs.existsSync( BUILD_DIRECTORY ) ) {
 		fs.mkdirSync( BUILD_DIRECTORY );
 	}
@@ -30,95 +32,125 @@ async function collectSvgIcons() {
 
 	fs.mkdirSync( ICONS_DIRECTORY );
 
-	const svgFilesData = glob.sync( '**/packages/*/theme/icons/*.svg', {
-		cwd: ROOT_DIRECTORY,
-		ignore: [ '**/build/**', '**/node_modules/**' ]
-	} ).map( svgFilePath => {
-		const normalizedPath = normalizePath( svgFilePath );
-		const [ , packageName, originalFilename ] = normalizedPath.match( /packages\/([^/]+)\S+\/([^/]+)\.svg/ );
-		const originalPath = normalizePath( path.resolve( svgFilePath ) );
-		const newFilename = `${ packageName }___${ originalFilename }.png`;
-		const newPath = path.join( ICONS_DIRECTORY, newFilename );
-		const shorthand = `{@icon @ckeditor/${ normalizedPath.split( 'packages/' )[ 1 ] }}`;
-		const isExternal = normalizedPath.includes( 'external/' );
+	logProcess( 'Collecting SVG icons...' );
 
-		return { packageName, originalFilename, originalPath, newFilename, newPath, shorthand, isExternal };
-	} ).sort( ( a, b ) => {
-		return a.isExternal - b.isExternal ||
-		a.packageName.localeCompare( b.packageName ) ||
-		a.originalFilename.localeCompare( b.originalFilename );
-	} );
+	const globOptions = { cwd: ROOT_DIRECTORY, ignore: [ '**/build/**', '**/node_modules/**' ] };
+	const svgFilesData = glob.sync( '**/packages/*/theme/icons/*.svg', globOptions ).map( getDataFromPath );
+
+	logProcess( 'Converting to PNG...' );
 
 	for ( const svgFileData of svgFilesData ) {
-		await sharp( svgFileData.originalPath ).png().toFile( svgFileData.newPath );
+		await sharp( svgFileData.originalPath ).png().toFile( svgFileData.pngFilePath );
 	}
+
+	logProcess( 'Creating HTML file...' );
 
 	const htmlPath = normalizePath( path.join( ICONS_DIRECTORY, 'index.html' ) );
 	fs.writeFileSync( htmlPath, createHtmlContent( svgFilesData ), 'utf-8' );
 
-	console.log( chalk.green( `Summary: ${ chalk.underline( htmlPath ) }` ) );
+	console.log( chalk.green( `✔️  Done. Summary: ${ chalk.underline( htmlPath ) }` ) );
+}
+
+function logProcess( message ) {
+	console.log( chalk.blue( `🔹 ${ message }` ) );
 }
 
 function normalizePath( _path ) {
 	return _path.split( path.sep ).join( path.posix.sep );
 }
 
+function getDataFromPath( svgFilePath ) {
+	const normalizedPath = normalizePath( svgFilePath );
+	const repositoryName = normalizedPath.startsWith( 'external/' ) ? normalizedPath.match( /external\/([^/]+)/ )[ 1 ] : 'ckeditor5';
+	const [ , packageName, iconName ] = normalizedPath.match( /packages\/([^/]+)\S+\/([^/]+)\.svg/ );
+	const originalPath = normalizePath( path.resolve( svgFilePath ) );
+	const newFilename = `${ packageName }__${ iconName }.png`;
+	const pngFilePath = path.join( ICONS_DIRECTORY, newFilename );
+	const umbertoSnippet = `{@icon @ckeditor/${ normalizedPath.split( 'packages/' )[ 1 ] }}`;
+
+	return { repositoryName, packageName, iconName, originalPath, newFilename, pngFilePath, umbertoSnippet };
+}
+
 function createHtmlContent( svgFilesData ) {
-	const svgFileDataByPackage = svgFilesData.reduce( ( svgFileDataByPackage, svgFileData ) => {
-		if ( !svgFileDataByPackage[ svgFileData.packageName ] ) {
-			svgFileDataByPackage[ svgFileData.packageName ] = [];
-		}
-
-		svgFileDataByPackage[ svgFileData.packageName ].push( svgFileData );
-
-		return svgFileDataByPackage;
-	}, {} );
-
 	return [
 		'<!DOCTYPE html>',
 		'<html>',
 		'	<head>',
+		'		<title>CKEditor 5 Icons</title>',
 		'		<style>',
-		'			body { font-family: arial }',
+		'			* { box-sizing: border-box; }',
+		'			body { font-family: Arial; }',
 		'			table { border-collapse: collapse; margin: auto; margin-bottom: 25vh; }',
-		'			thead tr:first-child th { padding-top: 20px; font-size: 150%; text-transform: uppercase; }',
-		'			td { border: 1px solid black; }',
+		'			.repositoryHeader tr:first-child th { padding-top: 20px; font-size: 200%; }',
+		'			.packageHeader tr:first-child th { padding-top: 20px; font-size: 150%; }',
+		'			tbody tr:nth-child(odd) td {background: #e5e5e5; }',
+		'			td { border: 1px solid black; padding: 2px; }',
 		'			img { max-height: 20px; }',
 		'		</style>',
 		'	</head>',
 		'	<body>',
 		'		<table>',
-		Object.values( svgFileDataByPackage ).map( createHtmlTableSection ).join( '\n' ),
+		...createTableContent( svgFilesData ),
 		'		</table>',
 		'	</body>',
 		'</html>'
 	].join( '\n' );
 }
 
-function createHtmlTableSection( svgFilesData ) {
-	const tableRows = svgFilesData.map( svgFileData => {
-		return [
-			'				<tr>',
-			`					<td>${ svgFileData.originalFilename }</td>`,
-			`					<td>${ svgFileData.shorthand }</td>`,
-			'					<td>',
-			`						<a target="_blank" href="${ svgFileData.newFilename }"><img src="${ svgFileData.newFilename }"></a>`,
-			'					</td>',
-			'				</tr>'
-		].join( '\n' );
-	} ).join( '\n' );
+function createTableContent( svgFilesData ) {
+	const repositories = [ ...new Set( svgFilesData.map( svgFileData => svgFileData.repositoryName ) ) ].sort();
+	const packages = [ ...new Set( svgFilesData.map( svgFileData => svgFileData.packageName ) ) ].sort();
 
+	const tableRows = [];
+
+	for ( const repositoryName of repositories ) {
+		tableRows.push(
+			'			<thead class="repositoryHeader">',
+			'				<tr>',
+			`					<th colspan="3">${ repositoryName }</th>`,
+			'				</tr>',
+			'			</thead>'
+		);
+
+		for ( const packageName of packages ) {
+			const packageBelongsToRepository = svgFilesData.some( svgFileData => {
+				return svgFileData.repositoryName === repositoryName && svgFileData.packageName === packageName;
+			} );
+
+			if ( !packageBelongsToRepository ) {
+				continue;
+			}
+
+			const svgFilesFromThisPackage = svgFilesData.filter( svgFileData => svgFileData.packageName === packageName )
+				.sort( ( a, b ) => a.iconName.localeCompare( b.iconName ) );
+
+			tableRows.push(
+				'			<thead class="packageHeader">',
+				'				<tr>',
+				`					<th colspan="3">@ckeditor/${ packageName }</th>`,
+				'				</tr>',
+				'				<tr>',
+				'					<th>Icon Name</th><th>Umberto snippet</th><th>PNG</th>',
+				'				</tr>',
+				'			</thead>',
+				'			<tbody>',
+				...svgFilesFromThisPackage.flatMap( createSvgFileRow ),
+				'			</tbody>'
+			);
+		}
+	}
+
+	return tableRows;
+}
+
+function createSvgFileRow( svgFileData ) {
 	return [
-		'			<thead>',
 		'				<tr>',
-		`					<th colspan="3">${ svgFilesData[ 0 ].packageName }</th>`,
-		'				</tr>',
-		'				<tr>',
-		'					<th>Icon Name</th><th>Shorthand</th><th>PNG</th>',
-		'				</tr>',
-		'			</thead>',
-		'			<tbody>',
-		tableRows,
-		'			</tbody>'
-	].join( '\n' );
+		`					<td>${ svgFileData.iconName }</td>`,
+		`					<td><code>${ svgFileData.umbertoSnippet }</code></td>`,
+		'					<td>',
+		`						<a href="${ svgFileData.newFilename }"><img src="${ svgFileData.newFilename }"></a>`,
+		'					</td>',
+		'				</tr>'
+	];
 }
