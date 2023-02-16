65de1cef55da0bb29c63f681b3247b90739d464c
Filip Sobol <filipsobol@users.noreply.github.com>
2023-02-16 14:30:19 +0100

    Output `.d.ts` files for all builds

---
+++ packages/ckeditor5-build-balloon/build/ckeditor.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import BalloonEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
+export default class BalloonEditor extends BalloonEditorBase {
+}
