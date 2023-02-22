4da7487282bf4e0f1bc7b865d0891f54868b9a81
Piotr Szczęśniak <szczesniakp@o2.pl>
2023-02-22 10:48:50 +0100

    Update dropdownLimit in docs.

--- packages/ckeditor5-mention/_src/mention.js
+++ packages/ckeditor5-mention/_src/mention.js
@@ -224,7 +224,8 @@ export default class Mention extends Plugin {
  * @property {Number} [minimumCharacters=0] Specifies after how many characters the autocomplete panel should be shown.
  * @property {Function} [itemRenderer] A function that renders a {@link module:mention/mention~MentionFeedItem}
  * to the autocomplete panel.
- * @property {Number} [dropdownLimit=10] Specify how many available elements per feeds will the users be able to see in the dropdown list.
+ * @property {Number} [dropdownLimit] An optional number to specify how many available elements per feeds
+ * will the users be able to see in the dropdown list.
  */
 
 /**
