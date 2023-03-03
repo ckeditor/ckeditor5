fba19b4c63412c2ccff4d474f36ed52266c7bde6
Kuba Niegowski <j.niegowski@cksource.com>
2023-03-03 11:04:26 +0100

    Updated module augmentation for paragraph package.

--- packages/ckeditor5-build-classic/src/ckeditor.ts
+++ packages/ckeditor5-build-classic/src/ckeditor.ts
@@ -24,7 +24,7 @@ import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import PictureEditing from '@ckeditor/ckeditor5-image/src/pictureediting';
 import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
