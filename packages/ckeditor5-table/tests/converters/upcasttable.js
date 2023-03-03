39bfca23bf9a1e9a799b77799e83a9f88e4c5a92
Michał Remiszewski <m.remiszewski@cksource.com>
2023-03-03 07:52:41 +0100

    Change detecting heading rows behavior.

--- packages/ckeditor5-table/tests/converters/upcasttable.js
+++ packages/ckeditor5-table/tests/converters/upcasttable.js
@@ -416,11 +416,11 @@ describe( 'upcastTable()', () => {
 				'<tbody>' +
 				// This row starts with 1 th (3 total).
 				'<tr><th>21</th><td>22</td><th>23</th><th>24</th></tr>' +
-				// This row starts with 2 th (2 total). This one has max number of heading columns: 2.
+				// This row starts with 2 th (2 total).
 				'<tr><th>31</th><th>32</th><td>33</td><td>34</td></tr>' +
 				// This row starts with 1 th (1 total).
 				'<tr><th>41</th><td>42</td><td>43</td><td>44</td></tr>' +
-				// This row starts with 0 th (3 total).
+				// This row starts with 0 th (3 total). This one has min number of heading columns: 0.
 				'<tr><td>51</td><th>52</th><th>53</th><th>54</th></tr>' +
 				'</tbody>' +
 				'<thead>' +
@@ -431,7 +431,63 @@ describe( 'upcastTable()', () => {
 			);
 
 			expectModel(
-				'<table headingColumns="2" headingRows="1">' +
+				'<table headingRows="1">' +
+				'<tableRow>' +
+					'<tableCell><paragraph>11</paragraph></tableCell>' +
+					'<tableCell><paragraph>12</paragraph></tableCell>' +
+					'<tableCell><paragraph>13</paragraph></tableCell>' +
+					'<tableCell><paragraph>14</paragraph></tableCell>' +
+				'</tableRow>' +
+				'<tableRow>' +
+					'<tableCell><paragraph>21</paragraph></tableCell>' +
+					'<tableCell><paragraph>22</paragraph></tableCell>' +
+					'<tableCell><paragraph>23</paragraph></tableCell>' +
+					'<tableCell><paragraph>24</paragraph></tableCell>' +
+				'</tableRow>' +
+				'<tableRow>' +
+					'<tableCell><paragraph>31</paragraph></tableCell>' +
+					'<tableCell><paragraph>32</paragraph></tableCell>' +
+					'<tableCell><paragraph>33</paragraph></tableCell>' +
+					'<tableCell><paragraph>34</paragraph></tableCell>' +
+				'</tableRow>' +
+				'<tableRow>' +
+					'<tableCell><paragraph>41</paragraph></tableCell>' +
+					'<tableCell><paragraph>42</paragraph></tableCell>' +
+					'<tableCell><paragraph>43</paragraph></tableCell>' +
+					'<tableCell><paragraph>44</paragraph></tableCell>' +
+				'</tableRow>' +
+				'<tableRow>' +
+					'<tableCell><paragraph>51</paragraph></tableCell>' +
+					'<tableCell><paragraph>52</paragraph></tableCell>' +
+					'<tableCell><paragraph>53</paragraph></tableCell>' +
+					'<tableCell><paragraph>54</paragraph></tableCell>' +
+				'</tableRow>' +
+				'</table>'
+			);
+		} );
+
+		it( 'should properly calculate heading columns when result is more than zero', () => {
+			editor.setData(
+				'<table>' +
+				'<tbody>' +
+				// This row starts with 2 th (3 total).
+				'<tr><th>31</th><th>32</th><td>33</td><th>34</th></tr>' +
+				// This row starts with 2 th (2 total).
+				'<tr><th>41</th><th>42</th><td>43</td><td>44</td></tr>' +
+				// This row starts with 1 th (1 total). This one has min number of heading columns: 1.
+				'<tr><th>51</th><td>52</td><td>53</td><td>54</td></tr>' +
+				// This row starts with 4 th (4 total).
+				'<tr><th>11</th><th>12</th><th>13</th><th>14</th></tr>' +
+				'</tbody>' +
+				'<thead>' +
+				// This row has 4 ths but it is a thead.
+				'<tr><th>21</th><th>22</th><th>23</th><th>24</th></tr>' +
+				'</thead>' +
+				'</table>'
+			);
+
+			expectModel(
+				'<table headingColumns="1" headingRows="2">' +
 				'<tableRow>' +
 					'<tableCell><paragraph>11</paragraph></tableCell>' +
 					'<tableCell><paragraph>12</paragraph></tableCell>' +
@@ -482,7 +538,7 @@ describe( 'upcastTable()', () => {
 			);
 
 			expectModel(
-				'<table headingColumns="3" headingRows="1">' +
+				'<table headingColumns="2" headingRows="1">' +
 				'<tableRow>' +
 					'<tableCell><paragraph>11</paragraph></tableCell>' +
 					'<tableCell><paragraph>12</paragraph></tableCell>' +
@@ -500,6 +556,147 @@ describe( 'upcastTable()', () => {
 					'<tableCell><paragraph>33</paragraph></tableCell>' +
 					'<tableCell><paragraph>34</paragraph></tableCell>' +
 				'</tableRow>' +
+				'</table>'
+			);
+		} );
+	} );
+
+	describe( 'heading', () => {
+		it( 'test1', () => {
+			editor.setData(
+				'<table>' +
+					'<tr>' +
+						'<th>a</th>' +
+						'<th>b</th>' +
+					'</tr>' +
+					'<tr>' +
+						'<th>c</th>' +
+						'<td>d</td>' +
+					'</tr>' +
+				'</table>'
+			);
+
+			expectModel(
+				'<table headingColumns="1" headingRows="1">' +
+					'<tableRow>' +
+						'<tableCell><paragraph>a</paragraph></tableCell>' +
+						'<tableCell><paragraph>b</paragraph></tableCell>' +
+					'</tableRow>' +
+					'<tableRow>' +
+						'<tableCell><paragraph>c</paragraph></tableCell>' +
+						'<tableCell><paragraph>d</paragraph></tableCell>' +
+					'</tableRow>' +
+				'</table>'
+			);
+		} );
+
+		it( 'test2', () => {
+			editor.setData(
+				'<table>' +
+					'<caption>Concerts</caption>' +
+					'<tbody>' +
+						'<tr>' +
+							'<th>Date</th>' +
+							'<th>Event</th>' +
+							'<th>Venue</th>' +
+						'</tr>' +
+						'<tr>' +
+							'<td>12 Feb</td>' +
+							'<td>Waltz with Strauss</td>' +
+							'<td>Main Hall</td>' +
+						'</tr>' +
+						'<tr>' +
+							'<td>24 Mar</td>' +
+							'<td>The Obelisks</td>' +
+							'<td>West Wing</td>' +
+						'</tr>' +
+						'<tr>' +
+							'<td>14 Apr</td>' +
+							'<td>The What</td>' +
+							'<td>Main Hall</td>' +
+						'</tr>' +
+					'</tbody>' +
+				'</table>'
+			);
+
+			expectModel(
+				'<table headingRows="1">' +
+					'<tableRow>' +
+						'<tableCell><paragraph>Date</paragraph></tableCell>' +
+						'<tableCell><paragraph>Event</paragraph></tableCell>' +
+						'<tableCell><paragraph>Venue</paragraph></tableCell>' +
+					'</tableRow>' +
+					'<tableRow>' +
+						'<tableCell><paragraph>12 Feb</paragraph></tableCell>' +
+						'<tableCell><paragraph>Waltz with Strauss</paragraph></tableCell>' +
+						'<tableCell><paragraph>Main Hall</paragraph></tableCell>' +
+					'</tableRow>' +
+					'<tableRow>' +
+						'<tableCell><paragraph>24 Mar</paragraph></tableCell>' +
+						'<tableCell><paragraph>The Obelisks</paragraph></tableCell>' +
+						'<tableCell><paragraph>West Wing</paragraph></tableCell>' +
+					'</tableRow>' +
+					'<tableRow>' +
+						'<tableCell><paragraph>14 Apr</paragraph></tableCell>' +
+						'<tableCell><paragraph>The What</paragraph></tableCell>' +
+						'<tableCell><paragraph>Main Hall</paragraph></tableCell>' +
+					'</tableRow>' +
+				'</table>'
+			);
+		} );
+
+		it( 'test3', () => {
+			editor.setData(
+				'<table>' +
+					'<tbody>' +
+						'<tr>' +
+							'<th> </th>' +
+						'</tr>' +
+						'<tr>' +
+							'<td> </td>' +
+						'</tr>' +
+					'</tbody>' +
+				'</table>'
+			);
+
+			expectModel(
+				'<table headingRows="1">' +
+					'<tableRow>' +
+						'<tableCell><paragraph></paragraph></tableCell>' +
+					'</tableRow>' +
+					'<tableRow>' +
+						'<tableCell><paragraph></paragraph></tableCell>' +
+					'</tableRow>' +
+				'</table>'
+			);
+		} );
+
+		it( 'test4', () => {
+			editor.setData(
+				'<table>' +
+					'<tbody>' +
+						'<tr>' +
+							'<th colspan="3">Heading</th>' +
+						'</tr>' +
+						'<tr>' +
+							'<td>Data</td>' +
+							'<td>Data</td>' +
+							'<td>Data</td>' +
+						'</tr>' +
+					'</tbody>' +
+				'</table>'
+			);
+
+			expectModel(
+				'<table headingRows="1">' +
+					'<tableRow>' +
+						'<tableCell colspan="3"><paragraph>Heading</paragraph></tableCell>' +
+					'</tableRow>' +
+					'<tableRow>' +
+						'<tableCell><paragraph>Data</paragraph></tableCell>' +
+						'<tableCell><paragraph>Data</paragraph></tableCell>' +
+						'<tableCell><paragraph>Data</paragraph></tableCell>' +
+					'</tableRow>' +
 				'</table>'
 			);
 		} );
