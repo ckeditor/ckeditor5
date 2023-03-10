0fe2fa610b239246c63ec3a06ed7d180de38f211
godai78 <b.biedrzycki@cksource.com>
2023-03-10 09:22:17 +0100

    Docs: unifying demos toolbars.

--- docs/_snippets/framework/tutorials/external-data-widget.js
+++ docs/_snippets/framework/tutorials/external-data-widget.js
@@ -197,7 +197,7 @@ class ExternalDataWidgetEditing extends Plugin {
 ClassicEditor
 	.create( document.querySelector( '#snippet-external-data-widget' ), {
 		plugins: [ Essentials, Paragraph, Heading, List, Bold, Italic, ExternalDataWidget ],
-		toolbar: [ 'external', '|', 'heading', 'bold', 'italic', 'numberedList', 'bulletedList', '|', 'undo', 'redo' ]
+		toolbar: [ 'undo', 'redo', '|', 'external', '|', 'heading', '|', 'bold', 'italic', '|', 'numberedList', 'bulletedList' ]
 	} )
 	.then( editor => {
 		console.log( 'Editor was initialized', editor );
