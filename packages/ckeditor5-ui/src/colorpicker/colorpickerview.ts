ca51f3b68b49a4a6dbb46bd2f590fe6ce920e80c
illia sheremetov <i.sheremetov@cksource.com>
2023-03-21 11:15:02 +0100

    implement hsl picker

--- packages/ckeditor5-ui/src/colorpicker/colorpickerview.ts
+++ packages/ckeditor5-ui/src/colorpicker/colorpickerview.ts
@@ -9,19 +9,19 @@
 
 import View from '../view';
 import { type Locale, global } from '@ckeditor/ckeditor5-utils';
-import 'vanilla-colorful';
 import 'vanilla-colorful/hex-input.js';
+import 'vanilla-colorful/hsl-string-color-picker.js';
 
 export default class ColorPickerView extends View {
 	/**
 	 * color picker
 	 */
-	declare private _picker: HTMLElement;
+	declare private picker: HTMLElement;
 
 	/**
 	 * color picker input
 	 */
-	declare private _input: HTMLElement;
+	declare private input: HTMLElement;
 
 	constructor( locale: Locale | undefined ) {
 		super( locale );
@@ -35,39 +35,47 @@ export default class ColorPickerView extends View {
 	}
 
 	public setColor( color: string | undefined ): void {
+		console.log( color );
 		if ( color ) {
-			this._picker.setAttribute( 'color', color );
+			this.picker.setAttribute( 'color', color );
 		}
 	}
 
 	public focus(): void {
-		this._picker.focus();
+		this.picker.focus();
 	}
 
 	public override render(): void {
 		super.render();
 
-		this._picker = global.document.createElement( 'hex-color-picker' );
-		this._input = global.document.createElement( 'hex-input' );
-		this._picker.setAttribute( 'color', '#ffffff' );
+		this.picker = global.document.createElement( 'hsl-string-color-picker' );
+		this.input = global.document.createElement( 'hex-input' );
 
 		if ( this.element ) {
-			this.element.appendChild( this._picker );
-			this.element.appendChild( this._input );
+			this.element.appendChild( this.picker );
+			this.element.appendChild( this.input );
 		}
 
-		this._picker.addEventListener( 'color-changed', event => {
-			const colorChangedEvent = event as CustomEvent;
-
-			this.fire( 'change', { value: colorChangedEvent.detail.value } );
+		this.picker.addEventListener( 'color-changed', event => {
+			const customEvent = event as CustomEvent;
+			this.fire( 'change', { value: customEvent.detail.value } );
 		} );
 
-		this._input.addEventListener( 'color-changed', event => {
-			const colorChangedEvent = event as CustomEvent;
-			const color = colorChangedEvent.detail.value || '#ffffff';
+		this.input.addEventListener( 'color-changed', event => {
+			const customEvent = event as CustomEvent;
+			const color = customEvent.detail.value || '#ffffff';
 
 			this.setColor( color );
 			this.fire( 'change', { value: color } );
 		} );
+
+		console.log( 'picker', this.picker.color );
+		console.log( 'element', this.element );
 	}
 }
+
+type CustomEvent = Event & {
+	detail: {
+		value: string;
+	};
+};
