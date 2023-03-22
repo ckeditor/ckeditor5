e71fdeae6133f6ead09ef75e6c8230696519d3de
illia sheremetov <i.sheremetov@cksource.com>
2023-03-22 09:57:18 +0100

    add custom input and api docs

--- packages/ckeditor5-ui/src/colorpicker/colorpickerview.ts
+++ packages/ckeditor5-ui/src/colorpicker/colorpickerview.ts
@@ -7,33 +7,67 @@
  * @module ui/colorpicker/colorpickerview
  */
 
-import View from '../view';
 import { type Locale, global } from '@ckeditor/ckeditor5-utils';
-import 'vanilla-colorful/hex-input.js';
-import 'vanilla-colorful/hsl-string-color-picker.js';
+import { debounce, type DebouncedFunc } from 'lodash-es';
+import View from '../view';
+import InputTextView from '../inputtext/inputtextview';
+
+import 'vanilla-colorful/hex-color-picker.js';
+import '../../theme/components/colorpicker/colorpicker.css';
 
 export default class ColorPickerView extends View {
 	/**
-	 * color picker
+	 * Color picker component.
 	 */
-	declare public picker: HTMLElement;
+	declare public picker: PickerType;
 
 	/**
-	 * color picker input
+	 * Input to defining custom colors in HEX.
 	 */
-	declare public input: HTMLElement;
+	declare public input: InputTextView;
+
+	/**
+	* Debounced event method. The `pickerEvent()` method is called the specified `waitingTime` after `debouncedPickerEvent()` is called,
+	* unless a new action happens in the meantime.
+	*/
+	declare private _debouncePickerEvent: DebouncedFunc< ( arg: string ) => void >;
+
+	/**
+	* Debounced event method. The `inputEvent()` method is called the specified `waitingTime` after `debouncedInputEvent()` is called,
+	* unless a new action happens in the meantime.
+	*/
+	declare private _debounceInputEvent: DebouncedFunc< ( arg: string ) => void >;
 
 	constructor( locale: Locale | undefined ) {
 		super( locale );
+		this.picker = global.document.createElement( 'hex-color-picker' );
+		this.input = this._createInput();
+
+		const children = this.createCollection();
+		children.add( this.input );
 
 		this.setTemplate( {
 			tag: 'div',
 			attributes: {
 				class: [ 'ck', 'ck-color-picker' ]
-			}
+			},
+			children
 		} );
+
+		const waitingTime = 150;
+
+		this._debouncePickerEvent = debounce( ( color: string ) => {
+			this.fire( 'change', { value: color } );
+			this._setInputData( color );
+		}, waitingTime );
+
+		this._debounceInputEvent = debounce( ( color: string ) => {
+			this.fire( 'change', { value: color } );
+			this.setColor( color );
+		}, waitingTime );
 	}
 
+	// Sets color in the color picker.
 	public setColor( color: string | undefined ): void {
 		console.log( color );
 		if ( color ) {
@@ -41,42 +75,52 @@ export default class ColorPickerView extends View {
 		}
 	}
 
-	public getColor(): void {
+	// Return current color from picker.
+	public getColor(): string {
 		return this.picker.color;
 	}
 
-	public focus(): void {
-		this.picker.focus();
-	}
-
+	// Renders color picker in the view.
 	public override render(): void {
 		super.render();
 
-		this.picker = global.document.createElement( 'hsl-string-color-picker' );
-		this.input = global.document.createElement( 'hex-input' );
+		this.picker = global.document.createElement( 'hex-color-picker' );
 
 		if ( this.element ) {
-			this.element.appendChild( this.picker );
-			this.element.appendChild( this.input );
+			this.element.insertBefore( this.picker, this.input.element );
 		}
 
 		this.picker.addEventListener( 'color-changed', event => {
 			const customEvent = event as CustomEvent;
-			this.fire( 'change', { value: customEvent.detail.value } );
+			const color = customEvent.detail.value;
+
+			this._debouncePickerEvent( color );
+		} );
+	}
+
+	// Creates input for defining custom colors in color picker.
+	private _createInput(): InputTextView {
+		const textInput = new InputTextView( this.locale );
+
+		textInput.extendTemplate( {
+			attributes: {
+				class: 'color-picker-hex-input',
+				placeholder: 'HEX'
+			}
 		} );
 
-		this.input.addEventListener( 'color-changed', event => {
-			const customEvent = event as CustomEvent;
-			const color = customEvent.detail.value || '#ffffff';
+		textInput.on( 'input', () => {
+			const inputValue = textInput.element!.value;
 
-			this.setColor( color );
-			this.fire( 'change', { value: color } );
+			this._debouncePickerEvent( inputValue );
 		} );
 
-		this.focus();
+		return textInput;
+	}
 
-		console.log( 'picker', this.picker.color );
-		console.log( 'element', this.element );
+	// Sets given value into input.
+	private _setInputData( value: string ): void {
+		this.input.element!.value = value;
 	}
 }
 
@@ -84,4 +128,8 @@ type CustomEvent = Event & {
 	detail: {
 		value: string;
 	};
+};
+
+type PickerType = HTMLElement & {
+	color: string;
 };
