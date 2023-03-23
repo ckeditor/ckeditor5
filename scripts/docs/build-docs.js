9d4d9aeb8c2872db0f9a9be0ddc1d49dc07b0602
Przemyslaw Zan <p.zan@cksource.com>
2023-03-23 11:44:20 +0100

    Switched default docs source to typedoc.

--- scripts/docs/build-docs.js
+++ scripts/docs/build-docs.js
@@ -9,12 +9,8 @@
 
 'use strict';
 
-const path = require( 'path' );
-const fs = require( 'fs' );
 const buildApiDocs = require( './buildapi' );
 const parseArguments = require( './parse-arguments' );
-
-const ROOT_DIRECTORY = path.join( __dirname, '..', '..' );
 
 buildDocs();
 
@@ -31,44 +27,13 @@ function buildDocs() {
 
 	return promise
 		.then( () => {
-			// Set the proper API type for Umberto if requested building the API docs for the TypeScript source code.
-			if ( options.ts ) {
-				setApiTypeForUmberto( 'typedoc' );
-			}
-		} )
-		.then( () => {
 			return runUmberto( options );
 		} )
 		.catch( err => {
 			console.error( err );
 
 			process.exitCode = 1;
-		} )
-		.finally( () => {
-			// Restore the default JSDoc-based API type for Umberto if the docs were built for the TypeScript source code.
-			if ( options.ts ) {
-				setApiTypeForUmberto( 'jsdoc' );
-			}
 		} );
-}
-
-/**
- * Updates the API type configuration for Umberto.
- *
- * @param {String} type
- */
-function setApiTypeForUmberto( type ) {
-	const umbertoConfigPath = path.join( ROOT_DIRECTORY, 'docs', 'umberto.json' );
-	const umbertoConfig = JSON.parse( fs.readFileSync( umbertoConfigPath, 'utf-8' ) );
-	const umbertoConfigApi = umbertoConfig.groups.find( group => group.id === 'api-reference' );
-
-	if ( !umbertoConfigApi ) {
-		return;
-	}
-
-	umbertoConfigApi.type = type;
-
-	fs.writeFileSync( umbertoConfigPath, JSON.stringify( umbertoConfig, null, '\t' ) + '\n', 'utf-8' );
 }
 
 /**
