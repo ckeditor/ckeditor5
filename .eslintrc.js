6325b271765d14ffc3921cd0a012edd2dc423848
Paweł Smyrek <p.smyrek@cksource.com>
2023-03-30 14:05:44 +0200

    Internal: Restored the `ckeditor5-rules/no-build-extensions` rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,16 +39,6 @@ module.exports = {
 			rules: {
 				'ckeditor5-rules/ckeditor-imports': 'off'
 			}
-		},
-		// Temporarily disable checking the "ckeditor5-rules/no-build-extensions" rule.
-		{
-			files: [
-				'docs/**/*.@(js|ts)',
-				'packages/*/docs/**/*.@(js|ts)'
-			],
-			rules: {
-				'ckeditor5-rules/no-build-extensions': 'off'
-			}
 		}
 	]
 };
