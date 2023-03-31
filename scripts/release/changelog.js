21fef109face2de6a912a0a734458a8405edae11
Kamil Piechaczek <pomek@users.noreply.github.com>
2023-03-31 14:00:52 +0200

    Internal: Enabled generating changelog from external repositories.

--- scripts/release/changelog.js
+++ scripts/release/changelog.js
@@ -29,7 +29,7 @@ Promise.resolve()
 		cwd: process.cwd(),
 		packages: 'packages',
 		releaseBranch: 'release',
-		from: 'v37.0.0-alpha.2',
+		from: 'v36.0.1',
 		highlightsPlaceholder: true,
 		transformScope: name => {
 			if ( name === 'ckeditor5' ) {
@@ -45,22 +45,21 @@ Promise.resolve()
 			}
 
 			return 'https://www.npmjs.com/package/@ckeditor/ckeditor5-' + name;
-		}
-		// Temporary, as we publish only packages from the `ckeditor/ckeditor5` repository.
-		// externalRepositories: [
-		// 	{
-		// 		cwd: CKEDITOR5_INTERNAL_PATH,
-		// 		packages: 'packages',
-		// 		skipLinks: true,
-		// 		from: 'v36.0.1' // TODO: To be removed once a new major version is live.
-		// 	},
-		// 	{
-		// 		cwd: COLLABORATION_FEATURES_PATH,
-		// 		packages: 'packages',
-		// 		skipLinks: true,
-		// 		from: 'v36.0.1' // TODO: To be removed once a new major version is live.
-		// 	}
-		// ]
+		},
+		externalRepositories: [
+			{
+				cwd: CKEDITOR5_INTERNAL_PATH,
+				packages: 'packages',
+				skipLinks: true,
+				from: 'v36.0.1' // TODO: To be removed once a new major version is live.
+			},
+			{
+				cwd: COLLABORATION_FEATURES_PATH,
+				packages: 'packages',
+				skipLinks: true,
+				from: 'v36.0.1' // TODO: To be removed once a new major version is live.
+			}
+		]
 	} ) )
 	.then( () => {
 		console.log( 'Done!' );
