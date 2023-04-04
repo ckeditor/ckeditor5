fdf53b55b2eeb384d238fa8075f6a3210ada1c29
Kamil Piechaczek <pomek@users.noreply.github.com>
2023-04-04 09:48:58 +0200

    Removed the types property from pkg.json.

--- scripts/ci/check-packages-code-coverage.js
+++ scripts/ci/check-packages-code-coverage.js
@@ -90,9 +90,7 @@ module.exports = function checkPackagesCodeCoverage( {
 
 		console.log( '* Updating the "main" field in `package.json`.' );
 
-		const pkgJsonMain = pkgJson.main;
-		pkgJson.main = pkgJsonMain.replace( /\.ts$/, 'js' );
-		pkgJson.types = pkgJsonMain.replace( /\.ts$/, 'd.ts' );
+		pkgJson.main = pkgJson.main.replace( /\.ts$/, 'js' );
 
 		fs.writeFileSync( pkgJsonPath, JSON.stringify( pkgJson, null, 2 ) + '\n', 'utf-8' );
 	}
