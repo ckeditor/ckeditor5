6d440a5b9975cb6536f6bfde24ef1ddfd94f4de3
Marta Motyczynska <m.motyczynska@cksource.com>
2023-04-05 16:23:24 +0200

    Add missing aria properties to splitbuttonview.

--- packages/ckeditor5-ui/src/dropdown/button/splitbuttonview.ts
+++ packages/ckeditor5-ui/src/dropdown/button/splitbuttonview.ts
@@ -150,6 +150,16 @@ export default class SplitButtonView extends View<HTMLDivElement> implements Dro
 	/**
 	 * @inheritDoc
 	 */
+	declare public ariaLabel?: string | undefined;
+
+	/**
+	 * @inheritDoc
+	 */
+	declare public ariaLabelledBy: string | undefined;
+
+	/**
+	 * @inheritDoc
+	 */
 	constructor( locale?: Locale ) {
 		super( locale );
 
