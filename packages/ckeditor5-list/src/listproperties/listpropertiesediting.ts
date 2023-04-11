511982467b5bb0ca767ed3a14c14d88ae291387b
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2023-04-11 17:59:17 +0200

    ListPropertiesEditing should not attempt converting list attributes during upcast if the list was disallowed in the first place.

--- packages/ckeditor5-list/src/listproperties/listpropertiesediting.ts
+++ packages/ckeditor5-list/src/listproperties/listpropertiesediting.ts
@@ -372,6 +372,11 @@ function createAttributeStrategies( enabledProperties: ListPropertiesConfig ) {
 function upcastListItemAttributes( attributeStrategies: Array<AttributeStrategy> ) {
 	return ( dispatcher: UpcastDispatcher ) => {
 		dispatcher.on<UpcastElementEvent>( 'element:li', ( evt, data, conversionApi ) => {
+			// https://github.com/ckeditor/ckeditor5/issues/13858
+			if ( !data.modelRange ) {
+				return;
+			}
+
 			const listParent = data.viewItem.parent as ViewElement;
 			const listItem = data.modelRange!.start.nodeAfter || data.modelRange!.end.nodeBefore;
 
