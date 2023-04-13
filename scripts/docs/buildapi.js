f80bb3d77fc4361e6a06419182bf056f43d93857
Kamil Piechaczek <pomek@users.noreply.github.com>
2023-04-13 06:59:23 +0200

    Do not build API pages from the declaration files.

--- scripts/docs/buildapi.js
+++ scripts/docs/buildapi.js
@@ -37,12 +37,12 @@ function getConfig() {
 		tsconfig: path.join( ROOT_DIRECTORY, 'tsconfig.typedoc.json' ),
 		sourceFiles: [
 			// CKEditor 5 sources.
-			'packages/@(ckeditor|ckeditor5)-*/src/**/*.ts',
-			'external/@(ckeditor5-internal|collaboration-features)/packages/@(ckeditor|ckeditor5)-*/src/**/*.ts',
+			'packages/ckeditor5-*/src/**/*.ts',
+			'external/@(ckeditor5-internal|collaboration-features)/packages/ckeditor5-*/src/**/*.ts',
 
 			// Ignore libraries or generated files.
-			'!packages/@(ckeditor|ckeditor5)-*/src/lib/**/*.ts',
-			'!external/@(ckeditor5-internal|collaboration-features)/packages/@(ckeditor|ckeditor5)-*/src/lib/**/*.ts',
+			'!packages/ckeditor5-*/src/lib/**/*.ts',
+			'!external/@(ckeditor5-internal|collaboration-features)/packages/ckeditor5-*/src/lib/**/*.ts',
 
 			// Ignore not a direct sources.
 			'!external/collaboration-features/packages/ckeditor5-operations-compressor/src/protobufdescriptions.ts',
@@ -51,9 +51,13 @@ function getConfig() {
 			'!packages/ckeditor5-build-*/src/**/*.ts',
 			'!external/@(ckeditor5-internal|collaboration-features)/packages/ckeditor5-build-*/src/**/*.ts',
 
+			// Ignore all declarations.
+			'!packages/ckeditor5-*/src/**/*.d.ts',
+			'!external/@(ckeditor5-internal|collaboration-features)/packages/ckeditor5-*/src/**/*.d.ts',
+
 			// Ignore augmentation files.
-			'!packages/@(ckeditor|ckeditor5)-*/src/augmentation.ts',
-			'!external/@(ckeditor5-internal|collaboration-features)/packages/@(ckeditor|ckeditor5)-*/src/augmentation.ts'
+			'!packages/ckeditor5-*/src/augmentation.ts',
+			'!external/@(ckeditor5-internal|collaboration-features)/packages/ckeditor5-*/src/augmentation.ts'
 		]
 	};
 }
