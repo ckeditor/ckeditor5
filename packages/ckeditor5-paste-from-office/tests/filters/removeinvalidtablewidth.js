025c51499a4a6937cc5b5db9802d5a365b65f890
Filip Sobol <filipsobol@users.noreply.github.com>
2023-04-19 11:17:39 +0200

    Add tests for Google sheets normalizer and filters

---
+++ packages/ckeditor5-paste-from-office/tests/filters/removeinvalidtablewidth.js
@@ -0,0 +1,40 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import HtmlDataProcessor from '@ckeditor/ckeditor5-engine/src/dataprocessor/htmldataprocessor';
+import removeInvalidTableWidth from '../../src/filters/removeinvalidtablewidth';
+import UpcastWriter from '@ckeditor/ckeditor5-engine/src/view/upcastwriter';
+import Document from '@ckeditor/ckeditor5-engine/src/view/document';
+import { StylesProcessor } from '@ckeditor/ckeditor5-engine/src/view/stylesmap';
+
+describe( 'PasteFromOffice - filters', () => {
+	const htmlDataProcessor = new HtmlDataProcessor( new Document( new StylesProcessor() ) );
+
+	describe( 'removeInvalidTableWidth', () => {
+		let writer, viewDocument;
+
+		before( () => {
+			viewDocument = new Document();
+			writer = new UpcastWriter( viewDocument );
+		} );
+
+		it( 'should remove "width:0px" from Google Sheets table', () => {
+			const inputData =
+				'<table style="width:0px">' +
+					'<tbody>' +
+						'<tr>' +
+							'<td>123</td>' +
+						'</tr>' +
+					'</tbody>' +
+				'</table>';
+
+			const documentFragment = htmlDataProcessor.toView( inputData );
+
+			removeInvalidTableWidth( documentFragment, writer );
+
+			expect( htmlDataProcessor.toData( documentFragment ) ).to.equal( '<table><tbody><tr><td>123</td></tr></tbody></table>' );
+		} );
+	} );
+} );
