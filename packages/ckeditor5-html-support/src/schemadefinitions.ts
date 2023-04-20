19490703904279cf658a28673253b0cce7c9ae62
Filip Sobol <filipsobol@users.noreply.github.com>
2023-04-20 15:50:01 +0200

    Fix (style): `removeFormat` command should remove styles

--- packages/ckeditor5-html-support/src/schemadefinitions.ts
+++ packages/ckeditor5-html-support/src/schemadefinitions.ts
@@ -519,98 +519,112 @@ export default {
 			model: 'htmlAcronym',
 			view: 'acronym',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlTt',
 			view: 'tt',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlFont',
 			view: 'font',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlTime',
 			view: 'time',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlVar',
 			view: 'var',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlBig',
 			view: 'big',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlSmall',
 			view: 'small',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlSamp',
 			view: 'samp',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlQ',
 			view: 'q',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlOutput',
 			view: 'output',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlKbd',
 			view: 'kbd',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlBdi',
 			view: 'bdi',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlBdo',
 			view: 'bdo',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlAbbr',
 			view: 'abbr',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
@@ -673,7 +687,8 @@ export default {
 			view: 'del',
 			coupledAttribute: 'strikethrough',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		// TODO According to HTML-spec can behave as div-like element, although CKE4 only handles it as an inline element.
@@ -681,7 +696,8 @@ export default {
 			model: 'htmlIns',
 			view: 'ins',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
@@ -724,35 +740,40 @@ export default {
 			model: 'htmlMark',
 			view: 'mark',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlSpan',
 			view: 'span',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlCite',
 			view: 'cite',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlLabel',
 			view: 'label',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 		{
 			model: 'htmlDfn',
 			view: 'dfn',
 			attributeProperties: {
-				copyOnEnter: true
+				copyOnEnter: true,
+				isFormatting: true
 			}
 		},
 
