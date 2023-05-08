13169d513cc1e67040c19a41a601b57d86038505
Marcin Panek <m.panek@cksource.com>
2023-05-08 12:07:55 +0200

    Added build script for TS and DLLs for root of ckeditor5 for the new release process.

---
+++ scripts/release/builddllsandtsforckeditor5root.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* eslint-env node */
+
+'use strict';
+
+const { tools } = require( '@ckeditor/ckeditor5-dev-utils' );
+
+module.exports = function buildDllsAndTsForCkeditor5Root() {
+	tools.shExec( 'yarn run build' );
+	tools.shExec( 'yarn run dll:build --skip-packages-dll' );
+};
