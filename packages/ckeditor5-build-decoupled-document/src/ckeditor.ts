41bc7c110401784953509f934d3db55d5efb666b
MaximVansteenkiste <vsmaxim@me.com>
2023-05-08 15:28:39 +0200

    Update ckeditor.ts

--- packages/ckeditor5-build-decoupled-document/src/ckeditor.ts
+++ packages/ckeditor5-build-decoupled-document/src/ckeditor.ts
@@ -48,9 +48,8 @@ import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
 import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
 
+// @ts-ignore
 import GeneralHtmlSupport from '../../ckeditor5-html-support/src/generalhtmlsupport';
-
-
 // @ts-ignore
 import { Iframe } from '@ftrprf/ckeditor5-iframe/src/index';
 // @ts-ignore
