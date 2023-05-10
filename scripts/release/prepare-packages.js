6e09a49004b1eaa6b00091075afcbe2c58eeef34
Kamil Piechaczek <pomek@users.noreply.github.com>
2023-05-10 13:19:09 +0200

    Integrates a script for preparing the repository.

--- scripts/release/prepare-packages.js
+++ scripts/release/prepare-packages.js
@@ -9,17 +9,30 @@
 
 'use strict';
 
+const fs = require( 'fs' );
+const upath = require( 'upath' );
 const releaseTools = require( '@ckeditor/ckeditor5-dev-release-tools' );
 const updateVersionReferences = require( './update-version-references' );
 const buildTsAndDllForCkeditor5Root = require( './buildtsanddllforckeditor5root' );
+const pkgJson = require( '../../package.json' );
 
 const abortController = new AbortController();
-
+const PACKAGES_DIRECTORY = 'packages';
 const RELEASE_DIRECTORY = 'release';
 
 process.on( 'SIGINT', () => {
 	abortController.abort( 'SIGINT' );
 } );
+
+process.on( 'unhandledRejection', error => {
+	console.error( error );
+
+	process.exit( 1 );
+} );
+
+abortController.signal.addEventListener( 'abort', () => {
+	process.exit( 1 );
+}, { once: true } );
 
 ( async () => {
 	const latestVersion = releaseTools.getLastFromChangelog();
@@ -29,12 +42,50 @@ process.on( 'SIGINT', () => {
 		shouldUpdateVersionCallback: require( './isckeditor5package' )
 	} );
 
+	// releaseTools.updateVersions( {
+	// 	packagesDirectory: PACKAGES_DIRECTORY,
+	// 	version: latestVersion
+	// } );
+
 	updateVersionReferences( {
 		version: latestVersion,
 		releaseDate: new Date()
 	} );
 
 	buildTsAndDllForCkeditor5Root();
+
+	await releaseTools.prepareRepository( {
+		outputDirectory: RELEASE_DIRECTORY,
+		packagesDirectory: PACKAGES_DIRECTORY,
+		rootPackageJson: {
+			name: pkgJson.name,
+			version: pkgJson.version,
+			keywords: pkgJson.keywords,
+			description: 'A set of ready-to-use rich text editors created with a powerful framework.' +
+				' Made with real-time collaborative editing in mind.',
+			dependencies: getCKEditor5Dependencies( pkgJson.dependencies ),
+			engines: pkgJson.engines,
+			author: pkgJson.author,
+			license: pkgJson.license,
+			homepage: pkgJson.homepage,
+			bugs: pkgJson.bugs,
+			repository: pkgJson.repository,
+			files: [
+				// Do not add the entire `build/` directory as it contains files produced by internal scripts:
+				// automated/manual tests, translations, documentation, content styles.
+				// If you need to release anything from the directory, insert a relative path to the file/directory.
+				'src/*.js',
+				'src/*.d.ts',
+				'build/ckeditor5-dll.js',
+				'build/ckeditor5-dll.manifest.json',
+				'build/translations/*.js',
+				// npm default files.
+				'CHANGELOG.md',
+				'LICENSE.md',
+				'README.md'
+			]
+		}
+	} );
 
 	await releaseTools.executeInParallel( {
 		packagesDirectory: RELEASE_DIRECTORY,
@@ -50,3 +101,32 @@ process.on( 'SIGINT', () => {
 		taskToExecute: require( './preparedllbuildscallback' )
 	} );
 } )();
+
+/**
+ * Returns an array that contains name of packages that the `ckeditor5` package should define as its dependencies.
+ *
+ * @param {Object} dependencies Dependencies to filter out.
+ * @returns {Array.<String>}
+ */
+function getCKEditor5Dependencies( dependencies ) {
+	// Short name of packages specified as DLL.
+	const dllPackages = fs.readdirSync( upath.join( __dirname, '..', '..', 'src' ) )
+		.map( directory => directory.replace( /\.[tj]s$/, '' ) );
+
+	// Name of packages that are listed in `src/` as DLL packages.
+	const ckeditor5Dependencies = Object.keys( dependencies )
+		.filter( packageName => {
+			const shortPackageName = packageName.replace( /@ckeditor\/ckeditor5?-/, '' );
+
+			return dllPackages.includes( shortPackageName );
+		} );
+
+	// The proper object for inserting into the `package.json` file.
+	const dependencyObject = {};
+
+	for ( const item of ckeditor5Dependencies ) {
+		dependencyObject[ item ] = dependencies[ item ];
+	}
+
+	return dependencyObject;
+}
