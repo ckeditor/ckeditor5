7a114f1461a57f25b2ceb30c53027a1e735e3274
Marta Motyczynska <m.motyczynska@cksource.com>
2023-05-17 11:38:26 +0200

    Introduce the ImageSizeAttributes plugin.

---
+++ packages/ckeditor5-image/src/imagesizeattributes.ts
@@ -0,0 +1,123 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module image/imagesizeattributes
+ */
+
+import { Plugin } from 'ckeditor5/src/core';
+import type { ViewElement } from 'ckeditor5/src/engine';
+import ImageUtils from './imageutils';
+
+/**
+ * TODO
+ */
+export default class ImageSizeAttributes extends Plugin {
+	/**
+	 * @inheritDoc
+	 */
+	public static get requires() {
+		return [ ImageUtils ] as const;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static get pluginName(): 'ImageSizeAttributes' {
+		return 'ImageSizeAttributes';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public init(): void {
+		this._registerSchema();
+		this._registerConverters( 'imageBlock' );
+		this._registerConverters( 'imageInline' );
+	}
+
+	/**
+	 * TODO
+	 */
+	private _registerSchema(): void {
+		if ( this.editor.plugins.has( 'ImageBlockEditing' ) ) {
+			this.editor.model.schema.extend( 'imageBlock', { allowAttributes: [ 'widthAttribute', 'heightAttribute' ] } );
+		}
+
+		if ( this.editor.plugins.has( 'ImageInlineEditing' ) ) {
+			this.editor.model.schema.extend( 'imageInline', { allowAttributes: [ 'widthAttribute', 'heightAttribute' ] } );
+		}
+	}
+
+	/**
+	 * Registers converters for `width` and `height` attributes.
+	 */
+	private _registerConverters( imageType: 'imageBlock' | 'imageInline' ): void {
+		const editor = this.editor;
+		const imageUtils = editor.plugins.get( 'ImageUtils' );
+		const viewElementName = imageType === 'imageBlock' ? 'figure' : 'img';
+
+		editor.conversion.for( 'upcast' )
+			.attributeToAttribute( {
+				view: {
+					name: viewElementName,
+					attributes: {
+						width: /.+/
+					}
+				},
+				model: {
+					key: 'widthAttribute',
+					value: ( viewElement: ViewElement ) => viewElement.getAttribute( 'width' )
+				}
+			} )
+			.attributeToAttribute( {
+				view: {
+					name: viewElementName,
+					attributes: {
+						height: /.+/
+					}
+				},
+				model: {
+					key: 'heightAttribute',
+					value: ( viewElement: ViewElement ) => viewElement.getAttribute( 'height' )
+				}
+			} );
+
+		// Dedicated converter to propagate attributes to the <img> element.
+		editor.conversion.for( 'downcast' ).add( dispatcher => {
+			dispatcher.on( `attribute:widthAttribute:${ imageType }`, ( evt, data, conversionApi ) => {
+				if ( !conversionApi.consumable.consume( data.item, evt.name ) ) {
+					return;
+				}
+
+				const viewWriter = conversionApi.writer;
+				const viewElement = conversionApi.mapper.toViewElement( data.item );
+				const img = imageUtils.findViewImgElement( viewElement );
+
+				if ( data.attributeNewValue !== null ) {
+					viewWriter.setAttribute( 'width', data.attributeNewValue, img );
+				} else {
+					viewWriter.removeAttribute( 'width', img );
+				}
+			} );
+
+			dispatcher.on( `attribute:heightAttribute:${ imageType }`, ( evt, data, conversionApi ) => {
+				if ( !conversionApi.consumable.consume( data.item, evt.name ) ) {
+					return;
+				}
+
+				const viewWriter = conversionApi.writer;
+				const viewElement = conversionApi.mapper.toViewElement( data.item );
+				const img = imageUtils.findViewImgElement( viewElement );
+
+				if ( data.attributeNewValue !== null ) {
+					viewWriter.setAttribute( 'height', data.attributeNewValue, img );
+				} else {
+					viewWriter.removeAttribute( 'height', img );
+				}
+			} );
+		} );
+	}
+}
