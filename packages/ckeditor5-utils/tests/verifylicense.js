de9c1157a3c10f519b8b461c670d6f3a6ee12e26
Piotr Szczęśniak <szczesniakp@o2.pl>
2023-05-23 18:04:17 +0200

    Rollback token parameter in test.

--- packages/ckeditor5-utils/tests/verifylicense.js
+++ packages/ckeditor5-utils/tests/verifylicense.js
@@ -19,7 +19,7 @@ describe( 'utils', () => {
 			it( 'when old token format is given', () => {
 				// old
 				// eslint-disable-next-line max-len
-				const string = 'K1RHbDJhb0RoYXpRQTI0alZOaUowZE5NTldHM05rc3ovYnRkTVZOR3VnZDBhLzVOa3Z1SUd2N2JEY2JVWk13YkNzUjBleC9tSlJiYSswNHNYTEdHTkRiV0xwR0h0ZkhZU095eEZDWHpZd3ZkQ2huaTA0UmxHQkZEM00wUFVzcXk=';
+				const string = 'YWZvb2JkcnphYXJhZWJvb290em9wbWJvbHJ1c21sZnJlYmFzdG1paXN1cm1tZmllenJhb2F0YmFvcmxvb3B6aWJvYWRiZWZzYXJ0bW9ibG8=';
 
 				expect( verifyLicense( string ) ).to.be.equal( 'VALID' );
 			} );
