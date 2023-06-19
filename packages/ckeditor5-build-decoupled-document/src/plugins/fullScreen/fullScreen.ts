304f93db5d8b36c324c543698be33a4dde097e33
R Govaert <govaertreeven@gmail.com>
2023-06-19 15:09:13 +0200

    possibly fix future issues, by making the plugins no rely on the attribute.onOpen for a certain plugin

--- packages/ckeditor5-build-decoupled-document/src/plugins/fullScreen/fullScreen.ts
+++ packages/ckeditor5-build-decoupled-document/src/plugins/fullScreen/fullScreen.ts
@@ -4,34 +4,35 @@ import maximizeIcon from './maximize.svg';
 import minimizeIcon from './minimize.svg';
 
 export default class FullScreen extends Plugin {
-	init() {
+	public init(): void {
 		const editor = this.editor;
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		// @ts-ignore
-		const open = editor.config._config.fullScreen.onOpen;
+		const open = editor.config._config.fullScreen?.onOpen;
 		// The button must be registered among the UI components of the editor
 		// to be displayed in the toolbar.
-		editor.ui.componentFactory.add('fullScreen', () => {
+		editor.ui.componentFactory.add( 'fullScreen', () => {
 			// The button will be an instance of ButtonView.
 			const button = new ButtonView();
 			let isFullScreen = false;
-			button.set({
+			button.set( {
 				label: 'FullScreen',
 				withText: false,
-				icon: maximizeIcon,
-			});
+				icon: maximizeIcon
+			} );
 
-			button.on('execute', () => {
+			button.on( 'execute', () => {
 				open();
 				isFullScreen = !isFullScreen;
 
-				button.set({
+				button.set( {
 					label: 'FullScreen',
 					withText: false,
-					icon: isFullScreen ? minimizeIcon : maximizeIcon,
-				});
-			});
+					icon: isFullScreen ? minimizeIcon : maximizeIcon
+				} );
+			} );
 
 			return button;
-		});
+		} );
 	}
 }
