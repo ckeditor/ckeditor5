01ea41e10e296ba2c64afd896f1168ba4e7e2e8c
Filip Sobol <filipsobol@users.noreply.github.com>
2023-06-22 17:14:15 +0200

    Fix (html-support): Fix inifite loop when adding a new elements to the nested document list

--- packages/ckeditor5-html-support/tests/manual/documentlist.js
+++ packages/ckeditor5-html-support/tests/manual/documentlist.js
@@ -34,7 +34,6 @@ ClassicEditor
 			'sourceEditing', '|',
 			'numberedList', 'bulletedList', '|',
 			'outdent', 'indent', '|',
-			'outdent', 'indent', '|',
 			'bold', 'italic', 'strikethrough'
 		],
 		list: {
