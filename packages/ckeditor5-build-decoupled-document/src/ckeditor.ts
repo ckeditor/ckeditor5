c9f6be1aa85d7968d2e44a59d2ba5e198c8ac5c7
R Govaert <govaertreeven@gmail.com>
2023-07-04 11:50:38 +0200

    add video and audio definitions

--- packages/ckeditor5-build-decoupled-document/src/ckeditor.ts
+++ packages/ckeditor5-build-decoupled-document/src/ckeditor.ts
@@ -14,7 +14,7 @@ import {
 	FontSize,
 	FontFamily,
 	FontColor,
-	FontBackgroundColor,
+	FontBackgroundColor
 } from '@ckeditor/ckeditor5-font';
 import { UploadAdapter } from '@ckeditor/ckeditor5-adapter-ckfinder';
 import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
@@ -24,7 +24,7 @@ import {
 	Strikethrough,
 	Underline,
 	Superscript,
-	Subscript,
+	Subscript
 } from '@ckeditor/ckeditor5-basic-styles';
 import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
 import { CKBox } from '@ckeditor/ckeditor5-ckbox';
@@ -41,7 +41,7 @@ import {
 	ImageToolbar,
 	ImageUpload,
 	PictureEditing,
-	AutoImage,
+	AutoImage
 } from '@ckeditor/ckeditor5-image';
 import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
 import { Link } from '@ckeditor/ckeditor5-link';
@@ -53,15 +53,15 @@ import {
 	Table,
 	TableToolbar,
 	TableProperties,
-	TableCellProperties,
+	TableCellProperties
 } from '@ckeditor/ckeditor5-table';
 import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
 import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
 import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
 
-import ClickObserver from '../../ckeditor5-engine/src/view/observer/clickobserver';
-import { GeneralHtmlSupport } from '../../ckeditor5-html-support/src/index';
+import ClickObserver from '@ckeditor/ckeditor5-engine/src/view/observer/clickobserver';
+import { GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support/src/index';
 // @ts-ignore
 import { Iframe } from '@ftrprf/ckeditor5-iframe/src/index';
 // @ts-ignore
@@ -77,6 +77,22 @@ import { StyledLink } from './plugins/styledLink/index';
 // @ts-ignore
 import { FullScreen } from './plugins/fullScreen/index';
 import { Source } from './plugins/source/index';
+// @ts-ignore
+import Video from '@ftrprf/ckeditor5-video/src/video';
+// @ts-ignore
+import VideoToolbar from '@ftrprf/ckeditor5-video/src/videotoolbar';
+// @ts-ignore
+import VideoStyle from '@ftrprf/ckeditor5-video/src/videostyle';
+// @ts-ignore
+import VideoInsert from '@ftrprf/ckeditor5-video/src/videoinsert';
+// @ts-ignore
+import Audio from '@ftrprf/ckeditor5-audio/src/audio';
+// @ts-ignore
+import AudioToolbar from '@ftrprf/ckeditor5-audio/src/audiotoolbar';
+// @ts-ignore
+import AudioStyle from '@ftrprf/ckeditor5-audio/src/audiostyle';
+// @ts-ignore
+import AudioInsert from '@ftrprf/ckeditor5-audio/src/audioinsert';
 
 export default class DecoupledEditor extends DecoupledEditorBase {
 	public static override builtinPlugins = [
@@ -135,6 +151,14 @@ export default class DecoupledEditor extends DecoupledEditorBase {
 		StyledLink,
 		FullScreen,
 		Source,
+		VideoToolbar,
+		Video,
+		VideoStyle,
+		VideoInsert,
+		Audio,
+		AudioInsert,
+		AudioStyle,
+		AudioToolbar
 	];
 
 	public static override defaultConfig = {
@@ -183,7 +207,11 @@ export default class DecoupledEditor extends DecoupledEditorBase {
 				'styledLink',
 				'fullScreen',
 				'source',
-			],
+				'|',
+				'videoInsert',
+				'audio',
+				'audioInsert'
+			]
 		},
 		image: {
 			resizeUnit: 'px' as const,
@@ -193,21 +221,55 @@ export default class DecoupledEditor extends DecoupledEditorBase {
 				'imageStyle:breakText',
 				'|',
 				'toggleImageCaption',
-				'imageTextAlternative',
-			],
+				'imageTextAlternative'
+			]
 		},
 		table: {
-			contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
+			contentToolbar: [
+				'tableColumn',
+				'tableRow',
+				'mergeTableCells'
+			]
 		},
 		list: {
 			properties: {
 				styles: true,
 				startIndex: true,
-				reversed: true,
-			},
+				reversed: true
+			}
+		},
+		audio: {
+			styles: [
+				'alignLeft',
+				'alignCenter',
+				'alignRight'
+			],
+
+			// You need to configure the audio toolbar, too, so it shows the new style
+			// buttons as well as the resize buttons.
+			toolbar: [
+				'audioStyle:alignLeft',
+				'audioStyle:alignCenter',
+				'audioStyle:alignRight'
+			]
+		},
+		video: {
+			styles: [
+				'alignLeft',
+				'alignCenter',
+				'alignRight'
+			],
+
+			// You need to configure the video toolbar, too, so it shows the new style
+			// buttons as well as the resize buttons.
+			toolbar: [
+				'videoStyle:alignLeft',
+				'videoStyle:alignCenter',
+				'videoStyle:alignRight'
+			]
 		},
 
 		// This value must be kept in sync with the language defined in webpack.config.js.
-		language: 'en',
+		language: 'en'
 	};
 }
