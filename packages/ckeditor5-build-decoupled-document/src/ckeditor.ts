ee4398f68746eeac0cc44aec8a391e213b11088b
Jannick Vandaele <info@jannickvandaele.be>
2023-07-05 13:27:08 +0200

    remove audio video plugin

--- packages/ckeditor5-build-decoupled-document/src/ckeditor.ts
+++ packages/ckeditor5-build-decoupled-document/src/ckeditor.ts
@@ -61,7 +61,8 @@ import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
 import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
 
 import ClickObserver from '@ckeditor/ckeditor5-engine/src/view/observer/clickobserver';
-import { GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support/src/index';
+import { GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support';
+
 // @ts-ignore
 import { Iframe } from '@ftrprf/ckeditor5-iframe/src/index';
 // @ts-ignore
@@ -77,22 +78,6 @@ import { StyledLink } from './plugins/styledLink/index';
 // @ts-ignore
 import { FullScreen } from './plugins/fullScreen/index';
 import { Source } from './plugins/source/index';
-// @ts-ignore
-import Video from '@ftrprf/ckeditor5-video/src/video';
-// @ts-ignore
-import VideoToolbar from '@ftrprf/ckeditor5-video/src/videotoolbar';
-// @ts-ignore
-import VideoStyle from '@ftrprf/ckeditor5-video/src/videostyle';
-// @ts-ignore
-import VideoInsert from '@ftrprf/ckeditor5-video/src/videoinsert';
-// @ts-ignore
-import Audio from '@ftrprf/ckeditor5-audio/src/audio';
-// @ts-ignore
-import AudioToolbar from '@ftrprf/ckeditor5-audio/src/audiotoolbar';
-// @ts-ignore
-import AudioStyle from '@ftrprf/ckeditor5-audio/src/audiostyle';
-// @ts-ignore
-import AudioInsert from '@ftrprf/ckeditor5-audio/src/audioinsert';
 
 export default class DecoupledEditor extends DecoupledEditorBase {
 	public static override builtinPlugins = [
@@ -150,15 +135,7 @@ export default class DecoupledEditor extends DecoupledEditorBase {
 		Modal,
 		StyledLink,
 		FullScreen,
-		Source,
-		VideoToolbar,
-		Video,
-		VideoStyle,
-		VideoInsert,
-		Audio,
-		AudioInsert,
-		AudioStyle,
-		AudioToolbar
+		Source
 	];
 
 	public static override defaultConfig = {
@@ -206,10 +183,7 @@ export default class DecoupledEditor extends DecoupledEditorBase {
 				'styledLink',
 				'fullScreen',
 				'source',
-				'|',
-				'videoInsert',
-				'audio',
-				'audioInsert'
+				'|'
 			]
 		},
 		image: {
