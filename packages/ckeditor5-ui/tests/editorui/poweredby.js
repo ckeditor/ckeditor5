b7a020123cd651e3975491e04baf2f0a3b0b1988
Piotr Szczęśniak <szczesniakp@o2.pl>
2023-07-19 16:05:10 +0200

    Tests refactor.

--- packages/ckeditor5-ui/tests/editorui/poweredby.js
+++ packages/ckeditor5-ui/tests/editorui/poweredby.js
@@ -489,8 +489,6 @@ describe( 'PoweredBy', () => {
 			parentWithOverflow.style.overflow = 'scroll';
 			// Is not enough height to be visible vertically.
 			parentWithOverflow.style.height = '99px';
-			// Enough width to be visible horizontally.
-			parentWithOverflow.style.width = '392px';
 
 			document.body.appendChild( parentWithOverflow );
 			parentWithOverflow.appendChild( domRoot );
@@ -506,11 +504,10 @@ describe( 'PoweredBy', () => {
 		it( 'should not show the balloon if the root is not visible horizontally', async () => {
 			const domRoot = editor.editing.view.getDomRoot();
 			const parentWithOverflow = document.createElement( 'div' );
+
 			parentWithOverflow.style.overflow = 'scroll';
-			// Enough height to be visible vertically.
-			parentWithOverflow.style.height = '100px';
 			// Is not enough width to be visible horizontally.
-			parentWithOverflow.style.width = '391px';
+			parentWithOverflow.style.width = '390px';
 
 			document.body.appendChild( parentWithOverflow );
 			parentWithOverflow.appendChild( domRoot );
