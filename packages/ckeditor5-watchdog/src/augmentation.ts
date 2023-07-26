6a60826c18908f83c059f978f0ff74ba1e0096ea
Szymon Cofalik <s.cofalik@cksource.com>
2023-07-26 18:17:54 +0200

    Remove unnecessary config declarations.

--- packages/ckeditor5-watchdog/src/augmentation.ts
+++ packages/ckeditor5-watchdog/src/augmentation.ts
@@ -4,22 +4,10 @@
  */
 
 // eslint-disable-next-line ckeditor5-rules/no-cross-package-imports
-import type { RootAttributes } from '@ckeditor/ckeditor5-editor-multi-root';
-
 import type { EditorData } from './editorwatchdog';
 
 declare module '@ckeditor/ckeditor5-core' {
 	interface EditorConfig {
-
-		/**
-		 * Initial roots attributes for the document roots.
-		 */
-		rootsAttributes?: Record<string, RootAttributes>;
-
-		/**
-		 * List of names of all the roots that exist in the document but are not initially loaded by the editor.
-		 */
-		lazyRoots?: Array<string>;
 
 		/**
 		 * The temporary property that is used for passing data to the plugin which restores the editor state.
