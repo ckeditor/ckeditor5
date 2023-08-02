3b647b9bdeba01816282ce2dda9aa4202f3553c4
Marta Motyczynska <m.motyczynska@cksource.com>
2023-08-02 13:00:02 +0200

    Use local images in imagesizeattributesallcases manual test.

--- packages/ckeditor5-image/tests/manual/imagesizeattributesallcases.js
+++ packages/ckeditor5-image/tests/manual/imagesizeattributesallcases.js
@@ -77,18 +77,18 @@ const editors = [
 		id: 'inline1',
 		title: '[Inline] plain (no attributes, no styles)',
 		config: commonConfig,
-		data: '<p><img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt=""></p>'
+		data: '<p><img src="game_boy.jpg" alt=""></p>'
 	},
 	{
 		id: 'inline2',
 		title: '[Inline] natural size | width + height attributes: Resize in %',
 		config: commonConfig,
-		data: '<p><img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt="" width="384" height="500"></p>'
+		data: '<p><img src="game_boy.jpg" alt="" width="384" height="500"></p>'
 	},
 	{
 		id: 'inline3',
 		config: commonConfig,
-		data: '<p><img style="width:22.69%;" src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" ' +
+		data: '<p><img style="width:22.69%;" src="game_boy.jpg" ' +
 			'alt="" width="384" height="500"></p>',
 		title: '[Inline] natural size | width + height attributes: Resized (width % style)'
 	},
@@ -96,34 +96,34 @@ const editors = [
 		id: 'inline4',
 		config: commonConfig,
 		data: '<p><img class="image_resized" style="width:22.69%;" ' +
-			'src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt="" width="384" height="500"></p>',
+			'src="game_boy.jpg" alt="" width="384" height="500"></p>',
 		title: '[Inline] natural size | width + height attributes: Resized (width % style)'
 	},
 	{
 		id: 'inline5',
 		title: '[Inline] natural size | width + height attributes: Resize in px',
 		config: configPx,
-		data: '<p><img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt="" width="384" height="500"></p>'
+		data: '<p><img src="game_boy.jpg" alt="" width="384" height="500"></p>'
 	},
 	{
 		id: 'inline6',
 		title: '[Inline] natural size | width + height attributes: Resized (width px style only)',
 		config: configPx,
 		data: '<p><img class="image_resized" style="width:307px;" ' +
-			'src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt="" width="384" height="500"></p>'
+			'src="game_boy.jpg" alt="" width="384" height="500"></p>'
 	},
 	{
 		id: 'inline7',
 		title: '[Inline] natural size | width + height attributes: Resized (width and height px style)',
 		config: configPx,
-		data: '<p><img style="height:400px;width:307px;" src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" ' +
+		data: '<p><img style="height:400px;width:307px;" src="game_boy.jpg" ' +
 			'alt="" width="384" height="500"></p>'
 	},
 	{
 		id: 'inline8',
 		title: '[Inline] natural size | styles only (w/o width & height attributes): Resize in %',
 		config: commonConfig,
-		data: '<p><img style="width:384px;height:500px;" src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" ' +
+		data: '<p><img style="width:384px;height:500px;" src="game_boy.jpg" ' +
 			'alt=""></p>'
 	},
 	{
@@ -131,39 +131,39 @@ const editors = [
 		title: '[Inline] natural size | only resize in % (only width style)',
 		config: commonConfig,
 		data: '<p><img class="image_resized" style="width:12.34%;" ' +
-			'src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt=""></p>'
+			'src="game_boy.jpg" alt=""></p>'
 	},
 	{
 		id: 'inline10',
 		title: '[Inline] natural size | styles only (w/o width & height attributes): Resize in px',
 		config: configPx,
 		data: '<p><img style="width:384px;height:500px;" ' +
-			'src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt=""></p>'
+			'src="game_boy.jpg" alt=""></p>'
 	},
 	{
 		id: 'inline11',
 		title: '[Inline] broken aspect ratio | width + height attributes',
 		config: commonConfig,
-		data: '<p><img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt="" width="500" height="500"></p>'
+		data: '<p><img src="game_boy.jpg" alt="" width="500" height="500"></p>'
 	},
 	{
 		id: 'inline12',
 		title: '[Inline] broken aspect ratio | styles only (w/o width & height attributes)',
 		config: commonConfig,
-		data: '<p><img style="width:500px;height:500px;" src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" ' +
+		data: '<p><img style="width:500px;height:500px;" src="game_boy.jpg" ' +
 			'alt=""></p>'
 	},
 	{
 		id: 'block1',
 		title: '[Block] plain (no attributes, no styles)',
 		config: commonConfig,
-		data: '<figure class="image"><img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt=""></figure>'
+		data: '<figure class="image"><img src="game_boy.jpg" alt=""></figure>'
 	},
 	{
 		id: 'block2',
 		title: '[Block] natural size | width + height attributes: Resize in %',
 		config: commonConfig,
-		data: '<figure class="image"><img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" width="384" ' +
+		data: '<figure class="image"><img src="game_boy.jpg" width="384" ' +
 			'height="500" alt=""></figure>'
 	},
 	{
@@ -171,20 +171,20 @@ const editors = [
 		title: '[Block] natural size | width + height attributes: Resized (width % style)',
 		config: commonConfig,
 		data: '<figure class="image" style="width:10.09%;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt="" width="384" height="500"></figure>'
+			'<img src="game_boy.jpg" alt="" width="384" height="500"></figure>'
 	},
 	{
 		id: 'block4',
 		title: '[Block] natural size | width + height attributes: Resized (width % style)',
 		config: commonConfig,
 		data: '<figure class="image image_resized" style="width:10.09%;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt="" width="384" height="500"></figure>'
+			'<img src="game_boy.jpg" alt="" width="384" height="500"></figure>'
 	},
 	{
 		id: 'block5',
 		title: '[Block] natural size | width + height attributes: Resize in px',
 		config: configPx,
-		data: '<figure class="image"><img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" ' +
+		data: '<figure class="image"><img src="game_boy.jpg" ' +
 			'width="384" height="500" alt=""></figure>'
 	},
 	{
@@ -192,88 +192,88 @@ const editors = [
 		title: '[Block] natural size | width + height attributes: Resized (width px style only)',
 		config: configPx,
 		data: '<figure class="image image_resized" style="width:115px;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt="" width="384" height="500"></figure>'
+			'<img src="game_boy.jpg" alt="" width="384" height="500"></figure>'
 	},
 	{
 		id: 'block7',
 		title: '[Block] natural size | width + height attributes: Resized (width and height px style)',
 		config: configPx,
 		data: '<figure class="image image_resized" style="height:150px;width:115px;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt="" width="384" height="500"></figure>'
+			'<img src="game_boy.jpg" alt="" width="384" height="500"></figure>'
 	},
 	{
 		id: 'block8',
 		title: '[Block] natural size | styles only (w/o width & height attributes): Resize in %',
 		config: commonConfig,
 		data: '<figure class="image" style="width:384px;height:500px;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt=""></figure>'
+			'<img src="game_boy.jpg" alt=""></figure>'
 	},
 	{
 		id: 'block9',
 		title: '[Block] natural size | only resize in % (only width style)',
 		config: commonConfig,
 		data: '<figure class="image image_resized" style="width:19.21%;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt=""></figure>'
+			'<img src="game_boy.jpg" alt=""></figure>'
 	},
 	{
 		id: 'block10',
 		title: '[Block] natural size | styles only (w/o width & height attributes): Resize in px',
 		config: configPx,
 		data: '<figure class="image" style="width:384px;height:500px;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt=""></figure>'
+			'<img src="game_boy.jpg" alt=""></figure>'
 	},
 	{
 		id: 'block11',
 		title: '[Block] broken aspect ratio | width + height attributes',
 		config: commonConfig,
 		data: '<figure class="image">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" width="500" height="500" alt=""></figure>'
+			'<img src="game_boy.jpg" width="500" height="500" alt=""></figure>'
 	},
 	{
 		id: 'block12',
 		title: '[Block] broken aspect ratio | styles only (w/o width & height attributes)',
 		config: commonConfig,
 		data: '<figure class="image" style="width:500px;height:500px;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/game_boy.jpg" alt=""></figure>'
+			'<img src="game_boy.jpg" alt=""></figure>'
 	},
 	{
 		id: 'inline101',
 		title: '[Inline] natural size | width + height attributes: Resized (height % style)',
 		config: commonConfig,
-		data: '<p><img style="height:20%;" src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/birds/parrot_2.jpg" ' +
+		data: '<p><img style="height:20%;" src="parrot_2.jpg" ' +
 			'alt="" width="300" height="451"></p>'
 	},
 	{
 		id: 'inline102',
 		title: '[Inline] natural size | width + height attributes: Resized (height px style)',
 		config: configPx,
-		data: '<p><img style="height:200px;" src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/birds/parrot_2.jpg" ' +
+		data: '<p><img style="height:200px;" src="parrot_2.jpg" ' +
 			'alt="" width="300" height="451"></p>'
 	},
 	{
 		id: 'inline103',
 		title: '[Inline] natural size | only resize in % (only height style)',
 		config: commonConfig,
-		data: '<p><img style="height:20%;" src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/birds/parrot_2.jpg" alt=""></p>'
+		data: '<p><img style="height:20%;" src="parrot_2.jpg" alt=""></p>'
 	},
 	{
 		id: 'inline104',
 		title: '[Inline] natural size | only resize in px (only height style)',
 		config: configPx,
-		data: '<p><img style="height:200px;" src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/birds/parrot_2.jpg" alt=""></p>'
+		data: '<p><img style="height:200px;" src="parrot_2.jpg" alt=""></p>'
 	},
 	{
 		id: 'inline105',
 		title: '[Inline] width + height attributes: Resized (height & width % style)',
 		config: commonConfig,
-		data: '<p><img style="width:20%;height:20%;" src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/birds/parrot_2.jpg" ' +
+		data: '<p><img style="width:20%;height:20%;" src="parrot_2.jpg" ' +
 			'alt="" width="300" height="451"></p>'
 	},
 	{
 		id: 'inline106',
 		title: '[Inline] only resize in % (height & width % style)',
 		config: commonConfig,
-		data: '<p><img style="width:20%;height:20%;" src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/birds/parrot_2.jpg" ' +
+		data: '<p><img style="width:20%;height:20%;" src="parrot_2.jpg" ' +
 			'alt=""></p>'
 	},
 	{
@@ -281,42 +281,42 @@ const editors = [
 		title: '[Block] natural size | width + height attributes: Resized (height % style)',
 		config: commonConfig,
 		data: '<figure class="image" style="height:20%;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/birds/parrot_2.jpg" alt="" width="300" height="451"></figure>'
+			'<img src="parrot_2.jpg" alt="" width="300" height="451"></figure>'
 	},
 	{
 		id: 'block102',
 		title: '[Block] natural size | width + height attributes: Resized (height px style)',
 		config: configPx,
 		data: '<figure class="image" style="height:200px;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/birds/parrot_2.jpg" alt="" width="300" height="451"></figure>'
+			'<img src="parrot_2.jpg" alt="" width="300" height="451"></figure>'
 	},
 	{
 		id: 'block103',
 		title: '[Block] natural size | only resize in % (only height style)',
 		config: commonConfig,
 		data: '<figure class="image" style="height:20%;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/birds/parrot_2.jpg" alt=""></figure>'
+			'<img src="parrot_2.jpg" alt=""></figure>'
 	},
 	{
 		id: 'block104',
 		title: '[Block] natural size | only resize in px (only height style)',
 		config: configPx,
 		data: '<figure class="image" style="height:200px;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/birds/parrot_2.jpg" alt=""></figure>'
+			'<img src="parrot_2.jpg" alt=""></figure>'
 	},
 	{
 		id: 'block105',
 		title: '[Block] width + height attributes: Resized (height & width % style)',
 		config: commonConfig,
 		data: '<figure class="image" style="width:20%;height:20%;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/birds/parrot_2.jpg" alt="" width="300" height="451"></figure>'
+			'<img src="parrot_2.jpg" alt="" width="300" height="451"></figure>'
 	},
 	{
 		id: 'block106',
 		title: '[Block] only resize in % (height & width % style)',
 		config: commonConfig,
 		data: '<figure class="image" style="width:20%;height:20%;">' +
-			'<img src="https://ckeditor.com/docs/ckeditor5/latest/assets/img/birds/parrot_2.jpg" alt=""></figure>'
+			'<img src="parrot_2.jpg" alt=""></figure>'
 	},
 	{
 		id: 'inline201',
