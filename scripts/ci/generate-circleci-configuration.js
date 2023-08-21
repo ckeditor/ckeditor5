499a13810b81da25eb97b4f09cd91acda6c093da
Kamil Piechaczek <pomek@users.noreply.github.com>
2023-08-21 08:17:16 +0200

    CircleCI: Initial configuration.

---
+++ scripts/ci/generate-circleci-configuration.js
@@ -0,0 +1,236 @@
+#!/usr/bin/env node
+
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* eslint-env node */
+
+// The script assumes that it is executed from the CKEditor 5 Commercial directory and aims to load
+// the template file (`.circleci/template.yml`) and store it under the `.circleci/config-tests.yml` path,
+// a source for a new workflow triggered from the main thread when a new build starts.
+//
+// See: https://circleci.com/docs/using-dynamic-configuration/.
+
+'use strict';
+
+const upath = require( 'upath' );
+const fs = require( 'fs/promises' );
+const { glob } = require( 'glob' );
+const yaml = require( 'js-yaml' );
+
+const CKEDITOR5_ROOT_DIRECTORY = upath.join( __dirname, '..', '..' );
+const CIRCLECI_CONFIGURATION_DIRECTORY = upath.join( CKEDITOR5_ROOT_DIRECTORY, '.circleci' );
+
+const NON_FULL_COVERAGE_PACKAGES = [
+	'ckeditor5-minimap'
+];
+
+// Constants as the functions due to `&ref` symbols in the generated output.
+
+const bootstrapCommands = () => ( [
+	'checkout_command',
+	'bootstrap_repositories_command',
+	'prepare_environment_command'
+] );
+
+const prepareCodeCoverageDirectories = () => ( {
+	run: {
+		when: 'always',
+		name: 'Prepare the code coverage directory',
+		command: 'mkdir .nyc_output .out'
+	}
+} );
+
+const persistToWorkspace = () => ( {
+	unless: {
+		condition: '<< pipeline.parameters.isNightly >>',
+		steps: {
+			persist_to_workspace: {
+				root: '.out',
+				paths: [
+					'combined_features.info'
+				]
+			}
+		}
+	}
+} );
+
+( async () => {
+	let frameworkPackages = ( await fs.readdir( upath.join( CKEDITOR5_ROOT_DIRECTORY, 'src' ) ) )
+		.map( filename => 'ckeditor5-' + filename.replace( /\.(js|ts)$/, '' ) );
+
+	let featurePackages = ( await glob( '*/', { cwd: upath.join( CKEDITOR5_ROOT_DIRECTORY, 'packages' ) } ) )
+		.filter( packageName => !frameworkPackages.includes( packageName ) );
+
+	featurePackages.sort();
+
+	featurePackages = featurePackages.slice( 0, 1 );
+	frameworkPackages = frameworkPackages.slice( 0, 1 );
+
+	/**
+	 * @type CircleCIConfiguration
+	 */
+	const config = yaml.load(
+		await fs.readFile( upath.join( CIRCLECI_CONFIGURATION_DIRECTORY, 'template.yml' ) )
+	);
+
+	config.jobs.cke5_tests_framework = {
+		machine: true,
+		steps: [
+			...bootstrapCommands(),
+			prepareCodeCoverageDirectories(),
+			...generateTestSteps( frameworkPackages, {
+				checkCoverage: true,
+				coverageFile: '.out/combined_framework.info'
+			} ),
+			persistToWorkspace()
+		]
+	};
+
+	config.jobs.cke5_tests_features = {
+		machine: true,
+		steps: [
+			...bootstrapCommands(),
+			prepareCodeCoverageDirectories(),
+			...generateTestSteps( featurePackages, {
+				checkCoverage: true,
+				coverageFile: '.out/combined_features.info'
+			} ),
+			persistToWorkspace()
+		]
+	};
+
+	Object.keys( config.jobs )
+		.filter( jobName => {
+			if ( jobName === 'release_prepare' ) {
+				return true;
+			}
+
+			if ( jobName.includes( 'tests' ) || jobName.includes( 'coverage' ) || jobName.includes( 'manual' ) ) {
+				return true;
+			}
+
+			return false;
+		} )
+		.forEach( jobName => {
+			const job = config.jobs[ jobName ];
+			job.environment = job.environment || {};
+
+			const { steps, environment } = job;
+			const stopCommand = [
+				// eslint-disable-next-line max-len,quotes
+				`[ $( node -p "require('./scripts/ci/should-run-short-flow.js')( './' ).toString()" ) == "true" ]`,
+				'circleci-agent step halt'
+			].join( ' && ' );
+
+			steps.splice( 3, 0, {
+				run: {
+					name: '⭐ Short flow breakpoint - Check if the build should continue',
+					// This command should not impact on the error code.
+					command: `${ stopCommand } || echo "" > /dev/null`
+				}
+			} );
+
+			environment.CKE5_IS_NIGHTLY_BUILD = '<< pipeline.parameters.isNightly >>';
+		} );
+
+	await fs.writeFile(
+		upath.join( CIRCLECI_CONFIGURATION_DIRECTORY, 'config-tests.yml' ),
+		yaml.dump( config, { lineWidth: -1 } )
+	);
+} )();
+
+/**
+ * @param {Array.<String>}packages
+ * @param {Object} options
+ * @param {Boolean} options.checkCoverage
+ * @param {String|null} [options.coverageFile=null]
+ * @returns {Array.<CircleCITask>}
+ */
+function generateTestSteps( packages, { checkCoverage, coverageFile = null } ) {
+	return packages.map( packageName => {
+		const allowNonFullCoverage = NON_FULL_COVERAGE_PACKAGES.includes( packageName );
+
+		const testCommand = [
+			'node',
+			'scripts/ci/check-unit-tests-for-package.js',
+			'--package-name',
+			packageName,
+			checkCoverage ? '--check-coverage' : null,
+			allowNonFullCoverage ? '--allow-non-full-coverage' : null,
+			coverageFile ? `--coverage-file ${ coverageFile }` : null
+		].filter( Boolean ).join( ' ' );
+
+		return {
+			run: {
+				// When a previous package failed, we still want to check the entire repository.
+				when: 'always',
+				name: `Execute tests for "${ packageName }"`,
+				command: testCommand
+			}
+		};
+	} );
+}
+
+/**
+ * This type partially covers supported options on CircleCI.
+ * To see the complete guide, follow: https://circleci.com/docs/configuration-reference.
+ *
+ * @typedef {Object} CircleCIConfiguration
+ *
+ * @property {String} version
+ *
+ * @property {Object.<String, CircleCIParameter>} parameters
+ *
+ * @property {Object.<String, CircleCIJob>} jobs
+ *
+ * @property {Object.<String, CircleCICommand>} command
+ *
+ * @property {Object} workflows
+ *
+ * @property {Boolean} [setup]
+ */
+
+/**
+ * @typedef {Object} CircleCIParameter
+ *
+ * @property {'string'|'boolean'|'integer'|'enum'} type
+ *
+ * @property {String|Number|Boolean} default
+ */
+
+/**
+ * @typedef {Object} CircleCIJob
+ *
+ * @property {Boolean} machine
+ *
+ * @property {Array.<String|CircleCITask>} steps
+ *
+ * @property {Object.<String, CircleCIParameter>} [parameters]
+ */
+
+/**
+ * @typedef {Object} CircleCICommand
+ *
+ * @property {String} description
+ *
+ * @property {Array.<String|CircleCITask>} steps
+ *
+ * @property {Object.<String, CircleCIParameter>} [parameters]
+ */
+
+/**
+ * @typedef {Object} CircleCITask
+ *
+ * @property {Object} run
+ *
+ * @property {String} run.name
+ *
+ * @property {String} run.command
+ *
+ * @property {String} [run.when]
+ *
+ * @property {String} [run.working_directory]
+ */
