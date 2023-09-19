8814f62b14a9ff8032e92dd76b7dbf537eb1329d
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2023-09-19 09:49:13 +0200

    TextareaView should fire an event upon auto-growing.

--- packages/ckeditor5-ui/src/index.ts
+++ packages/ckeditor5-ui/src/index.ts
@@ -56,7 +56,7 @@ export { default as InputView } from './input/inputview';
 export { default as InputTextView } from './inputtext/inputtextview';
 export { default as InputNumberView } from './inputnumber/inputnumberview';
 
-export { default as TextareaView } from './textarea/textareaview';
+export { default as TextareaView, type TextareaViewAutoGrowEvent } from './textarea/textareaview';
 
 export { default as IframeView } from './iframe/iframeview';
 
