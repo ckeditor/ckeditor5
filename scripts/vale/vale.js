02a1d2d0db15bdb818f2d67e3fdff55d0669269c
Przemyslaw Zan <p.zan@cksource.com>
2023-10-23 16:42:59 +0200

    Added option to save vale data.

--- scripts/vale/vale.js
+++ scripts/vale/vale.js
@@ -7,6 +7,7 @@
 
 'use strict';
 
+const fs = require( 'fs-extra' );
 const chalk = require( 'chalk' );
 const upath = require( 'upath' );
 const minimist = require( 'minimist' );
@@ -15,16 +16,39 @@ const { spawn } = require( 'child_process' );
 
 const ARGS_CHAR_LIMIT = 8000;
 const CKEDITOR5_ROOT = upath.join( __dirname, '..', '..' );
+const RESULTS_DIR = upath.join( CKEDITOR5_ROOT, 'scripts', 'vale', 'results' );
+const VALE_CONFIG_PATH = upath.join( CKEDITOR5_ROOT, '.vale.ini' );
+const READABILITY_FILES_GLOB = 'scripts/vale/styles/Readability/*.yml';
+
+const originalFileContents = {};
+
+const defaultPatterns = [
+	// "README.md", "CHANGELOG.md" etc.
+	'*.md',
+	'packages/*/*.md',
+	'external/ckeditor5-commercial/packages/*/*.md',
+
+	// All "docs" directories.
+	'docs',
+	'packages/*/docs',
+	'external/ckeditor5-commercial/docs',
+	'external/ckeditor5-commercial/packages/*/docs'
+];
 
 const minimistOptions = {
 	string: [
 		'directory'
 	],
+	boolean: [
+		'save'
+	],
 	alias: {
-		d: 'directory'
+		d: 'directory',
+		s: 'save'
 	},
 	default: {
-		directory: ''
+		directory: '',
+		save: false
 	}
 };
 
@@ -41,23 +65,11 @@ const globOptions = {
 	]
 };
 
-const defaultPatterns = [
-	// "README.md", "CHANGELOG.md" etc.
-	'*.md',
-	'packages/*/*.md',
-	'external/ckeditor5-commercial/packages/*/*.md',
-
-	// All "docs" directories.
-	'docs',
-	'packages/*/docs',
-	'external/ckeditor5-commercial/docs',
-	'external/ckeditor5-commercial/packages/*/docs'
-];
+const args = minimist( process.argv.slice( 2 ), minimistOptions );
 
 main();
 
 async function main() {
-	const args = minimist( process.argv.slice( 2 ), minimistOptions );
 	const patterns = args.directory ? [ `**/${ args.directory }/**/*.md` ] : args._;
 	const files = [];
 
@@ -75,6 +87,87 @@ async function main() {
 		files.push( ...globSync( defaultPatterns, globOptions ) );
 	}
 
+	const chunks = splitFilesIntoChunks( files );
+
+	if ( args.save ) {
+		return executeAndSave( chunks ).catch( err => {
+			console.log( chalk.red( '\nScript threw an error:' ) );
+			console.log( err );
+
+			console.log( chalk.blue( '\nRestoring config files...\n' ) );
+
+			restoreConfigFiles();
+		} );
+	}
+
+	return executeAndLog( chunks );
+}
+
+async function executeAndSave( chunks ) {
+	console.log( chalk.blue( '\nPreparing config files...' ) );
+
+	prepareConfigFiles();
+
+	console.log( chalk.blue( '\nExecuting vale...\n' ) );
+
+	const collectedValeData = {};
+
+	for ( let i = 0; i < chunks.length; i++ ) {
+		console.log( chalk.blue( `Processing chunk ${ i + 1 }/${ chunks.length }...` ) );
+
+		const valeData = await runVale( chunks[ i ] );
+
+		for ( const key in valeData ) {
+			const filePath = upath.toUnix( key );
+
+			if ( !collectedValeData[ filePath ] ) {
+				collectedValeData[ filePath ] = {
+					readability: {},
+					warnings: 0,
+					errors: 0
+				};
+			}
+
+			for ( const note of valeData[ key ] ) {
+				if ( note.Severity === 'error' ) {
+					collectedValeData[ filePath ].errors++;
+
+					continue;
+				}
+
+				if ( note.Severity !== 'warning' ) {
+					continue;
+				}
+
+				const match = note.Check.match( /(?<=Readability\.).+/ );
+
+				if ( !match ) {
+					collectedValeData[ filePath ].warnings++;
+
+					continue;
+				}
+
+				const [ readabilityMetric ] = match;
+				const readabilityScore = Number( note.Message );
+				collectedValeData[ filePath ].readability[ readabilityMetric ] = readabilityScore;
+			}
+		}
+	}
+
+	console.log( chalk.blue( '\nRestoring config files...' ) );
+
+	restoreConfigFiles();
+
+	const resultPath = upath.join( RESULTS_DIR, `${ Date.now() }.json` );
+
+	console.log( chalk.blue( '\nResult file saved:' ) );
+	console.log( chalk.underline( resultPath ) + '\n' );
+
+	fs.ensureDirSync( RESULTS_DIR );
+	fs.writeFileSync( resultPath, JSON.stringify( collectedValeData, null, '\t' ), 'utf-8' );
+}
+
+async function executeAndLog( chunks ) {
 	console.log( chalk.blue( '\nExecuting vale...\n' ) );
 
 	const collectedValeData = {
@@ -84,8 +177,6 @@ async function main() {
 		files: 0,
 		text: ''
 	};
-
-	const chunks = splitFilesIntoChunks( files );
 
 	for ( let i = 0; i < chunks.length; i++ ) {
 		console.log( chalk.blue( `Processing chunk ${ i + 1 }/${ chunks.length }...` ) );
@@ -126,10 +217,20 @@ function splitFilesIntoChunks( files ) {
 }
 
 function runVale( files ) {
+	const valeCommandPattern = /^\$ vale .+\n/m;
+	const commandInfoLinePattern = /^info Visit .+\n/m;
 	const valeFooterPattern = /\n?.*?(\d+) errors?.*?(\d+) warnings?.*?(\d+) suggestions?.*?(\d+) files?[\s\S]+/;
 
 	return new Promise( resolve => {
-		const vale = spawn( 'yarn', [ 'run', 'docs:vale', ...files ], spawnOptions );
+		const valeArgs = [ 'run', 'docs:vale' ];
+
+		if ( args.save ) {
+			valeArgs.push( '--output=JSON' );
+		}
+
+		valeArgs.push( ...files );
+
+		const vale = spawn( 'yarn', valeArgs, spawnOptions );
 
 		let text = '';
 
@@ -146,21 +247,58 @@ function runVale( files ) {
 		} );
 
 		vale.on( 'close', () => {
+			if ( args.save ) {
+				const rawJson = text
+					.replace( valeCommandPattern, '' )
+					.replace( commandInfoLinePattern, '' );
+
+				return resolve( JSON.parse( rawJson ) );
+			}
+
 			const match = text.match( valeFooterPattern );
 
 			if ( !match ) {
 				throw new Error( 'Vale output does not match the footer pattern:\n\n' + text );
 			}
 
-			resolve( {
+			return resolve( {
 				errors: Number( match[ 1 ] ),
 				warnings: Number( match[ 2 ] ),
 				suggestions: Number( match[ 3 ] ),
 				files: Number( match[ 4 ] ),
 				text: text
-					.replace( /^\$ vale .+\n/m, '' )
+					.replace( valeCommandPattern, '' )
 					.replace( valeFooterPattern, '' )
 			} );
 		} );
 	} );
+}
+
+function prepareConfigFiles() {
+	originalFileContents[ VALE_CONFIG_PATH ] = fs.readFileSync( VALE_CONFIG_PATH, 'utf-8' );
+
+	const newValeConfigContent = originalFileContents[ VALE_CONFIG_PATH ]
+		.replace( /(?<=\nMinAlertLevel = )[a-z]+(?=\n)/, 'warning' );
+
+	fs.writeFileSync( VALE_CONFIG_PATH, newValeConfigContent, 'utf-8' );
+
+	globSync( READABILITY_FILES_GLOB, globOptions )
+		.map( upath.toUnix )
+		.forEach( readabilityFilePath => {
+			originalFileContents[ readabilityFilePath ] = fs.readFileSync( readabilityFilePath, 'utf-8' );
+
+			const newReadabilityFileContent = originalFileContents[ readabilityFilePath ]
+				.replace( /(?<=\nmessage: ").+(?="\n)/, '%s' )
+				.replace( /(?<=\ncondition: ").+(?="\n)/, '> -999999' );
+
+			fs.writeFileSync( readabilityFilePath, newReadabilityFileContent, 'utf-8' );
+		} );
+}
+
+function restoreConfigFiles() {
+	for ( const filePath in originalFileContents ) {
+		const originalFileContent = originalFileContents[ filePath ];
+
+		fs.writeFileSync( filePath, originalFileContent, 'utf-8' );
+	}
 }
