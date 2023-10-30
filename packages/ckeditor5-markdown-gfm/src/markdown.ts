80464f75ecb613ffcf7822af07d979d9519c8106
Marcin Panek <m.panek@cksource.com>
2023-10-30 10:42:03 +0100

    Cleaned up tests for a previous version.

--- packages/ckeditor5-markdown-gfm/src/markdown.ts
+++ packages/ckeditor5-markdown-gfm/src/markdown.ts
@@ -42,7 +42,6 @@ export default class Markdown extends Plugin {
 		const view = editor.editing.view;
 		const viewDocument = view.document;
 
-		// TODO think about adding the paste logic only if ClipboardPipeline pluggin is added or make it required
 		const clipboardPipeline: ClipboardPipeline = editor.plugins.get( 'ClipboardPipeline' );
 
 		let shiftPressed = false;
