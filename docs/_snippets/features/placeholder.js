79f1f13824205ad896479e559fed1ab09c898726
godai78 <b.biedrzycki@cksource.com>
2023-11-06 14:43:43 +0100

    Docs: adding missing image toolbars. [short flow]

--- docs/_snippets/features/placeholder.js
+++ docs/_snippets/features/placeholder.js
@@ -3,9 +3,20 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-/* globals console, window, document, ClassicEditor */
+/* globals window, document, console */
 
+import { Superscript } from '@ckeditor/ckeditor5-basic-styles';
+import { TextPartLanguage } from '@ckeditor/ckeditor5-language';
+import { CKBox, CKBoxImageEdit } from '@ckeditor/ckeditor5-ckbox';
+import { PictureEditing, ImageResize, AutoImage } from '@ckeditor/ckeditor5-image';
+import { LinkImage } from '@ckeditor/ckeditor5-link';
 import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud-services-config';
+
+// Umberto combines all `packages/*/docs` into the `docs/` directory. The import path must be valid after merging all directories.
+import ClassicEditor from '../build-classic';
+
+ClassicEditor.builtinPlugins.push( TextPartLanguage, Superscript, PictureEditing,
+	ImageResize, AutoImage, LinkImage, CKBox, CKBoxImageEdit );
 
 ClassicEditor
 	.create( document.querySelector( '#snippet-placeholder' ), {
@@ -16,6 +27,12 @@ ClassicEditor
 			'|', 'link', 'uploadImage', 'insertTable', 'mediaEmbed',
 			'|', 'bulletedList', 'numberedList', 'outdent', 'indent'
 		],
+		image: {
+			toolbar: [
+				'imageStyle:inline', 'imageStyle:block', 'imageStyle:side', '|',
+				'toggleImageCaption', 'imageTextAlternative', 'ckboxImageEdit'
+			]
+		},
 		ui: {
 			viewportOffset: {
 				top: window.getViewportTopOffsetConfig()
