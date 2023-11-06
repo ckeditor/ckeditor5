cb0797716a9503b02c8987b298467545126dba03
godai78 <b.biedrzycki@cksource.com>
2023-11-06 12:08:31 +0100

    Docs: adding missing image toolbars. [short flow]

--- packages/ckeditor5-link/docs/_snippets/features/link.js
+++ packages/ckeditor5-link/docs/_snippets/features/link.js
@@ -16,6 +16,12 @@ ClassicEditor
 				'|', 'bulletedList', 'numberedList', 'outdent', 'indent'
 			]
 		},
+		image: {
+			toolbar: [
+				'imageStyle:inline', 'imageStyle:block', 'imageStyle:side', '|',
+				'toggleImageCaption', 'imageTextAlternative', 'ckboxImageEdit'
+			]
+		},
 		ui: {
 			viewportOffset: {
 				top: window.getViewportTopOffsetConfig()
