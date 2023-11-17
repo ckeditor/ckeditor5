b4ca475fb77267e99e5794a6f5240229a6164c1b
godai78 <b.biedrzycki@cksource.com>
2023-11-17 14:58:30 +0100

    Docs: adding demos. [short flow].

--- packages/ckeditor5-basic-styles/docs/_snippets/features/basic-styles.js
+++ packages/ckeditor5-basic-styles/docs/_snippets/features/basic-styles.js
@@ -13,8 +13,13 @@ ClassicEditor
 			items: [
 				'undo', 'redo', '|', 'heading',
 				'|', 'bold', 'italic', 'underline', 'strikethrough', 'subscript', 'superscript', 'code', '|', 'removeFormat',
-				'|', 'link', 'uploadImage', 'insertTable', 'mediaEmbed',
+				'|', 'link', 'insertImage', 'insertTable', 'mediaEmbed',
 				'|', 'bulletedList', 'numberedList', 'outdent', 'indent'
+			]
+		},
+		insert: {
+			integrations: [
+				'insertImageViaUrl'
 			]
 		},
 		ui: {
