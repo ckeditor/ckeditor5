1fe515b31e5c276dc029a70ad28cbf2c9686170b
Dariusz Jarzębski <dumluregn@gmail.com>
2023-11-21 15:42:58 +0100

    Update comment.

--- packages/ckeditor5-editor-classic/src/classiceditorui.ts
+++ packages/ckeditor5-editor-classic/src/classiceditorui.ts
@@ -231,7 +231,7 @@ export default class ClassicEditorUI extends EditorUI {
 		const stickyPanel = this.view.stickyPanel;
 
 		dialogView.on<DialogViewMoveToEvent>( 'moveTo', ( evt, data ) => {
-			// Engage only when the panel is sticky, and the dialog is using one of default positions, and the dialog is not a modal.
+			// Engage only when the panel is sticky, and the dialog is using one of default positions.
 			if ( !stickyPanel.isSticky || dialogView.wasMoved ) {
 				return;
 			}
