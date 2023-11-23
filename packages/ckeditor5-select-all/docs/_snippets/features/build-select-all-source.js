34aba4b29f8672a64390af25d8a7a97966e5da4a
godai78 <b.biedrzycki@cksource.com>
2023-11-23 11:12:22 +0100

    Docs: fixing demo. [short flow]

--- packages/ckeditor5-select-all/docs/_snippets/features/build-select-all-source.js
+++ packages/ckeditor5-select-all/docs/_snippets/features/build-select-all-source.js
@@ -3,7 +3,7 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-/* globals */
+/* globals window */
 
 import { Underline } from '@ckeditor/ckeditor5-basic-styles';
 import { CKBox, CKBoxImageEdit } from '@ckeditor/ckeditor5-ckbox';
@@ -14,3 +14,5 @@ import { LinkImage } from '@ckeditor/ckeditor5-link';
 import ClassicEditor from '../build-classic';
 
 ClassicEditor.builtinPlugins.push( Underline, PictureEditing, ImageInsert, ImageResize, AutoImage, LinkImage, CKBox, CKBoxImageEdit );
+
+window.ClassicEditor = ClassicEditor;
