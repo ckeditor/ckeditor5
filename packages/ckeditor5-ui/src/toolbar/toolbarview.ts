05e188e65efb950f207a1200afd2da3e06a3739a
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2023-12-19 11:14:57 +0100

    Made the ToolbarView#_buildItemsFromConfig public as #createItemsFromConfig. Fixed an issue with the grouped items dropdown not disappearing upon resetting toolbar's items.

--- packages/ckeditor5-ui/src/toolbar/toolbarview.ts
+++ packages/ckeditor5-ui/src/toolbar/toolbarview.ts
@@ -318,7 +318,7 @@ export default class ToolbarView extends View implements DropdownPanelFocusable 
 		factory: ComponentFactory,
 		removeItems?: Array<string>
 	): void {
-		this.items.addMany( this._buildItemsFromConfig( itemsOrConfig, factory, removeItems ) );
+		this.items.addMany( this.createItemsFromConfig( itemsOrConfig, factory, removeItems ) );
 	}
 
 	/**
@@ -329,7 +329,7 @@ export default class ToolbarView extends View implements DropdownPanelFocusable 
 	 * @param removeItems An array of items names to be removed from the configuration. When present, applies
 	 * to this toolbar and all nested ones as well.
 	 */
-	private _buildItemsFromConfig(
+	public createItemsFromConfig(
 		itemsOrConfig: ToolbarConfig | undefined,
 		factory: ComponentFactory,
 		removeItems?: Array<string>
@@ -549,7 +549,7 @@ export default class ToolbarView extends View implements DropdownPanelFocusable 
 		}
 
 		addToolbarToDropdown( dropdownView, () => (
-			dropdownView.toolbarView!._buildItemsFromConfig( items, componentFactory, removeItems )
+			dropdownView.toolbarView!.createItemsFromConfig( items, componentFactory, removeItems )
 		) );
 
 		return dropdownView;
@@ -919,6 +919,10 @@ class DynamicGrouping implements ToolbarBehavior {
 			}
 		}
 
+		if ( !this.groupedItems.length ) {
+			this._removeGroupedItemsDropdown();
+		}
+
 		if ( this.groupedItems.length !== initialGroupedItemsCount ) {
 			this.view.fire<ToolbarViewGroupedItemsUpdateEvent>( 'groupedItemsUpdate' );
 		}
@@ -1019,10 +1023,21 @@ class DynamicGrouping implements ToolbarBehavior {
 		this.ungroupedItems.add( this.groupedItems.remove( this.groupedItems.first! ) );
 
 		if ( !this.groupedItems.length ) {
-			this.viewChildren.remove( this.groupedItemsDropdown );
-			this.viewChildren.remove( this.viewChildren.last! );
-			this.viewFocusTracker.remove( this.groupedItemsDropdown.element! );
+			this._removeGroupedItemsDropdown();
 		}
+	}
+
+	/**
+	 * Removed the dropdown that hosts {@link #groupedItems} from the {@link #viewChildren} collection.
+	 */
+	private _removeGroupedItemsDropdown(): void {
+		if ( !this.viewChildren.has( this.groupedItemsDropdown ) ) {
+			return;
+		}
+
+		this.viewChildren.remove( this.groupedItemsDropdown );
+		this.viewChildren.remove( this.viewChildren.last! );
+		this.viewFocusTracker.remove( this.groupedItemsDropdown.element! );
 	}
 
 	/**
