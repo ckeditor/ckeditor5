e3b93bfdd65b8756519812576884434362add1d1
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2024-01-16 17:20:42 +0100

    Docs: API docs for a11y help dialog and its view. Code refactoring in plugin's CSS.

--- packages/ckeditor5-ui/src/editorui/accessibilityhelp.ts
+++ packages/ckeditor5-ui/src/editorui/accessibilityhelp.ts
@@ -3,33 +3,36 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
+/**
+ * @module ui/editorui/accessibilityhelp
+ */
+
 import { Plugin } from '@ckeditor/ckeditor5-core';
 import { ButtonView, Dialog } from '../../src/index.js';
 import AccessibilityHelpContentView from './accessibilityhelpcontentview.js';
-import { getEnvKeystrokeText } from '@ckeditor/ckeditor5-utils';
+import { CKEditorError, getEnvKeystrokeText } from '@ckeditor/ckeditor5-utils';
 
 import '../../theme/components/editorui/accessibilityhelp.css';
 
 const DEFAULT_CATEGORY_ID = 'contentEditing' as const;
 export const DEFAULT_GROUP_ID = 'common' as const;
 
-// TODO: Maybe just help? This could be more than just a11y shortcuts. For instance, editor version,
-// installed plugins, etc.
+/**
+ * A plugin that brings the accessibility help dialog to the editor available under the <kbd>Alt</kbd>+<kbd>0</kbd>
+ * keystroke and via the "Accessibility help" toolbar button.
+ */
 export default class AccessibilityHelp extends Plugin {
 	/**
-	 * TODO
+	 * The view that displays the dialog content (list of keystrokes).
+	 * Created when the dialog is opened for the first time.
 	 */
-	private _dialogContentView: AccessibilityHelpContentView | null = null;
+	public contentView: AccessibilityHelpContentView | null = null;
 
 	/**
-	 * TODO
+	 * Keystroke categories, groups and descriptions registered via ({@link #registerKeystrokeCategory}) and
+	 * {@link #registerKeystrokes}.
 	 */
-	private _keystrokeDefinitions: Array<KeystrokeDefinition> = [];
-
-	/**
-	 * TODO
-	 */
-	private _keystrokes = new Map<string, KeystrokesCategory>();
+	private _keystrokes = new Map<string, AccessibilityHelpKeystrokesCategory>();
 
 	/**
 	 * @inheritDoc
@@ -46,7 +49,7 @@ export default class AccessibilityHelp extends Plugin {
 	}
 
 	/**
-	 * @inheritdoc
+	 * @inheritDoc
 	 */
 	public init(): void {
 		const editor = this.editor;
@@ -72,6 +75,10 @@ export default class AccessibilityHelp extends Plugin {
 				{
 					label: t( 'Move focus to the visible contextual balloon' ),
 					keystroke: 'Tab'
+				},
+				{
+					label: t( 'Open the accessibility help dialog' ),
+					keystroke: 'Alt+0'
 				},
 				{
 					label: t( 'Move focus between fields (inputs and buttons) in balloons and dialogs' ),
@@ -115,60 +122,145 @@ export default class AccessibilityHelp extends Plugin {
 	}
 
 	/**
-	 * TODO
+	 * Registers a top-level category of keystrokes with its label and optional description. Categories organize
+	 * keystrokes and help users to find the right keystroke. Each category can have multiple groups of keystrokes
+	 * that narrow down the context in which the keystrokes are available. Every keystroke category comes with a
+	 * `'common'` group by default.
+	 *
+	 * By default, two categories are registered by the `AccessibilityHelp` plugin: `'contentEditing'` and `'navigation'`.
+	 *
+	 * You can add keystrokes to categories using {@link #registerKeystrokes} method.
 	 */
 	public registerKeystrokeCategory( categoryId: string, definition: { label: string; description?: string } ): void {
 		this._keystrokes.set( categoryId, {
 			...definition,
-			keystrokeGroups: new Map( [ [ DEFAULT_GROUP_ID, { keystrokes: [] } ] ] )
+			groups: new Map( [ [ DEFAULT_GROUP_ID, { keystrokes: [] } ] ] )
 		} );
 	}
 
 	/**
-	 * TODO
+	 * Registers keystrokes that will be displayed in the Accessibility help dialog.
+	 *
+	 * To simply register a single keystroke:
+	 *
+	 * ```js
+	 * const t = editor.t;
+	 * editor.plugins.get( 'AccessibilityHelp' ).registerKeystrokes( {
+	 * 	label: t( 'Bold text' ),
+	 * 	keystroke: 'CTRL+B'
+	 * } );
+	 * ```
+	 *
+	 * Please note that by default:
+	 * * two categories are registered by the `AccessibilityHelp` plugin: `'contentEditing'` and `'navigation'`,
+	 * * keystrokes are registered into the `'contentEditing'` category and the `'common'` keystroke group within that category.
+	 *
+	 * To register a keystroke in a specific group in the `'contentEditing'` category:
+	 *
+	 * ```js
+	 * const t = editor.t;
+	 *
+	 * editor.plugins.get( 'AccessibilityHelp' ).registerKeystrokes( {
+	 * 	// Add keystrokes to the "widget" group. If the group does not exist, it will be created.
+	 * 	group: 'widget',
+	 * 	// When creating a new group, you can provide its label.
+	 * 	label: t( 'Keystrokes that can be used when a widget is selected' ),
+	 * 	groupLabel: t( 'Keystrokes that can be used when a widget is selected' ),
+	 * 	keystrokes: [
+	 * 		{
+	 * 			label: t( 'Insert a new paragraph directly after a widget' ),
+	 * 			keystroke: 'Enter'
+	 * 		},
+	 * 		{
+	 * 			label: t( 'Insert a new paragraph directly before a widget' ),
+	 * 			keystroke: 'Shift+Enter'
+	 * 		}
+	 * 	]
+	 * } );
+	 *```
+	 *
+	 * You can add more keystroke categories using {@link #registerKeystrokeCategory} method. To add the keystrokes to the
+	 * `'navigation'` category:
+	 *
+	 * ```js
+	 * this.registerKeystrokes( {
+	 * 	category: 'navigation',
+	 * 	keystrokes: [
+	 * 		{
+	 * 			label: t( 'Close contextual balloons, dropdowns, and dialogs' ),
+	 * 			keystroke: 'Esc'
+	 * 		},
+	 * 		{
+	 * 			label: t( 'Move focus to the visible contextual balloon' ),
+	 * 			keystroke: 'Tab'
+	 * 		}
+	 * 	]
+	 * } );
+	 * ```
 	 */
-	public registerKeystrokes( definition: {
+	public registerKeystrokes( options: {
 		category?: string;
 		group?: string;
 		groupLabel?: string;
-		keystrokes: Array<KeystrokeDefinition>;
-	} | Array<KeystrokeDefinition> | KeystrokeDefinition ): void {
-		if ( definition instanceof Array ) {
-			definition = {
-				keystrokes: definition
+		keystrokes: Array<AccessibilityHelpKeystrokeDefinition>;
+	} | Array<AccessibilityHelpKeystrokeDefinition> | AccessibilityHelpKeystrokeDefinition ): void {
+		if ( options instanceof Array ) {
+			options = {
+				keystrokes: options
 			};
-		} else if ( typeof definition === 'object' && 'keystroke' in definition ) {
-			definition = {
-				keystrokes: [ definition ]
+		} else if ( typeof options === 'object' && 'keystroke' in options ) {
+			options = {
+				keystrokes: [ options ]
 			};
 		}
 
-		const categoryId = definition.category || DEFAULT_CATEGORY_ID;
+		const categoryId = options.category || DEFAULT_CATEGORY_ID;
 
 		if ( !this._keystrokes.has( categoryId ) ) {
-			throw new Error( `Cannot register keystrokes for unknown category: "${ categoryId }".` );
+			/**
+			 * Cannot register keystrokes in an unknown category. Use
+			 * {@link module:ui/editorui/accessibilityhelp~AccessibilityHelp#registerKeystrokeCategory}
+			 * to register a new category or make sure the specified category exists.
+			 *
+			 * @error accessibility-help-unknown-category
+			 * @param categoryId The id of the unknown keystroke category.
+			 */
+			throw new CKEditorError( 'accessibility-help-unknown-category', { categoryId } );
 		}
 
 		const category = this._keystrokes.get( categoryId )!;
-		const groupId = definition.group || DEFAULT_GROUP_ID;
+		const groupId = options.group || DEFAULT_GROUP_ID;
 
-		if ( !category.keystrokeGroups.has( groupId ) && !definition.groupLabel ) {
-			throw new Error( `Cannot register keystrokes for unknown group: "${ groupId }".` );
-		} else if ( definition.groupLabel ) {
-			category.keystrokeGroups.set( groupId, {
-				label: definition.groupLabel,
-				keystrokes: definition.keystrokes
+		if ( !category.groups.has( groupId ) && !options.groupLabel ) {
+			/**
+			 * Cannot register keystrokes in an unknown group.
+			 *
+			 * * If you want to create a new group, make sure you pass the `groupLabel` option to the
+			 * {@link module:ui/editorui/accessibilityhelp~AccessibilityHelp#registerKeystrokes} method.
+			 * * If you want to add keystrokes to an existing group, make sure it exists.
+			 *
+			 * @error accessibility-help-unknown-group
+			 * @param groupId The id of the unknown keystroke group.
+			 * @param categoryId The id of category the unknown group should belong to.
+			 */
+			throw new CKEditorError( 'accessibility-help-unknown-group', { groupId, categoryId } );
+		} else if ( options.groupLabel ) {
+			category.groups.set( groupId, {
+				label: options.groupLabel,
+				keystrokes: options.keystrokes
 			} );
 		} else {
-			category.keystrokeGroups.get( groupId )!.keystrokes.push( ...definition.keystrokes );
+			category.groups.get( groupId )!.keystrokes.push( ...options.keystrokes );
 		}
 	}
 
 	/**
-	 * TODO
+	 * Injects a help text into each editing root's `aria-label` attribute allowing assistive technology users
+	 * to discover the availability of the Accessibility help dialog.
 	 */
 	private _setupRootLabels() {
 		const editor = this.editor;
+		const t = editor.t;
 
 		editor.on( 'ready', () => {
 			editor.editing.view.change( writer => {
@@ -176,8 +268,10 @@ export default class AccessibilityHelp extends Plugin {
 					const viewRoot = editor.editing.view.document.getRoot( rootName );
 					const currentAriaLabel = viewRoot!.getAttribute( 'aria-label' );
 
-					writer.setAttribute( 'aria-label',
-						`${ currentAriaLabel }. Press ${ getEnvKeystrokeText( 'Alt+0' ) } for help.`, viewRoot!
+					writer.setAttribute(
+						'aria-label',
+						`${ currentAriaLabel }. ${ t( 'Press %0 for help.', [ getEnvKeystrokeText( 'Alt+0' ) ] ) }`,
+						viewRoot!
 					);
 				}
 			} );
@@ -185,39 +279,104 @@ export default class AccessibilityHelp extends Plugin {
 	}
 
 	/**
-	 * @inheritdoc
+	 * Shows the accessibility help dialog. Also, creates {@link #contentView} on demand.
 	 */
 	private _showDialog() {
 		const editor = this.editor;
 		const dialog = editor.plugins.get( 'Dialog' );
 		const t = editor.locale.t;
 
-		if ( !this._dialogContentView ) {
-			this._dialogContentView = new AccessibilityHelpContentView( editor.locale, this._keystrokes );
+		if ( !this.contentView ) {
+			this.contentView = new AccessibilityHelpContentView( editor.locale, this._keystrokes );
 		}
 
 		dialog.show( {
 			id: 'accessibilityHelp',
+			className: 'ck-accessibility-help-dialog',
 			title: t( 'Accessibility help' ),
 			hasCloseButton: true,
-			content: this._dialogContentView
+			content: this.contentView
 		} );
 	}
 }
 
-export interface KeystrokesCategory {
+/**
+ * A category of keystrokes. Top-level categories organize keystrokes and help users to find the right keystroke.
+ * Each category can have multiple groups of keystrokes that narrow down the context in which the keystrokes are available.
+ *
+ * @internal
+ */
+export interface AccessibilityHelpKeystrokesCategory {
+
+	/**
+	 * The label of the category. It gets displayed as a header in the Accessibility help dialog.
+	 */
 	label: string;
+
+	/**
+	 * The description of the category (optional). It gets displayed as a paragraph of text next to the category keystrokes
+	 * in the Accessibility help dialog.
+	 */
 	description?: string;
-	keystrokeGroups: Map<string, KeystrokeGroupDefinition>;
+
+	/**
+	 * Groups of keystrokes within the category.
+	 */
+	groups: Map<string, AccessibilityHelpKeystrokeGroupDefinition>;
 }
 
-export interface KeystrokeGroupDefinition {
+/**
+ * A sub-category of keystrokes. Groups narrow down the context in which the keystrokes are available.
+ *
+ * @internal
+ */
+export interface AccessibilityHelpKeystrokeGroupDefinition {
+
+	/**
+	 * The label of the group (optional). It gets displayed as a header next to the list of keystrokes in the group.
+	 */
 	label?: string;
-	keystrokes: Array<KeystrokeDefinition>;
+
+	/**
+	 * Keystrokes within the group.
+	 */
+	keystrokes: Array<AccessibilityHelpKeystrokeDefinition>;
 }
 
-export interface KeystrokeDefinition {
+/**
+ * A single keystroke definition to be displayed in the Accessibility help dialog.
+ */
+export interface AccessibilityHelpKeystrokeDefinition {
+
+	/**
+	 * The label of the keystroke. It should briefly describe the action that the keystroke performs. It may contain HTML.
+	 */
 	label: string;
+
+	/**
+	 * The keystroke string. In its basic form, it must be a combination of {@link module:utils/keyboard#keyCodes known key names}
+	 * joined by the `+` sign, just like in the keystroke format accepted by the
+	 * {@link module:utils/keystrokehandler~KeystrokeHandler#set `KeystrokeHandler#set()`} method used to register most of the
+	 * keystrokes in the editor.
+	 *
+	 * * The keystroke string can represent a single keystroke, for instance: `keystroke: 'Ctrl+B'`, `keystroke: 'Shift+Enter'`,
+	 * `keystroke: 'Alt+F10'`, etc.
+	 * * The keystroke can be activated by successive press of multiple keys. For instance `keystroke: [ [ 'arrowleft', 'arrowleft' ] ]`
+	 * will indicate that a specific action will be performed by pressing <kbd>←</kbd> twice in a row.
+	 * * Keystrokes can have alternatives. For instance `keystroke: [ [ 'Ctrl+Y' ], [ 'Ctrl+Shift+Z' ] ]` will indicate that
+	 * a specific action can be performed by pressing either <kbd>Ctrl</kbd> + <kbd>Y</kbd> or
+	 * <kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>Z</kbd>.
+	 *
+	 * Please note that the keystrokes are automatically translated to the environment-specific form. For example, `Ctrl+A`
+	 * will be rendered as `⌘A` in the {@link module:utils/env~EnvType#isMac Mac environment}. Always use the IBM PC keyboard
+	 * syntax, for instance `Ctrl` instead of `⌘`, `Alt` instead of `⌥`, etc.
+	 */
 	keystroke: string | Array<string> | Array<Array<string>>;
+
+	/**
+	 * This (optional) flag suggests that the keystroke(s) may require a function (<kbd>Fn</kbd>) key to be pressed
+	 * in order to work in the {@link module:utils/env~EnvType#isMac Mac environment}. If set `true`, an additional
+	 * information will be displayed next to the keystroke in the Accessibility help dialog.
+	 */
 	mayRequireFn?: boolean;
 }
--- packages/ckeditor5-ui/src/editorui/accessibilityhelpcontentview.ts
+++ packages/ckeditor5-ui/src/editorui/accessibilityhelpcontentview.ts
@@ -3,23 +3,32 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-import { createElement, getEnvKeystrokeText, type Locale } from '@ckeditor/ckeditor5-utils';
+/**
+ * @module ui/editorui/accessibilityhelpcontentview
+ */
+
 import {
-	type KeystrokeDefinition,
-	type KeystrokeGroupDefinition,
-	type KeystrokesCategory
+	createElement,
+	env,
+	getEnvKeystrokeText,
+	type Locale
+} from '@ckeditor/ckeditor5-utils';
+import {
+	type AccessibilityHelpKeystrokeDefinition,
+	type AccessibilityHelpKeystrokeGroupDefinition,
+	type AccessibilityHelpKeystrokesCategory
 } from './accessibilityhelp.js';
 import View from '../view.js';
 import LabelView from '../label/labelview.js';
 
 /**
- * TODO
+ * The view displaying keystrokes in the Accessibility help dialog.
  */
 export default class AccessibilityHelpContentView extends View<HTMLDivElement> {
 	/**
-	 * @inheritdoc
+	 * @inheritDoc
 	 */
-	constructor( locale: Locale, keystrokes: Map<string, KeystrokesCategory> ) {
+	constructor( locale: Locale, keystrokes: Map<string, AccessibilityHelpKeystrokesCategory> ) {
 		super( locale );
 
 		const t = locale.t;
@@ -30,7 +39,7 @@ export default class AccessibilityHelpContentView extends View<HTMLDivElement> {
 		this.setTemplate( {
 			tag: 'div',
 			attributes: {
-				class: [ 'ck', 'ck-accessibility-help-content' ],
+				class: [ 'ck', 'ck-accessibility-help-dialog__content' ],
 				'aria-labelledby': helpLabel.id,
 				role: 'document',
 				tabindex: -1
@@ -44,31 +53,31 @@ export default class AccessibilityHelpContentView extends View<HTMLDivElement> {
 	}
 
 	/**
-	 * @inheritdoc
+	 * @inheritDoc
 	 */
 	public focus(): void {
 		this.element!.focus();
 	}
 
 	/**
-	 * TODO
+	 * Creates `<section><h3>Category label</h3>...</section>` elements for each category of keystrokes.
 	 */
-	private _createCategories( keystrokes: Map<string, KeystrokesCategory> ): Array<HTMLElement> {
+	private _createCategories( keystrokes: Map<string, AccessibilityHelpKeystrokesCategory> ): Array<HTMLElement> {
 		return Array.from( keystrokes.entries() ).map( ( [ , category ] ) => {
 			return createElement( document, 'section', {}, [
 				// Category header.
 				createElement( document, 'h3', {}, category.label ),
 
 				// Category definitions (<dl>) and their optional headers (<h4>).
-				...Array.from( category.keystrokeGroups.entries() ).map( this._createGroup.bind( this ) ).flat()
+				...Array.from( category.groups.entries() ).map( this._createGroup.bind( this ) ).flat()
 			] );
 		} );
 	}
 
 	/**
-	 * TODO
+	 * Creates `[<h4>Optional label</h4>]<dl>...</dl>` elements for each group of keystrokes in a category.
 	 */
-	private _createGroup( [ , group ]: [ groupId: string, group: KeystrokeGroupDefinition ] ): Array<HTMLElement> {
+	private _createGroup( [ , group ]: [ groupId: string, group: AccessibilityHelpKeystrokeGroupDefinition ] ): Array<HTMLElement> {
 		const elements: Array<HTMLElement> = [
 			createElement( document, 'dl', {}, group.keystrokes
 				.sort( ( a, b ) => sortAlphabetically( a.label, b.label ) )
@@ -85,9 +94,9 @@ export default class AccessibilityHelpContentView extends View<HTMLDivElement> {
 	}
 
 	/**
-	 * TODO
+	 * Creates `<dt>Keystroke label</dt><dd>Keystroke definition</dd>` elements for each keystroke in a group.
 	 */
-	private _createGroupRow( keystrokeDef: KeystrokeDefinition ): [ HTMLElement, HTMLElement ] {
+	private _createGroupRow( keystrokeDef: AccessibilityHelpKeystrokeDefinition ): [ HTMLElement, HTMLElement ] {
 		const t = this.locale!.t;
 		const dt = createElement( document, 'dt' );
 		const dd = createElement( document, 'dd' );
@@ -100,7 +109,7 @@ export default class AccessibilityHelpContentView extends View<HTMLDivElement> {
 
 		dt.innerHTML = keystrokeDef.label;
 		dd.innerHTML = keystrokeAlternativeHTMLs.join( ', ' ) +
-			( keystrokeDef.mayRequireFn ? ` ${ t( '(may require <kbd>Fn</kbd>)' ) }` : '' );
+			( keystrokeDef.mayRequireFn && env.isMac ? ` ${ t( '(may require <kbd>Fn</kbd>)' ) }` : '' );
 
 		return [ dt, dd ];
 	}
@@ -118,12 +127,12 @@ function sortAlphabetically( a: string, b: string ) {
 	return a.localeCompare( b );
 }
 
-function normalizeKeystrokeDefinition( definition: KeystrokeDefinition[ 'keystroke' ] ): Array<Array<string>> {
+function normalizeKeystrokeDefinition( definition: AccessibilityHelpKeystrokeDefinition[ 'keystroke' ] ): Array<Array<string>> {
 	if ( typeof definition === 'string' ) {
 		return [ [ definition ] ];
 	}
 
-	if ( definition.every( entry => typeof entry == 'string' ) ) {
+	if ( typeof definition[ 0 ] === 'string' ) {
 		return [ definition as Array<string> ];
 	}
 
