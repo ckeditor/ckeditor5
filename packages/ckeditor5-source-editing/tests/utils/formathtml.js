14c02db1a80562bae455ca8ed7ed900fe18b8798
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2024-02-12 15:43:21 +0100

    Source editing should not add white spaces to preformatted code lines.

--- packages/ckeditor5-source-editing/tests/utils/formathtml.js
+++ packages/ckeditor5-source-editing/tests/utils/formathtml.js
@@ -207,6 +207,26 @@ describe( 'SourceEditing utils', () => {
 			expect( formatHtml( source ) ).to.equal( sourceFormatted );
 		} );
 
+		it( 'should not inject extra white spaces at the beginning of preformatted lines in <pre>', () => {
+			const source = '' +
+				'<blockquote>' +
+					'<pre><code>foo\n' +
+					'bar\n' +
+					'abc\n' +
+					'baz</code></pre>' +
+				'</blockquote>';
+
+			const sourceFormatted = '' +
+				'<blockquote>\n' +
+				'    <pre><code>foo\n' +
+				'bar\n' +
+				'abc\n' +
+				'baz</code></pre>\n' +
+				'</blockquote>';
+
+			expect( formatHtml( source ) ).to.equal( sourceFormatted );
+		} );
+
 		it( 'should keep all attributes unchanged', () => {
 			const source = '' +
 				'<p id="foo" class="class1 class2" data-value="bar" onclick="fn();">' +
