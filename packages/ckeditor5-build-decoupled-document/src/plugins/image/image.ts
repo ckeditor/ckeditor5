91fe19dd83aa8b4edcd3384275667d950093481d
Badlapje <koen@cornelis.it>
2024-02-19 15:37:08 +0100

    Fix image insert

--- packages/ckeditor5-build-decoupled-document/src/plugins/image/image.ts
+++ packages/ckeditor5-build-decoupled-document/src/plugins/image/image.ts
@@ -2,15 +2,15 @@ import { Plugin } from 'ckeditor5/src/core.js';
 import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview.js';
 import icon from './icon.svg';
 
-export default class Exercise extends Plugin {
+export default class Image extends Plugin {
 	public init(): void {
 		const editor = this.editor;
 		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		// @ts-ignore
-		if ( editor.config._config.image && editor.config._config.image.onOpen ) {
+		if ( editor.config._config.ownImage && editor.config._config.ownImage.onOpen ) {
 			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 			// @ts-ignore
-			const open = editor.config._config.image.onOpen;
+			const open = editor.config._config.ownImage.onOpen;
 			// The button must be registered among the UI components of the editor
 			// to be displayed in the toolbar.
 			editor.ui.componentFactory.add( 'ownImagePlugin', () => {
--- packages/ckeditor5-build-decoupled-document/src/plugins/image/index.ts
+++ packages/ckeditor5-build-decoupled-document/src/plugins/image/index.ts
@@ -1,7 +1,7 @@
 import icon from './icon.svg';
 
-export { default as Image } from './image';
+export { default as Image } from './image.js';
 
 export const icons = {
-	icon,
+	icon
 };
