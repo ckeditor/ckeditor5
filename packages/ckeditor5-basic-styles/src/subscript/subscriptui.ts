bce437813dd72b7d2d861af4cec518fc122218bf
Michał Remiszewski <m.remiszewski@cksource.com>
2024-02-22 19:50:29 +0100

    Add subscript integration.

--- packages/ckeditor5-basic-styles/src/subscript/subscriptui.ts
+++ packages/ckeditor5-basic-styles/src/subscript/subscriptui.ts
@@ -8,7 +8,7 @@
  */
 
 import { Plugin } from 'ckeditor5/src/core.js';
-import { ButtonView } from 'ckeditor5/src/ui.js';
+import { ButtonView, MenuBarMenuListItemButtonView } from 'ckeditor5/src/ui.js';
 import type AttributeCommand from '../attributecommand.js';
 
 import subscriptIcon from '../../theme/icons/subscript.svg';
@@ -34,26 +34,46 @@ export default class SubscriptUI extends Plugin {
 		const t = editor.t;
 
 		// Add subscript button to feature components.
-		editor.ui.componentFactory.add( SUBSCRIPT, locale => {
-			const command: AttributeCommand = editor.commands.get( SUBSCRIPT )!;
-			const view = new ButtonView( locale );
+		editor.ui.componentFactory.add( SUBSCRIPT, () => {
+			const buttonView = this._createButton( ButtonView );
+			const command = editor.commands.get( SUBSCRIPT )!;
 
-			view.set( {
-				label: t( 'Subscript' ),
-				icon: subscriptIcon,
-				tooltip: true,
-				isToggleable: true
-			} );
+			// Bind button model to command.
+			buttonView.bind( 'isOn' ).to( command, 'value' );
 
-			view.bind( 'isOn', 'isEnabled' ).to( command, 'value', 'isEnabled' );
-
-			// Execute command.
-			this.listenTo( view, 'execute', () => {
-				editor.execute( SUBSCRIPT );
-				editor.editing.view.focus();
-			} );
-
-			return view;
+			return buttonView;
 		} );
+
+		editor.ui.componentFactory.add( 'menuBar:' + SUBSCRIPT, () => {
+			return this._createButton( MenuBarMenuListItemButtonView );
+		} );
+	}
+
+	/**
+	 * Creates a button for subscript command to use either in toolbar or in menu bar.
+	 */
+	private _createButton<T extends typeof ButtonView | typeof MenuBarMenuListItemButtonView>( ButtonClass: T ): InstanceType<T> {
+		const editor = this.editor;
+		const locale = editor.locale;
+		const command: AttributeCommand = editor.commands.get( SUBSCRIPT )!;
+		const view = new ButtonClass( editor.locale ) as InstanceType<T>;
+		const t = locale.t;
+
+		view.set( {
+			label: t( 'Subscript' ),
+			icon: subscriptIcon,
+			tooltip: true,
+			isToggleable: true
+		} );
+
+		view.bind( 'isEnabled' ).to( command, 'isEnabled' );
+
+		// Execute the command.
+		this.listenTo( view, 'execute', () => {
+			editor.execute( SUBSCRIPT );
+			editor.editing.view.focus();
+		} );
+
+		return view;
 	}
 }
