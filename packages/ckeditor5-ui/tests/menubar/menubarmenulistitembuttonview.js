77cb96a16c3c5e4645d39690d45d813f086e1228
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2024-02-27 13:25:46 +0100

    Tests: MenuBarView sub-classes tests.

--- packages/ckeditor5-ui/tests/menubar/menubarmenulistitembuttonview.js
+++ packages/ckeditor5-ui/tests/menubar/menubarmenulistitembuttonview.js
@@ -3,26 +3,43 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
+import { Locale } from '@ckeditor/ckeditor5-utils';
+import {
+	ButtonView,
+	MenuBarMenuListItemButtonView
+} from '../../src/index.js';
+
 describe( 'MenuBarMenuListItemButtonView', () => {
+	let buttonView, locale;
+
+	beforeEach( () => {
+		locale = new Locale();
+		buttonView = new MenuBarMenuListItemButtonView( locale );
+	} );
+
+	afterEach( () => {
+		buttonView.destroy();
+	} );
+
 	describe( 'constructor()', () => {
 		it( 'should inherit from ButtonView', () => {
-
+			expect( buttonView ).to.be.instanceOf( ButtonView );
 		} );
 
 		it( 'should set #withText', () => {
-
+			expect( buttonView.withText ).to.be.true;
 		} );
 
 		it( 'should set #withKeystroke', () => {
-
+			expect( buttonView.withKeystroke ).to.be.true;
 		} );
 
 		it( 'should set #tooltip', () => {
-
+			expect( buttonView.tooltip ).to.be.false;
 		} );
 
 		it( 'should set #role', () => {
-
+			expect( buttonView.role ).to.equal( 'menuitem' );
 		} );
 	} );
 } );
--- packages/ckeditor5-ui/tests/menubar/menubarmenulistview.js
+++ packages/ckeditor5-ui/tests/menubar/menubarmenulistview.js
@@ -3,14 +3,28 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
+import { Locale } from '@ckeditor/ckeditor5-utils';
+import { ListView, MenuBarMenuListView } from '../../src/index.js';
+
 describe( 'MenuBarMenuListView', () => {
+	let listView, locale;
+
+	beforeEach( () => {
+		locale = new Locale();
+		listView = new MenuBarMenuListView( locale );
+	} );
+
+	afterEach( () => {
+		listView.destroy();
+	} );
+
 	describe( 'constructor()', () => {
 		it( 'should inherit from ListView', () => {
-
+			expect( listView ).to.be.instanceOf( ListView );
 		} );
 
 		it( 'should have #role set', () => {
-
+			expect( listView.role ).to.equal( 'menu' );
 		} );
 	} );
 } );
