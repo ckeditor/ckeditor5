2b1cbb4d6c1031cff63b9452a8f9911c941683b7
Mateusz Baginski <cziken58@gmail.com>
2024-02-27 15:01:22 +0100

    Fix linter issues

--- packages/ckeditor5-clipboard/src/clipboardmarkersutils.ts
+++ packages/ckeditor5-clipboard/src/clipboardmarkersutils.ts
@@ -2,6 +2,7 @@
  * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
+
 import { clone } from 'lodash-es';
 
 import { uid } from '@ckeditor/ckeditor5-utils';
