81652d0186712e672d0349bf3d41a68f66d70d56
godai78 <b.biedrzycki@cksource.com>
2024-02-27 08:46:10 +0100

    Docs: the story so far. [short flow]

--- packages/ckeditor5-image/docs/_snippets/features/image-responsive.js
+++ packages/ckeditor5-image/docs/_snippets/features/image-responsive.js
@@ -6,6 +6,7 @@
 /* globals ClassicEditor, console, window, document */
 
 import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud-services-config.js';
+import { TOKEN_URL } from '@ckeditor/ckeditor5-ckbox/tests/_utils/ckbox-config.js';
 
 ClassicEditor
 	.create( document.querySelector( '#snippet-responsive' ), {
@@ -65,6 +66,7 @@ ClassicEditor
 			]
 		},
 		ckbox: {
+			tokenUrl: TOKEN_URL,
 			forceDemoLabel: true,
 			allowExternalImagesEditing: [ /^data:/, 'origin' ]
 		},
