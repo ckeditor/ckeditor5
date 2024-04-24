48793fe3fd74b62f036b903725a2329c06ee0df2
Filip Sobol <filipsobol@users.noreply.github.com>
2024-04-24 08:12:05 +0200

    Feature (engine): Export `_getModelData` and `_setModelData`

--- packages/ckeditor5-engine/src/index.ts
+++ packages/ckeditor5-engine/src/index.ts
@@ -197,3 +197,9 @@ export * from './view/styles/border.js';
 export * from './view/styles/margin.js';
 export * from './view/styles/padding.js';
 export * from './view/styles/utils.js';
+
+// Development / testing utils
+export {
+	getData as _getModelData,
+	setData as _setModelData
+} from './dev-utils/model.js';
