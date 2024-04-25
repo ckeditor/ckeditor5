7eb2e0943c74241a8a206152eb6bd1464687069c
Filip Sobol <filipsobol@users.noreply.github.com>
2024-04-25 11:03:10 +0200

    Re-export `getData`, `setData`, `parse` and `stringify` for model and view

--- packages/ckeditor5-engine/src/index.ts
+++ packages/ckeditor5-engine/src/index.ts
@@ -201,5 +201,14 @@ export * from './view/styles/utils.js';
 // Development / testing utils.
 export {
 	getData as _getModelData,
-	setData as _setModelData
+	setData as _setModelData,
+	parse as _parseModel,
+	stringify as _stringifyModel
 } from './dev-utils/model.js';
+
+export {
+	getData as _getViewData,
+	setData as _setViewData,
+	parse as _parseView,
+	stringify as _stringifyView
+} from './dev-utils/view.js';
