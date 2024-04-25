da0b502ae12dc538abf20c4980c966bdefd46536
Kuba Niegowski <j.niegowski@cksource.com>
2024-04-25 16:53:14 +0200

    Updated code comment.

--- packages/ckeditor5-typing/src/inserttextobserver.ts
+++ packages/ckeditor5-typing/src/inserttextobserver.ts
@@ -112,21 +112,10 @@ export default class InsertTextObserver extends Observer {
 			// @if CK_DEBUG_TYPING // }
 
 			// How do we know where to insert the composed text?
-			// The selection observer is blocked and the view is not updated with the composition changes.
-			// There were three options:
-			//   - Store the selection on `compositionstart` and use it now. This wouldn't work in RTC
-			//     where the view would change and the stored selection might get incorrect.
-			//     We'd need to fallback to the current view selection anyway.
-			//   - Use the current view selection. This is a bit weird and non-intuitive because
-			//     this isn't necessarily the selection on which the user started composing.
-			//     We cannot even know whether it's still collapsed (there might be some weird
-			//     editor feature that changed it in unpredictable ways for us). But it's by far
-			//     the simplest solution and should be stable (the selection is definitely correct)
-			//     and probably mostly predictable (features usually don't modify the selection
-			//     unless called explicitly by the user).
-			//   - Try to follow it from the `beforeinput` events. This would be really complex as each
-			//     `beforeinput` would come with just the range it's changing and we'd need to calculate that.
-			// We decided to go with the 2nd option for its simplicity and stability.
+			// 1. The SelectionObserver is blocked and the view is not updated with the composition changes.
+			// 2. The last moment before it's locked is the `compositionstart` event.
+			// 3. The `SelectionObserver` is listening for `compositionstart` event and immediately converts
+			//    the selection. Handles this at the lowest priority so after the rendering is blocked.
 			viewDocument.fire( 'insertText', new DomEventData( view, domEvent, {
 				text: data
 			} ) );
@@ -173,7 +162,7 @@ export interface InsertTextEventData extends DomEventData {
 	 * The selection into which the text should be inserted.
 	 * If not specified, the insertion should occur at the current view selection.
 	 */
-	selection: ViewSelection | ViewDocumentSelection;
+	selection?: ViewSelection | ViewDocumentSelection;
 
 	/**
 	 * The range that view selection should be set to after insertion.
