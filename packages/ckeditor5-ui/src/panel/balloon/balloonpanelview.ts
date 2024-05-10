5b49f564c6c96c8e8afa6fe1124e9b6181b8421d
Filip Sobol <filipsobol@users.noreply.github.com>
2024-05-10 12:54:27 +0200

    Docs: Fix link to `BalloonPanelView.generatePositions`

--- packages/ckeditor5-ui/src/panel/balloon/balloonpanelview.ts
+++ packages/ckeditor5-ui/src/panel/balloon/balloonpanelview.ts
@@ -1165,7 +1165,7 @@ export default class BalloonPanelView extends View {
 	 * Positioning functions must be compatible with {@link module:utils/dom/position~DomPoint}.
 	 *
 	 * Default positioning functions with customized offsets can be generated using
-	 * {@link module:ui/panel/balloon/balloonpanelview~generatePositions}.
+	 * {@link module:ui/panel/balloon/balloonpanelview~BalloonPanelView.generatePositions}.
 	 *
 	 * The name that the position function returns will be reflected in the balloon panel's class that
 	 * controls the placement of the "arrow". See {@link #position} to learn more.
