c12d2194aa504cfe8a61b0ad78828cdc9c483713
LukaszGudel <l.gudel@cksource.com>
2024-06-04 11:10:20 +0200

    Fix paths

--- scripts/release/deploycdn.js
+++ scripts/release/deploycdn.js
@@ -9,22 +9,23 @@
 
 'use strict';
 
+const upath = require( 'upath' );
 const { Listr } = require( 'listr2' );
 const { tools } = require( '@ckeditor/ckeditor5-dev-utils' );
 const parseArguments = require( './utils/parsearguments' );
-const { CDN_S3_BUCKET, S3_COPY_DIR_ARGS, S3_COPY_FILE_ARGS, CDN_CLOUDFRONT_ID } = require( './utils/constants' );
+const { CKEDITOR5_ROOT_PATH, CDN_S3_BUCKET, S3_COPY_DIR_ARGS, S3_COPY_FILE_ARGS, CDN_CLOUDFRONT_ID } = require( './utils/constants' );
 const cliArguments = parseArguments( process.argv.slice( 2 ) );
 
 const name = 'ckeditor5';
 
-const { version } = require( 'package.json' );
+const { version } = require( upath.join( CKEDITOR5_ROOT_PATH, './package.json' ) );
 
 const tasks = new Listr( [
 	{
 		title: 'Upload dist to CDN',
 		task: () => {
-			return tools.shExec( // TODO: change ckeditor5-test/ to ${name}/
-				`aws s3 cp ./dist/ s3://${ CDN_S3_BUCKET }/ckeditor5-test/${ version }/ ${ S3_COPY_DIR_ARGS }`,
+			return tools.shExec(
+				`aws s3 cp ./dist/ s3://${ CDN_S3_BUCKET }/${ name }/${ version }/ ${ S3_COPY_DIR_ARGS }`,
 				{ verbosity: 'error' }
 			);
 		},
@@ -38,8 +39,8 @@ const tasks = new Listr( [
 				`cd dist && zip -r ../${ name }.zip ./*`,
 				{ verbosity: 'error' }
 			);
-			return tools.shExec( // TODO: change ckeditor5-test/ to ${name}/
-				`aws s3 cp ./${ name }.zip s3://${ CDN_S3_BUCKET }/ckeditor5-test/${ version }/zip/ ${ S3_COPY_FILE_ARGS }`,
+			return tools.shExec(
+				`aws s3 cp ./${ name }.zip s3://${ CDN_S3_BUCKET }/${ name }/${ version }/zip/ ${ S3_COPY_FILE_ARGS }`,
 				{ verbosity: 'error' }
 			);
 		},
@@ -49,15 +50,14 @@ const tasks = new Listr( [
 	{
 		title: 'Upload DLL files to CDN',
 		task: () => {
-			return tools.shExec( // TODO: change ckeditor5-test/ to ${name}/
-				`aws s3 cp ./release_dll/ s3://${ CDN_S3_BUCKET }/ckeditor5-test/${ version }/dll/ ${ S3_COPY_DIR_ARGS }`,
+			return tools.shExec(
+				`aws s3 cp ./release_dll/ s3://${ CDN_S3_BUCKET }/${ name }/${ version }/dll/ ${ S3_COPY_DIR_ARGS }`,
 				{ verbosity: 'error' }
 			);
 		},
 		// CDN should not be updated on nightly releases.
 		skip: cliArguments.nightly
 	},
-	// TODO: add step with edit of `index.html` file (should we commit this?)
 	{
 		title: 'Upload main files to CDN',
 		task: () => {
