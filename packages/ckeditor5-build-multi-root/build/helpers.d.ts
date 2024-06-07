022bd1a50bebd7370cdf9e9f720aca9713c63600
Lucas Samuel Fontes Vega <54158635+lucasfontes2604@users.noreply.github.com>
2024-06-07 13:01:09 -0300

    Switch NPM account from authorium to eagerworks. (#13)
    * Switch NPM account from authorium to eagerworks.
    
    We don't have the credentials for publishing the new version of the package
    in authorium, so we are changing the organization to publish the package
    throught the eagerworks npm account
    
    * Run build for packages/ckeditor5-build-multiroot

--- packages/ckeditor5-build-multi-root/build/helpers.d.ts
+++ packages/ckeditor5-build-multi-root/build/helpers.d.ts
@@ -1,10 +1,14 @@
-import { Editor } from '@ckeditor/ckeditor5-core';
-import { HeadingConfig } from '@ckeditor/ckeditor5-heading';
-import { ImageConfig, ImageStyleConfig } from '@ckeditor/ckeditor5-image/src/imageconfig';
-import { TableConfig } from '@ckeditor/ckeditor5-table';
+/**
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import type { Editor } from '@ckeditor/ckeditor5-core';
+import type { HeadingConfig } from '@ckeditor/ckeditor5-heading';
+import type { ImageConfig, ImageStyleConfig } from '@ckeditor/ckeditor5-image/src/imageconfig.js';
+import type { TableConfig } from '@ckeditor/ckeditor5-table';
 export declare function MentionCustomization(editor: Editor): void;
 interface CkeditorNumericFontSizeConfig {
-    options: (number | string)[];
+    options: Array<number | string>;
 }
 export interface LanguageConfig {
     language: string;
@@ -15,7 +19,7 @@ export declare const NumericFontSizeConfig: CkeditorNumericFontSizeConfig;
 export declare const ImageStyles: ImageStyleConfig;
 export declare const ImageConfiguration: ImageConfig;
 export declare const CodeBlockConfiguration: {
-    languages: LanguageConfig[];
+    languages: Array<LanguageConfig>;
 };
 export declare const HeadingConfiguration: HeadingConfig;
 export declare const TableConfiguration: TableConfig;
