ce4ac29bbedef189e3cb05d59072919b654c5769
Mateusz Baginski <cziken58@gmail.com>
2024-06-25 09:29:04 +0200

    Fix `Link` plugin checkbox role

--- packages/ckeditor5-link/src/linkui.ts
+++ packages/ckeditor5-link/src/linkui.ts
@@ -241,30 +241,32 @@ export default class LinkUI extends Plugin {
 	 */
 	private _createToolbarLinkButton(): void {
 		const editor = this.editor;
-		const linkCommand: LinkCommand = editor.commands.get( 'link' )!;
 
 		editor.ui.componentFactory.add( 'link', () => {
 			const button = this._createButton( ButtonView );
 
 			button.set( {
-				tooltip: true,
-				isToggleable: true
+				tooltip: true
 			} );
-
-			button.bind( 'isOn' ).to( linkCommand, 'value', value => !!value );
 
 			return button;
 		} );
 
 		editor.ui.componentFactory.add( 'menuBar:link', () => {
-			return this._createButton( MenuBarMenuListItemButtonView );
+			const button = this._createButton( MenuBarMenuListItemButtonView );
+
+			button.set( {
+				role: 'menuitemcheckbox'
+			} );
+
+			return button;
 		} );
 	}
 
 	/**
 	 * Creates a button for link command to use either in toolbar or in menu bar.
 	 */
-	private _createButton<T extends typeof ButtonView | typeof MenuBarMenuListItemButtonView>( ButtonClass: T ): InstanceType<T> {
+	private _createButton<T extends typeof ButtonView>( ButtonClass: T ): InstanceType<T> {
 		const editor = this.editor;
 		const locale = editor.locale;
 		const command = editor.commands.get( 'link' )!;
@@ -274,10 +276,12 @@ export default class LinkUI extends Plugin {
 		view.set( {
 			label: t( 'Link' ),
 			icon: linkIcon,
-			keystroke: LINK_KEYSTROKE
+			keystroke: LINK_KEYSTROKE,
+			isToggleable: true
 		} );
 
 		view.bind( 'isEnabled' ).to( command, 'isEnabled' );
+		view.bind( 'isOn' ).to( command, 'value', value => !!value );
 
 		// Show the panel on button click.
 		this.listenTo( view, 'execute', () => this._showUI( true ) );
