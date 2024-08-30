05fd655ca509733473dc72bdda3749ad91fd25fc
Mateusz Baginski <cziken58@gmail.com>
2024-08-30 07:34:03 +0200

    Add additional fallback for Safari while deducing focused element.

--- packages/ckeditor5-widget/src/widget.ts
+++ packages/ckeditor5-widget/src/widget.ts
@@ -24,7 +24,9 @@ import {
 	type Position,
 	type EditingView,
 	type ViewDocumentTabEvent,
-	type ViewDocumentKeyDownEvent
+	type ViewDocumentKeyDownEvent,
+	type ViewNode,
+	type ViewRange
 } from '@ckeditor/ckeditor5-engine';
 
 import { Delete, type ViewDocumentDeleteEvent } from '@ckeditor/ckeditor5-typing';
@@ -649,12 +651,15 @@ function findClosestEditableOrWidgetAncestor( element: ViewElement ): ViewElemen
  */
 function getElementFromMouseEvent( view: EditingView, domEventData: DomEventData<MouseEvent> ): ViewElement | null {
 	const domRange = getRangeFromMouseEvent( domEventData.domEvent );
+	let viewRange: ViewRange | null = null;
 
-	if ( !domRange ) {
-		return null;
+	if ( domRange ) {
+		viewRange = view.domConverter.domRangeToView( domRange );
+	} else {
+		// Fallback to create range on target element. It happens frequently on Safari browser.
+		// See more: https://github.com/ckeditor/ckeditor5/issues/16978
+		viewRange = view.createRangeIn( domEventData.target );
 	}
-
-	const viewRange = view.domConverter.domRangeToView( domRange );
 
 	if ( !viewRange ) {
 		return null;
@@ -672,12 +677,12 @@ function getElementFromMouseEvent( view: EditingView, domEventData: DomEventData
 		if ( viewPosition.isAtEnd && viewPosition.nodeBefore ) {
 			// Click after a widget tend to return position at the end of the editable element
 			// so use the node before it if range is at the end of a parent.
-			viewNode = viewPosition.nodeBefore as ViewElement;
+			viewNode = viewPosition.nodeBefore as ViewNode;
 		} else if ( viewPosition.isAtStart && viewPosition.nodeAfter ) {
 			// Click before a widget tend to return position at the start of the editable element
 			// so use the node after it if range is at the start of a parent.
 			// See more: https://github.com/ckeditor/ckeditor5/issues/16992
-			viewNode = viewPosition.nodeAfter as ViewElement;
+			viewNode = viewPosition.nodeAfter as ViewNode;
 		}
 	}
 
