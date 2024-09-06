99d0373d96c687d393c91b761400208ce1835c2e
Piotr Szczęśniak <szczesniakp@o2.pl>
2024-09-06 16:44:55 +0200

    Bookmark downcast (WIP).

--- packages/ckeditor5-bookmark/src/bookmarkediting.ts
+++ packages/ckeditor5-bookmark/src/bookmarkediting.ts
@@ -8,6 +8,11 @@
  */
 
 import { Plugin } from 'ckeditor5/src/core.js';
+import { Widget, toWidget } from 'ckeditor5/src/widget.js';
+import { IconView } from 'ckeditor5/src/ui.js';
+
+import bookmarkIcon from '../theme/icons/bookmark.svg';
+import type { ViewUIElement, DowncastWriter } from 'ckeditor5/src/engine.js';
 
 /**
  * The bookmark editing plugin.
@@ -23,8 +28,16 @@ export default class BookmarkEditing extends Plugin {
 	/**
 	 * @inheritDoc
 	 */
+	public static get requires() {
+		return [ Widget ] as const;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
 	public init(): void {
 		this._defineSchema();
+		this._defineConverters();
 	}
 
 	/**
@@ -37,6 +50,59 @@ export default class BookmarkEditing extends Plugin {
 			inheritAllFrom: '$inlineObject',
 			allowAttributes: 'bookmarkId',
 			disallowAttributes: 'linkHref'
+		} );
+	}
+
+	/**
+	 * Defines the converters for the bookmark feature.
+	 */
+	private _defineConverters() {
+		const { editor } = this;
+		const { conversion, t } = editor;
+
+		conversion.for( 'dataDowncast' ).elementToElement( {
+			model: 'bookmark',
+			view: ( modelElement, { writer } ) => {
+				return writer.createEmptyElement( 'a', {
+					'id': modelElement.getAttribute( 'bookmarkId' )
+				} );
+			}
+		} );
+
+		editor.conversion.for( 'editingDowncast' ).elementToElement( {
+			model: 'bookmark',
+			view: ( modelElement, { writer } ) => {
+				const id = modelElement.getAttribute( 'bookmarkId' );
+				const containerElement = writer.createContainerElement( 'a', {
+					id,
+					class: 'ck-bookmark'
+				}, [ this._createBookmarkUIElement( writer ) ] );
+
+				containerElement.getFillerOffset = () => null;
+
+				return toWidget( containerElement, writer, {
+					label: ` ${ t( 'Bookmark' ) } ${ id }`
+				} );
+			}
+		} );
+	}
+
+	/**
+	 * Creates a UI element for the bookmark representation in editing view.
+	 */
+	private _createBookmarkUIElement( writer: DowncastWriter ): ViewUIElement {
+		return writer.createUIElement( 'span', {}, function( domDocument ) {
+			const domElement = this.toDomElement( domDocument );
+
+			const icon = new IconView();
+
+			// TODO fillcolor management
+			icon.set( 'content', bookmarkIcon );
+			icon.render();
+
+			domElement.appendChild( icon.element! );
+
+			return domElement;
 		} );
 	}
 }
--- packages/ckeditor5-bookmark/tests/bookmarkediting.js
+++ packages/ckeditor5-bookmark/tests/bookmarkediting.js
@@ -14,8 +14,11 @@ import { Link } from '@ckeditor/ckeditor5-link';
 
 import ClassicTestEditor from '@ckeditor/ckeditor5-core/tests/_utils/classictesteditor.js';
 
+import { setData as setModelData } from '@ckeditor/ckeditor5-engine/src/dev-utils/model.js';
+import { getData as getViewData } from '@ckeditor/ckeditor5-engine/src/dev-utils/view.js';
+
 describe( 'BookmarkEditing', () => {
-	let editor, element, model;
+	let editor, element, model, view;
 
 	beforeEach( async () => {
 		element = document.createElement( 'div' );
@@ -28,6 +31,7 @@ describe( 'BookmarkEditing', () => {
 			} );
 
 		model = editor.model;
+		view = editor.editing.view;
 	} );
 
 	afterEach( () => {
@@ -48,6 +52,32 @@ describe( 'BookmarkEditing', () => {
 			expect( model.schema.checkAttribute( [ 'paragraph', 'bookmark' ], 'id' ) ).to.be.false;
 			expect( model.schema.checkAttribute( [ 'paragraph', 'bookmark' ], 'bookmarkId' ) ).to.be.true;
 			expect( model.schema.checkChild( [ 'paragraph' ], 'bookmark' ) ).to.be.true;
+		} );
+	} );
+
+	describe( 'dataDowncast', () => {
+		it( 'should properly downcast bookmark', () => {
+			setModelData( model, '<paragraph><bookmark bookmarkId="foo"></bookmark></paragraph>' );
+
+			expect( editor.getData() ).to.equal(
+				'<p>' +
+					'<a id="foo"></a>' +
+				'</p>'
+			);
+		} );
+	} );
+
+	describe( 'editingDowncast', () => {
+		it( 'should properly downcast bookmark', () => {
+			setModelData( model, '<paragraph><bookmark bookmarkId="foo"></bookmark></paragraph>' );
+
+			expect( getViewData( view, { withoutSelection: true } ) ).to.equal(
+				'<p>' +
+					'<a class="ck-bookmark ck-widget" contenteditable="false" id="foo">' +
+						'<span></span>' +
+					'</a>' +
+				'</p>'
+			);
 		} );
 	} );
 } );
