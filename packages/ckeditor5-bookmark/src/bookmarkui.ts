98301d91ffa4fa376d23c616ad70ca3ed73ac307
Kuba Niegowski <j.niegowski@cksource.com>
2024-09-24 15:52:39 +0200

    Review fixes.

--- packages/ckeditor5-bookmark/src/bookmarkui.ts
+++ packages/ckeditor5-bookmark/src/bookmarkui.ts
@@ -186,20 +186,21 @@ export default class BookmarkUI extends Plugin {
 		const commands = [ insertBookmarkCommand, updateBookmarkCommand ];
 
 		const formView = new ( CssTransitionDisablerMixin( BookmarkFormView ) )( locale, validators );
+
 		formView.idInputView.fieldView.bind( 'value' ).to( updateBookmarkCommand, 'value' );
 
 		// Form elements should be read-only when corresponding commands are disabled.
 		formView.idInputView.bind( 'isEnabled' ).toMany(
 			commands,
 			'isEnabled',
-			( ...areEnabled ) => areEnabled.some( isVisible => isVisible )
+			( ...areEnabled ) => areEnabled.some( isEnabled => isEnabled )
 		);
 
 		// Disable the "save" button if the command is disabled.
 		formView.insertButtonView.bind( 'isEnabled' ).toMany(
 			commands,
 			'isEnabled',
-			( ...areEnabled ) => areEnabled.some( isVisible => isVisible )
+			( ...areEnabled ) => areEnabled.some( isEnabled => isEnabled )
 		);
 
 		// Execute link command after clicking the "Save" button.
@@ -270,7 +271,7 @@ export default class BookmarkUI extends Plugin {
 		view.bind( 'isEnabled' ).toMany(
 			[ insertCommand, updateCommand ],
 			'isEnabled',
-			( ...areEnabled ) => areEnabled.some( isVisible => isVisible )
+			( ...areEnabled ) => areEnabled.some( isEnabled => isEnabled )
 		);
 
 		view.bind( 'isOn' ).to( updateCommand, 'value', value => !!value );
