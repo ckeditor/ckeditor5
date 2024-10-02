a7efcca4c2cd02e30eed6776b4b8f589d42e4cc4
Szymon Cofalik <s.cofalik@cksource.com>
2024-10-02 16:09:52 +0200

    Tests: Introduce automated manual test sample for performance testing.

---
+++ tests/manual/performance/2024/data/generated/index.js
@@ -0,0 +1,11 @@
+import paragraphs from './paragraphs.js';
+import lists from './lists.js';
+import tableHuge from './table-huge.js';
+import tablesMany from './tables-many-smaller.js';
+import formattingLongP from './formatting-long-paragraphs.js';
+import formattingShortP from './formatting-short-paragraphs.js';
+import inlineStyles from './inline-styles.js';
+
+export default {
+	paragraphs, lists, tableHuge, tablesMany, formattingLongP, formattingShortP, inlineStyles
+};
