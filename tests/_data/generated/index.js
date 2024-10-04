45e80bbe19351f04d4bc61bdb00ac34225e40dc5
Szymon Cofalik <s.cofalik@cksource.com>
2024-10-04 17:01:40 +0200

    Added three new data sets. Changed how `init-single` gets the initial data.

--- tests/_data/generated/index.js
+++ tests/_data/generated/index.js
@@ -10,7 +10,12 @@ import tablesMany from './tables-many-smaller.js';
 import formattingLongP from './formatting-long-paragraphs.js';
 import formattingShortP from './formatting-short-paragraphs.js';
 import inlineStyles from './inline-styles.js';
+import mixed from './mixed.js';
+// Below data sets were tested, and they do not differ vs above tests.
+// import paragraphsLong from './paragraphs-long.js';
+// import images from './images.js';
 
 export default {
-	paragraphs, lists, tableHuge, tablesMany, formattingLongP, formattingShortP, inlineStyles
+	paragraphs, lists, tableHuge, tablesMany, formattingLongP, formattingShortP, inlineStyles, mixed
+	//, images, paragraphsLong
 };
