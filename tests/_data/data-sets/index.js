17933ae7ffa3704a32cf8e457f2eacb909508613
Szymon Cofalik <s.cofalik@cksource.com>
2024-10-21 17:48:40 +0200

    Another fix.

--- tests/_data/data-sets/index.js
+++ tests/_data/data-sets/index.js
@@ -9,7 +9,7 @@ import tableHuge from './table-huge.js';
 import formattingLongP from './formatting-long-paragraphs.js';
 import inlineStyles from './inline-styles.js';
 import mixed from './mixed.js';
-import wiki from './mixed.js';
+import wiki from './wiki.js';
 
 export default {
 	paragraphs, lists, tableHuge, formattingLongP, inlineStyles, mixed, wiki
