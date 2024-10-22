2ebfcbc1ba00ed9099991061b52444438f91c5be
Szymon Cofalik <s.cofalik@cksource.com>
2024-10-22 14:15:01 +0200

    Introduce automated manual test sample for performance testing (#17205)
    Tests: Introduced automated manual test sample for performance testing and data sets to test on.

---
+++ tests/_data/data-sets/index.js
@@ -0,0 +1,16 @@
+/**
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import paragraphs from './paragraphs.js';
+import lists from './lists.js';
+import tableHuge from './table-huge.js';
+import formattingLongP from './formatting-long-paragraphs.js';
+import inlineStyles from './inline-styles.js';
+import mixed from './mixed.js';
+import wiki from './wiki.js';
+
+export default {
+	paragraphs, lists, tableHuge, formattingLongP, inlineStyles, mixed, wiki
+};
