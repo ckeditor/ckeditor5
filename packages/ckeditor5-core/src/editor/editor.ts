868ddca585d0368e02e0ae9b857947ec33a06200
Mateusz Baginski <cziken58@gmail.com>
2024-10-23 14:20:02 +0200

    Add comments

--- packages/ckeditor5-core/src/editor/editor.ts
+++ packages/ckeditor5-core/src/editor/editor.ts
@@ -497,6 +497,11 @@ export default abstract class Editor extends /* #__PURE__ */ ObservableMixin() {
 						editor: collectUsageData( editor )
 					};
 
+					/**
+					 * This part of the code is not executed in open-source implementations (GPL key).
+					 * It only runs when a specific license key is provided. If you're uncertain whether
+					 * this applies to your application, please contact our support team.
+					 */
 					editor._sendUsageRequest( licensePayload.usageEndpoint, request ).then( response => {
 						const { status, message } = response;
 
@@ -1020,6 +1025,13 @@ export default abstract class Editor extends /* #__PURE__ */ ObservableMixin() {
 		this._showLicenseError = () => {};
 	}
 
+	/**
+	 * This part of the code is not executed in open-source implementations (GPL key).
+	 * It only runs when a specific license key is provided. If you're uncertain whether
+	 * this applies to your application, please contact our support team.
+	 *
+	 * @private
+	 */
 	private async _sendUsageRequest( endpoint: string, request: unknown ) {
 		const headers = new Headers( { 'Content-Type': 'application/json' } );
 		const response = await fetch( new URL( endpoint ), {
