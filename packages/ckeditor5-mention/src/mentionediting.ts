ece3d94158a14aa7baa3b4fa963ade990f0449c7
Kuba Niegowski <j.niegowski@cksource.com>
2024-11-14 16:56:59 +0100

    Fixed mention upcast conversion (all attributes must be consumed).

--- packages/ckeditor5-mention/src/mentionediting.ts
+++ packages/ckeditor5-mention/src/mentionediting.ts
@@ -63,7 +63,7 @@ export default class MentionEditing extends Plugin {
 		editor.conversion.for( 'upcast' ).elementToAttribute( {
 			view: {
 				name: 'span',
-				key: 'data-mention',
+				attributes: 'data-mention',
 				classes: 'mention'
 			},
 			model: {
