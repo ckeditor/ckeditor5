2dd924b9dd0c8bcd6a0f2ea34d78c30e77e5cbe1
Przemysław Zań <przemyslaw.zan@gmail.com>
2025-01-30 17:31:55 +0100

    Created database fixture.

--- packages/ckeditor5-emoji/tests/integration.js
+++ packages/ckeditor5-emoji/tests/integration.js
@@ -3,22 +3,32 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
  */
 
-/* global document */
-
-import Emoji from '../src/emoji.js';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
+/* global document window Response */
 
 import ClassicTestEditor from '@ckeditor/ckeditor5-core/tests/_utils/classictesteditor.js';
+import Emoji from '../src/emoji.js';
 import EmojiPicker from '../src/emojipicker.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
+
+import database from './fixtures/database.json';
+import testUtils from '@ckeditor/ckeditor5-core/tests/_utils/utils.js';
 
 describe( 'Emoji integration', () => {
-	let editor, element, emojiPicker;
+	let editor, element, emojiPicker, fetchStub, fetchStubResolve;
 
 	beforeEach( () => {
 		element = document.createElement( 'div' );
 		document.body.appendChild( element );
+
+		fetchStub = testUtils.sinon.stub( window, 'fetch' ).resolves( new Response( '[]' ) );
+
+		fetchStub.returns( new Promise( resolve => {
+			fetchStubResolve = resolve;
+		} ) );
+
+		fetchStubResolve( new Response( JSON.stringify( database ) ) );
 
 		return ClassicTestEditor
 			.create( element, {
@@ -44,7 +54,7 @@ describe( 'Emoji integration', () => {
 		emojiPicker.showUI();
 		emojiPicker.emojiPickerView.gridView.element.scrollTo( 999, 999 );
 
-		expect( emojiPicker.emojiPickerView.gridView.element.scrollTop > 300 ).to.be.true;
+		expect( emojiPicker.emojiPickerView.gridView.element.scrollTop ).to.equal( 335 );
 
 		document.querySelector( '.ck-emoji__categories-list > button:nth-child(2)' ).click();
 
