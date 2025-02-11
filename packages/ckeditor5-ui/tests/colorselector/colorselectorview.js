a0b28c0dcd0d324ea33ee6fd55989fa95a079437
Mateusz Bagiński <cziken58@gmail.com>
2025-02-11 07:17:00 +0100

    Fix font color picker not working properly on first change on Chrome (#17109)
    Fix (ui): Fixed an issue where the first selected color was applied instead of the second selected color when using the font color picker for the first time after loading the page. Closes #17069

--- packages/ckeditor5-ui/tests/colorselector/colorselectorview.js
+++ packages/ckeditor5-ui/tests/colorselector/colorselectorview.js
@@ -24,6 +24,7 @@ import testUtils from '@ckeditor/ckeditor5-core/tests/_utils/utils.js';
 import removeButtonIcon from '@ckeditor/ckeditor5-core/theme/icons/eraser.svg';
 import checkButtonIcon from '@ckeditor/ckeditor5-core/theme/icons/check.svg';
 import cancelButtonIcon from '@ckeditor/ckeditor5-core/theme/icons/cancel.svg';
+import { env } from '@ckeditor/ckeditor5-utils';
 
 const colorDefinitions = [
 	{
@@ -432,6 +433,11 @@ describe( 'ColorSelectorView', () => {
 		} );
 
 		it( 'should execute when color picker is focused and enter pressed', () => {
+			// Focusing input and then the color picker breaks focus handling in the test
+			// suite that uses a headless Chrome browser. It is a workaround for that, as
+			// this deactivates focusing input before the color picker.
+			env.isBlink = false;
+
 			const keyEvtData = {
 				keyCode: keyCodes.enter,
 				preventDefault: sinon.spy(),
