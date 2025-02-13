3ee7e8b48d1bbbf58b41195ed29d06ce3684c485
Przemysław Zań <przemyslaw.zan@gmail.com>
2025-02-13 13:06:53 +0100

    Cleanup.

--- packages/ckeditor5-utils/tests/manual/duplicated-import/duplicated-import.js
+++ packages/ckeditor5-utils/tests/manual/duplicated-import/duplicated-import.js
@@ -5,7 +5,10 @@
 
 /* global document, console */
 
-import ClassicEditor from '../../../../../docs/_snippets/build-classic.js';
+// The CKEditor 5 library is loaded both here, and in HTML via CDN.
+// Hence, we expect to throw the error:
+// https://ckeditor.com/docs/ckeditor5/latest/support/error-codes.html#error-ckeditor-duplicated-modules
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {} )
