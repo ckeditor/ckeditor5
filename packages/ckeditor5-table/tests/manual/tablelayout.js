59e0a3205259fa6ac4fe33f56b7a7d8317b9a8ae
Marta Motyczynska <m.motyczynska@cksource.com>
2025-02-13 23:58:51 +0100

    Table layout ui tests.

---
+++ packages/ckeditor5-table/tests/manual/tablelayout.js
@@ -0,0 +1,32 @@
+/**
+ * @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
+ */
+
+/* globals console, window, document */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset.js';
+import TableLayout from '../../src/tablelayout.js';
+
+ClassicEditor
+	.create( document.querySelector( '#editor' ), {
+		image: { toolbar: [ 'toggleImageCaption', 'imageTextAlternative' ] },
+		plugins: [ ArticlePluginSet, TableLayout ],
+		toolbar: [
+			'heading', '|', 'insertTable', 'insertTableLayout', '|', 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote'
+		],
+		table: {
+			contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ],
+			tableToolbar: [ 'bold', 'italic' ]
+		},
+		menuBar: {
+			isVisible: true
+		}
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
