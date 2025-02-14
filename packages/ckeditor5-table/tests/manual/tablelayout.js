42372be708650975020cd8e2810f0fe247b46b1b
Marta Motyczynska <m.motyczynska@cksource.com>
2025-02-14 20:13:15 +0100

    Extend manual test with more table features.

--- packages/ckeditor5-table/tests/manual/tablelayout.js
+++ packages/ckeditor5-table/tests/manual/tablelayout.js
@@ -7,17 +7,34 @@
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset.js';
+import TableToolbar from '../../src/tabletoolbar.js';
+import TableSelection from '../../src/tableselection.js';
+import TableClipboard from '../../src/tableclipboard.js';
+import TableProperties from '../../src/tableproperties.js';
+import TableCellProperties from '../../src/tablecellproperties.js';
+import TableCaption from '../../src/tablecaption.js';
 import TableLayout from '../../src/tablelayout.js';
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
 		image: { toolbar: [ 'toggleImageCaption', 'imageTextAlternative' ] },
-		plugins: [ ArticlePluginSet, TableLayout ],
+		plugins: [
+			ArticlePluginSet,
+			TableToolbar,
+			TableSelection,
+			TableClipboard,
+			TableProperties,
+			TableCellProperties,
+			TableCaption,
+			TableLayout
+		],
 		toolbar: [
 			'heading', '|', 'insertTable', 'insertTableLayout', '|', 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote'
 		],
 		table: {
-			contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ],
+			contentToolbar: [
+				'tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties', 'toggleTableCaption'
+			],
 			tableToolbar: [ 'bold', 'italic' ]
 		},
 		menuBar: {
