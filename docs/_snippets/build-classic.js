a97beec82027f868c9d2e0bebafaf9c92bd7e081
Filip Sobol <filipsobol@users.noreply.github.com>
2025-03-03 15:31:49 +0100

    Fix formatting

--- docs/_snippets/build-classic.js
+++ docs/_snippets/build-classic.js
@@ -3,17 +3,32 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
  */
 
-/*
- * This is a JavaScript version of CKEditor 5 Classic build.
- * It is used for all snippets used in the documentation to avoid importing a build source from the `src/` directory.
- * See: https://github.com/ckeditor/ckeditor5/issues/13552 to learn why it is a problem.
- */
-
-// The editor creator to use.
 import {
-	ClassicEditor as ClassicEditorBase, Essentials, Autoformat, Bold, Italic, BlockQuote, CKBox, EasyImage, Heading,
-	Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload, PictureEditing, Indent, Link, List, MediaEmbed,
-	Paragraph, PasteFromOffice, Table, TableToolbar, TextTransformation, CloudServices
+	ClassicEditor as ClassicEditorBase,
+	Essentials,
+	Autoformat,
+	Bold,
+	Italic,
+	BlockQuote,
+	CKBox,
+	EasyImage,
+	Heading,
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	PictureEditing,
+	Indent,
+	Link,
+	List,
+	MediaEmbed,
+	Paragraph,
+	PasteFromOffice,
+	Table,
+	TableToolbar,
+	TextTransformation,
+	CloudServices
 } from 'ckeditor5';
 
 export default class ClassicEditor extends ClassicEditorBase {
