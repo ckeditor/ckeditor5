eb03ac1d8b88251d41a875a75a3c5e752c3776af
Marta Motyczynska <m.motyczynska@cksource.com>
2025-05-20 22:12:02 +0200

    Downcast listItemFontFamily to inline style.

--- packages/ckeditor5-list/src/listitemformatting/listitemfontfamilyintegration.ts
+++ packages/ckeditor5-list/src/listitemformatting/listitemfontfamilyintegration.ts
@@ -8,6 +8,7 @@
  */
 
 import { Plugin } from 'ckeditor5/src/core.js';
+import ListEditing from '../list/listediting.js';
 
 /**
  * The list item font family integration plugin.
@@ -30,9 +31,17 @@ export default class ListItemFontFamilyIntegration extends Plugin {
 	/**
 	 * @inheritDoc
 	 */
+	public static get requires() {
+		return [ ListEditing ] as const;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
 	public afterInit(): void {
 		const editor = this.editor;
 		const model = editor.model;
+		const listEditing: ListEditing = editor.plugins.get( 'ListEditing' );
 
 		if ( !editor.plugins.has( 'FontFamily' ) ) {
 			return;
@@ -41,6 +50,19 @@ export default class ListItemFontFamilyIntegration extends Plugin {
 		model.schema.extend( '$listItem', { allowAttributes: 'listItemFontFamily' } );
 		model.schema.setAttributeProperties( 'listItemFontFamily', {
 			isFormatting: true
+		} );
+
+		listEditing.registerDowncastStrategy( {
+			scope: 'item',
+			attributeName: 'listItemFontFamily',
+
+			setAttributeOnDowncast( writer, value, viewElement ) {
+				if ( value ) {
+					writer.setStyle( 'fontFamily', value as string, viewElement );
+				} else {
+					writer.removeStyle( 'fontFamily', viewElement );
+				}
+			}
 		} );
 	}
 }
