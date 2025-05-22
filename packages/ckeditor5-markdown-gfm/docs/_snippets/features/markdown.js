e7bf1f6d1b058e13a97244e5ef4a4c47ae45fe27
Mateusz Baginski <cziken58@gmail.com>
2025-05-22 09:31:51 +0200

    Use prerendered code-block component in markdown snippet.

--- packages/ckeditor5-markdown-gfm/docs/_snippets/features/markdown.js
+++ packages/ckeditor5-markdown-gfm/docs/_snippets/features/markdown.js
@@ -77,24 +77,13 @@ ClassicEditor
 	.then( editor => {
 		window.editor = editor;
 
-		const outputElement = document.querySelector( '#snippet-markdown-output > pre > code' );
+		const { codeBlock } = document.querySelector( '#snippet-markdown-output' );
+		const throttleRefreshPreview = window.umberto.throttle( () => {
+			codeBlock.setCode( editor.getData() );
+		}, 200 );
 
-		editor.model.document.on( 'change', () => {
-			outputElement.innerText = editor.getData();
-
-			window.umberto.highlightCode( editor.getData(), 'markdown' )
-				.then( highlightedHTML => {
-					outputElement.innerHTML = highlightedHTML;
-				} );
-		} );
-
-		// Set the initial data with delay so hightlight.js doesn't catch it.
-		setTimeout( () => {
-			window.umberto.highlightCode( editor.getData(), 'markdown' )
-				.then( highlightedHTML => {
-					outputElement.innerHTML = highlightedHTML;
-				} );
-		}, 500 );
+		editor.model.document.on( 'change', throttleRefreshPreview );
+		throttleRefreshPreview();
 	} )
 	.catch( err => {
 		console.error( err.stack );
