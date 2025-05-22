2c85ff3c50da9646a0b81e1c1585991e7495b85c
Marta Motyczynska <m.motyczynska@cksource.com>
2025-05-22 21:47:11 +0200

    Upcast font family to listItemFontFamily attribute.

--- packages/ckeditor5-list/src/listformatting/listitemfontfamilyintegration.ts
+++ packages/ckeditor5-list/src/listformatting/listitemfontfamilyintegration.ts
@@ -8,6 +8,8 @@
  */
 
 import { Plugin } from 'ckeditor5/src/core.js';
+import { type ViewElement } from 'ckeditor5/src/engine.js';
+
 import ListEditing from '../list/listediting.js';
 
 /**
@@ -52,6 +54,18 @@ export default class ListItemFontFamilyIntegration extends Plugin {
 			isFormatting: true
 		} );
 
+		model.schema.addAttributeCheck( ( context, attributeName ) => {
+			const item = context.last;
+
+			if ( attributeName != 'listItemFontFamily' ) {
+				return;
+			}
+
+			if ( !item.getAttribute( 'listItemId' ) ) {
+				return false;
+			}
+		} );
+
 		listEditing.registerDowncastStrategy( {
 			scope: 'item',
 			attributeName: 'listItemFontFamily',
@@ -61,6 +75,21 @@ export default class ListItemFontFamilyIntegration extends Plugin {
 					writer.setStyle( 'font-family', value as string, viewElement );
 				} else {
 					writer.removeStyle( 'font-family', viewElement );
+				}
+			}
+		} );
+
+		editor.conversion.for( 'upcast' ).attributeToAttribute( {
+			model: {
+				key: 'listItemFontFamily',
+				value: ( viewElement: ViewElement ) => {
+					return viewElement.getStyle( 'font-family' );
+				}
+			},
+			view: {
+				name: 'li',
+				styles: {
+					'font-family': /.*/
 				}
 			}
 		} );
