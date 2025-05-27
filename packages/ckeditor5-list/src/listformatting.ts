8388b1cd1cf99ed7332f2b3470455b4c9edb3d55
Marta Motyczynska <m.motyczynska@cksource.com>
2025-05-27 15:17:31 +0200

    Postfixer for list item formatting (inline content only).

--- packages/ckeditor5-list/src/listformatting.ts
+++ packages/ckeditor5-list/src/listformatting.ts
@@ -10,6 +10,14 @@
 import { Plugin } from 'ckeditor5/src/core.js';
 
 import ListItemFontFamilyIntegration from './listformatting/listitemfontfamilyintegration.js';
+// import ListEditing from '../src/list/listediting.js';
+import type {
+	Element,
+	Model,
+	Writer
+} from 'ckeditor5/src/engine.js';
+
+import { isListItemBlock } from '../src/list/utils/model.js';
 
 /**
  * The list formatting plugin. It enables integration with formatting plugins to style the list marker.
@@ -40,5 +48,112 @@ export default class ListFormatting extends Plugin {
 	 */
 	public static get requires() {
 		return [ ListItemFontFamilyIntegration ] as const;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public init(): void {
+		const editor = this.editor;
+		const model = editor.model;
+		// const listEditing: ListEditing = editor.plugins.get( ListEditing );
+
+		model.document.registerPostFixer( writer => {
+			const changes = editor.model.document.differ.getChanges();
+			let returnValue = false;
+
+			for ( const entry of changes ) {
+				if ( !( entry.type == 'attribute' ) || !( entry.attributeKey == 'fontFamily' ) ) {
+					continue;
+				}
+
+				const parent = entry.range.start.parent;
+
+				if ( !isListItemBlock( parent ) ) {
+					continue;
+				}
+
+				const fontFamily = entry.attributeNewValue as string | null;
+				const listItemFontFamily = parent.getAttribute( 'listItemFontFamily' );
+
+				if ( fontFamily ) {
+					if ( this._isListItemConsistentlyFormatted( model, parent, 'fontFamily' ) ) {
+						if ( listItemFontFamily !== fontFamily ) {
+							this._addFormattingToListItem( writer, parent, 'listItemFontFamily', fontFamily );
+							returnValue = true;
+						}
+					} else if ( listItemFontFamily ) {
+						this._removeFormattingFromListItem( writer, parent, 'listItemFontFamily' );
+						returnValue = true;
+					}
+				} else if ( listItemFontFamily ) {
+					this._removeFormattingFromListItem( writer, parent, 'listItemFontFamily' );
+					returnValue = true;
+				}
+			}
+
+			return returnValue;
+		} );
+	}
+
+	/**
+	 * TODO
+	 */
+	private _addFormattingToListItem(
+		writer: Writer,
+		listItem: Element,
+		attributeKey: string,
+		attributeValue: string
+	): void {
+		if ( !listItem.hasAttribute( attributeKey ) || listItem.getAttribute( attributeKey ) !== attributeValue ) {
+			writer.setAttribute( attributeKey, attributeValue, listItem );
+		}
+	}
+
+	/**
+	 * TODO
+	 */
+	private _removeFormattingFromListItem(
+		writer: Writer,
+		listItem: Element,
+		attributeKey: string
+	): void {
+		if ( listItem.hasAttribute( attributeKey ) ) {
+			writer.removeAttribute( attributeKey, listItem );
+		}
+	}
+
+	/**
+	 * TODO
+	 */
+	private _isListItemConsistentlyFormatted( model: Model, listItem: Element, attributeKey: string ): boolean {
+		let hasListItemConsistentFormat = false;
+		let prevFontFamily;
+
+		for ( const child of listItem.getChildren() ) {
+			if ( model.schema.checkAttribute( child, attributeKey ) ) {
+				const fontFamily = child.getAttribute( attributeKey );
+
+				// First child.
+				if ( !prevFontFamily ) {
+					if ( fontFamily ) {
+						prevFontFamily = fontFamily;
+						hasListItemConsistentFormat = true;
+						continue;
+					} else {
+						hasListItemConsistentFormat = false;
+						break;
+					}
+				}
+
+				// Second and next children.
+				if ( fontFamily !== prevFontFamily ) {
+					hasListItemConsistentFormat = false;
+					break;
+				}
+			}
+		}
+
+		return hasListItemConsistentFormat;
 	}
 }
