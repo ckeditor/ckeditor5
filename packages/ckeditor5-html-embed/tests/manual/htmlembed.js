f8d32b1ade17f762344576969f3a2904750379e5
Mateusz Baginski <cziken58@gmail.com>
2025-06-03 15:14:39 +0200

    Fix more exports.

--- packages/ckeditor5-html-embed/tests/manual/htmlembed.js
+++ packages/ckeditor5-html-embed/tests/manual/htmlembed.js
@@ -3,7 +3,7 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
  */
 
-import { sanitizeHtml } from 'sanitize-html';
+import sanitizeHtml from 'sanitize-html';
 import { clone } from 'es-toolkit/compat';
 import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import { ArticlePluginSet } from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset.js';
