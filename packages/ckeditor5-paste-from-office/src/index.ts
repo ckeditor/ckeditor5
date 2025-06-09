18d6d21dfd0574caa854a34f7c87d5d73aa5a8ae
Mateusz Baginski <cziken58@gmail.com>
2025-06-09 14:18:27 +0200

    Apply CR remarks.

--- packages/ckeditor5-paste-from-office/src/index.ts
+++ packages/ckeditor5-paste-from-office/src/index.ts
@@ -12,31 +12,31 @@ export type { Normalizer, NormalizerData } from './normalizer.js';
 export { MSWordNormalizer } from './normalizers/mswordnormalizer.js';
 export { parseHtml } from './filters/parse.js';
 
-export { transformBookmarks as _transformPastedOfficeBookmarks } from './filters/bookmark.js';
-export { transformBlockBrsToParagraphs as _transformPastedOfficeBlockBrsToParagraphs } from './filters/br.js';
+export { transformBookmarks as _transformPasteOfficeBookmarks } from './filters/bookmark.js';
+export { transformBlockBrsToParagraphs as _transformPasteOfficeBlockBrsToParagraphs } from './filters/br.js';
 export {
-	replaceImagesSourceWithBase64 as _replacePastedOfficeImagesSourceWithBase64,
+	replaceImagesSourceWithBase64 as _replacePasteOfficeImagesSourceWithBase64,
 	_convertHexToBase64
 } from './filters/image.js';
 export {
-	transformListItemLikeElementsIntoLists as _transformPastedOfficeListItemLikeElementsIntoLists,
-	unwrapParagraphInListItem as _unwrapPastedOfficeParagraphInListItem
+	transformListItemLikeElementsIntoLists as _transformPasteOfficeListItemLikeElementsIntoLists,
+	unwrapParagraphInListItem as _unwrapPasteOfficeParagraphInListItem
 } from './filters/list.js';
 export { removeBoldWrapper as _removeOfficeBoldWrapper } from './filters/removeboldwrapper.js';
 export { removeGoogleSheetsTag as _removePastedGoogleOfficeSheetsTag } from './filters/removegooglesheetstag.js';
-export { removeInvalidTableWidth as _removePastedOfficeInvalidTableWidths } from './filters/removeinvalidtablewidth.js';
+export { removeInvalidTableWidth as _removePasteOfficeInvalidTableWidths } from './filters/removeinvalidtablewidth.js';
 export { removeMSAttributes as _removePastedMSOfficeAttributes } from './filters/removemsattributes.js';
-export { removeStyleBlock as _removePastedOfficeStyleBlock } from './filters/removestyleblock.js';
-export { removeXmlns as _removePastedOfficeXmlnsAttributes } from './filters/removexmlns.js';
+export { removeStyleBlock as _removePasteOfficeStyleBlock } from './filters/removestyleblock.js';
+export { removeXmlns as _removePasteOfficeXmlnsAttributes } from './filters/removexmlns.js';
 export {
-	normalizeSpacing as _normalizePastedOfficeSpacing,
-	normalizeSpacerunSpans as _normalizePastedOfficeSpaceRunSpans
+	normalizeSpacing as _normalizePasteOfficeSpacing,
+	normalizeSpacerunSpans as _normalizePasteOfficeSpaceRunSpans
 } from './filters/space.js';
-export { transformTables as _transformPastedOfficeTables } from './filters/table.js';
+export { transformTables as _transformPasteOfficeTables } from './filters/table.js';
 export {
-	convertCssLengthToPx as _convertPastedOfficeCssLengthToPx,
-	isPx as _isPastedOfficePxValue,
-	toPx as _toPastedOfficePxValue
+	convertCssLengthToPx as _convertPasteOfficeCssLengthToPx,
+	isPx as _isPasteOfficePxValue,
+	toPx as _toPasteOfficePxValue
 } from './filters/utils.js';
 export { GoogleDocsNormalizer as PasteFromOfficeGoogleDocsNormalizer } from './normalizers/googledocsnormalizer.js';
 export { GoogleSheetsNormalizer as PasteFromOfficeGoogleSheetsNormalizer } from './normalizers/googlesheetsnormalizer.js';
