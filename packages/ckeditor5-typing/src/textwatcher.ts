b112600b8d512b8db827ba91d52f72628f76c01b
Marcin Panek <m.panek@cksource.com>
2025-06-13 00:15:49 +0200

    Added re-exports and renamed package members for `ckeditor5-typing`.

--- packages/ckeditor5-typing/src/textwatcher.ts
+++ packages/ckeditor5-typing/src/textwatcher.ts
@@ -153,7 +153,7 @@ export class TextWatcher extends /* #__PURE__ */ ObservableMixin() {
 		const testResult = this.testCallback( text );
 
 		if ( !testResult && this.hasMatch ) {
-			this.fire<TextWatcherUnmatchedEvent>( 'unmatched' );
+			this.fire<TextWatcherUnmatchedTypingEvent>( 'unmatched' );
 		}
 
 		this._hasMatch = !!testResult;
@@ -189,10 +189,10 @@ export type TextWatcherMatchedEvent<TCallbackResult extends Record<string, unkno
  */
 export type TextWatcherMatchedDataEvent<TCallbackResult extends Record<string, unknown>> = {
 	name: 'matched:data';
-	args: [ data: TextWatcherMatchedDataEventData & TCallbackResult ];
+	args: [ data: TextWatcherMatchedTypingDataEventData & TCallbackResult ];
 };
 
-export interface TextWatcherMatchedDataEventData {
+export interface TextWatcherMatchedTypingDataEventData {
 
 	/**
 	 * The full text before selection to which the regexp was applied.
@@ -214,12 +214,12 @@ export interface TextWatcherMatchedDataEventData {
  * @param data Event data.
  * @param data.testResult The additional data returned from the {@link module:typing/textwatcher~TextWatcher#testCallback}.
  */
-export type TextWatcherMatchedSelectionEvent<TCallbackResult extends Record<string, unknown>> = {
+export type TextWatcherMatchedTypingSelectionEvent<TCallbackResult extends Record<string, unknown>> = {
 	name: 'matched:selection';
-	args: [ data: TextWatcherMatchedSelectionEventData & TCallbackResult ];
+	args: [ data: TextWatcherMatchedTypingSelectionEventData & TCallbackResult ];
 };
 
-export interface TextWatcherMatchedSelectionEventData {
+export interface TextWatcherMatchedTypingSelectionEventData {
 
 	/**
 	 * The full text before selection.
@@ -237,7 +237,7 @@ export interface TextWatcherMatchedSelectionEventData {
  *
  * @eventName ~TextWatcher#unmatched
  */
-export type TextWatcherUnmatchedEvent = {
+export type TextWatcherUnmatchedTypingEvent = {
 	name: 'unmatched';
 	args: [];
 };
