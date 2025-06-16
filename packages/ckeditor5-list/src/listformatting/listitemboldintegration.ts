8c0bea773632a88a526dbd15908af46ffb291952
Marta Motyczynska <m.motyczynska@cksource.com>
2025-06-16 15:47:31 +0200

    List item bold integration.

---
+++ packages/ckeditor5-list/src/listformatting/listitemboldintegration.ts
@@ -0,0 +1,103 @@
+/**
+ * @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
+ */
+
+/**
+ * @module list/listformatting/listitemboldintegration
+ */
+
+import { Plugin } from 'ckeditor5/src/core.js';
+
+import ListEditing from '../list/listediting.js';
+import type ListFormatting from '../listformatting.js';
+
+/**
+ * The list item bold integration plugin.
+ */
+export default class ListItemBoldIntegration extends Plugin {
+	/**
+	 * @inheritDoc
+	 */
+	public static get pluginName() {
+		return 'ListItemBoldIntegration' as const;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static override get isOfficialPlugin(): true {
+		return true;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public static get requires() {
+		return [ ListEditing ] as const;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public init(): void {
+		const editor = this.editor;
+		const ListFormatting: ListFormatting = editor.plugins.get( 'ListFormatting' );
+		const listEditing = editor.plugins.get( ListEditing );
+
+		if ( !editor.plugins.has( 'BoldEditing' ) ) {
+			return;
+		}
+
+		ListFormatting.registerFormatAttribute( 'listItemBold', 'bold' );
+
+		// Register the downcast strategy in init() so that the attribute name is registered  before the list editing
+		// registers its converters.
+		// This ensures that the attribute is recognized by downcast strategies and bogus paragraphs are handled correctly.
+		listEditing.registerDowncastStrategy( {
+			scope: 'item',
+			attributeName: 'listItemBold',
+
+			setAttributeOnDowncast( writer, value, viewElement ) {
+				if ( value ) {
+					writer.addClass( 'ck-bold', viewElement );
+				} else {
+					writer.removeClass( 'ck-bold', viewElement );
+				}
+			}
+		} );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public afterInit(): void {
+		const editor = this.editor;
+		const model = editor.model;
+
+		if ( !editor.plugins.has( 'BoldEditing' ) ) {
+			return;
+		}
+
+		model.schema.extend( '$listItem', { allowAttributes: 'listItemBold' } );
+		model.schema.setAttributeProperties( 'listItemBold', {
+			isFormatting: true
+		} );
+
+		model.schema.addAttributeCheck( context => {
+			const item = context.last;
+
+			if ( !item.getAttribute( 'listItemId' ) ) {
+				return false;
+			}
+		}, 'listItemBold' );
+
+		editor.conversion.for( 'upcast' ).attributeToAttribute( {
+			model: 'listItemBold',
+			view: {
+				name: 'li',
+				classes: 'ck-bold'
+			}
+		} );
+	}
+}
