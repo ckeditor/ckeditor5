02caaf9fc0d7d3c55a7f325b2958fcebc06fc52d
Kuba Niegowski <j.niegowski@cksource.com>
2025-07-10 17:52:59 +0200

    Simplified tap code. Added more tap points.

--- packages/ckeditor5-watchdog/src/actionsrecorder.ts
+++ packages/ckeditor5-watchdog/src/actionsrecorder.ts
@@ -36,6 +36,7 @@ export class ActionsRecorder extends Plugin {
 
 		this._tapCommands();
 		this._tapOperationApply();
+		this._tapModelMethods();
 		this._tapComponentFactory();
 		this._tapViewDocumentEvents();
 
@@ -51,76 +52,102 @@ export class ActionsRecorder extends Plugin {
 		}
 
 		// Tap commands registered after the constructor was called.
-		_tapMethod( this.editor.commands, 'add', {
-			before: ( commandName, command ) => {
+		this._tapMethod( this.editor.commands, 'add', {
+			before: ( callContext, [ commandName, command ] ) => {
 				this._tapCommand( commandName, command );
-				return null;
+
+				return false;
 			}
 		} );
 	}
 
 	private _tapOperationApply() {
-		_tapMethod( this.editor.model, 'applyOperation', {
-			before( operation ) {
-				if ( operation.baseVersion !== null ) {
-					console.log( 'model before operation apply:', operation );
-					return { operation };
-				} else {
-					return null;
+		this._tapMethod( this.editor.model, 'applyOperation', {
+			before: ( callContext, [ operation ] ) => {
+				if ( operation.baseVersion === null ) {
+					return false;
 				}
+
+				console.log( 'model before operation apply:', operation );
+				callContext.operation = operation;
+
+				return true;
 			},
-			after() {
-				console.log( 'model after operation apply:', this.operation );
+			after: callContext => {
+				console.log( 'model after operation apply:', callContext.operation );
 			},
-			error( error ) {
-				console.log( 'model operation apply error:', this.operation, error );
+			error: ( callContext, error ) => {
+				console.log( 'model operation apply error:', callContext.operation, error );
 			}
 		} );
 	}
 
+	private _tapModelMethods() {
+		for ( const methodName of [ 'insertContent', 'insertObject', 'deleteContent' ] ) {
+			this._tapMethod( this.editor.model, methodName, {
+				before: ( callContext, ...params ) => {
+					console.log( 'model before', methodName, '(', params, ')' );
+					callContext.params = params;
+
+					return true;
+				},
+				after: ( callContext, result ) => {
+					console.log( 'model after', methodName, '(', callContext.params, ')', result );
+				},
+				error: ( callContext, error ) => {
+					console.log( 'model error', methodName, '(', callContext.params, ')', error );
+				}
+			}, { source: 'model API' } );
+		}
+	}
+
 	private _tapCommand( commandName: string, command: Command ) {
-		_tapMethod( command, 'execute', {
-			before( ...params ) {
+		this._tapMethod( command, 'execute', {
+			before: ( callContext, params ) => {
 				console.log( 'before command execute:', commandName, '(', params, ')' );
-				return { params };
+				callContext.params = params;
+
+				return true;
 			},
-			after( result ) {
-				console.log( 'after command execute:', commandName, '(', this.params, ') =>', result );
+			after: ( callContext, result ) => {
+				console.log( 'after command execute:', commandName, '(', callContext.params, ') =>', result );
 			},
-			error( error ) {
-				console.log( 'command execute error:', commandName, '(', this.params, ')', error );
+			error: ( callContext, error ) => {
+				console.log( 'command execute error:', commandName, '(', callContext.params, ')', error );
 			}
 		} );
 	}
 
 	private _tapComponentFactory() {
-		_tapMethod( this.editor.ui.componentFactory, 'create', {
-			before( name ) {
-				return { componentName: name };
+		this._tapMethod( this.editor.ui.componentFactory, 'create', {
+			before: ( callContext, [ componentName ] ) => {
+				callContext.componentName = componentName;
+
+				return true;
 			},
-			after( componentInstance ) {
-				console.log( 'component created:', this.componentName, componentInstance );
+			after: ( callContext, componentInstance ) => {
+				console.log( 'component created:', callContext.componentName, componentInstance );
 
 				if ( typeof componentInstance.fire == 'function' ) {
-					_tapFireMethod( componentInstance, this.componentName );
+					this._tapFireMethod( componentInstance, [ 'execute' ], callContext );
 				}
 
 				if ( typeof componentInstance.panelView?.fire == 'function' ) {
-					_tapFireMethod( componentInstance.panelView, this.componentName );
+					this._tapFireMethod( componentInstance.panelView, [ 'execute' ], callContext );
 				}
 
 				if ( typeof componentInstance.buttonView?.actionView?.fire == 'function' ) {
-					_tapFireMethod( componentInstance.buttonView.actionView, this.componentName );
+					this._tapFireMethod( componentInstance.buttonView.actionView, [ 'execute' ], callContext );
 				}
 			},
-			error( error ) {
-				console.log( 'component error:', this.componentName, error );
+			error: ( callContext, error ) => {
+				console.log( 'component error:', callContext.componentName, error );
 			}
 		} );
 	}
 
 	private _tapViewDocumentEvents() {
-		_tapFireMethod( this.editor.editing.view.document, 'editing view document', [
+		this._tapFireMethod( this.editor.editing.view.document, [
 			'click',
 			'mousedown',
 			'mouseup',
@@ -141,70 +168,78 @@ export class ActionsRecorder extends Plugin {
 			'paste',
 			'copy',
 			'cut',
+			'dragstart',
 			'drop',
 			'imageLoaded',
 			'todoCheckboxChange'
-		] );
+		], { source: 'observers' } );
 	}
-}
 
-function _tapFireMethod( emitter: any, name: string, eventNames: Array<string> = [ 'execute' ] ) {
-	_tapMethod( emitter, 'fire', {
-		name,
+	private _tapFireMethod( emitter: any, eventNames: Array<string>, context: Record<string, any> = {} ) {
+		this._tapMethod( emitter, 'fire', {
+			before: ( callContext, [ eventInfoOrName, ...params ] ) => {
+				const eventName = typeof eventInfoOrName == 'string' ? eventInfoOrName : eventInfoOrName.name;
 
-		before( eventInfoOrName, ...params ) {
-			const eventName = typeof eventInfoOrName == 'string' ? eventInfoOrName : eventInfoOrName.name;
+				if ( !eventNames.includes( eventName ) ) {
+					return false;
+				}
 
-			if ( eventNames.includes( eventName ) ) {
-				console.log( 'before fire:', this.name, 'event', eventName, '(', params, ')' );
-				return { eventName, params };
-			} else {
-				return null;
+				console.log( 'before fire:', callContext, 'event', eventName, '(', params, ')' );
+				callContext.eventName = eventName;
+				callContext.params = params;
+
+				return true;
+			},
+			after: ( callContext, result ) => {
+				console.log( 'after fire:', callContext, 'event', callContext.eventName, '(', callContext.params, ')', result );
+			},
+			error: ( callContext, error ) => {
+				console.log( 'error fire:', callContext, 'event', callContext.eventName, '(', callContext.params, ')', error );
 			}
-		},
-		after( result ) {
-			console.log( 'after fire:', this.name, 'event', this.eventName, '(', this.params, ')', result );
-		},
-		error( error ) {
-			console.log( 'error fire:', this.name, 'event', this.eventName, '(', this.params, ')', error );
-		}
-	} );
-}
-
-function _tapMethod(
-	object: any,
-	methodName: string,
-	handler: MethodTap
-) {
-	const originalMethod = object[ methodName ];
-
-	if ( originalMethod[ Symbol.for( 'Tapped method' ) ] ) {
-		return;
+		}, context );
 	}
 
-	object[ methodName ] = ( ...args: Array<any> ) => {
-		let newHandler;
+	private _tapMethod(
+		object: any,
+		methodName: string,
+		tap: MethodTap,
+		context: Record<string, any> = {}
+	) {
+		const originalMethod = object[ methodName ];
 
-		try {
-			const beforeResult = handler.before?.( ...args );
-			newHandler = beforeResult !== null ? Object.assign( {}, handler, beforeResult ) : {};
-
-			const result = originalMethod.apply( object, args );
-			newHandler.after?.( result );
-
-			return result;
-		} catch ( error ) {
-			( newHandler || handler ).error?.( error );
-
-			throw error;
+		if ( originalMethod[ Symbol.for( 'Tapped method' ) ] ) {
+			return;
 		}
-	};
 
-	object[ methodName ][ Symbol.for( 'Tapped method' ) ] = originalMethod;
+		object[ methodName ] = ( ...args: Array<any> ) => {
+			const callContext = Object.assign( {}, context );
+			let shouldHandle;
+
+			try {
+				shouldHandle = tap.before?.( callContext, args );
+
+				const result = originalMethod.apply( object, args );
+
+				if ( shouldHandle ) {
+					tap.after?.( callContext, result );
+				}
+
+				return result;
+			} catch ( error ) {
+				if ( shouldHandle ) {
+					tap.error?.( callContext, error );
+				}
+
+				throw error;
+			}
+		};
+
+		object[ methodName ][ Symbol.for( 'Tapped method' ) ] = originalMethod;
+	}
 }
 
 interface MethodTap extends Record<string, any> {
-	before?: ( ...args: Array<any> ) => MethodTap | null;
-	after?: ( result: any ) => void;
-	error?: ( error: any ) => void;
+	before?: ( context: Record<string, any>, args: Array<any> ) => boolean;
+	after?: ( context: Record<string, any>, result: any ) => void;
+	error?: ( context: Record<string, any>, error: any ) => void;
 }
