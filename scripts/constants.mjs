2de2ae4d8edb0ac8358521ac2a5ee7040e8e98ac
Filip Sobol <filipsobol@users.noreply.github.com>
2025-08-07 16:00:40 +0200

    Swap repository structure.

--- scripts/constants.mjs
+++ scripts/constants.mjs
@@ -8,7 +8,7 @@ import { PACKAGES_DIRECTORY } from './release/utils/constants.mjs';
 
 export const CKEDITOR5_ROOT_PATH = upath.join( import.meta.dirname, '..' );
 export const CKEDITOR5_PACKAGES_PATH = upath.join( CKEDITOR5_ROOT_PATH, PACKAGES_DIRECTORY );
-export const CKEDITOR5_COMMERCIAL_PATH = upath.join( CKEDITOR5_ROOT_PATH, 'external', 'ckeditor5-commercial' );
+export const CKEDITOR5_COMMERCIAL_PATH = upath.join( CKEDITOR5_ROOT_PATH, '..', '..' );
 export const CKEDITOR5_COMMERCIAL_PACKAGES_PATH = upath.join( CKEDITOR5_COMMERCIAL_PATH, PACKAGES_DIRECTORY );
 export const CKEDITOR5_PREMIUM_FEATURES_PATH = upath.join( CKEDITOR5_COMMERCIAL_PACKAGES_PATH, 'ckeditor5-premium-features' );
 
