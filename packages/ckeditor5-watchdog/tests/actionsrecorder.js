b47b06ef1a77304b7219d3183e8587d49d4bf2bc
Kuba Niegowski <j.niegowski@cksource.com>
2025-08-20 19:18:12 +0200

    Updated tests.

--- packages/ckeditor5-watchdog/tests/actionsrecorder.js
+++ packages/ckeditor5-watchdog/tests/actionsrecorder.js
@@ -53,19 +53,19 @@ describe( 'ActionsRecorder', () => {
 		it( 'should tap already registered commands during initialization', () => {
 			// The paragraph command should already be registered and tapped
 			// since it's included in the plugins array before ActionsRecorder
-			plugin.flushRecords();
+			plugin.flushEntries();
 
 			editor.execute( 'paragraph' );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			const paragraphRecord = records.find( record => record.event === 'commands.paragraph:execute' );
 
 			expect( paragraphRecord ).to.exist;
 			expect( paragraphRecord.event ).to.equal( 'commands.paragraph:execute' );
 		} );
 
-		it( 'should register onBeforeAction callback from config', async () => {
-			const onBeforeActionSpy = sinon.spy();
+		it( 'should register onError callback from config', async () => {
+			const onErrorSpy = sinon.spy();
 
 			await editor.destroy();
 			element.remove();
@@ -76,50 +76,35 @@ describe( 'ActionsRecorder', () => {
 			editor = await ClassicTestEditor.create( element, {
 				plugins: [ ActionsRecorder, Paragraph ],
 				actionsRecorder: {
-					onBeforeAction: onBeforeActionSpy
+					onError: onErrorSpy
 				}
+			} );
+
+			let error;
+
+			editor.commands.get( 'paragraph' ).on( 'execute', () => {
+				error = new Error( 'Test' );
+
+				throw error;
 			} );
 
 			plugin = editor.plugins.get( 'ActionsRecorder' );
 
-			editor.execute( 'paragraph' );
+			expect( () => editor.execute( 'paragraph' ) ).to.throw( error );
 
-			expect( onBeforeActionSpy ).to.have.been.called;
-		} );
-
-		it( 'should register onAfterAction callback from config', async () => {
-			const onAfterActionSpy = sinon.spy();
-
-			await editor.destroy();
-			element.remove();
-
-			element = global.document.createElement( 'div' );
-			global.document.body.appendChild( element );
-
-			editor = await ClassicTestEditor.create( element, {
-				plugins: [ ActionsRecorder, Paragraph ],
-				actionsRecorder: {
-					onAfterAction: onAfterActionSpy
-				}
-			} );
-
-			plugin = editor.plugins.get( 'ActionsRecorder' );
-
-			editor.execute( 'paragraph' );
-
-			expect( onAfterActionSpy ).to.have.been.called;
+			expect( onErrorSpy ).to.have.been.called;
 		} );
 	} );
 
 	describe( 'recording functionality', () => {
 		beforeEach( () => {
-			plugin.flushRecords();
+			plugin.flushEntries();
 		} );
 
 		it( 'should record command executions', () => {
 			editor.execute( 'paragraph' );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			expect( records ).to.have.length( 1 );
 			expect( records[ 0 ].event ).to.equal( 'commands.paragraph:execute' );
 			expect( records[ 0 ] ).to.have.property( 'timeStamp' );
@@ -134,7 +119,7 @@ describe( 'ActionsRecorder', () => {
 				writer.insertText( 'Hello', editor.model.document.selection.getFirstPosition() );
 			} );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			const operationRecords = records.filter( record => record.event === 'model.applyOperation' );
 
 			expect( operationRecords.length ).to.be.greaterThan( 0 );
@@ -146,7 +131,7 @@ describe( 'ActionsRecorder', () => {
 				editor.model.insertContent( textElement );
 			} );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			const insertContentRecord = records.find( record => record.event === 'model.insertContent' );
 
 			expect( insertContentRecord ).to.exist;
@@ -156,7 +141,7 @@ describe( 'ActionsRecorder', () => {
 		it( 'should record nested operations with parent frames', () => {
 			editor.execute( 'paragraph' );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 
 			// At least one record should have been created from the command execution
 			expect( records.length ).to.be.greaterThan( 0 );
@@ -176,7 +161,7 @@ describe( 'ActionsRecorder', () => {
 				target: element
 			} );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			const clickRecord = records.find( record => record.event === 'observers:click' );
 
 			expect( clickRecord ).to.exist;
@@ -185,7 +170,7 @@ describe( 'ActionsRecorder', () => {
 		it( 'should capture state snapshots correctly', () => {
 			editor.execute( 'paragraph' );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			const record = records[ 0 ];
 
 			expect( record.before ).to.have.property( 'documentVersion' );
@@ -211,7 +196,7 @@ describe( 'ActionsRecorder', () => {
 			editor.commands.add( 'foo', new FooCommand( editor ) );
 			editor.execute( 'foo' );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			const commandRecord = records.find( record => record.event === 'commands.foo:execute' );
 
 			expect( commandRecord ).to.exist;
@@ -236,7 +221,7 @@ describe( 'ActionsRecorder', () => {
 				// Expected error
 			}
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			const commandRecord = records.find( record => record.event === 'commands.foo:execute' );
 
 			expect( commandRecord ).to.exist;
@@ -246,27 +231,27 @@ describe( 'ActionsRecorder', () => {
 
 	describe( 'record management', () => {
 		beforeEach( () => {
-			plugin.flushRecords();
+			plugin.flushEntries();
 		} );
 
-		it( 'should return all records via getRecords()', () => {
+		it( 'should return all records via getEntries()', () => {
 			editor.execute( 'paragraph' );
 			editor.execute( 'heading', { value: 'heading1' } );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			// Filter to only count command executions, not nested operations
 			const commandRecords = records.filter( record => record.event.startsWith( 'commands.' ) );
 			expect( commandRecords ).to.have.length( 2 );
 		} );
 
-		it( 'should clear all records via flushRecords()', () => {
+		it( 'should clear all records via flushEntries()', () => {
 			editor.execute( 'paragraph' );
 
-			expect( plugin.getRecords() ).to.have.length( 1 );
+			expect( plugin.getEntries() ).to.have.length( 1 );
 
-			plugin.flushRecords();
+			plugin.flushEntries();
 
-			expect( plugin.getRecords() ).to.have.length( 0 );
+			expect( plugin.getEntries() ).to.have.length( 0 );
 		} );
 
 		it( 'should enforce maxEntries limit', async () => {
@@ -290,226 +275,8 @@ describe( 'ActionsRecorder', () => {
 			editor.execute( 'paragraph' );
 			editor.execute( 'paragraph' );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			expect( records ).to.have.length( 2 );
-		} );
-	} );
-
-	describe( 'observers', () => {
-		beforeEach( () => {
-			plugin.flushRecords();
-		} );
-
-		describe( 'observeBeforeActions', () => {
-			it( 'should register and call before action observers', () => {
-				const observerSpy = sinon.spy();
-
-				plugin.observeBeforeActions( observerSpy );
-				editor.execute( 'paragraph' );
-
-				expect( observerSpy ).to.have.been.calledOnce;
-				expect( observerSpy.firstCall.args[ 0 ] ).to.have.property( 'event', 'commands.paragraph:execute' );
-				expect( observerSpy.firstCall.args[ 1 ] ).to.be.an( 'array' );
-			} );
-
-			it( 'should unregister observers via returned function', () => {
-				const observerSpy = sinon.spy();
-
-				const unregister = plugin.observeBeforeActions( observerSpy );
-				unregister();
-
-				editor.execute( 'paragraph' );
-
-				expect( observerSpy ).to.not.have.been.called;
-			} );
-
-			it( 'should handle observer errors gracefully', () => {
-				const consoleStub = sinon.stub( console, 'error' );
-
-				const observerWithError = () => {
-					throw new Error( 'Observer error' );
-				};
-				const workingObserver = sinon.spy();
-
-				plugin.observeBeforeActions( observerWithError );
-				plugin.observeBeforeActions( workingObserver );
-
-				editor.execute( 'paragraph' );
-
-				expect( workingObserver ).to.have.been.called;
-				expect( consoleStub ).to.have.been.calledWithMatch( 'ActionsRecorder before observer error' );
-
-				consoleStub.restore();
-			} );
-
-			it( 'should register multiple before observers and call them all', () => {
-				const observer1 = sinon.spy();
-				const observer2 = sinon.spy();
-
-				plugin.observeBeforeActions( observer1 );
-				plugin.observeBeforeActions( observer2 );
-
-				editor.execute( 'paragraph' );
-
-				expect( observer1 ).to.have.been.calledOnce;
-				expect( observer2 ).to.have.been.calledOnce;
-			} );
-
-			it( 'should pass previous records to before observers', () => {
-				const argsStack = [];
-				const beforeObserver = sinon.spy( ( record, records ) => {
-					// Enforce copy of records as they are modified after broadcasting.
-					argsStack.push( [ ...records ] );
-				} );
-
-				plugin.observeBeforeActions( beforeObserver );
-				plugin.flushRecords();
-
-				// Register custom command
-				class FooCommand extends Command {
-					isEnabled = true;
-					execute() {
-						return 'foo';
-					}
-				}
-
-				editor.commands.add( 'foo', new FooCommand( editor ) );
-				editor.execute( 'foo' );
-
-				expect( beforeObserver ).to.have.been.calledOnce;
-				expect( argsStack[ 0 ] ).to.have.length( 0 );
-
-				editor.execute( 'foo' );
-
-				expect( beforeObserver ).to.have.been.calledTwice;
-				expect( argsStack[ 1 ] ).to.have.length( 1 );
-			} );
-
-			it( 'should not call unregistered observers', () => {
-				const observer1 = sinon.spy();
-				const observer2 = sinon.spy();
-
-				plugin.observeBeforeActions( observer1 );
-				const unregister2 = plugin.observeBeforeActions( observer2 );
-
-				// Unregister observer2
-				unregister2();
-
-				editor.execute( 'paragraph' );
-
-				expect( observer1 ).to.have.been.calledOnce;
-				expect( observer2 ).to.not.have.been.called;
-			} );
-
-			it( 'should handle before observer that modifies the records array', () => {
-				const beforeObserver = ( record, records ) => {
-					// Try to modify the records array (should not affect internal state)
-					records.push( { fake: 'record' } );
-				};
-
-				plugin.observeBeforeActions( beforeObserver );
-
-				// Register custom command
-				class FooCommand extends Command {
-					isEnabled = true;
-					execute() {
-						return 'foo';
-					}
-				}
-
-				editor.commands.add( 'foo', new FooCommand( editor ) );
-
-				const initialLength = plugin.getRecords().length;
-
-				editor.execute( 'foo' );
-
-				// Should add execute record and then one custom record.
-				expect( plugin.getRecords().length ).to.equal( initialLength + 2 );
-			} );
-		} );
-
-		describe( 'observeAfterActions', () => {
-			it( 'should register and call after action observers', () => {
-				const observerSpy = sinon.spy();
-
-				plugin.observeAfterActions( observerSpy );
-				editor.execute( 'paragraph' );
-
-				expect( observerSpy ).to.have.been.calledOnce;
-				expect( observerSpy.firstCall.args[ 0 ] ).to.have.property( 'event', 'commands.paragraph:execute' );
-			} );
-
-			it( 'should unregister observers via returned function', () => {
-				const observerSpy = sinon.spy();
-
-				const unregister = plugin.observeAfterActions( observerSpy );
-				unregister();
-
-				editor.execute( 'paragraph' );
-
-				expect( observerSpy ).to.not.have.been.called;
-			} );
-
-			it( 'should register multiple after observers and call them all', () => {
-				const observer1 = sinon.spy();
-				const observer2 = sinon.spy();
-
-				plugin.observeAfterActions( observer1 );
-				plugin.observeAfterActions( observer2 );
-
-				editor.execute( 'paragraph' );
-
-				expect( observer1 ).to.have.been.calledOnce;
-				expect( observer2 ).to.have.been.calledOnce;
-			} );
-
-			it( 'should pass result to after observers on successful command execution', () => {
-				class FooCommand extends Command {
-					isEnabled = true;
-
-					execute() {
-						return { success: true };
-					}
-				}
-
-				editor.commands.add( 'foo', new FooCommand( editor ) );
-
-				const afterObserver = sinon.spy();
-				plugin.observeAfterActions( afterObserver );
-
-				editor.execute( 'foo' );
-
-				expect( afterObserver ).to.have.been.calledOnce;
-				expect( afterObserver.firstCall.args[ 1 ] ).to.deep.equal( { success: true } );
-				expect( afterObserver.firstCall.args[ 2 ] ).to.be.undefined;
-			} );
-
-			it( 'should pass error to after observers on failed command execution', () => {
-				class FooCommand extends Command {
-					isEnabled = true;
-
-					execute() {
-						throw new Error( 'Test error' );
-					}
-				}
-
-				editor.commands.add( 'foo', new FooCommand( editor ) );
-
-				const afterObserver = sinon.spy();
-
-				plugin.observeAfterActions( afterObserver );
-
-				try {
-					editor.execute( 'foo' );
-				// eslint-disable-next-line no-unused-vars
-				} catch ( error ) {
-					// Expected error
-				}
-
-				expect( afterObserver ).to.have.been.calledOnce;
-				expect( afterObserver.firstCall.args[ 1 ] ).to.be.undefined;
-				expect( afterObserver.firstCall.args[ 2 ] ).to.be.instanceOf( Error );
-			} );
 		} );
 	} );
 
@@ -535,7 +302,7 @@ describe( 'ActionsRecorder', () => {
 			editor.execute( 'paragraph' );
 
 			expect( filterSpy ).to.have.been.called;
-			expect( plugin.getRecords() ).to.have.length( 0 );
+			expect( plugin.getEntries() ).to.have.length( 0 );
 		} );
 
 		it( 'should include records when filter returns true', async () => {
@@ -559,7 +326,7 @@ describe( 'ActionsRecorder', () => {
 			editor.execute( 'paragraph' );
 
 			expect( filterSpy ).to.have.been.called;
-			expect( plugin.getRecords() ).to.have.length.greaterThan( 0 );
+			expect( plugin.getEntries() ).to.have.length.greaterThan( 0 );
 		} );
 
 		it( 'should pass record and previous records to filter callback', async () => {
@@ -590,14 +357,14 @@ describe( 'ActionsRecorder', () => {
 
 	describe( 'UI component recording', () => {
 		beforeEach( () => {
-			plugin.flushRecords();
+			plugin.flushEntries();
 		} );
 
 		it( 'should record component factory creation', () => {
 			// Use a component that actually exists in the editor
 			editor.ui.componentFactory.create( 'bold' );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			const componentRecord = records.find( record => record.event === 'component-factory.create:bold' );
 
 			expect( componentRecord ).to.exist;
@@ -606,13 +373,13 @@ describe( 'ActionsRecorder', () => {
 
 	describe( 'serialization', () => {
 		beforeEach( () => {
-			plugin.flushRecords();
+			plugin.flushEntries();
 		} );
 
 		it( 'should serialize command parameters', () => {
 			editor.execute( 'heading', { value: 'heading1' } );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			const headingRecord = records.find( record => record.event === 'commands.heading:execute' );
 
 			expect( headingRecord.params ).to.exist;
@@ -624,7 +391,7 @@ describe( 'ActionsRecorder', () => {
 
 			editor.execute( 'paragraph' );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			const record = records[ 0 ];
 
 			expect( record.before.modelSelection ).to.have.property( 'ranges' );
@@ -642,7 +409,7 @@ describe( 'ActionsRecorder', () => {
 				target: element
 			} );
 
-			const records = plugin.getRecords();
+			const records = plugin.getEntries();
 			const clickRecord = records.find( record => record.event === 'observers:click' );
 
 			expect( clickRecord ).to.exist;
