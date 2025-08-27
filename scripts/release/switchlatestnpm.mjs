7598ef2b39966cfdedc5a22af4d2a50aef8cb3e0
Filip Sobol <filipsobol@users.noreply.github.com>
2025-08-27 11:09:37 +0200

    Fix paths in various scripts.

--- scripts/release/switchlatestnpm.mjs
+++ scripts/release/switchlatestnpm.mjs
@@ -9,14 +9,18 @@ import upath from 'upath';
 import * as releaseTools from '@ckeditor/ckeditor5-dev-release-tools';
 import fs from 'fs-extra';
 import { globSync } from 'glob';
-import { CKEDITOR5_ROOT_PATH } from '../constants.mjs';
+import {
+	CKEDITOR5_ROOT_PATH,
+	CKEDITOR5_PACKAGES_PATH,
+	CKEDITOR5_COMMERCIAL_PACKAGES_PATH
+} from '../constants.mjs';
 
 const rootPkgJson = fs.readJsonSync( upath.join( CKEDITOR5_ROOT_PATH, 'package.json' ) );
 
 const GLOB_PATTERNS = [
 	'package.json',
-	'packages/*/package.json',
-	'external/ckeditor5-commercial/packages/*/package.json'
+	CKEDITOR5_PACKAGES_PATH + '/*/package.json',
+	CKEDITOR5_COMMERCIAL_PACKAGES_PATH + '/*/package.json'
 ];
 
 Promise.resolve()
