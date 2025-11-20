4a6ef84335b8b5c845d2fff41a509f9c7aa9ba69
Filip Sobol <filipsobol@users.noreply.github.com>
2025-11-20 11:51:28 +0100

    Build API docs, `ckeditor5` and `ckeditor5-premium-features` in parallel.

---
+++ scripts/docs/workers/build-commercial.mjs
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+/**
+ * @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
+ */
+
+import url from 'node:url';
+import { workerData } from 'node:worker_threads';
+import upath from 'upath';
+import { CKEDITOR5_COMMERCIAL_PATH, IS_ISOLATED_REPOSITORY } from '../../constants.mjs';
+
+try {
+	if ( workerData.skipCommercial || IS_ISOLATED_REPOSITORY ) {
+		console.log( 'Skipping `ckeditor5-premium-features`.' );
+	} else {
+		console.log( 'Started building `ckeditor5-premium-features`.' );
+
+		const scriptPath = upath.join( CKEDITOR5_COMMERCIAL_PATH, 'scripts', 'docs', 'generate-ckeditor5-premium-features-docs-build.mjs' );
+		const { href } = url.pathToFileURL( scriptPath );
+		const { default: generateCKEditor5PremiumFeaturesDocsBuild } = await import( href );
+
+		await generateCKEditor5PremiumFeaturesDocsBuild(
+			upath.join( workerData.basePath, 'ckeditor5-premium-features/ckeditor5-premium-features.js' ),
+			workerData.skipObfuscation
+		);
+
+		console.log( 'Finished building `ckeditor5-premium-features`.' );
+	}
+} catch ( error ) {
+	console.error( error );
+	process.exitCode = 1;
+}
