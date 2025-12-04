48654281416aa1f94373df204c1cbed106409f31
Mateusz Baginski <cziken58@gmail.com>
2025-12-04 08:57:24 +0100

    Add testing for heading columns.

--- packages/ckeditor5-table/tests/tablecellproperties/tablecelltypecollaboration.js
+++ packages/ckeditor5-table/tests/tablecellproperties/tablecelltypecollaboration.js
@@ -159,6 +159,272 @@ describe( 'collaboration', () => {
 				] )
 			);
 		} );
+
+		it( 'should work properly if john sets header row and kate unsets it at the same time', () => {
+			const initialModel = modelTable( [
+				[ '00[]', '1' ],
+				[ '10', '12' ]
+			] );
+
+			john.setData( initialModel );
+			kate.setData( initialModel );
+
+			selectCells( john, [ 0, 0 ], [ 0, 0 ] );
+			selectCells( kate, [ 0, 0 ], [ 0, 0 ] );
+
+			syncClients();
+
+			john._processExecute( 'setTableRowHeader' );
+			kate._processExecute( 'setTableRowHeader', { forceValue: false } );
+
+			syncClients();
+
+			expectClients(
+				modelTable( [
+					[
+						{ contents: '00', tableCellType: 'header' },
+						{ contents: '1', tableCellType: 'header' }
+					],
+					[ '10', '12' ]
+				], { headingRows: 1 } )
+			);
+		} );
+
+		it( 'should work properly if john sets 2 header rows and kate sets 1 header row at the same time', () => {
+			const initialModel = modelTable( [
+				[ '00[]', '1' ],
+				[ '10', '12' ]
+			] );
+
+			john.setData( initialModel );
+			kate.setData( initialModel );
+
+			selectCells( john, [ 0, 0 ], [ 1, 1 ] );
+			selectCells( kate, [ 0, 0 ], [ 0, 1 ] );
+
+			syncClients();
+
+			john._processExecute( 'setTableRowHeader' );
+			kate._processExecute( 'setTableRowHeader' );
+
+			syncClients();
+
+			expectClients(
+				modelTable( [
+					[
+						{ contents: '00', tableCellType: 'header' },
+						{ contents: '1', tableCellType: 'header' }
+					],
+					[
+						{ contents: '10', tableCellType: 'header' },
+						{ contents: '12', tableCellType: 'header' }
+					]
+				], { headingRows: 2 } )
+			);
+		} );
+	} );
+
+	describe( 'header columns', () => {
+		it( 'should work properly if only one user sets header column', () => {
+			const initialModel = modelTable( [
+				[ '00[]', '1' ],
+				[ '10', '12' ]
+			] );
+
+			john.setData( initialModel );
+			kate.setData( initialModel );
+
+			selectCells( john, [ 0, 0 ], [ 0, 0 ] );
+
+			syncClients();
+
+			john._processExecute( 'setTableColumnHeader' );
+
+			syncClients();
+
+			expectClients(
+				modelTable( [
+					[
+						{ contents: '00', tableCellType: 'header' },
+						'1'
+					],
+					[
+						{ contents: '10', tableCellType: 'header' },
+						'12'
+					]
+				], { headingColumns: 1 } )
+			);
+		} );
+
+		it( 'should work properly if both users set header column on the first column at the same time', () => {
+			const initialModel = modelTable( [
+				[ '00[]', '1' ],
+				[ '10', '12' ]
+			] );
+
+			john.setData( initialModel );
+			kate.setData( initialModel );
+
+			selectCells( john, [ 0, 0 ], [ 0, 0 ] );
+			selectCells( kate, [ 0, 0 ], [ 0, 0 ] );
+
+			syncClients();
+
+			john._processExecute( 'setTableColumnHeader' );
+			kate._processExecute( 'setTableColumnHeader' );
+
+			syncClients();
+
+			expectClients(
+				modelTable( [
+					[
+						{ contents: '00', tableCellType: 'header' },
+						'1'
+					],
+					[
+						{ contents: '10', tableCellType: 'header' },
+						'12'
+					]
+				], { headingColumns: 1 } )
+			);
+		} );
+
+		it( 'should work properly if john sets header column on first column and kate on the second column at the same time', () => {
+			const initialModel = modelTable( [
+				[ '00[]', '1' ],
+				[ '10', '12' ]
+			] );
+
+			john.setData( initialModel );
+			kate.setData( initialModel );
+
+			selectCells( john, [ 0, 0 ], [ 0, 0 ] );
+			selectCells( kate, [ 0, 1 ], [ 1, 1 ] );
+
+			syncClients();
+
+			john._processExecute( 'setTableColumnHeader' );
+			kate._processExecute( 'setTableColumnHeader' );
+
+			syncClients();
+
+			expectClients(
+				modelTable( [
+					[
+						{ contents: '00', tableCellType: 'header' },
+						{ contents: '1', tableCellType: 'header' }
+					],
+					[
+						{ contents: '10', tableCellType: 'header' },
+						{ contents: '12', tableCellType: 'header' }
+					]
+				], { headingColumns: 2 } )
+			);
+		} );
+
+		it( 'should work properly if john sets header column on both columns and kate removes first header column at the same time', () => {
+			const initialModel = modelTable( [
+				[
+					{ contents: '00[]', tableCellType: 'header' },
+					'01'
+				],
+				[
+					{ contents: '10', tableCellType: 'header' },
+					'12'
+				],
+				[
+					{ contents: '20', tableCellType: 'header' },
+					'22'
+				]
+			], { headingColumns: 1 } );
+
+			john.setData( initialModel );
+			kate.setData( initialModel );
+
+			selectCells( john, [ 0, 0 ], [ 1, 1 ] );
+			selectCells( kate, [ 0, 0 ], [ 1, 0 ] );
+
+			syncClients();
+
+			john._processExecute( 'setTableColumnHeader' );
+			kate._processExecute( 'setTableColumnHeader', { forceValue: false } );
+
+			syncClients();
+
+			expectClients(
+				modelTable( [
+					[ '00', '01' ],
+					[ '10', '12' ],
+					[ '20', '22' ]
+				] )
+			);
+		} );
+
+		it( 'should work properly if john sets header column and kate unsets it at the same time', () => {
+			const initialModel = modelTable( [
+				[ '00[]', '1' ],
+				[ '10', '12' ]
+			] );
+
+			john.setData( initialModel );
+			kate.setData( initialModel );
+
+			selectCells( john, [ 0, 0 ], [ 0, 0 ] );
+			selectCells( kate, [ 0, 0 ], [ 0, 0 ] );
+
+			syncClients();
+
+			john._processExecute( 'setTableColumnHeader' );
+			kate._processExecute( 'setTableColumnHeader', { forceValue: false } );
+
+			syncClients();
+
+			expectClients(
+				modelTable( [
+					[
+						{ contents: '00', tableCellType: 'header' },
+						'1'
+					],
+					[
+						{ contents: '10', tableCellType: 'header' },
+						'12'
+					]
+				], { headingColumns: 1 } )
+			);
+		} );
+
+		it( 'should work properly if john sets 2 header columns and kate sets 1 header column at the same time', () => {
+			const initialModel = modelTable( [
+				[ '00[]', '1' ],
+				[ '10', '12' ]
+			] );
+
+			john.setData( initialModel );
+			kate.setData( initialModel );
+
+			selectCells( john, [ 0, 0 ], [ 1, 1 ] );
+			selectCells( kate, [ 0, 0 ], [ 1, 0 ] );
+
+			syncClients();
+
+			john._processExecute( 'setTableColumnHeader' );
+			kate._processExecute( 'setTableColumnHeader' );
+
+			syncClients();
+
+			expectClients(
+				modelTable( [
+					[
+						{ contents: '00', tableCellType: 'header' },
+						{ contents: '1', tableCellType: 'header' }
+					],
+					[
+						{ contents: '10', tableCellType: 'header' },
+						{ contents: '12', tableCellType: 'header' }
+					]
+				], { headingColumns: 2 } )
+			);
+		} );
 	} );
 } );
 
