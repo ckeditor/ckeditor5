f5a434f17b0f96cebc9896806c01277b921ce4b3
Mateusz Baginski <cziken58@gmail.com>
2026-01-12 08:42:27 +0100

    Migrate more testing

--- packages/ckeditor5-style/src/integrations/table.ts
+++ packages/ckeditor5-style/src/integrations/table.ts
@@ -100,7 +100,7 @@ export class TableStyleSupport extends Plugin {
 			return block.name == 'tableCell';
 		}
 
-		if ( [ 'thead', 'tbody' ].includes( definition.element ) ) {
+		if ( [ 'thead', 'tbody', 'tfoot' ].includes( definition.element ) ) {
 			return block.name == 'table';
 		}
 
@@ -132,13 +132,16 @@ export class TableStyleSupport extends Plugin {
 			}
 		}
 
-		if ( [ 'thead', 'tbody' ].includes( definition.element ) ) {
+		if ( [ 'thead', 'tbody', 'tfoot' ].includes( definition.element ) ) {
 			const headingRows = block.getAttribute( 'headingRows' ) as number || 0;
+			const footerRows = block.getAttribute( 'footerRows' ) as number || 0;
 
 			if ( definition.element == 'thead' ) {
 				return headingRows > 0;
+			} else if ( definition.element == 'tfoot' ) {
+				return footerRows > 0;
 			} else {
-				return headingRows < this._tableUtils.getRows( block );
+				return headingRows + footerRows < this._tableUtils.getRows( block );
 			}
 		}
 
