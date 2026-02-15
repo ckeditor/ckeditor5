ec97e6f754e6cb905f07b5aaccc3dd76b58a64f8
Marta Motyczynska <m.motyczynska@cksource.com>
2026-02-15 14:30:34 +0100

    Update packages/ckeditor5-list/tests/list/listindentcommand.js
    Refactor.
    
    Co-authored-by: Kuba Niegowski <1232187+niegowski@users.noreply.github.com>

--- packages/ckeditor5-list/tests/list/listindentcommand.js
+++ packages/ckeditor5-list/tests/list/listindentcommand.js
@@ -6,7 +6,6 @@
 import { ListIndentCommand } from '../../src/list/listindentcommand.js';
 import { stubUid } from './_utils/uid.js';
 import { modelList } from './_utils/utils.js';
-import { ListEditing } from '../../src/list/listediting.js';
 
 import { Editor } from '@ckeditor/ckeditor5-core';
 import { Model, _setModelData, _getModelData } from '@ckeditor/ckeditor5-engine';
