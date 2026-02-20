93c602abd189022b24c914e29cf5b97c6f61d874
Mateusz Baginski <cziken58@gmail.com>
2026-02-20 12:04:57 +0100

    Add arabic indic list style type.

--- packages/ckeditor5-paste-from-office/src/filters/list.ts
+++ packages/ckeditor5-paste-from-office/src/filters/list.ts
@@ -455,6 +455,7 @@ function mapListStyleDefinition( value: string ) {
 			return 'upper-roman';
 		case 'roman-lower':
 			return 'lower-roman';
+		case 'arabic-indic':
 		case 'circle':
 		case 'disc':
 		case 'square':
