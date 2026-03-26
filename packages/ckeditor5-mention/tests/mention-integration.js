5a873bb3a94d0be4f7e2f37cdabdb2042747cc86
Arkadiusz Filipczak <ark.flp@gmail.com>
2026-03-26 12:08:53 +0100

    Adjusted mention tests.

--- packages/ckeditor5-mention/tests/mention-integration.js
+++ packages/ckeditor5-mention/tests/mention-integration.js
@@ -325,7 +325,8 @@ describe( 'Mention feature - integration', () => {
 
 					editor.execute( 'delete' );
 
-					expect( panelView.isVisible ).to.be.false;
+					expect( panelView.isVisible ).to.be.true;
+					expect( balloon.visibleView === mentionsView ).to.be.false;
 				} );
 		} );
 	} );
