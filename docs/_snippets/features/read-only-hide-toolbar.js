1c0f27d64ba02948dd2c65584322d51eae03de10
Mateusz Baginski <cziken58@gmail.com>
2026-03-27 09:21:28 +0100

    Update snippets to use V2 export pdf config.

--- docs/_snippets/features/read-only-hide-toolbar.js
+++ docs/_snippets/features/read-only-hide-toolbar.js
@@ -49,12 +49,16 @@ ReadOnlyEditor
 			fileName: 'export-pdf-demo.pdf',
 			appID: 'cke5-docs',
 			converterOptions: {
-				format: 'A4',
-				margin_top: '15mm',
-				margin_bottom: '15mm',
-				margin_right: '15mm',
-				margin_left: '15mm',
-				page_orientation: 'portrait'
+				document: {
+					size: 'A4',
+					orientation: 'portrait',
+					margins: {
+						top: '15mm',
+						bottom: '15mm',
+						right: '15mm',
+						left: '15mm'
+					}
+				}
 			},
 			tokenUrl: false
 		},
