e346bfcdd1306f5acd8b1e3d165eba7bf59cf125
CKEditorBot <ckeditor-bot@cksource.com>
2026-04-08 06:07:41 +0000

    Release: v47.6.2. [skip ci]

--- packages/ckeditor5-utils/src/version.ts
+++ packages/ckeditor5-utils/src/version.ts
@@ -9,10 +9,10 @@
 
 import { CKEditorError } from './ckeditorerror.js';
 
-export const version = '47.6.1';
+export const version = '47.6.2';
 
 // The second argument is not a month. It is `monthIndex` and starts from `0`.
-export const releaseDate = new Date( 2026, 2, 11 );
+export const releaseDate = new Date( 2026, 3, 8 );
 
 declare global {
 	var CKEDITOR_VERSION: string;
