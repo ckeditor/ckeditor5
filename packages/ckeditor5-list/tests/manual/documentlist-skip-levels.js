9c50fc1d6f291295403d722c5a2fd9973d31b0fc
Marta Motyczynska <m.motyczynska@cksource.com>
2026-04-15 18:12:20 +0200

    Improve manual test for skip level lists to be able to dynamically add/remove some features.

--- packages/ckeditor5-list/tests/manual/documentlist-skip-levels.js
+++ packages/ckeditor5-list/tests/manual/documentlist-skip-levels.js
@@ -9,7 +9,7 @@ import { ImageResize, ImageUpload, Image, ImageCaption, ImageStyle, ImageToolbar
 import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
 import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
 import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
-import { HtmlComment } from '@ckeditor/ckeditor5-html-support';
+import { HtmlComment, GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support';
 import { LinkImage, Link } from '@ckeditor/ckeditor5-link';
 import { PageBreak } from '@ckeditor/ckeditor5-page-break';
 import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
@@ -21,20 +21,50 @@ import { Heading } from '@ckeditor/ckeditor5-heading';
 import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
 import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
 import { TodoList } from '../../src/todolist.js';
 
 import { List } from '../../src/list.js';
 import { ListProperties } from '../../src/listproperties.js';
 
-ClassicEditor
-	.create( {
-		attachTo: document.querySelector( '#editor' ),
-		plugins: [
-			Essentials, BlockQuote, Bold, Heading, Image, ImageCaption, ImageStyle, ImageToolbar, Indent, IndentBlock, Italic, Link,
-			MediaEmbed, Paragraph, Table, TableToolbar, CodeBlock, TableCaption, ImageResize, LinkImage,
-			HtmlEmbed, HtmlComment, Alignment, PageBreak, HorizontalLine, ImageUpload,
-			SourceEditing, List, ListProperties, TodoList
-		],
+const editorElement = document.querySelector( '#editor' );
+
+const controls = {
+	skipLevels: document.querySelector( '#skipLevels' ),
+	indentBlock: document.querySelector( '#indentBlock' ),
+	ghs: document.querySelector( '#ghs' ),
+	pfo: document.querySelector( '#pfo' ),
+	listProperties: document.querySelector( '#listProperties' )
+};
+
+let editor = null;
+
+function getEditorConfig() {
+	const plugins = [
+		Essentials, BlockQuote, Bold, Heading, Image, ImageCaption, ImageStyle, ImageToolbar, Indent, Italic, Link,
+		MediaEmbed, Paragraph, Table, TableToolbar, CodeBlock, TableCaption, ImageResize, LinkImage,
+		HtmlEmbed, HtmlComment, Alignment, PageBreak, HorizontalLine, ImageUpload,
+		SourceEditing, List, TodoList
+	];
+
+	if ( controls.indentBlock.checked ) {
+		plugins.push( IndentBlock );
+	}
+
+	if ( controls.ghs.checked ) {
+		plugins.push( GeneralHtmlSupport );
+	}
+
+	if ( controls.pfo.checked ) {
+		plugins.push( PasteFromOffice );
+	}
+
+	if ( controls.listProperties.checked ) {
+		plugins.push( ListProperties );
+	}
+
+	const config = {
+		plugins,
 		toolbar: [
 			'sourceEditing', '|',
 			'numberedList', 'bulletedList', 'todoList', '|',
@@ -91,15 +121,47 @@ ClassicEditor
 				startIndex: true,
 				reversed: true
 			},
-			allowSkipLevels: true
+			allowSkipLevels: controls.skipLevels.checked
+		},
+		htmlSupport: {
+			allow: [
+				{
+					name: /^.*$/,
+					styles: true,
+					attributes: true,
+					classes: true
+				}
+			]
 		},
 		menuBar: {
 			isVisible: true
 		}
-	} )
-	.then( editor => {
-		window.editor = editor;
-	} )
-	.catch( err => {
-		console.error( err.stack );
+	};
+
+	return config;
+}
+
+function createEditor() {
+	const initialize = () =>
+		ClassicEditor.create( {
+			...getEditorConfig(),
+			attachTo: editorElement
+		} )
+			.then( newEditor => {
+				editor = newEditor;
+				window.editor = editor;
+			} );
+
+	return Promise.resolve()
+		.then( () => editor && editor.destroy() )
+		.then( initialize )
+		.catch( err => console.error( err ) );
+}
+
+createEditor();
+
+Object.values( controls ).forEach( input => {
+	input.addEventListener( 'change', () => {
+		createEditor();
 	} );
+} );
