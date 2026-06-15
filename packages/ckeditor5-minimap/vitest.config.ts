3ed2c6899079f0d918da64596e84c8a263adb82f
CKEditorBot <ckeditor-bot@cksource.com>
2026-06-15 07:53:10 +0000

    Migrated the minimap package tests to Vitest.
    Revision: 0afeccb4d1abe009364653914048547ff011dfcb
    Co-authored-by: Marcin Panek <m.panek@cksource.com>

---
+++ packages/ckeditor5-minimap/vitest.config.ts
@@ -0,0 +1,23 @@
+/**
+ * @license Copyright (c) 2003-2026, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
+ */
+
+import type { ViteUserConfig } from 'vitest/config';
+import { createVitestConfig } from '../../vitest.config';
+
+const config: ViteUserConfig = createVitestConfig( {
+	name: 'minimap',
+	exclude: [
+		'tests/fixtures.js'
+	]
+} );
+
+const testConfig = config.test as NonNullable<ViteUserConfig[ 'test' ]>;
+
+testConfig.coverage = {
+	...testConfig.coverage,
+	include: undefined
+};
+
+export default config;
